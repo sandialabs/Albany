463ccea64757bd4f5da1d2a3f06308fa946434bc
Alejandro Mota <amota@antares.ran.sandia.gov>
2010-12-22 17:57:08 -0800

    Added more functions. Skeleton should be functional.

--- src/LCM/utils/Tensor.h
+++ src/LCM/utils/Tensor.h
@@ -4,6 +4,8 @@
 #if !defined(LCM_Tensor_h)
 #define LCM_Tensor_h
 
+#include <iostream>
+
 #include "Intrepid_FieldContainer.hpp"
 
 namespace LCM {
@@ -19,26 +21,17 @@ namespace LCM {
   {
   public:
     Vector();
-    Vector(const Scalar & s);
-    Vector(const Scalar & s0, const Scalar & s1, const Scalar & s2);
-    Vector(const Scalar s[MaxDim]);
+    Vector(const Scalar s);
+    Vector(const Scalar s0, const Scalar s1, const Scalar s2);
     Vector(const Vector & v);
     ~Vector();
-
-    const Scalar & operator[](const Index i) const;
-    Scalar & operator[](const Index i);
 
     const Scalar & operator()(const Index i) const;
     Scalar & operator()(const Index i);
 
     Vector<Scalar> & operator=  (const Vector<Scalar> & v);
-    Vector<Scalar>   operator+  (const Vector<Scalar> & v) const;
-    Vector<Scalar>   operator-  (const Vector<Scalar> & v) const;
     Vector<Scalar> & operator+= (const Vector<Scalar> & v);
     Vector<Scalar> & operator-= (const Vector<Scalar> & v);
-
-    Scalar operator*(const Vector<Scalar> & v) const;
-    Vector<Scalar> operator*(const Scalar & s) const;
 
     void clear();
 
@@ -54,11 +47,11 @@ namespace LCM {
   {
   public:
     Tensor();
-    Tensor(const Scalar & s);
-    Tensor(const Scalar & s00, const Scalar & s01, const Scalar & s02,
-        const Scalar & s10, const Scalar & s11, const Scalar & s12,
-        const Scalar & s20, const Scalar & s21, const Scalar & s22);
-    Tensor(const Scalar s[MaxDim][MaxDim]);
+    Tensor(const Scalar s);
+    Tensor(
+        const Scalar s00, const Scalar s01, const Scalar s02,
+        const Scalar s10, const Scalar s11, const Scalar s12,
+        const Scalar s20, const Scalar s21, const Scalar s22);
     Tensor(const Tensor & A);
     ~Tensor();
 
@@ -66,14 +59,8 @@ namespace LCM {
     Scalar & operator()(const Index i, const Index j);
 
     Tensor<Scalar> & operator=  (const Tensor<Scalar> & A);
-    Tensor<Scalar>   operator+  (const Tensor<Scalar> & A) const;
-    Tensor<Scalar>   operator-  (const Tensor<Scalar> & A) const;
     Tensor<Scalar> & operator+= (const Tensor<Scalar> & A);
     Tensor<Scalar> & operator-= (const Tensor<Scalar> & A);
-
-    Tensor<Scalar>   operator*  (const Scalar & s) const;
-    Tensor<Scalar>   operator*  (const Tensor<Scalar> & A) const;
-    Vector<Scalar>   operator*  (const Vector<Scalar> & v) const;
 
     void clear();
 
@@ -82,40 +69,287 @@ namespace LCM {
   };
 
   //
+  // Third order tensor
+  //
+  template<typename Scalar>
+  class Tensor3
+  {
+  public:
+    Tensor3();
+    Tensor3(const Scalar s);
+    Tensor3(const Tensor3 & A);
+    ~Tensor3();
+
+    const Scalar & operator()(
+        const Index i, const Index j, const Index k) const;
+
+    Scalar & operator()(const Index i, const Index j, const Index k);
+
+    Tensor3<Scalar> & operator=  (const Tensor3<Scalar> & A);
+    Tensor3<Scalar> & operator+= (const Tensor3<Scalar> & A);
+    Tensor3<Scalar> & operator-= (const Tensor3<Scalar> & A);
+
+    void clear();
+
+  private:
+    Scalar e[MaxDim][MaxDim][MaxDim];
+  };
+
+  //
+  // Fourth order tensor
+  //
+  template<typename Scalar>
+  class Tensor4
+  {
+  public:
+    Tensor4();
+    Tensor4(const Scalar s);
+    Tensor4(const Tensor4 & A);
+    ~Tensor4();
+
+    const Scalar & operator()(
+        const Index i, const Index j, const Index k, const Index l) const;
+
+    Scalar & operator()(
+        const Index i, const Index j,
+        const Index k, const Index l);
+
+    Tensor4<Scalar> & operator=  (const Tensor4<Scalar> & A);
+    Tensor4<Scalar> & operator+= (const Tensor4<Scalar> & A);
+    Tensor4<Scalar> & operator-= (const Tensor4<Scalar> & A);
+
+    void clear();
+
+  private:
+    Scalar e[MaxDim][MaxDim][MaxDim][MaxDim];
+  };
+
+  //
   // Prototypes for utilities
   //
   template<typename Scalar>
-  Scalar dot(const Vector<Scalar> & u, const Vector<Scalar> & v);
+  Vector<Scalar>
+  operator+(const Vector<Scalar> & u, const Vector<Scalar> & v);
 
   template<typename Scalar>
-  Vector<Scalar> dot(const Tensor<Scalar> & A, const Vector<Scalar> & u);
+  Vector<Scalar>
+  operator-(const Vector<Scalar> & u, const Vector<Scalar> & v);
 
   template<typename Scalar>
-  Vector<Scalar> cross(const Vector<Scalar> & u, const Vector<Scalar> & v);
+  Scalar
+  operator*(const Vector<Scalar> & u, const Vector<Scalar> & v);
 
   template<typename Scalar>
-  Vector<Scalar> dot(const Vector<Scalar> & u, const Tensor<Scalar> & A);
+  Vector<Scalar>
+  operator*(const Scalar s, const Vector<Scalar> & u);
 
   template<typename Scalar>
-  Scalar norm(const Vector<Scalar> & v);
+  Vector<Scalar>
+  operator*(const Vector<Scalar> & u, const Scalar s);
 
   template<typename Scalar>
-  Tensor<Scalar> dot(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+  Scalar
+  dot(const Vector<Scalar> & u, const Vector<Scalar> & v);
 
   template<typename Scalar>
-  Scalar dotdot(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+  Vector<Scalar>
+  dot(const Tensor<Scalar> & A, const Vector<Scalar> & u);
 
   template<typename Scalar>
-  Scalar norm(const Tensor<Scalar> & A);
+  Vector<Scalar>
+  cross(const Vector<Scalar> & u, const Vector<Scalar> & v);
 
   template<typename Scalar>
-  Tensor<Scalar> dyad(const Vector<Scalar> & u, const Vector<Scalar> & v);
+  Vector<Scalar>
+  dot(const Vector<Scalar> & u, const Tensor<Scalar> & A);
 
   template<typename Scalar>
-  Tensor<Scalar> bun(const Vector<Scalar> & u, const Vector<Scalar> & v);
+  Scalar
+  norm(const Vector<Scalar> & u);
 
   template<typename Scalar>
-  Tensor<Scalar> eye();
+  Scalar
+  norm_1(const Vector<Scalar> & u);
+
+  template<typename Scalar>
+  Scalar
+  norm_infinity(const Vector<Scalar> & u);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  operator+(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  operator-(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  operator*(const Scalar s, const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  operator*(const Tensor<Scalar> & A, const Scalar s);
+
+  template<typename Scalar>
+  Vector<Scalar>
+  operator*(const Tensor<Scalar> & A, const Vector<Scalar> & v);
+
+  template<typename Scalar>
+  Vector<Scalar>
+  operator*(const Vector<Scalar> & v, const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  operator*(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  dot(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Scalar
+  dotdot(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  dotdot(const Tensor4<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  dotdot(const Tensor<Scalar> & B, const Tensor4<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor4<Scalar>
+  tensor(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Tensor4<Scalar>
+  odot(const Tensor<Scalar> & A, const Tensor<Scalar> & B);
+
+  template<typename Scalar>
+  Scalar
+  norm(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  norm_1(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  norm_infinity(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  dyad(const Vector<Scalar> & u, const Vector<Scalar> & v);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  tensor(const Vector<Scalar> & u, const Vector<Scalar> & v);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  bun(const Vector<Scalar> & u, const Vector<Scalar> & v);
+
+  template<typename Scalar>
+  const Tensor<Scalar>
+  eye();
+
+  template<typename Scalar>
+  const Tensor<Scalar>
+  identity();
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  transpose(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  symm(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  skew(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  skew(const Vector<Scalar> & u);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  inverse(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  det(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  trace(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  I1(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  I2(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Scalar
+  I3(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  exp(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  Tensor<Scalar>
+  log(const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  const Tensor4<Scalar>
+  identity_1();
+
+  template<typename Scalar>
+  const Tensor4<Scalar>
+  identity_2();
+
+  template<typename Scalar>
+  const Tensor4<Scalar>
+  identity_3();
+
+  template<typename Scalar>
+  std::istream &
+  operator>>(std::istream & is, Vector<Scalar> & u);
+
+  template<typename Scalar>
+  std::ostream &
+  operator<<(std::ostream & os, const Vector<Scalar> & u);
+
+  template<typename Scalar>
+  std::istream &
+  operator>>(std::istream & is, Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  std::ostream &
+  operator<<(std::ostream & os, const Tensor<Scalar> & A);
+
+  template<typename Scalar>
+  std::istream &
+  operator>>(std::istream & is, Tensor3<Scalar> & A);
+
+  template<typename Scalar>
+  std::ostream &
+  operator<<(std::ostream & os, const Tensor3<Scalar> & A);
+
+  template<typename Scalar>
+  std::istream &
+  operator>>(std::istream & is, Tensor4<Scalar> & A);
+
+  template<typename Scalar>
+  std::ostream &
+  operator<<(std::ostream & os, const Tensor4<Scalar> & A);
 
 } // namespace LCM
 
--- src/LCM/utils/Tensor.i.cc
+++ src/LCM/utils/Tensor.i.cc
@@ -3,9 +3,12 @@
 //
 #if !defined(LCM_Tensor_i_cc)
 
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <limits>
+
+#include "Teuchos_TestForException.hpp"
 
 namespace LCM {
 
@@ -28,7 +31,7 @@ namespace LCM {
   //
   template<typename Scalar>
   inline
-  Vector<Scalar>::Vector(const Scalar & s)
+  Vector<Scalar>::Vector(const Scalar s)
   {
     e[0] = s;
     e[1] = s;
@@ -42,27 +45,11 @@ namespace LCM {
   //
   template<typename Scalar>
   inline
-  Vector<Scalar>::Vector(const Scalar & s0,
-     const Scalar & s1,
-     const Scalar & s2)
+  Vector<Scalar>::Vector(const Scalar s0, const Scalar s1, const Scalar s2)
   {
     e[0] = s0;
     e[1] = s1;
     e[2] = s2;
-
-    return;
-  }
-
-  //
-  //
-  //
-  template<typename Scalar>
-  inline
-  Vector<Scalar>::Vector(const Scalar s[MaxDim])
-  {
-    e[0] = s[0];
-    e[1] = s[1];
-    e[2] = s[2];
 
     return;
   }
@@ -131,28 +118,6 @@ namespace LCM {
   //
   //
   template<typename Scalar>
-  inline const Scalar &
-  Vector<Scalar>::operator[](const Index i) const
-  {
-    assert(i < MaxDim);
-    return e[i];
-  }
-
-  //
-  //
-  //
-  template<typename Scalar>
-  inline Scalar &
-  Vector<Scalar>::operator[](const Index i)
-  {
-    assert(i < MaxDim);
-    return e[i];
-  }
-
-  //
-  //
-  //
-  template<typename Scalar>
   inline Vector<Scalar> &
   Vector<Scalar>::operator=(const Vector<Scalar> & v)
   {
@@ -169,13 +134,13 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Vector<Scalar>
-  Vector<Scalar>::operator+(const Vector<Scalar> & v) const
+  operator+(const Vector<Scalar> & u, const Vector<Scalar> & v)
   {
     Vector<Scalar> s;
 
-    s.e[0] = e[0] + v.e[0];
-    s.e[1] = e[1] + v.e[1];
-    s.e[2] = e[2] + v.e[2];
+    s(0) = u(0) + v(0);
+    s(1) = u(1) + v(1);
+    s(2) = u(2) + v(2);
 
     return s;
   }
@@ -185,13 +150,13 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Vector<Scalar>
-  Vector<Scalar>::operator-(const Vector<Scalar> & v) const
+  operator-(const Vector<Scalar> & u, const Vector<Scalar> & v)
   {
     Vector<Scalar> s;
 
-    s.e[0] = e[0] - v.e[0];
-    s.e[1] = e[1] - v.e[1];
-    s.e[2] = e[2] - v.e[2];
+    s(0) = u(0) - v(0);
+    s(1) = u(1) - v(1);
+    s(2) = u(2) - v(2);
 
     return s;
   }
@@ -229,9 +194,9 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Scalar
-  Vector<Scalar>::operator*(const Vector<Scalar> & v) const
+  operator*(const Vector<Scalar> & u, const Vector<Scalar> & v)
   {
-    return dot(*this, v);
+    return dot(u, v);
   }
 
   //
@@ -239,15 +204,19 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Vector<Scalar>
-  Vector<Scalar>::operator*(const Scalar & s) const
+  operator*(const Scalar s, const Vector<Scalar> & u)
   {
-    Vector<Scalar> v;
+    return Vector<Scalar>(s*u(0), s*u(1), s*u(2));
+  }
 
-    v.e[0] = s * e[0];
-    v.e[1] = s * e[1];
-    v.e[2] = s * e[2];
-
-    return v;
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Vector<Scalar>
+  operator*(const Vector<Scalar> & u, const Scalar s)
+  {
+    return s * u;
   }
 
   //
@@ -281,9 +250,29 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Scalar
-  norm(const Vector<Scalar> & v)
+  norm(const Vector<Scalar> & u)
   {
-    return sqrt(dot(v, v));
+    return sqrt(u(0)*u(0) + u(1)*u(1) + u(2)*u(2));
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  norm_1(const Vector<Scalar> & u)
+  {
+    return std::fabs(u(0)) + std::fabs(u(1)) + std::fabs(u(2));
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  norm_infinity(const Vector<Scalar> & u)
+  {
+    return std::max(std::max(std::fabs(u(0)),std::fabs(u(1))),std::fabs(u(2)));
   }
 
   //
@@ -313,7 +302,7 @@ namespace LCM {
   //
   template<typename Scalar>
   inline
-  Tensor<Scalar>::Tensor(const Scalar & s)
+  Tensor<Scalar>::Tensor(const Scalar s)
   {
     e[0][0] = s;
     e[0][1] = s;
@@ -336,9 +325,9 @@ namespace LCM {
   template<typename Scalar>
   inline
   Tensor<Scalar>::Tensor(
-      const Scalar & s00, const Scalar & s01, const Scalar & s02,
-      const Scalar & s10, const Scalar & s11, const Scalar & s12,
-      const Scalar & s20, const Scalar & s21, const Scalar & s22)
+      const Scalar s00, const Scalar s01, const Scalar s02,
+      const Scalar s10, const Scalar s11, const Scalar s12,
+      const Scalar s20, const Scalar s21, const Scalar s22)
   {
     e[0][0] = s00;
     e[0][1] = s01;
@@ -351,28 +340,6 @@ namespace LCM {
     e[2][0] = s20;
     e[2][1] = s21;
     e[2][2] = s22;
-
-    return;
-  }
-
-  //
-  //
-  //
-  template<typename Scalar>
-  inline
-  Tensor<Scalar>::Tensor(const Scalar s[MaxDim][MaxDim])
-  {
-    e[0][0] = s[0][0];
-    e[0][1] = s[0][1];
-    e[0][2] = s[0][2];
-
-    e[1][0] = s[1][0];
-    e[1][1] = s[1][1];
-    e[1][2] = s[1][2];
-
-    e[2][0] = s[2][0];
-    e[2][1] = s[2][1];
-    e[2][2] = s[2][2];
 
     return;
   }
@@ -482,54 +449,6 @@ namespace LCM {
   //
   //
   template<typename Scalar>
-  inline Tensor<Scalar>
-  Tensor<Scalar>::operator+(const Tensor<Scalar> & A) const
-  {
-    Tensor<Scalar> S;
-
-    S.e[0][0] = e[0][0] + A.e[0][0];
-    S.e[0][1] = e[0][1] + A.e[0][1];
-    S.e[0][2] = e[0][2] + A.e[0][2];
-
-    S.e[1][0] = e[1][0] + A.e[1][0];
-    S.e[1][1] = e[1][1] + A.e[1][1];
-    S.e[1][2] = e[1][2] + A.e[1][2];
-
-    S.e[2][0] = e[2][0] + A.e[2][0];
-    S.e[2][1] = e[2][1] + A.e[2][1];
-    S.e[2][2] = e[2][2] + A.e[2][2];
-
-    return S;
-  }
-
-  //
-  //
-  //
-  template<typename Scalar>
-  inline Tensor<Scalar>
-  Tensor<Scalar>::operator-(const Tensor<Scalar> & A) const
-  {
-    Tensor<Scalar> S;
-
-    S.e[0][0] = e[0][0] - A.e[0][0];
-    S.e[0][1] = e[0][1] - A.e[0][1];
-    S.e[0][2] = e[0][2] - A.e[0][2];
-
-    S.e[1][0] = e[1][0] - A.e[1][0];
-    S.e[1][1] = e[1][1] - A.e[1][1];
-    S.e[1][2] = e[1][2] - A.e[1][2];
-
-    S.e[2][0] = e[2][0] - A.e[2][0];
-    S.e[2][1] = e[2][1] - A.e[2][1];
-    S.e[2][2] = e[2][2] - A.e[2][2];
-
-    return S;
-  }
-
-  //
-  //
-  //
-  template<typename Scalar>
   inline Tensor<Scalar> &
   Tensor<Scalar>::operator+=(const Tensor<Scalar> & A)
   {
@@ -576,9 +495,23 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Tensor<Scalar>
-  Tensor<Scalar>::operator*(const Tensor<Scalar> & A) const
+  operator+(const Tensor<Scalar> & A, const Tensor<Scalar> & B)
   {
-    return dot(*this, A);
+    Tensor<Scalar> S;
+
+    S(0,0) = A(0,0) + B(0,0);
+    S(0,1) = A(0,1) + B(0,1);
+    S(0,2) = A(0,2) + B(0,2);
+
+    S(1,0) = A(1,0) + B(1,0);
+    S(1,1) = A(1,1) + B(1,1);
+    S(1,2) = A(1,2) + B(1,2);
+
+    S(2,0) = A(2,0) + B(2,0);
+    S(2,1) = A(2,1) + B(2,1);
+    S(2,2) = A(2,2) + B(2,2);
+
+    return S;
   }
 
   //
@@ -586,24 +519,79 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Tensor<Scalar>
-  Tensor<Scalar>::operator*(const Scalar & s) const
+  operator-(const Tensor<Scalar> & A, const Tensor<Scalar> & B)
   {
-    Tensor<Scalar> A;
+    Tensor<Scalar> S;
 
-    A.e[0][0] = s * e[0][0];
-    A.e[0][1] = s * e[0][1];
-    A.e[0][2] = s * e[0][2];
+    S(0,0) = A(0,0) - B(0,0);
+    S(0,1) = A(0,1) - B(0,1);
+    S(0,2) = A(0,2) - B(0,2);
 
-    A.e[1][0] = s * e[1][0];
-    A.e[1][1] = s * e[1][1];
-    A.e[1][2] = s * e[1][2];
+    S(1,0) = A(1,0) - B(1,0);
+    S(1,1) = A(1,1) - B(1,1);
+    S(1,2) = A(1,2) - B(1,2);
 
-    A.e[2][0] = s * e[2][0];
-    A.e[2][1] = s * e[2][1];
-    A.e[2][2] = s * e[2][2];
+    S(2,0) = A(2,0) - B(2,0);
+    S(2,1) = A(2,1) - B(2,1);
+    S(2,2) = A(2,2) - B(2,2);
 
-    return A;
+    return S;
   }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  operator*(const Tensor<Scalar> & A, const Tensor<Scalar> & B)
+  {
+    return dot(A, B);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  operator*(const Scalar s, const Tensor<Scalar> & A)
+  {
+    return Tensor<Scalar>(
+        s*A(0,0), s*A(0,1), s*A(0,2),
+        s*A(1,0), s*A(1,1), s*A(1,2),
+        s*A(2,0), s*A(2,1), s*A(2,2));
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  operator*(const Tensor<Scalar> & A, const Scalar s)
+  {
+    return s * A;
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Vector<Scalar>
+  operator*(const Tensor<Scalar> & A, const Vector<Scalar> & u)
+  {
+    return dot(A,u);
+  }
+
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Vector<Scalar>
+  operator*(const Vector<Scalar> & u, const Tensor<Scalar> & A)
+  {
+    return dot(u,A);
+  }
+
 
   //
   //
@@ -639,9 +627,9 @@ namespace LCM {
   {
     Vector<Scalar> v;
 
-    v(0) = A(0,0) * u(0) + A(0,1) * u(1) + A(0,2) * u(2);
-    v(1) = A(1,0) * u(0) + A(1,1) * u(1) + A(1,2) * u(2);
-    v(2) = A(2,0) * u(0) + A(2,1) * u(1) + A(2,2) * u(2);
+    v(0) = A(0,0)*u(0) + A(0,1)*u(1) + A(0,2)*u(2);
+    v(1) = A(1,0)*u(0) + A(1,1)*u(1) + A(1,2)*u(2);
+    v(2) = A(2,0)*u(0) + A(2,1)*u(1) + A(2,2)*u(2);
 
     return v;
   }
@@ -656,9 +644,9 @@ namespace LCM {
   {
     Vector<Scalar> v;
 
-    v(0) = A(0,0) * u(0) + A(1,0) * u(1) + A(2,0) * u(2);
-    v(1) = A(0,1) * u(0) + A(1,1) * u(1) + A(2,1) * u(2);
-    v(2) = A(0,2) * u(0) + A(1,2) * u(1) + A(2,2) * u(2);
+    v(0) = A(0,0)*u(0) + A(1,0)*u(1) + A(2,0)*u(2);
+    v(1) = A(0,1)*u(0) + A(1,1)*u(1) + A(2,1)*u(2);
+    v(2) = A(0,2)*u(0) + A(1,2)*u(1) + A(2,2)*u(2);
 
     return v;
   }
@@ -701,6 +689,34 @@ namespace LCM {
   //
   //
   template<typename Scalar>
+  inline Scalar
+  norm_1(const Tensor<Scalar> & A)
+  {
+    Scalar s0 = std::fabs(A(0,0)) + std::fabs(A(1,0)) + std::fabs(A(2,0));
+    Scalar s1 = std::fabs(A(0,1)) + std::fabs(A(1,1)) + std::fabs(A(2,1));
+    Scalar s2 = std::fabs(A(0,2)) + std::fabs(A(1,2)) + std::fabs(A(2,2));
+
+    return std::max(std::max(s0,s1),s2);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  norm_infinity(const Tensor<Scalar> & A)
+  {
+    Scalar s0 = std::fabs(A(0,0)) + std::fabs(A(0,1)) + std::fabs(A(0,2));
+    Scalar s1 = std::fabs(A(1,0)) + std::fabs(A(1,1)) + std::fabs(A(1,2));
+    Scalar s2 = std::fabs(A(2,0)) + std::fabs(A(2,1)) + std::fabs(A(2,2));
+
+    return std::max(std::max(s0,s1),s2);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
   inline Tensor<Scalar>
   dyad(const Vector<Scalar> & u, const Vector<Scalar> & v)
   {
@@ -722,7 +738,7 @@ namespace LCM {
   }
 
   //
-  //
+  // Just for Jay
   //
   template<typename Scalar>
   inline Tensor<Scalar>
@@ -736,11 +752,287 @@ namespace LCM {
   //
   template<typename Scalar>
   inline Tensor<Scalar>
+  tensor(const Vector<Scalar> & u, const Vector<Scalar> & v)
+  {
+    return dyad(u,v);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline const Tensor<Scalar>
   eye()
   {
     return Tensor<Scalar>(1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0);
   }
 
+  //
+  //
+  //
+  template<typename Scalar>
+  inline const Tensor<Scalar>
+  identity()
+  {
+    return Tensor<Scalar>(1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  transpose(const Tensor<Scalar> & A)
+  {
+    return Tensor<Scalar>(
+        A(0,0),A(1,0),A(2,0),
+        A(0,1),A(1,1),A(2,1),
+        A(0,2),A(1,2),A(2,2));
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  symm(const Tensor<Scalar> & A)
+  {
+    const Scalar s00 = A(0,0);
+    const Scalar s11 = A(1,1);
+    const Scalar s22 = A(2,2);
+
+    const Scalar s01 = 0.5*(A(0,1)+A(1,0));
+    const Scalar s02 = 0.5*(A(0,2)+A(2,0));
+    const Scalar s12 = 0.5*(A(1,2)+A(2,1));
+
+    return Tensor<Scalar>(
+        s00, s01, s02,
+        s01, s11, s12,
+        s02, s12, s22);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  skew(const Tensor<Scalar> & A)
+  {
+    const Scalar s01 = 0.5*(A(0,1)-A(1,0));
+    const Scalar s02 = 0.5*(A(0,2)-A(2,0));
+    const Scalar s12 = 0.5*(A(1,2)-A(2,1));
+
+    return Tensor<Scalar>(
+         0.0,  s01,  s02,
+        -s01,  0.0,  s12,
+        -s02, -s12,  0.0);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  skew(const Vector<Scalar> & u)
+  {
+    return Tensor<Scalar>(
+         0.0, -u(2),  u(1),
+        u(2),   0.0, -u(0),
+       -u(1),  u(0),   0.0);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  inverse(const Tensor<Scalar> & A)
+  {
+    const Scalar d = det(A);
+    TEST_FOR_EXCEPT_MSG(d == 0.0, "Attempted to invert a singular Tensor.");
+    Tensor<Scalar> B(
+        -A(1,2)*A(2,1) + A(1,1)*A(2,2),
+         A(0,2)*A(2,1) - A(0,1)*A(2,2),
+        -A(0,2)*A(1,1) + A(0,1)*A(1,2),
+         A(1,2)*A(2,0) - A(1,0)*A(2,2),
+        -A(0,2)*A(2,0) + A(0,0)*A(2,2),
+         A(0,2)*A(1,0) - A(0,0)*A(1,2),
+        -A(1,1)*A(2,0) + A(1,0)*A(2,1),
+         A(0,1)*A(2,0) - A(0,0)*A(2,1),
+        -A(0,1)*A(1,0) + A(0,0)*A(1,1)
+    );
+    return (1.0 / d) * B;
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  det(const Tensor<Scalar> & A)
+  {
+    return
+        -A(0,2)*A(1,1)*A(2,0) + A(0,1)*A(1,2)*A(2,0) +
+         A(0,2)*A(1,0)*A(2,1) - A(0,0)*A(1,2)*A(2,1) -
+         A(0,1)*A(1,0)*A(2,2) + A(0,0)*A(1,1)*A(2,2);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  trace(const Tensor<Scalar> & A)
+  {
+    return A(0,0) + A(1,1) + A(2,2);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  I1(const Tensor<Scalar> & A)
+  {
+    return trace(A);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  I2(const Tensor<Scalar> & A)
+  {
+    const Scalar trA = trace(A);
+
+    return 0.5 * (trA*trA - A(0,0)*A(0,0) - A(1,1)*A(1,1) - A(2,2)*A(2,2)) -
+        A(0,1)*A(1,0) - A(0,2)*A(2,0) - A(1,2)*A(2,1);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar
+  I3(const Tensor<Scalar> & A)
+  {
+    return det(A);
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  exp(const Tensor<Scalar> & A)
+  {
+    const Index maxNumIter = 128;
+    const Scalar tol = std::numeric_limits<Scalar>::epsilon();
+
+    Index k = 0;
+    const Tensor<Scalar> term = identity<Scalar>();
+
+    // Relative error taken wrt to the first term, which is I and norm = 1
+    Scalar relError = 1.0;
+
+    Tensor<Scalar> B = term;
+
+    while (relError > tol && k < maxNumIter) {
+      term = (1.0 / (k + 1.0)) * term * A;
+      B = B + term;
+      relError = norm_1(term);
+      ++k;
+    }
+
+    return B;
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Tensor<Scalar>
+  log(const Tensor<Scalar> & A)
+  {
+    const Index maxNumIter = 128;
+    const Scalar tol = std::numeric_limits<Scalar>::epsilon();
+
+    Index k = 1;
+    const Scalar normA = norm_1(A);
+    const Tensor<Scalar> Am1 = A - identity<Scalar>();
+    Tensor<Scalar> term = Am1;
+    Scalar normTerm = norm_1(term);
+    Scalar relError = normTerm / normA;
+
+    Tensor<Scalar> B = term;
+
+    while (relError > tol && k < maxNumIter) {
+      term = - (k / (k + 1.0)) * term * Am1;
+      B = B + term;
+      normTerm = norm_1(term);
+      relError = normTerm / normA;
+      ++k;
+    }
+
+    return B;
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline const Scalar &
+  Tensor3<Scalar>::operator()(const Index i, const Index j, const Index k) const
+  {
+    assert(i < MaxDim);
+    assert(j < MaxDim);
+    assert(k < MaxDim);
+    return e[i][j][k];
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar &
+  Tensor3<Scalar>::operator()(const Index i, const Index j, const Index k)
+  {
+    assert(i < MaxDim);
+    assert(j < MaxDim);
+    assert(k < MaxDim);
+    return e[i][j][k];
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline const Scalar &
+  Tensor4<Scalar>::operator()(
+      const Index i, const Index j, const Index k, const Index l) const
+  {
+    assert(i < MaxDim);
+    assert(j < MaxDim);
+    assert(k < MaxDim);
+    assert(l < MaxDim);
+    return e[i][j][k][l];
+  }
+
+  //
+  //
+  //
+  template<typename Scalar>
+  inline Scalar &
+  Tensor4<Scalar>::operator()(
+      const Index i, const Index j, const Index k, const Index l)
+  {
+    assert(i < MaxDim);
+    assert(j < MaxDim);
+    assert(k < MaxDim);
+    assert(l < MaxDim);
+    return e[i][j][k][l];
+  }
 
 } // namespace LCM
 
