b4b13b296eb9f18c61ae9f5cc09b620ea12b7b94
David Littlewood <djlittl@sandia.gov>
2011-10-05 17:20:25 -0600

    Progress toward unit testing the LameStress evaluator.
    One difficulty in unit testing an evaluator is the need to
    have all the required fields evaluated prior to calling
    evaluateFields() on the evaluator you care about.  The
    approach I've taken is to create, within the unit test,
    an evaluator who's purpose is to fill in all the required
    fields for the evaluator to be tested.  In the LameStress
    unit test, this evaluator is currently called DefGradUnitTest;
    it has no dependent fields and fills in the deformation gradient
    per the unit test so that LameStress.evaluateFields() can be called
    as directly as possible.  The plan is to generalize this and create
    an evaluator called something like UnitTestRequiredFieldEvaluator
    that fills in fields with explicitly supplied values from within
    the unit test.
    
    An alternative approach is to construct the full evaluation tree.
    In the case of LameStress, this involves a half dozen evaluators,
    which introduces unwanted and difficult-to-manage dependencies into
    the unit test.

--- src/LCM/test/unit_tests/utLameStress_elastic.cpp
+++ src/LCM/test/unit_tests/utLameStress_elastic.cpp
@@ -24,12 +24,71 @@ using namespace std;
 
 namespace {
 
+// Create a DefGrad evaluator that does not have any dependent fields.
+// This will allow for calls to the LameStress evaluator without constructing the entire evaluation tree.
+// Instead, the deformation gradient will be set explicitly in DefGradUnitTest, and will then be passed
+// to the LameStress evaluator via the FieldManager.
+template<typename EvalT, typename Traits>
+class DefGradUnitTest : public PHX::EvaluatorWithBaseImpl<Traits>,
+		public PHX::EvaluatorDerived<EvalT, Traits>  {
+
+public:
+
+  DefGradUnitTest(const Teuchos::ParameterList& p) :
+    defgrad(p.get<std::string>("DefGrad Name"), p.get<Teuchos::RCP<PHX::DataLayout> >("QP Tensor Data Layout") )
+  {
+    Teuchos::RCP<PHX::DataLayout> tensor_dl =
+      p.get< Teuchos::RCP<PHX::DataLayout> >("QP Tensor Data Layout");
+    
+    std::vector<PHX::DataLayout::size_type> dims;
+    tensor_dl->dimensions(dims);
+    worksetSize  = dims[0];
+    numQPs  = dims[1];
+    numDims = dims[2];
+
+    this->addEvaluatedField(defgrad);
+
+    this->setName("DefGrad"+PHX::TypeString<EvalT>::value);
+  }
+
+  void postRegistrationSetup(typename Traits::SetupData d,
+                             PHX::FieldManager<Traits>& fm)
+  {
+    this->utils.setFieldData(defgrad,fm);
+  }
+
+  void evaluateFields(typename Traits::EvalData workset)
+  {
+    // Set the deformation gradient to the identity tensor.
+    for(std::size_t cell=0; cell<workset.numCells; ++cell){
+      for(std::size_t qp=0; qp<numQPs; ++qp){
+        for(std::size_t i=0; i<numDims; ++i){
+          for(std::size_t j=0; j < numDims; ++j){
+            defgrad(cell,qp,i,j) = 0.0;
+          }
+          defgrad(cell,qp,i,i) += 1.0;
+        }
+      }
+    }
+  }
+
+private:
+
+  typedef typename EvalT::ScalarT ScalarT;
+  typedef typename EvalT::MeshScalarT MeshScalarT;
+  
+  // Output:
+  PHX::MDField<ScalarT,Cell,QuadPoint,Dim,Dim> defgrad;
+
+  unsigned int numQPs;
+  unsigned int numDims;
+  unsigned int worksetSize;
+};
+
+
 TEUCHOS_UNIT_TEST( LameStress_elastic, Instantiation )
 {
-  Teuchos::ParameterList parameterList("Stress");
-  parameterList.set<string>("DefGrad Name", "Deformation Gradient");
-  parameterList.set<string>("Stress Name", "Stress");
-
+  // Set up the data layout
   const int worksetSize = 1;
   const int numQPts = 1;
   const int numDim = 3;
@@ -37,18 +96,60 @@ TEUCHOS_UNIT_TEST( LameStress_elastic, Instantiation )
     Teuchos::rcp(new PHX::MDALayout<Cell, QuadPoint>(worksetSize, numQPts));
   Teuchos::RCP<PHX::MDALayout<Cell, QuadPoint, Dim, Dim> > qp_tensor =
     Teuchos::rcp(new PHX::MDALayout<Cell, QuadPoint, Dim, Dim>(worksetSize, numQPts, numDim, numDim));
-  parameterList.set< Teuchos::RCP<PHX::DataLayout> >("QP Scalar Data Layout", qp_scalar);
-  parameterList.set< Teuchos::RCP<PHX::DataLayout> >("QP Tensor Data Layout", qp_tensor);
 
-  parameterList.set<string>("Lame Material Model", "Elastic");
-  Teuchos::ParameterList& materialModelParametersList = parameterList.sublist("Lame Material Parameters");
+  // Instantiate the required evaluators with EvalT = PHAL::AlbanyTraits::Residual and Traits = PHAL::AlbanyTraits
+
+  // DefGradUnitTest evaluator
+  Teuchos::ParameterList defGradUnitTestParameterList("DefGrad");
+  defGradUnitTestParameterList.set<bool>("avgJ Name", false);
+  defGradUnitTestParameterList.set<bool>("volavgJ Name", false);
+  defGradUnitTestParameterList.set<string>("Weights Name", "Weights");
+  defGradUnitTestParameterList.set<string>("Gradient QP Variable Name", "Displacement Gradient");
+  defGradUnitTestParameterList.set<string>("DefGrad Name", "Deformation Gradient");
+  defGradUnitTestParameterList.set<string>("DetDefGrad Name", "Determinant of Deformation Gradient"); 
+  defGradUnitTestParameterList.set< Teuchos::RCP<PHX::DataLayout> >("QP Scalar Data Layout", qp_scalar);
+  defGradUnitTestParameterList.set< Teuchos::RCP<PHX::DataLayout> >("QP Tensor Data Layout", qp_tensor);
+  Teuchos::RCP<DefGradUnitTest<PHAL::AlbanyTraits::Residual, PHAL::AlbanyTraits> > defGradUnitTest = 
+    Teuchos::rcp(new DefGradUnitTest<PHAL::AlbanyTraits::Residual, PHAL::AlbanyTraits>(defGradUnitTestParameterList));
+
+  // LameStress evaluator
+  Teuchos::RCP<Teuchos::ParameterList> lameStressParameterList = Teuchos::rcp(new Teuchos::ParameterList("Stress"));
+  //int type = LameStressUnitTestFactoryTraits<PHAL::AlbanyTraits>::id_lame_stress;
+  //lameStressParameterList->set<int>("Type", type);
+  lameStressParameterList->set<string>("DefGrad Name", "Deformation Gradient");
+  lameStressParameterList->set<string>("Stress Name", "Stress");
+  lameStressParameterList->set< Teuchos::RCP<PHX::DataLayout> >("QP Scalar Data Layout", qp_scalar);
+  lameStressParameterList->set< Teuchos::RCP<PHX::DataLayout> >("QP Tensor Data Layout", qp_tensor);
+  lameStressParameterList->set<string>("Lame Material Model", "Elastic");
+  Teuchos::ParameterList& materialModelParametersList = lameStressParameterList->sublist("Lame Material Parameters");
   materialModelParametersList.set<double>("Youngs Modulus", 1.0);
   materialModelParametersList.set<double>("Poissons Ratio", 0.25);
+  Teuchos::RCP<LCM::LameStress<PHAL::AlbanyTraits::Residual, PHAL::AlbanyTraits> > lameStress = 
+    Teuchos::rcp(new LCM::LameStress<PHAL::AlbanyTraits::Residual, PHAL::AlbanyTraits>(*lameStressParameterList));
 
-  LCM::LameStress<PHAL::AlbanyTraits::Residual, PHAL::AlbanyTraits> lameStress(parameterList);
+  // Instantiate a field manager.
+  PHX::FieldManager<PHAL::AlbanyTraits> fieldManager;
 
-  int i1 = 5;
-  TEST_EQUALITY_CONST( i1, 5 );
+  // Register the evaluators with the field manager
+  fieldManager.registerEvaluator<PHAL::AlbanyTraits::Residual>(defGradUnitTest);
+  fieldManager.registerEvaluator<PHAL::AlbanyTraits::Residual>(lameStress);
+
+  // Set the LameStress evaluated fields as required fields
+  for(std::vector<Teuchos::RCP<PHX::FieldTag> >::const_iterator it = lameStress->evaluatedFields().begin() ; it != lameStress->evaluatedFields().end() ; it++)
+    fieldManager.requireField<PHAL::AlbanyTraits::Residual>(**it);
+ 
+  PHAL::AlbanyTraits::SetupData setupData = "Test String";
+  fieldManager.postRegistrationSetup(setupData);
+
+  // \todo Need workset.stateArrayPtr, which is an Albany::StateArray*; will need a StateManager for this.
+  PHAL::Workset workset;
+  workset.numCells = worksetSize;
+
+//   void* voidPtr(0);
+
+//   fieldManager.preEvaluate<PHAL::AlbanyTraits::Residual>(voidPtr);
+//   fieldManager.evaluateFields<PHAL::AlbanyTraits::Residual>(workset);
+//   fieldManager.postEvaluate<PHAL::AlbanyTraits::Residual>(voidPtr);
 }
 
 } // namespace
