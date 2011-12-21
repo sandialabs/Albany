4f6c626f6d615cb44103721c9a06338c6bc5534e
Jake Ostien <jtostie@sandia.gov>
2011-12-21 09:53:41 -0800

    Add Time as a parameter to the NonlinearElasticityProblem
    To help me debug Sierra....don't ask.

--- src/LCM/problems/NonlinearElasticityProblem.hpp
+++ src/LCM/problems/NonlinearElasticityProblem.hpp
@@ -148,6 +148,7 @@ namespace Albany {
 #include "SaturationExponent.hpp"
 #include "DislocationDensity.hpp"
 #include "TLElasResid.hpp"
+#include "Time.hpp"
 
 template <typename EvalT>
 void Albany::NonlinearElasticityProblem::constructEvaluators(
@@ -233,6 +234,22 @@ void Albany::NonlinearElasticityProblem::constructEvaluators(
 
   // Temporary variable used numerous times below
   Teuchos::RCP<PHX::Evaluator<AlbanyTraits> > ev;
+
+  { // Time
+    RCP<ParameterList> p = rcp(new ParameterList);
+    
+    p->set<string>("Time Name", "Time");
+    p->set<string>("Delta Time Name", "Delta Time");
+    p->set< RCP<DataLayout> >("Workset Scalar Data Layout", dl->workset_scalar);
+    p->set<RCP<ParamLib> >("Parameter Library", paramLib);
+    p->set<bool>("Disable Transient", true);
+
+    ev = rcp(new LCM::Time<EvalT,AlbanyTraits>(*p));
+    fm0.template registerEvaluator<EvalT>(ev);
+    p = stateMgr.registerStateVariable("Time",dl->workset_scalar, dl->dummy,"zero", true);
+    ev = rcp(new PHAL::SaveStateField<EvalT,AlbanyTraits>(*p));
+    fm0.template registerEvaluator<EvalT>(ev);
+  }
 
   { // Elastic Modulus
     RCP<ParameterList> p = rcp(new ParameterList);
