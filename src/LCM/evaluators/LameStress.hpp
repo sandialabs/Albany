eba4fd085e9ac34cae9dbf77878937239e3f8599
Andy Salinger <agsalin@sandia.gov>
2012-01-06 15:19:55 -0700

    Added Finite Difference probing around Lame.
    In LameStress evaluator, I made the previous implementation
    into a function call that is called for  Residual evaluations.
    For Jacobian and Tangent evaluations, this function is probed
    with finite differences. I used inheritance from a base class
    and template specialization to keep redundant code down. Could
    use a code review....
    
    I changed the LameMultiMaterial problems to use the Jacobian
    and Ifpack preconditioning, which makes their times reasonable.
    
    Changed LameStaticElasticity3D problem to run both MatrixFree and
    ProbeLame versions. All problems with a matrix also do sensitivity
    analysis with the Tangent calculation.
    
    1000 and 4000 element block problems now run well (except for
    excessive screen output).

--- src/LCM/evaluators/LameStress.hpp
+++ src/LCM/evaluators/LameStress.hpp
@@ -29,23 +29,43 @@ namespace LCM {
 /** \brief Evaluates stress using the Library for Advanced Materials for Engineering (LAME).
 */
 
+// Base class 
+// (1) Implements constructor and postRegistrationSetup for all inherited
+// and specialized cases below. Implements a dummy evaluateFields that
+// just throw's a Not Implemented
+// (2) Implements private functions with calls to Lame with doubles.
+//
 template<typename EvalT, typename Traits>
-class LameStress : public PHX::EvaluatorWithBaseImpl<Traits>,
-		    public PHX::EvaluatorDerived<EvalT, Traits>  {
+class LameStressBase : public PHX::EvaluatorWithBaseImpl<Traits>,
+		         public PHX::EvaluatorDerived<EvalT, Traits>  {
 
 public:
 
-  LameStress(Teuchos::ParameterList& p);
+  LameStressBase(Teuchos::ParameterList& p);
 
   void postRegistrationSetup(typename Traits::SetupData d,
                       PHX::FieldManager<Traits>& vm);
 
-  void evaluateFields(typename Traits::EvalData d);
+  virtual void evaluateFields(typename Traits::EvalData d);
 
-private:
+protected:
 
   typedef typename EvalT::ScalarT ScalarT;
   typedef typename EvalT::MeshScalarT MeshScalarT;
+
+  // Protected function for stress calc, only for RealType 
+  void calcStressRealType(PHX::MDField<RealType,Cell,QuadPoint,Dim,Dim>& stressFieldRef,
+                          PHX::MDField<RealType,Cell,QuadPoint,Dim,Dim>& defGradFieldRef,
+                          typename Traits::EvalData workset,
+                          Teuchos::RCP<lame::matParams>& matp);
+
+  // Allocate material parameter arrays -- always doubles
+  void setMatP(Teuchos::RCP<lame::matParams>& matp,
+               typename Traits::EvalData workset);
+
+  // Free material pointer arrays -- always doubles
+  void freeMatP(Teuchos::RCP<lame::matParams>& matp);
+
 
   // Input:
   PHX::MDField<ScalarT,Cell,QuadPoint,Dim,Dim> defGradField;
@@ -53,6 +73,7 @@ private:
   std::string defGradName, stressName;
   unsigned int numQPs;
   unsigned int numDims;
+  Teuchos::RCP<PHX::DataLayout> tensor_dl;
 
   // Output:
   PHX::MDField<ScalarT,Cell,QuadPoint,Dim,Dim> stressField;
@@ -69,6 +90,43 @@ private:
   // Vector of the fields corresponding to the LAME material model state variables
   std::vector< PHX::MDField<ScalarT,Cell,QuadPoint,Dim,Dim> > lameMaterialModelStateVariableFields;
 };
+
+// Inherted classes 
+template<typename EvalT, typename Traits> class LameStress;
+
+// For all cases except those specialized below, just fall through to base class.
+// The base class throws "Not Implemented" for evaluate fields.
+template<typename EvalT, typename Traits>
+class LameStress : public LameStressBase<EvalT, Traits> {
+public:
+  LameStress(Teuchos::ParameterList& p) : LameStressBase<EvalT, Traits>(p) {};
+};
+
+
+// Template Specialization: Residual Eval calls Lame with doubles.
+template<typename Traits>
+class LameStress<PHAL::AlbanyTraits::Residual, Traits> : public LameStressBase<PHAL::AlbanyTraits::Residual, Traits> {
+public:
+  LameStress(Teuchos::ParameterList& p) : LameStressBase<PHAL::AlbanyTraits::Residual,Traits>(p) {};
+  void evaluateFields(typename Traits::EvalData d);
+};
+
+// Template Specialization: Jacobian Eval does finite difference of Lame with doubles.
+template<typename Traits>
+class LameStress<PHAL::AlbanyTraits::Jacobian, Traits> : public LameStressBase<PHAL::AlbanyTraits::Jacobian, Traits> {
+public:
+  LameStress(Teuchos::ParameterList& p) : LameStressBase<PHAL::AlbanyTraits::Jacobian,Traits>(p) {};
+  void evaluateFields(typename Traits::EvalData d);
+};
+
+// Template Specialization: Tangent Eval does finite difference of Lame with doubles.
+template<typename Traits>
+class LameStress<PHAL::AlbanyTraits::Tangent, Traits> : public LameStressBase<PHAL::AlbanyTraits::Tangent, Traits> {
+public:
+  LameStress(Teuchos::ParameterList& p) : LameStressBase<PHAL::AlbanyTraits::Tangent,Traits>(p) {};
+  void evaluateFields(typename Traits::EvalData d);
+};
+
 }
 
 #endif
