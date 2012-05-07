3d02c5cab74293c83da2998c0010615e3c84e207
Steve Sun <wsun@sandia.gov>
2012-05-06 20:19:18 -0700

    Add van Genuchten permeability model for unsaturated porous media. (work in progress)

---
+++ src/LCM/evaluators/VanGenuchtenPermeability_Def.hpp
@@ -0,0 +1,185 @@
+/********************************************************************\
+*            Albany, Copyright (2010) Sandia Corporation             *
+*                                                                    *
+* Notice: This computer software was prepared by Sandia Corporation, *
+* hereinafter the Contractor, under Contract DE-AC04-94AL85000 with  *
+* the Department of Energy (DOE). All rights in the computer software*
+* are reserved by DOE on behalf of the United States Government and  *
+* the Contractor as provided in the Contract. You are authorized to  *
+* use this computer software for Governmental purposes but it is not *
+* to be released or distributed to the public. NEITHER THE GOVERNMENT*
+* NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR      *
+* ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE. This notice    *
+* including this sentence must appear on any copies of this software.*
+*    Questions to Andy Salinger, agsalin@sandia.gov                  *
+\********************************************************************/
+
+
+#include <fstream>
+#include "Teuchos_TestForException.hpp"
+#include "Phalanx_DataLayout.hpp"
+#include "Sacado_ParameterRegistration.hpp"
+#include "Albany_Utils.hpp"
+
+namespace LCM {
+
+template<typename EvalT, typename Traits>
+VanGenuchtenPermeability<EvalT, Traits>::
+VanGenuchtenPermeability(Teuchos::ParameterList& p) :
+  vgPermeability(p.get<std::string>("Van Genuchten Permeability Name"),
+		 p.get<Teuchos::RCP<PHX::DataLayout> >("QP Scalar Data Layout"))
+{
+  Teuchos::ParameterList* elmd_list = 
+    p.get<Teuchos::ParameterList*>("Parameter List");
+
+  Teuchos::RCP<PHX::DataLayout> vector_dl =
+    p.get< Teuchos::RCP<PHX::DataLayout> >("QP Vector Data Layout");
+  std::vector<PHX::DataLayout::size_type> dims;
+  vector_dl->dimensions(dims);
+  numQPs  = dims[1];
+  numDims = dims[2];
+
+  Teuchos::RCP<ParamLib> paramLib = 
+    p.get< Teuchos::RCP<ParamLib> >("Parameter Library", Teuchos::null);
+
+  std::string type = elmd_list->get("Van Genuchten Permeability Type", "Constant");
+  if (type == "Constant") {
+    is_constant = true;
+    constant_value = elmd_list->get("Value", 1.0e-5); // default value=1, identical to Terzaghi stress
+
+    // Add Van Genuchten Permeability as a Sacado-ized parameter
+    new Sacado::ParameterRegistration<EvalT, SPL_Traits>(
+	"Van Genuchten Permeability", this, paramLib);
+  }
+  else if (type == "Truncated KL Expansion") {
+    is_constant = false;
+    PHX::MDField<MeshScalarT,Cell,QuadPoint,Dim>
+      fx(p.get<string>("QP Coordinate Vector Name"), vector_dl);
+    coordVec = fx;
+    this->addDependentField(coordVec);
+
+    exp_rf_kl = 
+      Teuchos::rcp(new Stokhos::KL::ExponentialRandomField<MeshScalarT>(*elmd_list));
+    int num_KL = exp_rf_kl->stochasticDimension();
+
+    // Add KL random variables as Sacado-ized parameters
+    rv.resize(num_KL);
+    for (int i=0; i<num_KL; i++) {
+      std::string ss = Albany::strint("Van Genuchten Permeability KL Random Variable",i);
+      new Sacado::ParameterRegistration<EvalT, SPL_Traits>(ss, this, paramLib);
+      rv[i] = elmd_list->get(ss, 0.0);
+    }
+  }
+  else {
+	  TEUCHOS_TEST_FOR_EXCEPTION(true, Teuchos::Exceptions::InvalidParameter,
+		       "Invalid Van Genuchten Permeability type " << type);
+  } 
+
+  // Optional dependence on Temperature (E = E_ + dEdT * T)
+  // Switched ON by sending Temperature field in p
+
+  if ( p.isType<string>("Porosity Name") ) {
+    Teuchos::RCP<PHX::DataLayout> scalar_dl =
+      p.get< Teuchos::RCP<PHX::DataLayout> >("QP Scalar Data Layout");
+    PHX::MDField<ScalarT,Cell,QuadPoint>
+      tp(p.get<string>("Porosity Name"), scalar_dl);
+    porosity = tp;
+    this->addDependentField(porosity);
+    isPoroElastic = true;
+
+  }
+  else {
+    isPoroElastic = false;
+
+  }
+
+  if ( p.isType<string>("QP Pore Pressure Name") ) {
+         Teuchos::RCP<PHX::DataLayout> scalar_dl =
+           p.get< Teuchos::RCP<PHX::DataLayout> >("QP Scalar Data Layout");
+         PHX::MDField<ScalarT,Cell,QuadPoint>
+           ppn(p.get<string>("QP Pore Pressure Name"), scalar_dl);
+         porePressure = ppn;
+         isCompressibleSolidPhase = true;
+         isCompressibleFluidPhase = true;
+         isPoroElastic = true;
+         this->addDependentField(porePressure);
+
+         waterUnitWeight = elmd_list->get("Water Unit Weight", 9810.0); // typically Kgrain >> Kskeleton
+                    new Sacado::ParameterRegistration<EvalT, SPL_Traits>(
+          "Water Unit Weight", this, paramLib);
+  }
+
+
+  this->addEvaluatedField(vgPermeability);
+  this->setName("Van Genuchten Permeability"+PHX::TypeString<EvalT>::value);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+void VanGenuchtenPermeability<EvalT, Traits>::
+postRegistrationSetup(typename Traits::SetupData d,
+                      PHX::FieldManager<Traits>& fm)
+{
+  this->utils.setFieldData(vgPermeability,fm);
+  if (!is_constant) this->utils.setFieldData(coordVec,fm);
+  if (isPoroElastic) this->utils.setFieldData(porosity,fm);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+void VanGenuchtenPermeability<EvalT, Traits>::
+evaluateFields(typename Traits::EvalData workset)
+{
+  std::size_t numCells = workset.numCells;
+
+  if (is_constant) {
+    for (std::size_t cell=0; cell < numCells; ++cell) {
+      for (std::size_t qp=0; qp < numQPs; ++qp) {
+    	  vgPermeability(cell,qp) = constant_value;
+      }
+    }
+  }
+  else {
+    for (std::size_t cell=0; cell < numCells; ++cell) {
+      for (std::size_t qp=0; qp < numQPs; ++qp) {
+	Teuchos::Array<MeshScalarT> point(numDims);
+	for (std::size_t i=0; i<numDims; i++)
+	  point[i] = Sacado::ScalarValue<MeshScalarT>::eval(coordVec(cell,qp,i));
+		  vgPermeability(cell,qp) = exp_rf_kl->evaluate(point, rv);
+      }
+    }
+  }
+  if (isPoroElastic) {
+    for (std::size_t cell=0; cell < numCells; ++cell) {
+      for (std::size_t qp=0; qp < numQPs; ++qp) {
+    	  // van Genuchten permeability equation
+    	  vgPermeability(cell,qp) = constant_value*porosity(cell,qp)*porosity(cell,qp)*porosity(cell,qp)/
+    			                    (  1.0-porosity(cell,qp)*porosity(cell,qp) )/
+    			                    std::pow( 1.0 + std::pow(50.0*porePressure(cell,qp)/waterUnitWeight, 4.0),0.9);
+      }
+    }
+  }
+}
+
+// **********************************************************************
+template<typename EvalT,typename Traits>
+typename VanGenuchtenPermeability<EvalT,Traits>::ScalarT&
+VanGenuchtenPermeability<EvalT,Traits>::getValue(const std::string &n)
+{
+  if (n == "Van Genuchten Permeability")
+    return constant_value;
+  for (int i=0; i<rv.size(); i++) {
+    if (n == Albany::strint("Van Genuchten Permeability KL Random Variable",i))
+      return rv[i];
+  }
+  TEUCHOS_TEST_FOR_EXCEPTION(true, Teuchos::Exceptions::InvalidParameter,
+		     std::endl <<
+		     "Error! Logic error in getting parameter " << n
+		     << " in VanGenuchtenPermeability::getValue()" << std::endl);
+  return constant_value;
+}
+
+// **********************************************************************
+// **********************************************************************
+}
+
