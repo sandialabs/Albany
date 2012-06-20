522f6597ee6dc45c91d1a9163641e635d201851e
Glen Hansen <gahanse@sandia.gov>
2012-06-20 13:48:03 -0600

    Fix Intrepid issue in Neumann BC class.
    Fix std::invalid_argument being thrown by Intrepid ArrayTools::dotMultiplyDataData
    function inside Neumann BC dudxdudy test, noted by Jake Ostien. I used the wrong function
    for the shape of the return data array.
    
    Also, added the ability to specify element-block specific initial conditions.
    
    Add the beginnings of a Cahn-Hilliard example problem (work-in-progress).

--- src/Albany_Application.cpp
+++ src/Albany_Application.cpp
@@ -193,10 +193,10 @@ Application(const RCP<const Epetra_Comm>& comm_,
   if (initial_guess != Teuchos::null) *initial_x = *initial_guess;
   else {
     overlapped_x->Import(*initial_x, *importer, Insert);
-    Albany::InitialConditions(overlapped_x, wsElNodeEqID, coords, neq, numDim,
+    Albany::InitialConditions(overlapped_x, wsElNodeEqID, wsEBNames, coords, neq, numDim,
                               problemParams->sublist("Initial Condition"),
                               disc->hasRestartSolution());
-    Albany::InitialConditions(overlapped_xdot,  wsElNodeEqID, coords, neq, numDim,
+    Albany::InitialConditions(overlapped_xdot,  wsElNodeEqID, wsEBNames, coords, neq, numDim,
                               problemParams->sublist("Initial Condition Dot"));
     initial_x->Export(*overlapped_x, *exporter, Insert);
     initial_x_dot->Export(*overlapped_xdot, *exporter, Insert);
