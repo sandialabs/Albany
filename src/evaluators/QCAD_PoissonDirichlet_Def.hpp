4b99fda1dd2edb6e8a119f0d6be5efecab14ee05
Erik Nielsen <enielse@sandia.gov>
2012-07-03 15:01:05 -0600

    Fixed a bug in the decision branching in QCAD_PoissonDirichlet_Def.hpp, which caused material database errors in QCAD problems that used MetalPolysilicon gates whose nodesets were specified as this material indirectly through an element block name in the material database.  The updated code checks the "Category" property of the material to determine whether it is a metal or semiconductor.

--- src/evaluators/QCAD_PoissonDirichlet_Def.hpp
+++ src/evaluators/QCAD_PoissonDirichlet_Def.hpp
@@ -46,8 +46,11 @@ PoissonDirichlet(Teuchos::ParameterList& p) :
   // obtain material or eb name for a given nodeset 
   std::string nodeSetName = PHAL::DirichletBase<EvalT,Traits>::nodeSetID;
   material = materialDB->getNodeSetParam<string>(nodeSetName,"material","");
-  if (material.length() == 0) 
-    this->ebName = materialDB->getNodeSetParam<string>(nodeSetName,"elementBlock","");
+  if (material.length() == 0) {
+    ebName = materialDB->getNodeSetParam<string>(nodeSetName,"elementBlock","");
+    if (ebName.length() > 0)
+      material = materialDB->getElementBlockParam<string>(ebName,"material","");
+  }
 
   // private scaling parameter (note: kbT is used in calculating qPhiRef below)
   const double kbBoltz = 8.617343e-05; //[eV/K]
@@ -95,88 +98,116 @@ void PoissonDirichlet<EvalT, Traits>::
 evaluateFields(typename Traits::EvalData dirichletWorkset)
 {
   ScalarT bcValue = 0.0; 
-  
-  //! Contacts on insulator
+
+
   if (material.length() > 0)  
   {
-    double metalWorkFunc = materialDB->getMaterialParam<double>(material,"Work Function");
-    ScalarT offsetDueToWorkFunc = (metalWorkFunc-qPhiRef)/1.0;  // 1.0 converts from [eV] to [V]
+    std::string category = materialDB->getMaterialParam<std::string>(material,"Category");
 
-    bcValue = (user_value - offsetDueToWorkFunc);  //[V]
-  }
-  
-  //! Ohmic contacts on semiconductor (charge neutrality and equilibrium ) 
-  else if (ebName.length() > 0)  
-  {
-    // Universal constants
-    const double kbBoltz = 8.617343e-05;  // [eV/K]
-    const double eleQ = 1.602176487e-19;  // [C]
-    const double m0 = 9.10938215e-31;     // [kg]
-    const double hbar = 1.054571628e-34;  // [J.s]
-    const double pi = 3.141592654; 
-    
-    // Temperature-independent material parameters
-    double mdn = materialDB->getElementBlockParam<double>(ebName,"Electron DOS Effective Mass");
-    double mdp = materialDB->getElementBlockParam<double>(ebName,"Hole DOS Effective Mass");
-    double Tref = materialDB->getElementBlockParam<double>(ebName,"Reference Temperature");
-    
-    double Eg0 = materialDB->getElementBlockParam<double>(ebName,"Zero Temperature Band Gap");
-    double alpha = materialDB->getElementBlockParam<double>(ebName,"Band Gap Alpha Coefficient");
-    double beta = materialDB->getElementBlockParam<double>(ebName,"Band Gap Beta Coefficient");
-    double Chi = materialDB->getElementBlockParam<double>(ebName,"Electron Affinity");
-    
-    // Constant prefactor in calculating Nc and Nv in [cm-3]
-    double NcvFactor = 2.0*pow((kbBoltz*eleQ*m0*Tref)/(2*pi*pow(hbar,2)),3./2.)*1.e-6;
-            // eleQ converts kbBoltz in [eV/K] to [J/K], 1e-6 converts [m-3] to [cm-3]
-    
-    // Strong temperature-dependent material parameters
-    ScalarT Nc;  // conduction band effective DOS in [cm-3]
-    ScalarT Nv;  // valence band effective DOS in [cm-3]
-    ScalarT Eg;  // band gap at T [K] in [eV]
-    
-    Nc = NcvFactor*pow(mdn,1.5)*pow(temperature/Tref,1.5);  // in [cm-3]
-    Nv = NcvFactor*pow(mdp,1.5)*pow(temperature/Tref,1.5); 
-    Eg = Eg0-alpha*pow(temperature,2.0)/(beta+temperature); // in [eV]
-    
-    ScalarT builtinPotential = 0.0; 
-    std::string dopantType = materialDB->getElementBlockParam<std::string>(ebName,"Dopant Type","None");
+    //! Contacts on insulator or metal
+    if (category != "Semiconductor") {
+      double metalWorkFunc = materialDB->getMaterialParam<double>(material,"Work Function");
+      ScalarT offsetDueToWorkFunc = (metalWorkFunc-qPhiRef)/1.0;  // 1.0 converts from [eV] to [V]
 
-    // Intrinsic semiconductor (no doping)
-    if (dopantType == "None")  
-    {
-      // apply charge neutrality (p=n) and MB statistics
-      builtinPotential = (qPhiRef-Chi-0.5*Eg)/1.0 + 0.5*kbT*log(Nv/Nc)/1.0;
-      bcValue = (user_value + builtinPotential);  //[V]
+      bcValue = (user_value - offsetDueToWorkFunc);  //[V]
     }
-    
-    else // Extrinsic semiconductor (doped)
-    {
-      double dopingConc = materialDB->getElementBlockParam<double>(ebName,"Doping Value");
-      double dopantActE = materialDB->getElementBlockParam<double>(ebName,"Dopant Activation Energy", 0.045);
+  
+    //! Ohmic contacts on semiconductor (charge neutrality and equilibrium ) 
+    else {
 
-      if ((carrierStatistics=="Boltzmann Statistics") && (incompIonization=="False"))
-        builtinPotential = potentialForMBComplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc);
-    
-      else if ((carrierStatistics=="Boltzmann Statistics") && (incompIonization=="True"))
-        builtinPotential = potentialForMBIncomplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc,dopantActE);
+      // Universal constants
+      const double kbBoltz = 8.617343e-05;  // [eV/K]
+      const double eleQ = 1.602176487e-19;  // [C]
+      const double m0 = 9.10938215e-31;     // [kg]
+      const double hbar = 1.054571628e-34;  // [J.s]
+      const double pi = 3.141592654;
 
-      else if ((carrierStatistics=="Fermi-Dirac Statistics") && (incompIonization=="False"))
-        builtinPotential = potentialForFDComplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc);
+	// Temperature-independent material parameters
+      double mdn, mdp, Tref, Eg0, alpha, beta, Chi;
+      if (ebName.length() > 0)  {
+	mdn = materialDB->getElementBlockParam<double>(ebName,"Electron DOS Effective Mass");
+	mdp = materialDB->getElementBlockParam<double>(ebName,"Hole DOS Effective Mass");
+	Tref = materialDB->getElementBlockParam<double>(ebName,"Reference Temperature");
+	
+	Eg0 = materialDB->getElementBlockParam<double>(ebName,"Zero Temperature Band Gap");
+	alpha = materialDB->getElementBlockParam<double>(ebName,"Band Gap Alpha Coefficient");
+	beta = materialDB->getElementBlockParam<double>(ebName,"Band Gap Beta Coefficient");
+	Chi = materialDB->getElementBlockParam<double>(ebName,"Electron Affinity");
+      }
+      else {
+	mdn = materialDB->getMaterialParam<double>(material,"Electron DOS Effective Mass");
+	mdp = materialDB->getMaterialParam<double>(material,"Hole DOS Effective Mass");
+	Tref = materialDB->getMaterialParam<double>(material,"Reference Temperature");
+	
+	Eg0 = materialDB->getMaterialParam<double>(material,"Zero Temperature Band Gap");
+	alpha = materialDB->getMaterialParam<double>(material,"Band Gap Alpha Coefficient");
+	beta = materialDB->getMaterialParam<double>(material,"Band Gap Beta Coefficient");
+	Chi = materialDB->getMaterialParam<double>(material,"Electron Affinity");
+      }
 
-      else if ((carrierStatistics=="0-K Fermi-Dirac Statistics") && (incompIonization=="False"))
-        builtinPotential = potentialForZeroKFDComplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc);
+      // Constant prefactor in calculating Nc and Nv in [cm-3]
+      double NcvFactor = 2.0*pow((kbBoltz*eleQ*m0*Tref)/(2*pi*pow(hbar,2)),3./2.)*1.e-6;
+          // eleQ converts kbBoltz in [eV/K] to [J/K], 1e-6 converts [m-3] to [cm-3]
     
-      // For cases of FD and 0-K FD with incompIonization==True, one needs to 
-      // numerically solve a non-trivial equation. Since when incompIonization==True
-      // is enabled, the MB almost always holds, so use potentialForMBIncomplIon.
-      else  
-        builtinPotential = potentialForMBIncomplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc,dopantActE);
+      // Strong temperature-dependent material parameters
+      ScalarT Nc;  // conduction band effective DOS in [cm-3]
+      ScalarT Nv;  // valence band effective DOS in [cm-3]
+      ScalarT Eg;  // band gap at T [K] in [eV]
+    
+      Nc = NcvFactor*pow(mdn,1.5)*pow(temperature/Tref,1.5);  // in [cm-3]
+      Nv = NcvFactor*pow(mdp,1.5)*pow(temperature/Tref,1.5); 
+      Eg = Eg0-alpha*pow(temperature,2.0)/(beta+temperature); // in [eV]
+    
+      ScalarT builtinPotential = 0.0; 
+      std::string dopantType;
+      if (ebName.length() > 0)
+	dopantType = materialDB->getElementBlockParam<std::string>(ebName,"Dopant Type","None");
+      else
+	dopantType = materialDB->getMaterialParam<std::string>(material,"Dopant Type","None");
+
+      // Intrinsic semiconductor (no doping)
+      if (dopantType == "None")  
+      {
+	// apply charge neutrality (p=n) and MB statistics
+	builtinPotential = (qPhiRef-Chi-0.5*Eg)/1.0 + 0.5*kbT*log(Nv/Nc)/1.0;
+	bcValue = (user_value + builtinPotential);  //[V]
+      }
+    
+      else // Extrinsic semiconductor (doped)
+      {
+	double dopingConc, dopantActE;
+	if (ebName.length() > 0) {
+	  dopingConc = materialDB->getElementBlockParam<double>(ebName,"Doping Value");
+	  dopantActE = materialDB->getElementBlockParam<double>(ebName,"Dopant Activation Energy", 0.045);
+	}
+	else {
+	  dopingConc = materialDB->getMaterialParam<double>(material,"Doping Value");
+	  dopantActE = materialDB->getMaterialParam<double>(material,"Dopant Activation Energy", 0.045);
+	}
+
+	if ((carrierStatistics=="Boltzmann Statistics") && (incompIonization=="False"))
+	  builtinPotential = potentialForMBComplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc);
+    
+	else if ((carrierStatistics=="Boltzmann Statistics") && (incompIonization=="True"))
+	  builtinPotential = potentialForMBIncomplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc,dopantActE);
+
+	else if ((carrierStatistics=="Fermi-Dirac Statistics") && (incompIonization=="False"))
+	  builtinPotential = potentialForFDComplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc);
+	
+	else if ((carrierStatistics=="0-K Fermi-Dirac Statistics") && (incompIonization=="False"))
+	  builtinPotential = potentialForZeroKFDComplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc);
+    
+	// For cases of FD and 0-K FD with incompIonization==True, one needs to 
+	// numerically solve a non-trivial equation. Since when incompIonization==True
+	// is enabled, the MB almost always holds, so use potentialForMBIncomplIon.
+	else  
+	  builtinPotential = potentialForMBIncomplIon(Nc,Nv,Eg,Chi,dopantType,dopingConc,dopantActE);
         
-      bcValue = (user_value + builtinPotential);  // [V]
+	bcValue = (user_value + builtinPotential);  // [V]
+      }
     }
+  } // end of if (material.length() > 0)
     
-  } // end of else if (ebName.length() > 0)
-  
   //! Otherwise, just use the user_value. 
   else
     bcValue = user_value;
