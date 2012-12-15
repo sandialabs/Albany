c5a2e43694d2f9d471e1866b28d4d8af6450aeb8
Julien Cortial <jcortia@sandia.gov>
2012-12-14 18:21:16 -0800

    MOR: Added Albany-specific facade for the MOR module
    Introduced a single point of entry to centralize and streamline the
    interaction of Albany and the MOR module.

---
+++ src/MOR/Albany_MORFacade.cpp
@@ -0,0 +1,65 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include "Albany_MORFacade.hpp"
+
+#include "Albany_LinearReducedSpaceFactory.hpp"
+#include "Albany_ReducedOrderModelFactory.hpp"
+#include "Albany_MORObserverFactory.hpp"
+
+#include "Albany_BasisInputFile.hpp"
+#include "Albany_StkBasisProvider.hpp"
+
+#include "Albany_STKDiscretization.hpp"
+
+namespace Albany {
+
+class MORFacadeImpl : public MORFacade {
+public:
+  MORFacadeImpl(
+      const Teuchos::RCP<STKDiscretization> &disc,
+      const Teuchos::RCP<Teuchos::ParameterList> &params);
+
+  virtual Teuchos::RCP<ReducedOrderModelFactory> modelFactory() const;
+  virtual Teuchos::RCP<MORObserverFactory> observerFactory() const;
+
+private:
+  Teuchos::RCP<LinearReducedSpaceFactory> spaceFactory_;
+  Teuchos::RCP<ReducedOrderModelFactory> modelFactory_;
+  Teuchos::RCP<MORObserverFactory> observerFactory_;
+};
+
+Teuchos::RCP<ReducedOrderModelFactory> MORFacadeImpl::modelFactory() const
+{
+  return modelFactory_;
+}
+
+Teuchos::RCP<MORObserverFactory> MORFacadeImpl::observerFactory() const
+{
+  return observerFactory_;
+}
+
+MORFacadeImpl::MORFacadeImpl(
+    const Teuchos::RCP<STKDiscretization> &disc,
+    const Teuchos::RCP<Teuchos::ParameterList> &params) :
+  spaceFactory_(new LinearReducedSpaceFactory),
+  modelFactory_(new ReducedOrderModelFactory(spaceFactory_, params)),
+  observerFactory_(new MORObserverFactory(spaceFactory_, params))
+{
+  spaceFactory_->extend("File", Teuchos::rcp(new BasisInputFile(*disc->getMap())));
+  spaceFactory_->extend("Stk", Teuchos::rcp(new StkBasisProvider(disc)));
+}
+
+Teuchos::RCP<MORFacade> createMORFacade(
+    const Teuchos::RCP<AbstractDiscretization> &disc,
+    const Teuchos::RCP<Teuchos::ParameterList> &params)
+{
+  const Teuchos::RCP<STKDiscretization> disc_actual =
+    Teuchos::rcp_dynamic_cast<STKDiscretization>(disc);
+  return Teuchos::rcp(new MORFacadeImpl(disc_actual, params));
+}
+
+}
