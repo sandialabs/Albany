bd544ef2a0b6828284d4d387bb53fd1ad24dbae4
Alejandro Mota <amota@sandia.gov>
2012-12-21 15:36:50 -0800

    Moved geometry utilities to tensor directory too.

---
+++ src/LCM/utils/tensor/Geometry.i.cc
@@ -0,0 +1,16 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#if !defined(LCM_Geometry_i_cc)
+#define LCM_Geometry_i_cc
+
+
+namespace LCM {
+
+} // namespace LCM
+
+#endif // LCM_Geometry_i_cc
+
