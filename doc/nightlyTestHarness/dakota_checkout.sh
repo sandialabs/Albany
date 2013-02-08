b73df3ae04f6283a9f6eda53bcc0c350b8c96054
Andy Salinger <agsalin@sandia.gov>
2013-02-07 17:15:47 -0700

    Updated Dakota checkout script to match new path and name of tarball.
    Changes in Dakota were matched by changes in Trilinos, so a pull of
    Trilinos will require a new Dakota.

--- doc/nightlyTestHarness/dakota_checkout.sh
+++ doc/nightlyTestHarness/dakota_checkout.sh
@@ -13,9 +13,9 @@
 # setup and housekeeping
 #-------------------------------------------
 
-if [ -a $NIGHTLYDIR/Dakota_votd.src.tar ]; then \rm $NIGHTLYDIR/Dakota_votd.src.tar
+if [ -a $NIGHTLYDIR/Dakota_stable.src.tar ]; then \rm $NIGHTLYDIR/Dakota_stable.src.tar
 fi
-if [ -a $NIGHTLYDIR/Dakota_votd.src.tar.gz ]; then \rm $NIGHTLYDIR/Dakota_votd.src.tar.gz
+if [ -a $NIGHTLYDIR/Dakota_stable.src.tar.gz ]; then \rm $NIGHTLYDIR/Dakota_stable.src.tar.gz
 fi
 
 if [ -a $DAKOUTDIR ]; then \rm -rf $DAKOUTDIR
@@ -29,10 +29,12 @@ cd $NIGHTLYDIR
 #-------------------------------------------
 
 wget -nv --no-check-certificate \
- https://development.sandia.gov/dakota/distributions/dakota/votd/Dakota_votd.src.tar.gz \
+ https://development.sandia.gov/dakota/distributions/dakota/stable/Dakota_stable.src.tar.gz \
  >  $DAKOUTDIR/dakota_wget.out 
-gunzip Dakota_votd.src.tar.gz
+gunzip Dakota_stable.src.tar.gz
 
 cd $TRIKOTADIR
 
-tar xvf $NIGHTLYDIR/Dakota_votd.src.tar > $DAKOUTDIR/dakota_untar.out 2>&1
+tar xvf $NIGHTLYDIR/Dakota_stable.src.tar > $DAKOUTDIR/dakota_untar.out 2>&1
+
+mv dakota-5.3.src Dakota
