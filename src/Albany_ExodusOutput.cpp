eb48a583a4c500a6d09bc552978a57ddcb90caec
Julien Cortial <jcortia@sandia.gov>
2013-04-19 16:28:59 -0700

    Fixed formatting

--- src/Albany_ExodusOutput.cpp
+++ src/Albany_ExodusOutput.cpp
@@ -20,8 +20,8 @@ ExodusOutput::ExodusOutput(const Teuchos::RCP<AbstractDiscretization> &disc) :
 
 void ExodusOutput::writeSolution(double stamp, const Epetra_Vector &solution, const bool overlapped)
 {
-   Teuchos::TimeMonitor exoOutTimer(*exoOutTime_);
-   stkDisc_->outputToExodus(solution, stamp, overlapped);
+  Teuchos::TimeMonitor exoOutTimer(*exoOutTime_);
+  stkDisc_->outputToExodus(solution, stamp, overlapped);
 }
 
 }
