f084939e99ab085f5bc2268dce56339cae2af4d6
Julien Cortial <jcortia@sandia.gov>
2013-10-18 10:57:47 -0700

    MOR: Introduced default RB factory constructor
    Moved corresponding Albany-independent logic to portable core of the MOR
    component.

--- src/MOR/Albany_MORFacade.cpp
+++ src/MOR/Albany_MORFacade.cpp
@@ -7,6 +7,7 @@
 #include "Albany_MORFacade.hpp"
 
 #include "MOR_ReducedBasisFactory.hpp"
+#include "MOR_DefaultReducedBasisFactory.hpp"
 #include "MOR_ReducedSpaceFactory.hpp"
 #include "MOR_SampleDofListFactory.hpp"
 #include "MOR_DefaultSampleDofListProviders.hpp"
@@ -14,8 +15,6 @@
 #include "MOR_ReducedOrderModelFactory.hpp"
 #include "MOR_ObserverFactory.hpp"
 
-#include "MOR_IdentityBasisSource.hpp"
-#include "MOR_FileReducedBasisSource.hpp"
 #include "MOR_TruncatedReducedBasisSource.hpp"
 
 #include "Albany_StkEpetraMVSource.hpp"
@@ -56,16 +55,12 @@ Teuchos::RCP<MOR::ObserverFactory> MORFacadeImpl::observerFactory() const
 MORFacadeImpl::MORFacadeImpl(
     const Teuchos::RCP<STKDiscretization> &disc,
     const Teuchos::RCP<Teuchos::ParameterList> &params) :
-  basisFactory_(new MOR::ReducedBasisFactory),
+  basisFactory_(MOR::defaultReducedBasisFactoryNew(*disc->getMap())),
   samplingFactory_(MOR::defaultSampleDofListFactoryNew(disc->getMap())),
   spaceFactory_(new MOR::ReducedSpaceFactory(basisFactory_, samplingFactory_)),
   modelFactory_(new MOR::ReducedOrderModelFactory(spaceFactory_, params)),
   observerFactory_(new MOR::ObserverFactory(spaceFactory_, params))
 {
-  // Default reduced basis sources
-  basisFactory_->extend("Identity", Teuchos::rcp(new MOR::IdentityBasisSource(*disc->getMap())));
-  basisFactory_->extend("File", Teuchos::rcp(new MOR::FileReducedBasisSource(*disc->getMap())));
-
   // Albany-specific reduced basis source
   const Teuchos::RCP<MOR::EpetraMVSource> stkMVSource(new StkEpetraMVSource(disc));
   basisFactory_->extend("Stk", Teuchos::rcp(new MOR::DefaultTruncatedReducedBasisSource(stkMVSource)));
