0ca6de10d4b56862006b73a4209f77f19377d01e
Julien Cortial <jcortia@sandia.gov>
2013-10-21 15:47:12 -0700

    MOR: Handled Matrix Market I/O of local maps
    Worked around the limitations of the EpetraExt Matrix Market
    serialization capabilities for multivectors with locally replicated
    maps.

---
+++ src/MOR/MOR_EpetraLocalMapMVMatrixMarketUtils.hpp
@@ -0,0 +1,29 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+#ifndef MOR_EPETRALOCALMAPMVMATRIXMARKETUTILS_HPP
+#define MOR_EPETRALOCALMAPMVMATRIXMARKETUTILS_HPP
+
+#include "Epetra_MultiVector.h"
+#include "Epetra_Comm.h"
+
+#include "Teuchos_RCP.hpp"
+
+#include <string>
+
+namespace MOR {
+
+void writeLocalMapMultiVectorToMatrixMarket(
+    const std::string &fileName,
+    const Epetra_MultiVector &localMapMv);
+
+Teuchos::RCP<Epetra_MultiVector> readLocalMapMultiVectorFromMatrixMarket(
+    const std::string &fileName,
+    const Epetra_Comm &comm,
+    int vectorSize);
+
+} // namespace MOR
+
+#endif /* MOR_EPETRALOCALMAPMVMATRIXMARKETUTILS_HPP */
