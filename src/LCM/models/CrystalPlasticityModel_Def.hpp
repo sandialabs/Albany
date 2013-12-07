120f0dfc42d620e06b291ae12acbf8fca370911f
Reese Jones <rjones@sandia.gov>
2013-12-07 08:30:23 -0800

    LCM: with ifdef's it runs

--- src/LCM/models/CrystalPlasticityModel_Def.hpp
+++ src/LCM/models/CrystalPlasticityModel_Def.hpp
@@ -139,46 +139,32 @@ computeState(typename Traits::EvalData workset,
   PHX::MDField<ScalarT> Fp = *eval_fields[Fp_string];
   PHX::MDField<ScalarT> source = *eval_fields[source_string];
 
-  // get State Variables
+  // get state variables
   Albany::MDArray Fpold =
       (*workset.stateArrayPtr)[Fp_string + "_old"];
 
-  ScalarT kappa, mu; 
-  ScalarT dgam;
+  ScalarT c11,c12,c44;
+  ScalarT tau, dgamma, dt;
+  ScalarT g0;
+  dt = 1.; // HACK
 
 #ifndef REMOVE_THIS 
-  PHX::MDField<ScalarT> yieldStrength = *dep_fields["Yield Strength"];
-  PHX::MDField<ScalarT> hardeningModulus = *dep_fields["Hardening Modulus"];
+  ScalarT Y,nu;
   std::string eqps_string = (*field_name_map_)["eqps"];
   PHX::MDField<ScalarT> eqps = *eval_fields[eqps_string];
   Albany::MDArray eqpsold =
       (*workset.stateArrayPtr)[eqps_string + "_old"];
-  ScalarT mubar, K, Y;
-  ScalarT Jm23, trace, smag2, smag, f, p;
-  ScalarT sq23(std::sqrt(2. / 3.));
+  ScalarT p;
 #endif
 
-  Intrepid::Tensor<ScalarT> F(num_dims_), Fe(num_dims_); 
+  Intrepid::Tensor<ScalarT> F(num_dims_), Fe(num_dims_), Ee(num_dims_); 
   Intrepid::Tensor<ScalarT> Fpn(num_dims_), Fpinv(num_dims_);
-  Intrepid::Tensor<ScalarT> A(num_dims_), expA(num_dims_), Fpnew(num_dims_);
+  Intrepid::Tensor<ScalarT> L(num_dims_), expL(num_dims_), Fpnew(num_dims_);
   Intrepid::Tensor<ScalarT> s(num_dims_), sigma(num_dims_);
   Intrepid::Tensor<ScalarT> I(Intrepid::eye<ScalarT>(num_dims_));
-#ifndef REMOVE_THIS 
-  Intrepid::Tensor<ScalarT> be(num_dims_); 
-  Intrepid::Tensor<ScalarT> N(num_dims_); 
-  Intrepid::Tensor<ScalarT> Cpinv(num_dims_);
-#endif
 
   for (std::size_t cell(0); cell < workset.numCells; ++cell) {
     for (std::size_t pt(0); pt < num_pts_; ++pt) {
-      // elastic modulis NOTE make anisotropic
-      // c11, c12, c44
-      kappa = elastic_modulus(cell, pt)
-          / (3. * (1. - 2. * poissons_ratio(cell, pt)));
-      mu = elastic_modulus(cell, pt) / (2. * (1. + poissons_ratio(cell, pt)));
-      //K = hardeningModulus(cell, pt);
-      //Y = yieldStrength(cell, pt);
-      //Jm23 = std::pow(J(cell, pt), -2. / 3.);
 
       // fill local tensors
       F.fill(&def_grad(cell, pt, 0, 0));
@@ -191,117 +177,50 @@ computeState(typename Traits::EvalData workset,
       // compute trial state
       Fpinv = Intrepid::inverse(Fpn);
       Fe = F * Fpinv;
+      Ee = 0.5*( Intrepid::transpose(Fe) * Fe - I);
 
 #if 0
-      Cpinv = Fpinv * Intrepid::transpose(Fpinv);
-      be = Jm23 * F * Cpinv * Intrepid::transpose(F);
-      s = mu * Intrepid::dev(be);
-      mubar = Intrepid::trace(be) * mu / (num_dims_);
+      // compute stress 
+      // elastic modulis NOTE make anisotropic
+      Y = elastic_modulus(cell, pt);
+      nu = poissons_ratio(cell, pt);
+      Y *= 1./((1.+nu)*(1.-2.*nu));
+      c11 = (1.   -nu)*Y;
+      c12 =        nu *Y;
+      c44 = (1.-2.*nu)*Y;
+      sigma = c44*Ee;
+      sigma(0,0) = c11*Ee(0,0)+c12*(Ee(1,1)+Ee(2,2));
+      sigma(1,1) = c11*Ee(1,1)+c12*(Ee(0,0)+Ee(2,2));
+      sigma(2,2) = c11*Ee(2,2)+c12*(Ee(1,1)+Ee(0,0));
 
-      // check yield condition
-      smag = Intrepid::norm(s);
-      f = smag - sq23 * (Y + K * eqpsold(cell, pt)
-          + sat_mod_ * (1. - std::exp(-sat_exp_ * eqpsold(cell, pt))));
+      //HACK L.initialize(0.);
+      for (int i; i < num_slip_; ++i) {
+        // compute resolved shear stresses
+        tau = 1.;
+         
+        // compute  dgammas
+        g0 = slip_systems_[i].gamma_dot_0_;
+        dgamma = dt*g0*tau;
+
+        // compute velocity gradient
+        // HACK L += dgamma* (slip_systems_[i].projectors_); 
+      }
+
+      // update plastic deformation gradient
+      expL = Intrepid::exp(L);
+      Fpnew = expL * Fpn;
 #endif
 
-//    std::cout << "!!! in cp compute state elastic only\n";
-#if 0
-      if (f > 1E-12) {
-        // return mapping algorithm
-        bool converged = false;
-        ScalarT g = f;
-        ScalarT H = 0.0;
-        ScalarT dH = 0.0;
-        ScalarT alpha = 0.0;
-        ScalarT res = 0.0;
-        int count = 0;
-        dgam = 0.0;
-
-        LocalNonlinearSolver<EvalT, Traits> solver;
-
-        std::vector<ScalarT> F(1);
-        std::vector<ScalarT> dFdX(1);
-        std::vector<ScalarT> X(1);
-
-        F[0] = f;
-        X[0] = 0.0;
-        dFdX[0] = (-2. * mubar) * (1. + H / (3. * mubar));
-        while (!converged && count < 30)
-        {
-          count++;
-          solver.solve(dFdX, X, F);
-          alpha = eqpsold(cell, pt) + sq23 * X[0];
-          H = K * alpha + sat_mod_ * (1. - exp(-sat_exp_ * alpha));
-          dH = K + sat_exp_ * sat_mod_ * exp(-sat_exp_ * alpha);
-          F[0] = smag - (2. * mubar * X[0] + sq23 * (Y + H));
-          dFdX[0] = -2. * mubar * (1. + dH / (3. * mubar));
-
-          res = std::abs(F[0]);
-          if (res < 1.e-11 || res / f < 1.E-11)
-            converged = true;
-
-          TEUCHOS_TEST_FOR_EXCEPTION(count > 30, std::runtime_error,
-              std::endl <<
-              "Error in return mapping, count = " <<
-              count <<
-              "\nres = " << res <<
-              "\nrelres = " << res/f <<
-              "\ng = " << F[0] <<
-              "\ndg = " << dFdX[0] <<
-              "\nalpha = " << alpha << std::endl);
-        }
-        solver.computeFadInfo(dFdX, X, F);
-        dgam = X[0];
-
-        // plastic direction
-        N = (1 / smag) * s;
-
-        // update s
-        s -= 2 * mubar * dgam * N;
-
-        // update eqps
-        eqps(cell, pt) = alpha;
-
-        // mechanical source
-        if (delta_time(0) > 0) {
-          source(cell, pt) =
-              sq23 * dgam / delta_time(0) * (Y + H);
-        }
-
-        // exponential map to get Fpnew
-        A = dgam * N;
-        expA = Intrepid::exp(A);
-        Fpnew = expA * Fpn;
-        for (std::size_t i(0); i < num_dims_; ++i) {
-          for (std::size_t j(0); j < num_dims_; ++j) {
-            Fp(cell, pt, i, j) = Fpnew(i, j);
-          }
-        }
-      } else {
-        eqps(cell, pt) = eqpsold(cell, pt);
-        source(cell, pt) = 0.0;
-        for (std::size_t i(0); i < num_dims_; ++i) {
-          for (std::size_t j(0); j < num_dims_; ++j) {
-            Fp(cell, pt, i, j) = Fpn(i, j);
-          }
+      // history
+      eqps(cell, pt) = eqpsold(cell, pt);
+      source(cell, pt) = 0.0;
+      for (std::size_t i(0); i < num_dims_; ++i) {
+        for (std::size_t j(0); j < num_dims_; ++j) {
+          Fp(cell, pt, i, j) = Fpnew(i, j);
         }
       }
-#else 
-      // history
-        eqps(cell, pt) = eqpsold(cell, pt);
-        source(cell, pt) = 0.0;
-        for (std::size_t i(0); i < num_dims_; ++i) {
-          for (std::size_t j(0); j < num_dims_; ++j) {
-            Fp(cell, pt, i, j) = Fpn(i, j);
-          }
-        }
-#endif
 
-      // compute pressure
-      p = 0.5 * kappa * (J(cell, pt) - 1. / (J(cell, pt)));
-
-      // compute stress
-      sigma = p * I + s / J(cell, pt);
+      // store stress
       for (std::size_t i(0); i < num_dims_; ++i) {
         for (std::size_t j(0); j < num_dims_; ++j) {
           stress(cell, pt, i, j) = sigma(i, j);
