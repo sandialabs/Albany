fab68a054eedd69dccc41cdfc2570338ad5e7bba
Irina Kalashnikova <ikalash@hopper08.(none)>
2013-12-13 09:03:43 -0800

    Modifying hopper scripts to code builds with new Hopper environment.

--- doc/nightlyTestHarness/hopper/hopper_modules.sh
+++ doc/nightlyTestHarness/hopper/hopper_modules.sh
@@ -1,5 +1,5 @@
 
 module unload cmake netcdf-hdf5parallel/4.2.0 python
-module swap PrgEnv-pgi PrgEnv-gnu; module load cmake/2.8.9 python netcdf-hdf5parallel/4.2.0 usg-default-modules/1.0
+module swap PrgEnv-pgi PrgEnv-gnu; module load cmake/2.8.9 python netcdf-hdf5parallel/4.3.0 usg-default-modules/1.0
 module load boost
 module list
