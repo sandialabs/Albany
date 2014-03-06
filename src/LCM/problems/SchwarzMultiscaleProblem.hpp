64f4fd434a360684aa5b513219c1710525bf03cd
Alejandro Mota <amota@sandia.gov>
2014-03-05 16:14:16 -0800

    LCM: Added template for Schwarz overlapped alternating coupling. Cleanup in concurrent multiscale.

--- src/LCM/problems/SchwarzMultiscaleProblem.hpp
+++ src/LCM/problems/SchwarzMultiscaleProblem.hpp
@@ -4,8 +4,8 @@
 //    in the file "license.txt" in the top-level Albany directory  //
 //*****************************************************************//
 
-#if !defined(LCM_ConcurrentMultiscaleProblem_hpp)
-#define LCM_ConcurrentMultiscaleProblem_hpp
+#if !defined(LCM_SchwarzMultiscaleProblem_hpp)
+#define LCM_SchwarzMultiscaleProblem_hpp
 
 #include "Teuchos_RCP.hpp"
 #include "Teuchos_ParameterList.hpp"
@@ -20,9 +20,9 @@
 namespace Albany {
 
 ///
-/// \brief Definition for the ConcurrentMultiscale Problem
+/// \brief Definition for the SchwarzMultiscale Problem
 ///
-class ConcurrentMultiscaleProblem : public Albany::AbstractProblem {
+class SchwarzMultiscaleProblem : public Albany::AbstractProblem {
 public:
 
   typedef Intrepid::FieldContainer<RealType> FC;
@@ -30,7 +30,7 @@ public:
   ///
   /// Default constructor
   ///
-  ConcurrentMultiscaleProblem(
+  SchwarzMultiscaleProblem(
       Teuchos::RCP<Teuchos::ParameterList> const & params,
       Teuchos::RCP<ParamLib> const & param_lib,
       int const num_dims,
@@ -40,7 +40,7 @@ public:
   /// Destructor
   ///
   virtual
-  ~ConcurrentMultiscaleProblem();
+  ~SchwarzMultiscaleProblem();
 
   ///
   Teuchos::RCP<std::map<std::string, std::string> >
@@ -94,13 +94,13 @@ private:
   ///
   /// Private to prohibit copying
   ///
-  ConcurrentMultiscaleProblem(ConcurrentMultiscaleProblem const &);
+  SchwarzMultiscaleProblem(SchwarzMultiscaleProblem const &);
 
   ///
   /// Private to prohibit copying
   ///
-  ConcurrentMultiscaleProblem &
-  operator=(ConcurrentMultiscaleProblem const &);
+  SchwarzMultiscaleProblem &
+  operator=(SchwarzMultiscaleProblem const &);
 
   QCAD::MaterialDatabase &
   matDB()
@@ -215,7 +215,7 @@ protected:
 //
 template <typename EvalT>
 Teuchos::RCP<const PHX::FieldTag>
-Albany::ConcurrentMultiscaleProblem::
+Albany::SchwarzMultiscaleProblem::
 constructEvaluators(
     PHX::FieldManager<PHAL::AlbanyTraits> & fm0,
     Albany::MeshSpecsStruct const & mesh_specs,
@@ -250,7 +250,7 @@ constructEvaluators(
       "A material model must be defined for block: " + eb_name);
 
 #ifdef ALBANY_VERBOSE
-  *out << "In ConcurrentMultiscaleProblem::constructEvaluators" << '\n';
+  *out << "In SchwarzMultiscaleProblem::constructEvaluators" << '\n';
   *out << "element block name: " << eb_name << '\n';
   *out << "material model name: " << material_model_name << '\n';
 #endif
@@ -765,4 +765,4 @@ constructEvaluators(
   return Teuchos::null;
 }
 
-#endif // LCM_ConcurrentMultiscaleProblem_hpp
+#endif // LCM_SchwarzMultiscaleProblem_hpp
