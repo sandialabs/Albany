a798c7968e782b22c99aae823b154017748a6ea0
Irina Kalashnikova <ikalash@sandia.gov>
2014-03-11 16:36:48 -0600

    Minor tweaks in Hopper ctest scripts of FELIX.  These will run now as a part of the cronjob there.

--- doc/nightlyTestHarness/hopper/send_email_ctest.sh
+++ doc/nightlyTestHarness/hopper/send_email_ctest.sh
@@ -4,6 +4,6 @@ source $1
 
 TTT=`grep "tests failed" $ALBOUTDIR/albany_runtests.out`
 
-#/usr/bin/mail -s "Hopper Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" -f "ikalash@nersc.gov" < $ALBOUTDIR/albany_runtests.out
+/usr/bin/mail -s "Hopper Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" -f "ikalash@nersc.gov" < $ALBOUTDIR/albany_runtests.out
 /usr/bin/mail -s "Hopper Albany ($ALBANY_BRANCH): $TTT" "ikalashn@gmail.com" -f "ikalash@nersc.gov" < $ALBOUTDIR/albany_runtests.out
 
