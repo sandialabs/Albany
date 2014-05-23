5a1b6f9618938820a5a922e51531548b8dacd79b
James Overfelt <jroverf@sandia.gov>
2014-05-23 09:46:00 -0600

    Add  DOFGradInterpolation evaluators for Aeras.

---
+++ src/Aeras/evaluators/Aeras_DOFGradInterpolation_Def.hpp
@@ -0,0 +1,191 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include "Teuchos_TestForException.hpp"
+#include "Phalanx_DataLayout.hpp"
+
+#include "Intrepid_FunctionSpaceTools.hpp"
+
+namespace PHAL {
+
+//**********************************************************************
+template<typename EvalT, typename Traits>
+DOFGradInterpolation<EvalT, Traits>::
+DOFGradInterpolation(const Teuchos::ParameterList& p,
+                              const Teuchos::RCP<Albany::Layouts>& dl) :
+  val_node    (p.get<std::string>   ("Variable Name"), dl->node_scalar),
+  GradBF      (p.get<std::string>   ("Gradient BF Name"), dl->node_qp_gradient),
+  grad_val_qp (p.get<std::string>   ("Gradient Variable Name"), dl->qp_gradient)
+{
+  this->addDependentField(val_node);
+  this->addDependentField(GradBF);
+  this->addEvaluatedField(grad_val_qp);
+
+  this->setName("DOFGradInterpolation"+PHX::TypeString<EvalT>::value);
+
+  std::vector<PHX::DataLayout::size_type> dims;
+  GradBF.fieldTag().dataLayout().dimensions(dims);
+  numNodes = dims[1];
+  numQPs   = dims[2];
+  numDims  = dims[3];
+}
+
+//**********************************************************************
+template<typename EvalT, typename Traits>
+void DOFGradInterpolation<EvalT, Traits>::
+postRegistrationSetup(typename Traits::SetupData d,
+                      PHX::FieldManager<Traits>& fm)
+{
+  this->utils.setFieldData(val_node,fm);
+  this->utils.setFieldData(GradBF,fm);
+  this->utils.setFieldData(grad_val_qp,fm);
+}
+
+//**********************************************************************
+template<typename EvalT, typename Traits>
+void DOFGradInterpolation<EvalT, Traits>::
+evaluateFields(typename Traits::EvalData workset)
+{
+  //Intrepid Version:
+  // for (int i=0; i < grad_val_qp.size() ; i++) grad_val_qp[i] = 0.0;
+  // Intrepid::FunctionSpaceTools:: evaluate<ScalarT>(grad_val_qp, val_node, GradBF);
+
+    for (std::size_t cell=0; cell < workset.numCells; ++cell) {
+        for (std::size_t qp=0; qp < numQPs; ++qp) {
+          for (std::size_t dim=0; dim<numDims; dim++) {
+            ScalarT& gvqp = grad_val_qp(cell,qp,dim);
+            gvqp = val_node(cell, 0) * GradBF(cell, 0, qp, dim);
+            for (std::size_t node= 1 ; node < numNodes; ++node) {
+              gvqp += val_node(cell, node) * GradBF(cell, node, qp, dim);
+          }
+        }
+      }
+    }
+}
+
+//**********************************************************************
+template<typename Traits>
+DOFGradInterpolation<PHAL::AlbanyTraits::Jacobian, Traits>::
+DOFGradInterpolation(const Teuchos::ParameterList& p,
+                              const Teuchos::RCP<Albany::Layouts>& dl) :
+  val_node    (p.get<std::string>   ("Variable Name"), dl->node_scalar),
+  GradBF      (p.get<std::string>   ("Gradient BF Name"), dl->node_qp_gradient),
+  grad_val_qp (p.get<std::string>   ("Gradient Variable Name"), dl->qp_gradient)
+{
+  this->addDependentField(val_node);
+  this->addDependentField(GradBF);
+  this->addEvaluatedField(grad_val_qp);
+
+  this->setName("DOFGradInterpolation"+PHX::TypeString<PHAL::AlbanyTraits::Jacobian>::value);
+
+  std::vector<PHX::DataLayout::size_type> dims;
+  GradBF.fieldTag().dataLayout().dimensions(dims);
+  numNodes = dims[1];
+  numQPs   = dims[2];
+  numDims  = dims[3];
+
+  offset = p.get<int>("Offset of First DOF");
+}
+
+//**********************************************************************
+template<typename Traits>
+void DOFGradInterpolation<PHAL::AlbanyTraits::Jacobian, Traits>::
+postRegistrationSetup(typename Traits::SetupData d,
+                      PHX::FieldManager<Traits>& fm)
+{
+  this->utils.setFieldData(val_node,fm);
+  this->utils.setFieldData(GradBF,fm);
+  this->utils.setFieldData(grad_val_qp,fm);
+}
+
+//**********************************************************************
+template<typename Traits>
+void DOFGradInterpolation<PHAL::AlbanyTraits::Jacobian, Traits>::
+evaluateFields(typename Traits::EvalData workset)
+{
+  //Intrepid Version:
+  // for (int i=0; i < grad_val_qp.size() ; i++) grad_val_qp[i] = 0.0;
+  // Intrepid::FunctionSpaceTools:: evaluate<ScalarT>(grad_val_qp, val_node, GradBF);
+
+  int num_dof = val_node(0,0,0).size();
+  int neq = num_dof / numNodes;
+
+    for (std::size_t cell=0; cell < workset.numCells; ++cell) {
+        for (std::size_t qp=0; qp < numQPs; ++qp) {
+          for (std::size_t dim=0; dim<numDims; dim++) {
+            ScalarT& gvqp = grad_val_qp(cell,qp,dim);
+            gvqp = FadType(num_dof, val_node(cell, 0).val() * GradBF(cell, 0, qp, dim));
+            gvqp.fastAccessDx(offset) = val_node(cell, 0).fastAccessDx(offset) * GradBF(cell, 0, qp, dim);
+            for (std::size_t node= 1 ; node < numNodes; ++node) {
+              gvqp.val() += val_node(cell, node).val() * GradBF(cell, node, qp, dim);
+              gvqp.fastAccessDx(neq*node+offset) += val_node(cell, node).fastAccessDx(neq*node+offset) * GradBF(cell, node, qp, dim);
+          }
+        }
+      }
+    }
+}
+
+//**********************************************************************
+//**********************************************************************
+
+template<typename EvalT, typename Traits>
+DOFGradInterpolation_noDeriv<EvalT, Traits>::
+DOFGradInterpolation_noDeriv(const Teuchos::ParameterList& p,
+                             const Teuchos::RCP<Albany::Layouts>& dl) :
+  val_node    (p.get<std::string>   ("Variable Name"), dl->node_scalar),
+  GradBF      (p.get<std::string>   ("Gradient BF Name"), dl->node_qp_gradient),
+  grad_val_qp (p.get<std::string>   ("Gradient Variable Name"), dl->qp_gradient)
+{
+  this->addDependentField(val_node);
+  this->addDependentField(GradBF);
+  this->addEvaluatedField(grad_val_qp);
+
+  this->setName("DOFGradInterpolation_noDeriv"+PHX::TypeString<EvalT>::value);
+
+  std::vector<PHX::DataLayout::size_type> dims;
+  GradBF.fieldTag().dataLayout().dimensions(dims);
+  numNodes = dims[1];
+  numQPs   = dims[2];
+  numDims  = dims[3];
+}
+
+//**********************************************************************
+template<typename EvalT, typename Traits>
+void DOFGradInterpolation_noDeriv<EvalT, Traits>::
+postRegistrationSetup(typename Traits::SetupData d,
+                      PHX::FieldManager<Traits>& fm)
+{
+  this->utils.setFieldData(val_node,fm);
+  this->utils.setFieldData(GradBF,fm);
+  this->utils.setFieldData(grad_val_qp,fm);
+}
+
+//**********************************************************************
+template<typename EvalT, typename Traits>
+void DOFGradInterpolation_noDeriv<EvalT, Traits>::
+evaluateFields(typename Traits::EvalData workset)
+{
+  //Intrepid Version:
+  // for (int i=0; i < grad_val_qp.size() ; i++) grad_val_qp[i] = 0.0;
+  // Intrepid::FunctionSpaceTools:: evaluate<ScalarT>(grad_val_qp, val_node, GradBF);
+
+    for (std::size_t cell=0; cell < workset.numCells; ++cell) {
+        for (std::size_t qp=0; qp < numQPs; ++qp) {
+          for (std::size_t dim=0; dim<numDims; dim++) {
+            MeshScalarT& gvqp = grad_val_qp(cell,qp,dim);
+            gvqp = val_node(cell, 0) * GradBF(cell, 0, qp, dim);
+            for (std::size_t node= 1 ; node < numNodes; ++node) {
+              gvqp += val_node(cell, node) * GradBF(cell, node, qp, dim);
+          }
+        }
+      }
+    }
+}
+
+//**********************************************************************
+
+}
+
