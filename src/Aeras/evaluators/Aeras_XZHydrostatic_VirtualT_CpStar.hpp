a737fe07f30c670b45a8089bc547ac65f3e93265
smith <tmsmith@sandia.gov>
2014-06-10 17:09:58 -0600

    Virtual Temperature and Cp Star evaluators for the the moisture equation of state (p=rho*R*Tv)

---
+++ src/Aeras/evaluators/Aeras_XZHydrostatic_VirtualT_CpStar.hpp
@@ -0,0 +1,52 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#ifndef AERAS_XZHYDROSTATIC_DENSITY_HPP
+#define AERAS_XZHYDROSTATIC_DENSITY_HPP
+
+#include "Phalanx_ConfigDefs.hpp"
+#include "Phalanx_Evaluator_WithBaseImpl.hpp"
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
+#include "Aeras_Layouts.hpp"
+
+namespace Aeras {
+/** \brief Density for XZHydrostatic atmospheric model
+
+    This evaluator computes the density for the XZHydrostatic model
+    of atmospheric dynamics.
+
+*/
+
+template<typename EvalT, typename Traits>
+class XZHydrostatic_Density : public PHX::EvaluatorWithBaseImpl<Traits>,
+                   public PHX::EvaluatorDerived<EvalT, Traits> {
+
+public:
+  typedef typename EvalT::ScalarT ScalarT;
+  typedef typename EvalT::MeshScalarT MeshScalarT;
+
+  XZHydrostatic_Density(const Teuchos::ParameterList& p,
+                const Teuchos::RCP<Aeras::Layouts>& dl);
+
+  void postRegistrationSetup(typename Traits::SetupData d,
+			     PHX::FieldManager<Traits>& vm);
+
+  void evaluateFields(typename Traits::EvalData d);
+
+private:
+  // Output:
+  PHX::MDField<ScalarT,Cell,Node> density;
+  PHX::MDField<ScalarT,Cell,Node> pressure;
+  PHX::MDField<ScalarT,Cell,Node> temperature;
+
+  const int numNodes;
+  const int numLevels;
+
+};
+}
+
+#endif
