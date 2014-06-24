06bb641861af72a1f9405dd6941ed76f3996a35f
James Overfelt <jroverf@sandia.gov>
2014-06-24 16:22:29 -0600

    Cleanup

--- src/Aeras/problems/Aeras_HydrostaticProblem.hpp
+++ src/Aeras/problems/Aeras_HydrostaticProblem.hpp
@@ -123,8 +123,6 @@ namespace Aeras {
 #include "Albany_ResponseUtilities.hpp"
 #include "PHAL_Neumann.hpp"
 
-#include "Aeras_XZHydrostaticResid.hpp"
-
 template <typename EvalT>
 Teuchos::RCP<const PHX::FieldTag>
 Aeras::HydrostaticProblem::constructEvaluators(
