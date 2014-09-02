2320fe801beb6c04c666766f4717470d72b0c95d
Irina Kalashnikova <ikalash@sandia.gov>
2014-09-01 22:14:16 -0600

    Converted Partition and Subdivision tests to Tpetra.

---
+++ examples/LCM/Subdivision/run_subdivisionT.py
@@ -0,0 +1,37 @@
+##*****************************************************************//
+##    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+##    This Software is released under the BSD license detailed     //
+##    in the file "license.txt" in the top-level Albany directory  //
+##*****************************************************************//
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+from subprocess import Popen
+
+result = 0
+    
+log_file_name = "subdivision.log"
+if os.path.exists(log_file_name):
+    os.remove(log_file_name)
+logfile = open(log_file_name, 'w')
+
+# run the partition test
+command = ["./SubdivisionT", "--input=\"one_tet.g\""]
+p = Popen(command, stdout=logfile, stderr=logfile)
+return_code = p.wait()
+if return_code != 0:
+    result = return_code
+
+# run exodiff
+command = ["./exodiff", "-m", "-f", \
+               "one_tet.exodiff", \
+               "one_tet.gold.e", \
+               "output.e"]
+p = Popen(command, stdout=logfile, stderr=logfile)
+return_code = p.wait()
+if return_code != 0:
+    result = return_code
+    
+sys.exit(result)
