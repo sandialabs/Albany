777aa42420a2c6a36c9e6fdfd2f881aa0b497d0c
Irina Kalashnikova <ikalash@sandia.gov>
2014-09-02 15:40:48 -0600

    Converted MPS executable / tests to Tpetra.

---
+++ examples/LCM/MaterialPointSimulator/Neohookean/NeohookeanT.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+import sys
+
+from runtestT import runtest
+
+result = 0
+
+print "test 1 - uniaxial"
+name = "Neohookean-uniaxial"
+result = runtest(name)
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+    sys.exit(result)
+
+print "test 2 - shear"
+name = "Neohookean-shear"
+result = runtest(name)
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+    sys.exit(result)
+
+print "test 3 - hydrostatic"
+name = "Neohookean-hydrostatic"
+result = runtest(name)
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+    sys.exit(result)
+
+print "test 3 - general"
+name = "Neohookean-general"
+result = runtest(name)
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+    sys.exit(result)
+
+sys.exit(result)
