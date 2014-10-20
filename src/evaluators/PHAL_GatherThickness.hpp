c88925e578eafd0f37237da07dd5e43866918755
Andrew M. Bradley <ambradl@sandia.gov>
2014-10-20 17:26:48 -0600

    Restoring files lost in the initial merge.
    These files were lost when this merge branch was initially created, specifically
    in the second commit setting up this branch. They are now restored and the
    CMakeLists.txt updated. Another set was restored in the nodal block
    reintegration earlier.

---
+++ src/evaluators/PHAL_GatherThickness.hpp
@@ -0,0 +1,52 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#ifndef PHAL_GATHER_THICKNESS_HPP
+#define PHAL_GATHER_THICKNESS_HPP
+
+#include "Phalanx_ConfigDefs.hpp"
+#include "Phalanx_Evaluator_WithBaseImpl.hpp"
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
+
+#include "Albany_Layouts.hpp"
+
+#include "Teuchos_ParameterList.hpp"
+
+namespace PHAL {
+/** \brief Gathers surface basal friction into the nodal fields of the field manager
+
+    Currently makes an assumption that the stride is constant for dofs
+    and that the nmber of dofs is equal to the size of the coordinates
+    names vector.
+*/
+
+template<typename EvalT, typename Traits> 
+class GatherThickness : public PHX::EvaluatorWithBaseImpl<Traits>,
+                          public PHX::EvaluatorDerived<EvalT, Traits>  {
+  
+public:
+  
+  GatherThickness(const Teuchos::ParameterList& p);
+
+  void postRegistrationSetup(typename Traits::SetupData d,
+                      PHX::FieldManager<Traits>& vm);
+  
+  void evaluateFields(typename Traits::EvalData d);
+  
+private:
+
+  typedef typename EvalT::ScalarT ScalarT;
+  typedef typename EvalT::MeshScalarT MeshScalarT;
+
+  PHX::MDField<ScalarT,Cell,Vertex> thickness;
+ 
+  std::size_t worksetSize;
+  std::size_t numVertices;
+};
+}
+
+#endif
