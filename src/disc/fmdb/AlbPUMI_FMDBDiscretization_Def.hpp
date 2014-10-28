d46e5dc057124966edcba96687f3b97fe7bd76cb
Glen Hansen <gahanse@sandia.gov>
2014-10-27 20:49:16 -0600

    More edits to AlbPUMI_FMDBDiscretization_Def.hpp
    Andrew Bradley found two bugs in the previous commit:
    
    1. The first issue is that setupML now uses 'nodes', which wasn't
    initialized. Glen, reorder the calls like this:
      computeOwnedNodesAndUnknowns();
      computeOverlapNodesAndUnknowns();
      setupMLCoords();
    
    2. Next, in this loop:
        for(size_t j = 0; j < mesh_dim; ++i)
                xxyyzz[j*numOwnedNodes + node_lid] = lcoords[j];
       replace the ++i with ++j.
    
    Thanks Andrew!

--- src/disc/fmdb/AlbPUMI_FMDBDiscretization_Def.hpp
+++ src/disc/fmdb/AlbPUMI_FMDBDiscretization_Def.hpp
@@ -317,6 +317,7 @@ AlbPUMI::FMDBDiscretization<Output>::setupMLCoords()
   if (rigidBodyModes->isMueLuUsed()) {
 
     std::cout << "MueLu selected in FMDB!" << std::endl;
+    std::cout << "mesh dim is = : " << mesh_dim << std::endl;
     double *xxyyzz; //make this ST?
     rigidBodyModes->getCoordArraysMueLu(&xxyyzz);
 
@@ -331,7 +332,7 @@ AlbPUMI::FMDBDiscretization<Output>::setupMLCoords()
       GO node_gid = apf::getNumber(globalNumbering, node);
       int node_lid = node_mapT->getLocalElement(node_gid);
       apf::getComponents(f, nodes[i].entity, nodes[i].node, lcoords);
-      for(size_t j = 0; j < mesh_dim; ++i)
+      for(size_t j = 0; j < mesh_dim; ++j)
             xxyyzz[j*numOwnedNodes + node_lid] = lcoords[j];
 
     }
@@ -1286,8 +1287,8 @@ void
 AlbPUMI::FMDBDiscretization<Output>::updateMesh(bool shouldTransferIPData)
 {
   computeOwnedNodesAndUnknowns();
-  setupMLCoords();
   computeOverlapNodesAndUnknowns();
+  setupMLCoords();
   computeGraphs();
   getCoordinates(); //fill the coordinates array
   computeWorksetInfo();
