dfaba18a7e14598f98d3445c41152af68e596766
Irina Kalashnikova <ikalash@hopper06.(none)>
2014-11-04 08:50:52 -0800

    Updating nightly cronjob scripts on hopper for Trilinos, Albany and CISM-Albany to work with temporary_tpetra_newSTK branch of Albany.

--- doc/nightlyTestHarness/hopper/hopper_modules.sh
+++ doc/nightlyTestHarness/hopper/hopper_modules.sh
@@ -4,5 +4,5 @@
 
 module unload cmake netcdf-hdf5parallel/4.2.0 python usg-default-modules/1.1
 module swap PrgEnv-pgi PrgEnv-gnu; module load cmake/2.8.12.1 python netcdf-hdf5parallel/4.3.0 usg-default-modules/1.0
-module load boost
+module load boost/1.55
 module list
