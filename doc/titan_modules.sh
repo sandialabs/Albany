6c749ba5466bcb21fe0bb65770e6b76865f777ca
Irina Kalashnikova <ikalash@sandia.gov>
2014-11-12 15:59:34 -0800

    Updating build scripts for Trilinos and Albany on Titan to work with Tpetra and new master branch of Albany.  titan-albany-cism-cmake is also for CISM-Albany.

--- doc/titan_modules.sh
+++ doc/titan_modules.sh
@@ -1,5 +1,5 @@
 #!\bin\bash -l
 module swap PrgEnv-pgi PrgEnv-gnu   
 #module unload cray-hdf5-parallel
-module load netcdf-hdf5parallel git cmake boost
+module load cray-netcdf-hdf5parallel git cmake/2.8.11.2 
 module list
