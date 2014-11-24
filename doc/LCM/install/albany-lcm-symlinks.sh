e727d4455aef95310579fc88fb6ba62d9c27b166
Alejandro Mota <amota@sandia.gov>
2014-11-24 14:16:08 -0800

    LCM: Added convenience script to create symlinks with different names to build script so it can perform different actions.

---
+++ doc/LCM/install/albany-lcm-symlinks.sh
@@ -0,0 +1,22 @@
+cd ~/LCM
+ln -s Albany/doc/LCM/build/env-single.sh .
+ln -s Albany/doc/LCM/build/env-all.sh .
+ln -s Albany/doc/LCM/build/trilinos-config.sh .
+ln -s Albany/doc/LCM/build/albany-config.sh .
+ln -s Albany/doc/LCM/build/build.sh .
+ln -s Albany/doc/LCM/build/build-all.sh .
+ln -s build.sh clean-config-build-test-mail.sh
+ln -s build.sh config-build-test-mail.sh
+ln -s build.sh build-test-mail.sh
+ln -s build.sh test-mail.sh
+ln -s build.sh mail.sh
+ln -s build.sh clean-config-build-test.sh
+ln -s build.sh config-build-test.sh
+ln -s build.sh build-test.sh
+ln -s build.sh test.sh
+ln -s build.sh clean-config-build.sh
+ln -s build.sh config-build.sh
+ln -s build.sh clean-config.sh
+ln -s build.sh config.sh
+ln -s build.sh clean.sh
+
