f1f42fe968154f6bd3a4d222cf88c85109ca414b
Mauro Perego <mperego@sandia.gov>
2015-01-26 00:51:24 -0700

    FELIX MPAS interface:        - enable floating and dirichlet boundary conditions,        based on fields passed by MPAS,        - use physical quantities provided by MPAS (similar to what's in CISM)        - cleaning
    	modified:   CMakeLists.txt
    	modified:   src/FELIX/interface_with_mpas/Albany_MpasSTKMeshStruct.cpp
    	modified:   src/FELIX/interface_with_mpas/Albany_MpasSTKMeshStruct.hpp
    	modified:   src/FELIX/interface_with_mpas/Interface.cpp
    	modified:   src/FELIX/interface_with_mpas/Interface.hpp
    	modified:   src/FELIX/problems/FELIX_StokesFO.hpp

--- src/FELIX/interface_with_mpas/Interface.cpp
+++ src/FELIX/interface_with_mpas/Interface.cpp
@@ -17,24 +17,15 @@
 #include <Ionit_Initializer.h>
 #include "Albany_OrdinarySTKFieldContainer.hpp"
 
-//#define MPAS_USE_EPETRA
-
-// ===================================================
-//! Namespaces
-// ===================================================
-//using namespace LifeV;
-
-//typedef std::list<exchange> exchangeList_Type;
-
-// ice_problem pointer
-//ICEProblem *iceProblemPtr = 0;
-Teuchos::RCP<Albany::MpasSTKMeshStruct> meshStruct2D;
 Teuchos::RCP<Albany::MpasSTKMeshStruct> meshStruct;
 Teuchos::RCP<Albany::Application> albanyApp;
 Teuchos::RCP<Teuchos::ParameterList> paramList;
 Teuchos::RCP<const Teuchos_Comm> mpiCommT;
 Teuchos::RCP<Teuchos::ParameterList> discParams;
 Teuchos::RCP<Albany::SolverFactory> slvrfctry;
+
+double MPAS_gravity(9.8), MPAS_rho_ice(910), MPAS_rho_seawater(1028);
+
 #ifdef MPAS_USE_EPETRA
   Teuchos::RCP<const Epetra_Comm> mpiComm;
   Teuchos::RCP<Thyra::ModelEvaluator<double> > solver;
@@ -52,469 +43,6 @@ typedef struct TET_ {
 } TET;
 
 /***********************************************************/
-
-//extern "C" {
-#ifdef HAVE_PHG
-extern void phg_init_(int *fComm);
-extern void *phgImportParallelGrid(void *old_grid,
-    int nvert,
-    int nelem,
-    int nvert_global,
-    int nelem_global,
-    int *L2Gmap_vert,
-    int *L2Gmap_elem,
-    double *coord,
-    TET *tet,
-    MPI_Comm comm
-);
-extern void phgSolveIceStokes(void *g_ptr,
-    int nlayer,
-    const double *T,
-    const double *beta,
-    double *U);
-#endif
-
-// ===================================================
-//! Interface functions
-// ===================================================
-
-#ifdef HAVE_PHG
-static void
-check_face (RegionMesh<LinearTetra>& mesh, ID i, ID j)
-{
-  ID faceId = mesh.localFaceId (i, j);
-
-  if ( (ID) faceId != NotAnId)
-  {
-    RegionMesh<LinearTetra>::face_Type& face
-    = mesh.face (faceId);
-
-    if (0)
-    fprintf (stdout, "  face: %5d %5d, ",
-        faceId, face.id() );
-
-    ID v0, v1, v2, gv0, gv1, gv2;
-    v0 = LinearTetra::faceToPoint (j, 0);
-    v1 = LinearTetra::faceToPoint (j, 1);
-    v2 = LinearTetra::faceToPoint (j, 2);
-    // fprintf(stdout, "[%5d %5d %5d], ", v0, v1, v2);
-
-    gv0 = mesh.element (i).point (v0).id();
-    gv1 = mesh.element (i).point (v1).id();
-    gv2 = mesh.element (i).point (v2).id();
-    fprintf (stdout, "[ %7d %7d %7d ], ", gv0, gv1, gv2);
-
-    int mark = 0;
-    if (face.flag() &
-        EntityFlags::PHYSICAL_BOUNDARY)
-    {
-      mark = 1;
-    }
-    if (face.flag() &
-        EntityFlags::SUBDOMAIN_INTERFACE)
-    {
-      mark += 2;
-    }
-    fprintf (stdout, "mark: %d, ", mark);
-
-    ID vol0, vol1;
-    ID pos0, pos1;
-    vol0 = face.firstAdjacentElementIdentity();
-    pos0 = face.firstAdjacentElementPosition();
-    vol1 = face.secondAdjacentElementIdentity();
-    pos1 = face.secondAdjacentElementPosition();
-
-    if (vol0 == NotAnId)
-    {
-      vol0 = -1;
-    }
-    if (vol1 == NotAnId)
-    {
-      vol1 = -1;
-    }
-    if (pos0 == NotAnId)
-    {
-      pos0 = -1;
-    }
-    if (pos1 == NotAnId)
-    {
-      pos1 = -1;
-    }
-
-    fprintf (stdout, "first: %5d %d, second: %5d %d",
-        vol0, pos0, vol1, pos1);
-
-    fprintf (stdout, "\n");
-  }
-  else
-  {
-    fprintf (stdout, "Invalid face!!!\n");
-  }
-}
-#endif
-
-#ifdef HAVE_PHG
-void velocity_solver_init_stokes (double const* levelsRatio_F)
-{
-  int verb = 1;
-
-  // set velocity 0
-  velocityOnVertices.resize (2 * nVertices * (nLayers + 1), 0.);
-  velocityOnCells.resize (2 * nCells_F * (nLayers + 1), 0.);
-
-  //
-  // interface to phg
-  //
-  printf ("* Init full stokes?\n");
-  if (isDomainEmpty)
-  {
-    return;
-  }
-
-  layersRatio.resize (nLayers);
-  // !!Indexing of layers is reversed
-  for (UInt i = 0; i < nLayers; i++)
-  {
-    layersRatio[i] = levelsRatio_F[nLayers - 1 - i];
-  }
-  //std::copy(levelsRatio_F, levelsRatio_F+nLayers, layersRatio.begin());
-
-  mapCellsToVertices (velocityOnCells, velocityOnVertices, 2, nLayers, LayerWise);
-
-  printf ("* Init full stokes.\n");
-  //
-  // Output 3D mesh
-  //
-  RegionMesh<LinearTetra>& mesh = * (iceProblemPtr->mesh3DPtr);
-  unsigned int numVertices = mesh.numVertices();
-  unsigned int numElements = mesh.numElements();
-
-  std::vector<double > coord (3 * numVertices);
-
-  TET* tet;
-  tet = (TET*) calloc (numElements, sizeof (*tet) );
-
-  // update faces
-  mesh.updateElementFaces();
-  //if (verb > 0) printf("has local face :%d\n", mesh.hasLocalFaces());
-
-  // Num of verts (local, global)
-  if (verb > 0) printf ("nvert:%5d, nvert_global:%5d\n",
-      mesh.numVertices(), mesh.numGlobalVertices() );
-
-  // Num of elements (local, global)
-  if (verb > 0) printf ("nedge:%5d, nedge_global:%5d\n",
-      mesh.numElements(), mesh.numGlobalElements() );
-
-  if (verb > 0) printf ("nface:%5d, nface_global:%5d\n",
-      mesh.numFaces(), mesh.numGlobalFaces() );
-
-  if (verb > 0) printf ("nelem:%5d, nelem_global:%5d\n",
-      mesh.numElements(), mesh.numGlobalElements() );
-
-  //
-  // Element:
-  //
-  // verts[4] (local)
-  // bdry_type[4]
-  // neigh[4]
-  //
-  for (unsigned int i = 0; i < numElements; i++)
-  {
-    if (verb > 1)
-    {
-      printf ("elem: %d\n", i);
-    }
-    for (unsigned int j = 0; j < 4; j++)
-    {
-      const double* c = &mesh.element (i).point (j).coordinates() [0];
-      if (verb > 1) printf ("   vert: (%16.8f, %16.8f, %16.8f) %5d %5d\n",
-          c[0], c[1], c[2],
-          mesh.element (i).point (j).localId(),
-          mesh.element (i).point (j).id() //global Id
-      );
-      int vert = mesh.element (i).point (j).localId();
-      tet[i].verts[j] = vert;
-      coord[vert * 3 ] = c[0];
-      coord[vert * 3 + 1] = c[1];
-      coord[vert * 3 + 2] = c[2];
-    }
-
-    for (unsigned int j = 0; j < 4; j++)
-    {
-
-      //  Life tet face (ElementShapes)
-      //  0, 2, 1,
-      //  0, 1, 3,
-      //  1, 2, 3,
-      //  0, 3, 2
-      //
-      //  PHG tet face (utils.c)
-      //  1, 2, 3,
-      //  0, 2, 3,
-      //  0, 1, 3,
-      //  0, 1, 2
-      //
-      //  map Life to PHG
-      //  3, 2, 0, 1
-      //  reverse
-      //  2, 3, 1, 0
-      //
-      if (verb > 1)
-      {
-        check_face (mesh, i, j);
-      }
-
-      int faceId = mesh.localFaceId (i, j);
-      if (verb > 1)
-      {
-        printf (" faceId %d\n", faceId);
-      }
-      assert ( (ID) faceId != NotAnId);
-      static int map_[4] = {3, 2, 0, 1};
-
-      RegionMesh<LinearTetra>::face_Type& face
-      = mesh.face (faceId);
-
-      ID vol0, vol1;
-      ID pos0, pos1;
-      vol0 = face.firstAdjacentElementIdentity();
-      pos0 = face.firstAdjacentElementPosition();
-      vol1 = face.secondAdjacentElementIdentity();
-      pos1 = face.secondAdjacentElementPosition();
-
-      //
-      // bound_type: 1. interior(local), 2.interior(remote), [3|4|5].bdry
-      //
-      if (face.flag() & EntityFlags::PHYSICAL_BOUNDARY)
-      {
-        assert (vol0 == i);
-        assert (pos0 == j);
-        assert (vol1 == NotAnId);
-        assert (pos1 == NotAnId);
-
-        //
-        // lower face: 1; upper face 2,
-        //    pass as: 3(low), 4(up), 5(lateral, other)
-        // Note: marker comes from mpas
-        //
-        unsigned int lowerSurfaceMarker = 1;
-        unsigned int upperSurfaceMarker = 2;
-
-        tet[i].neighbours[map_[j]] = -1;
-        if (face.markerID() == lowerSurfaceMarker)
-        {
-          tet[i].bound_type[map_[j]] = 3;
-        }
-        else if (face.markerID() == upperSurfaceMarker)
-        {
-          tet[i].bound_type[map_[j]] = 4;
-        }
-        else
-        {
-          tet[i].bound_type[map_[j]] = 5;
-        }
-
-      }
-      else if (face.flag() & EntityFlags::SUBDOMAIN_INTERFACE)
-      {
-        assert (vol0 == i);
-        assert (pos0 == j);
-        assert (vol1 == NotAnId);
-        assert (pos1 == NotAnId);
-
-        tet[i].neighbours[map_[j]] = -1;
-        tet[i].bound_type[map_[j]] = 2;
-      }
-      else
-      {
-        if (vol0 == i)
-        {
-          assert (pos0 == j);
-          tet[i].neighbours[map_[j]] = vol1;
-          tet[i].bound_type[map_[j]] = 1;
-        }
-        else if (vol1 == i)
-        {
-          assert (pos1 == j);
-          tet[i].neighbours[map_[j]] = vol0;
-          tet[i].bound_type[map_[j]] = 1;
-        }
-        else
-        {
-          abort();
-        }
-      }
-    }
-  }
-
-  // L2G map vert
-  std::vector<int > L2Gmap_vert (numVertices);
-  for (unsigned int i = 0; i < numVertices; i++)
-  {
-    L2Gmap_vert[i] = mesh.point (i).id();
-    if (verb > 1) printf ("  L2G vert: %5d %5d\n", i,
-        mesh.point (i).id() );
-  }
-
-  // L2G map elem
-  std::vector<int > L2Gmap_elem (numElements);
-  for (unsigned int i = 0; i < numElements; i++)
-  {
-    L2Gmap_elem[i] = mesh.element (i).id();
-    if (verb > 1) printf ("  L2G elem: %5d %5d\n", i,
-        mesh.element (i).id() );
-  }
-
-  // if (verb > 0) printf("check all face\n");
-  // for (unsigned int i = 0; i < numFaces; i++) {
-  //  check_face(mesh, i);
-  // }
-
-  printf ("  Call PHG\n");
-  phgGrid = phgImportParallelGrid (phgGrid,// old grid
-      numVertices,
-      numElements,
-      mesh.numGlobalVertices(),
-      mesh.numGlobalElements(),
-      &L2Gmap_vert[0],
-      &L2Gmap_elem[0],
-      &coord[0],
-      tet,
-      reducedComm
-  );
-
-  return;
-}
-#endif
-
-#define GET_HERE printf("get %25s, line: %d\n", __FILE__, __LINE__);
-
-#ifdef HAVE_PHG
-void velocity_solver_solve_stokes (double const* lowerSurface_F, double const* thickness_F,
-    double const* beta_F, double const* temperature_F,
-    double* u_normal_F,
-    double* /*heatIntegral_F*/, double* /*viscosity_F*/)
-{
-  std::fill (u_normal_F, u_normal_F + nEdges_F * nLayers, 0.);
-
-  if (!isDomainEmpty)
-  {
-
-    RegionMesh<LinearTetra>& mesh3D = * (iceProblemPtr->mesh3DPtr);
-    //  RegionMesh<LinearTriangle>& mesh2D =  *(iceProblemPtr->mesh2DPtr);
-
-    // Full Stokes solution
-    //   [nvert+nedge][u|v|w]
-    std::vector<Real> Velocity_FS (3 * mesh3D.numVertices()
-        + 3 * mesh3D.numEdges() );
-    // P1:   [u|v][nvert] using velocityOnVertices
-
-    // P2:   [u|v][nvert+nedge]
-    std::vector<Real> Velocity_P2 (2 * mesh3D.numVertices()
-        + 2 * mesh3D.numEdges() );
-
-    importP0Temperature (temperature_F);// import temperature to
-    // temperatureOnTetra[numElements]
-    import2DFields (lowerSurface_F, thickness_F, beta_F);// import surf, thick and beta
-    // to [numVertices]
-
-    // Note:
-    // Grid is supposed to be layerwise to import beta
-    phgSolveIceStokes (phgGrid,
-        nLayers,
-        &temperatureOnTetra[0],
-        &betaData[0],
-        &Velocity_FS[0]);
-
-    unsigned int nEdges3D = mesh3D.numEdges();
-    unsigned int numElements = mesh3D.numElements();
-    GET_HERE;
-    unsigned int nVertices3D = mesh3D.numVertices();
-    assert (nEdges3D > 0);
-
-    GET_HERE;
-    for (unsigned int i = 0; i < numElements; i++)
-    {
-
-      // Vert
-      for (unsigned int j = 0; j < 4; j++)
-      {
-        int v = mesh3D.element (i).point (j).localId();
-        velocityOnVertices[v] = Velocity_FS[3 * v];
-        velocityOnVertices[nVertices3D + v] = Velocity_FS[3 * v + 1];
-        Velocity_P2[v] = Velocity_FS[3 * v];
-        Velocity_P2[nVertices3D + nEdges3D + v] = Velocity_FS[3 * v + 1];
-      }
-
-      //  Life tet edge (ElementShapes)
-      //  0, 1,
-      //  1, 2,
-      //  2, 0,
-      //  0, 3,
-      //  1, 3,
-      //  2, 3
-      //
-      //  PHG tet face (utils.c)
-      //  0, 1
-      //  0, 2
-      //  0, 3
-      //  1, 2
-      //  1, 3
-      //  2, 3
-      //
-      //  map Life to PHG
-      //  0, 3, 1, 2, 4, 5
-      //  reverse
-      //  0, 2, 3, 1, 4, 5
-      //
-
-      // for (unsigned int j = 0; j < 4; j++) {
-      //  int e = mesh3D.element(i).edge(j).localId();
-      //  Velocity_P1[e] =  Velocity_FS[3*e];
-      //  Velocity_P1[nVertices + v]  = Velocity_FS[3*e + 1];
-      //  Velocity_P2[e] =  Velocity_FS[3*v];
-      //  Velocity_P2[nVertices+nEdges + e]  = Velocity_FS[3*e + 1];
-      // }
-    }
-
-    //save velocity to be used as an initial guess next iteration
-    GET_HERE;
-    std::vector<int> mpasIndexToVertexID (nVertices);
-    for (int i = 0; i < nVertices; i++)
-    {
-      mpasIndexToVertexID[i] = indexToCellID_F[vertexToFCell[i]];
-    }
-    get_tetraP1_velocity_on_FEdges (u_normal_F, velocityOnVertices, edgeToFEdge, mpasIndexToVertexID);
-
-  }
-
-  GET_HERE;
-  mapVerticesToCells (velocityOnVertices, &velocityOnCells[0], 2, nLayers, LayerWise);
-
-  GET_HERE;
-  allToAll (u_normal_F, &sendEdgesListReversed, &recvEdgesListReversed, nLayers);
-
-  GET_HERE;
-  allToAll (u_normal_F, sendEdgesList_F, recvEdgesList_F, nLayers);
-  GET_HERE;
-
-  return;
-}
-#endif
-
-void velocity_solver_export_2d_data(MPI_Comm reducedComm,
-    const std::vector<double>& elevationData,
-    const std::vector<double>& thicknessData,
-    const std::vector<double>& betaData,
-    const std::vector<int>& indexToVertexID) {
-
-  Teuchos::RCP<stk::io::StkMeshIoBroker> mesh_data = Teuchos::rcp(new stk::io::StkMeshIoBroker(reducedComm));
-  mesh_data->set_bulk_data(*meshStruct2D->bulkData);
-  size_t idx = mesh_data->create_output_mesh("mesh2D.exo", stk::io::WRITE_RESULTS);
-  mesh_data->process_output_request(idx, 0.0);
-}
-
 
 
 void velocity_solver_solve_fo(int nLayers, int nGlobalVertices,
@@ -540,7 +68,12 @@ void velocity_solver_solve_fo(int nLayers, int nGlobalVertices,
   int elemLayerShift = (ordering == 0) ? 3 : 3 * nLayers;
 
   const bool interleavedOrdering = meshStruct->getInterleavedOrdering();
-  Albany::AbstractSTKFieldContainer::VectorFieldType* solutionField;
+
+  typedef Albany::AbstractSTKFieldContainer::VectorFieldType VectorFieldType;
+  typedef Albany::AbstractSTKFieldContainer::ScalarFieldType ScalarFieldType;
+  typedef Albany::AbstractSTKFieldContainer::QPScalarFieldType ElemScalarFieldType;
+
+  VectorFieldType* solutionField;
 
   if (interleavedOrdering)
     solutionField = Teuchos::rcp_dynamic_cast<
@@ -551,10 +84,10 @@ void velocity_solver_solve_fo(int nLayers, int nGlobalVertices,
         Albany::OrdinarySTKFieldContainer<false> >(
         meshStruct->getFieldContainer())->getSolutionField();
 
-  typedef Albany::AbstractSTKFieldContainer::ScalarFieldType ScalarFieldType;
-  typedef Albany::AbstractSTKFieldContainer::QPScalarFieldType ElemScalarFieldType;
-
-
+  ScalarFieldType* surfaceHeightField = meshStruct->metaData->get_field <ScalarFieldType> (stk::topology::NODE_RANK, "surface_height");
+  ScalarFieldType* thicknessField = meshStruct->metaData->get_field <ScalarFieldType> (stk::topology::NODE_RANK, "thickness");
+  VectorFieldType* dirichletField = meshStruct->metaData->get_field <VectorFieldType> (stk::topology::NODE_RANK, "dirichlet_field");
+  ScalarFieldType* basalFrictionField = meshStruct->metaData->get_field <ScalarFieldType> (stk::topology::NODE_RANK, "basal_friction");
 
   for (UInt j = 0; j < numVertices3D; ++j) {
     int ib = (ordering == 0) * (j % lVertexColumnShift)
@@ -566,15 +99,18 @@ void velocity_solver_solve_fo(int nLayers, int nGlobalVertices,
     double* coord = stk::mesh::field_data(*meshStruct->getCoordinatesField(), node);
     coord[2] = elevationData[ib] - levelsNormalizedThickness[nLayers - il] * regulThk[ib];
 
-     double* sHeight = stk::mesh::field_data(*meshStruct->metaData->get_field <ScalarFieldType> (stk::topology::NODE_RANK, "surface_height"), node);
+     double* sHeight = stk::mesh::field_data(*surfaceHeightField, node);
     sHeight[0] = elevationData[ib];
-    double* thickness = stk::mesh::field_data(*meshStruct->metaData->get_field <ScalarFieldType> (stk::topology::NODE_RANK, "thickness"), node);
+    double* thickness = stk::mesh::field_data(*thicknessField, node);
     thickness[0] = thicknessData[ib];
     double* sol = stk::mesh::field_data(*solutionField, node);
     sol[0] = velocityOnVertices[j];
     sol[1] = velocityOnVertices[j + numVertices3D];
+    double* dirichletVel = stk::mesh::field_data(*dirichletField, node);
+    dirichletVel[0]=1;
+    dirichletVel[1]=1;
     if (il == 0) {
-      double* beta = stk::mesh::field_data(*meshStruct->metaData->get_field <ScalarFieldType> (stk::topology::NODE_RANK, "basal_friction"), node);
+      double* beta = stk::mesh::field_data(*basalFrictionField, node);
       beta[0] = std::max(betaData[ib], minBeta);
     }
   }
@@ -713,7 +249,11 @@ void velocity_solver_compute_2d_grid(MPI_Comm reducedComm) {
   mpiCommT = Albany::createTeuchosCommFromMpiComm(reducedComm);
 }
 
-
+void velocity_solver_set_physical_parameters(double gravity_, double rho_ice_, double rho_seawater_) {
+  MPAS_gravity=gravity_;
+  MPAS_rho_ice = rho_ice_;
+  MPAS_rho_seawater = rho_seawater_;
+}
 
 void velocity_solver_extrude_3d_grid(int nLayers, int nGlobalTriangles,
     int nGlobalVertices, int nGlobalEdges, int Ordering, MPI_Comm reducedComm,
@@ -727,11 +267,33 @@ void velocity_solver_extrude_3d_grid(int nLayers, int nGlobalTriangles,
     const std::vector<int>& trianglesPositionsOnEdge,
     const std::vector<int>& verticesOnEdge,
     const std::vector<int>& indexToEdgeID,
-    const std::vector<GO>& indexToTriangleID) {
+    const std::vector<GO>& indexToTriangleID,
+    const std::vector<int>& dirichletNodesIds,
+    const std::vector<int>& floating2dEdgesIds) {
 
+  std::cout<<std::endl;
   slvrfctry = Teuchos::rcp(
       new Albany::SolverFactory("albany_input.xml", mpiCommT));
   paramList = Teuchos::rcp(&slvrfctry->getParameters(), false);
+
+
+  //Physical Parameters
+  paramList->sublist("Problem").sublist("Physical Parameters").set("Gravity", MPAS_gravity);
+  paramList->sublist("Problem").sublist("Physical Parameters").set("Ice Density", MPAS_rho_ice);
+  paramList->sublist("Problem").sublist("Physical Parameters").set("Water Density", MPAS_rho_seawater);
+
+  Teuchos::RCP<Teuchos::Array<double> >inputArrayBasal = Teuchos::rcp(new Teuchos::Array<double> (1, 1.0));
+  paramList->sublist("Problem").sublist("Neumann BCs").set("Cubature Degree", 3);
+  paramList->sublist("Problem").sublist("Neumann BCs").set("NBC on SS basalside for DOF all set basal_scalar_field", *inputArrayBasal);
+
+  //Lateral floating ice BCs
+  Teuchos::RCP<Teuchos::Array<double> >inputArrayLateral = Teuchos::rcp(new Teuchos::Array<double> (1, MPAS_rho_ice/MPAS_rho_seawater));
+  paramList->sublist("Problem").sublist("Neumann BCs").set("NBC on SS floatingside for DOF all set lateral", *inputArrayLateral);
+
+  //Dirichlet BCs
+  paramList->sublist("Problem").sublist("Dirichlet BCs").set("DBC on NS dirichlet for DOF U0 prescribe Field", "dirichlet_field");
+  paramList->sublist("Problem").sublist("Dirichlet BCs").set("DBC on NS dirichlet for DOF U1 prescribe Field", "dirichlet_field");
+
   discParams = Teuchos::sublist(paramList, "Discretization", true);
 
   Albany::AbstractFieldContainer::FieldContainerRequirements req;
@@ -752,6 +314,7 @@ void velocity_solver_extrude_3d_grid(int nLayers, int nGlobalTriangles,
       mpasIndexToVertexID, verticesCoords, isVertexBoundary, nGlobalVertices,
       verticesOnTria, isBoundaryEdge, trianglesOnEdge, trianglesPositionsOnEdge,
       verticesOnEdge, indexToEdgeID, nGlobalEdges, indexToTriangleID,
+      dirichletNodesIds, floating2dEdgesIds,
       meshStruct->getMeshSpecs()[0]->worksetSize, nLayers, Ordering);
 }
 //}
