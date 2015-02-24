2b1fa6a958a470152cd4ec9135052e1ca0ac2527
Alejandro Mota <amota@sandia.gov>
2015-02-24 13:45:36 -0800

    LCM: Fixed error caused by trailing white space and sorted and grouped CMake directives to try to make the script clearer.

--- doc/LCM/build/trilinos-config.sh
+++ doc/LCM/build/trilinos-config.sh
@@ -34,13 +34,15 @@ cmake \
  -D BoostLib_INCLUDE_DIRS:FILEPATH="/usr/include/boost" \
  -D BoostLib_LIBRARY_DIRS:FILEPATH="/usr/lib64" \
 \
- -D Trilinos_ENABLE_CXX11:BOOL=ON \
- -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
- -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF \
  -D Trilinos_ASSERT_MISSING_PACKAGES:BOOL=OFF \
- -D Trilinos_WARNINGS_AS_ERRORS_FLAGS:STRING="" \
+ -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF \
+ -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
+ -D Trilinos_ENABLE_CXX11:BOOL=ON \
+ -D Trilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON \
  -D Trilinos_VERBOSE_CONFIGURE:BOOL=OFF \
+ -D Trilinos_WARNINGS_AS_ERRORS_FLAGS:STRING="" \
 \
+ -D Trilinos_ENABLE_Amesos2:BOOL=ON \
  -D Trilinos_ENABLE_Amesos:BOOL=ON \
  -D Trilinos_ENABLE_Anasazi:BOOL=ON \
  -D Trilinos_ENABLE_AztecOO:BOOL=ON \
@@ -48,17 +50,25 @@ cmake \
  -D Trilinos_ENABLE_EXAMPLES:BOOL=OFF \
  -D Trilinos_ENABLE_Epetra:BOOL=ON \
  -D Trilinos_ENABLE_EpetraExt:BOOL=ON \
+ -D Trilinos_ENABLE_Ifpack2:BOOL=ON \
  -D Trilinos_ENABLE_Ifpack:BOOL=ON \
  -D Trilinos_ENABLE_Intrepid:BOOL=ON \
+ -D Trilinos_ENABLE_Kokkos:BOOL=ON \
+ -D Trilinos_ENABLE_KokkosCore:BOOL=ON \
  -D Trilinos_ENABLE_ML:BOOL=ON \
  -D Trilinos_ENABLE_MOOCHO:BOOL=OFF \
+ -D Trilinos_ENABLE_MueLu:BOOL=ON \
  -D Trilinos_ENABLE_NOX:BOOL=ON \
  -D Trilinos_ENABLE_Pamgen:BOOL=ON \
  -D Trilinos_ENABLE_Phalanx:BOOL=ON \
  -D Trilinos_ENABLE_Piro:BOOL=ON \
  -D Trilinos_ENABLE_Rythmos:BOOL=ON \
+ -D Trilinos_ENABLE_SEACAS:BOOL=ON \
  -D Trilinos_ENABLE_STK:BOOL=ON \
+ -D Trilinos_ENABLE_STKClassic:BOOL=OFF \
  -D Trilinos_ENABLE_STKDoc_tests:BOOL=OFF \
+ -D Trilinos_ENABLE_STKIO:BOOL=ON \
+ -D Trilinos_ENABLE_STKMesh:BOOL=ON \
  -D Trilinos_ENABLE_STKSearch:BOOL=OFF \
  -D Trilinos_ENABLE_STKSearchUtil:BOOL=OFF \
  -D Trilinos_ENABLE_STKTopology:BOOL=ON \
@@ -74,40 +84,29 @@ cmake \
  -D Trilinos_ENABLE_Teuchos:BOOL=ON \
  -D Trilinos_ENABLE_ThreadPool:BOOL=ON \
  -D Trilinos_ENABLE_Thyra:BOOL=ON \
+ -D Trilinos_ENABLE_Tpetra:BOOL=ON \
+ -D Trilinos_ENABLE_Zoltan2:BOOL=ON \
  -D Trilinos_ENABLE_Zoltan:BOOL=ON \
 \
  -D Intrepid_ENABLE_TESTS:BOOL=OFF \
  -D Sacado_ENABLE_COMPLEX:BOOL=ON \
  -D Teuchos_ENABLE_COMPLEX:BOOL=ON \
 \
- -D Trilinos_ENABLE_STKClassic:BOOL=OFF \
- -D Trilinos_ENABLE_STKIO:BOOL=ON \
- -D Trilinos_ENABLE_STKMesh:BOOL=ON \
-\
- -D Trilinos_ENABLE_SEACAS:BOOL=ON \
- -D TPL_ENABLE_Netcdf:BOOL=ON \
- -D TPL_Netcdf_INCLUDE_DIRS:PATH=/usr/include \
- -D TPL_ENABLE_HDF5:BOOL=OFF \
  -D EpetraExt_USING_HDF5:BOOL=OFF \
+ -D TPL_ENABLE_HDF5:BOOL=OFF \
  -D TPL_ENABLE_Matio:BOOL=OFF \
+ -D TPL_ENABLE_Netcdf:BOOL=ON \
  -D TPL_ENABLE_X11:BOOL=OFF \
+ -D TPL_Netcdf_INCLUDE_DIRS:PATH=/usr/include \
 \
- -D Trilinos_ENABLE_Tpetra:BOOL=ON \
- -D Trilinos_ENABLE_KokkosCore:BOOL=ON \
- -D Trilinos_ENABLE_Kokkos:BOOL=ON \
- -D Phalanx_KOKKOS_DEVICE_TYPE:STRING="SERIAL" \
- -D Phalanx_INDEX_SIZE_TYPE:STRING="INT" \
- -D Phalanx_SHOW_DEPRECATED_WARNINGS:BOOL=OFF \
- -D Kokkos_ENABLE_Serial:BOOL=ON \
- -D Kokkos_ENABLE_OpenMP:BOOL=OFF \
- -D Kokkos_ENABLE_Pthread:BOOL=OFF \ 
- -D Trilinos_ENABLE_Ifpack2:BOOL=ON \
- -D Trilinos_ENABLE_Amesos2:BOOL=ON \
- -D Trilinos_ENABLE_Zoltan2:BOOL=ON \
- -D Trilinos_ENABLE_MueLu:BOOL=ON \
  -D Amesos2_ENABLE_KLU2:BOOL=ON \
+ -D Kokkos_ENABLE_OpenMP:BOOL=OFF \
+ -D Kokkos_ENABLE_Pthread:BOOL=OFF \
+ -D Kokkos_ENABLE_Serial:BOOL=ON \
+ -D Phalanx_INDEX_SIZE_TYPE:STRING="INT" \
+ -D Phalanx_KOKKOS_DEVICE_TYPE:STRING="SERIAL" \
+ -D Phalanx_SHOW_DEPRECATED_WARNINGS:BOOL=OFF \
  -D Tpetra_ENABLE_CLASSIC_VBR:BOOL=ON \
 \
  -D BUILD_SHARED_LIBS:BOOL=ON \
- -D Trilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON \
   package_dir
