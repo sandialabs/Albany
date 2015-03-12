538665cb5f0be41a4fdf9a30bdc959e76f5b4243
Alejandro Mota <amota@sandia.gov>
2015-03-12 10:15:46 -0700

    LCM: Template to show usage of bifurcation utilities and parametrization classes. Empty at the moment.

---
+++ src/LCM/test/utils/BifurcationTest.cc
@@ -0,0 +1,23 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+// Simple mesh partitioning program
+//
+
+// Define only if Zoltan is enabled
+#include <iomanip>
+
+#include <Intrepid_MiniTensor.h>
+
+//
+// Simple tests for parametrizations of the bifurcation tensor.
+//
+int main(int ac, char* av[])
+{
+  // Build an elasticity tensor.
+
+  return 0;
+
+}
