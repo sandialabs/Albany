4b887e72cbd05150c5471502ac5441fff52b0914
Alejandro Mota <amota@sandia.gov>
2015-03-26 20:58:34 -0700

    LCM: Minor changes in preparation to hook up Schwarz BC.

--- src/LCM/Schwarz_BoundaryJacobian.cpp
+++ src/LCM/Schwarz_BoundaryJacobian.cpp
@@ -18,52 +18,55 @@ mm_counter = 0;
 #endif // WRITE_TO_MATRIX_MARKET
 
 LCM::Schwarz_BoundaryJacobian::Schwarz_BoundaryJacobian(
-    Teuchos::RCP<Teuchos_Comm const> const & commT)
+    Teuchos::RCP<Teuchos_Comm const> const & commT) :
+        commT_(commT), b_use_transpose_(false), b_initialized_(false),
+        n_models_(0)
 {
-  commT_ = commT;
-  b_use_transpose_ = false;
-  b_initialized_ = false;
+  return;
 }
 
 LCM::Schwarz_BoundaryJacobian::~Schwarz_BoundaryJacobian()
 {
 }
 
-
-//! Initialize the operator with everything needed to apply it
-void LCM::Schwarz_BoundaryJacobian::initialize() 
+// Initialize the operator with everything needed to apply it
+void LCM::Schwarz_BoundaryJacobian::initialize()
 {
-  //FIXME: add parameter list argument, member parameters for specifying boundary conditions.
-  //These can be stored in an array of Tpetra_CrsMatrices like the jacobians.
+  // FIXME: add parameter list argument, member parameters for
+  // specifying boundary conditions.
+  // These can be stored in an array of Tpetra_CrsMatrices like the jacobians.
   // Set member variables
 
-  std::cout << __PRETTY_FUNCTION__ << "\n"; 
+  std::cout << __PRETTY_FUNCTION__ << "\n";
 }
 
-
-//! Returns the result of a Tpetra_Operator applied to a Tpetra_MultiVector X in Y.
-void LCM::Schwarz_BoundaryJacobian::apply(const Tpetra_MultiVector& X, Tpetra_MultiVector& Y, 
-                                       Teuchos::ETransp mode,
-                                       ST alpha, ST beta) const
-{ 
-  std::cout << __PRETTY_FUNCTION__ << "\n"; 
+// Returns the result of a Tpetra_Operator applied to a
+// Tpetra_MultiVector X in Y.
+void LCM::Schwarz_BoundaryJacobian::apply(
+    const Tpetra_MultiVector& X,
+    Tpetra_MultiVector& Y,
+    Teuchos::ETransp mode,
+    ST alpha,
+    ST beta) const
+{
+  std::cout << __PRETTY_FUNCTION__ << "\n";
 
 #ifdef WRITE_TO_MATRIX_MARKET
-  //writing to MatrixMarket file for debug -- initial X where we will set Y = Jac*X
+  // writing to MatrixMarket file for debug
+  // initial X where we will set Y = Jac*X
   char name[100];  //create string for file name
   sprintf(name, "X_%i.mm", mm_counter);
   Tpetra_MatrixMarket_Writer::writeDenseFile(name, X);
 #endif  // WRITE_TO_MATRIX_MARKET
 
   //FIXME: fill in!
-      
-  
+
 #ifdef WRITE_TO_MATRIX_MARKET
-  //writing to MatrixMarket file for debug -- final solution Y (after all the operations to set Y = Jac*X
+  // writing to MatrixMarket file for debug
+  // final solution Y (after all the operations to set Y = Jac*X
   sprintf(name, "Y_%i.mm", mm_counter);
   Tpetra_MatrixMarket_Writer::writeDenseFile(name, Y);
   ++mm_counter;
 #endif  // WRITE_TO_MATRIX_MARKET
 }
-
 
