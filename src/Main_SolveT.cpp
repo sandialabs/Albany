f338b44001b1a406200e680defe6436022ad7965
Irina K. Tezaur <ikalash@sandia.gov>
2015-03-29 16:18:03 -0700

    Factoring out part of createCombinedTpetraVector (used in tpetraFromThyraProdVec) that creates the map of the combibned tpetra vector as separate function.
    Rewrite of writing out of xfinal to matrix market so that the map is obtained
    from xfinal, rather than from the app object.  This is required for coupled
    Schwarz problems for which the app object will not be defined.

--- src/Main_SolveT.cpp
+++ src/Main_SolveT.cpp
@@ -43,6 +43,143 @@ bool TpetraBuild = true;
 const Tpetra::global_size_t INVALID = Teuchos::OrdinalTraits<Tpetra::global_size_t>::invalid ();
 
 
+Teuchos::RCP<const Tpetra_Map>
+getSolutionMap(Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > vecs)
+{
+  int  n_vecs = vecs.size();
+
+  // Figure out how many local and global elements are in the
+  // combined map by summing these quantities over each vector.
+  LO local_num_elements = 0;
+  GO global_num_elements = 0;
+
+  for (int m = 0; m < n_vecs; ++m) {
+    local_num_elements += vecs[m]->getMap()->getNodeNumElements();
+    global_num_elements += vecs[m]->getMap()->getGlobalNumElements();
+  }
+  //Create global element indices array for combined map for this processor,
+  //to be used to create the combined map.
+  std::vector<GO> my_global_elements(local_num_elements);
+
+  LO counter_local = 0;
+  GO counter_global = 0;
+
+  
+  for (int m = 0; m < n_vecs; ++m) {
+    LO local_num_elements_n = vecs[m]->getMap()->getNodeNumElements();
+    GO global_num_elements_n = vecs[m]->getMap()->getGlobalNumElements();
+    Teuchos::ArrayView<GO const> disc_global_elements = vecs[m]->getMap()->getNodeElementList();
+ 
+    for (int l = 0; l < local_num_elements_n; ++l) {
+      my_global_elements[counter_local + l] = counter_global + disc_global_elements[l];
+    }
+    counter_local += local_num_elements_n;
+    counter_global += global_num_elements_n;
+  }
+
+  Teuchos::ArrayView<GO> const
+  my_global_elements_AV =
+      Teuchos::arrayView(&my_global_elements[0], local_num_elements);
+
+  Teuchos::RCP<const Tpetra_Map>  combined_map = Teuchos::rcp(
+      new Tpetra_Map(global_num_elements, my_global_elements_AV, 0, vecs[0]->getMap()->getComm()));
+ 
+  return combined_map; 
+}
+
+Teuchos::RCP<Tpetra_Vector>
+createCombinedTpetraVector(
+    Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > vecs)
+{
+  int  n_vecs = vecs.size();
+
+  Teuchos::RCP<const Tpetra_Map>  combined_map = getSolutionMap(vecs); 
+ 
+  Teuchos::RCP<Tpetra_Vector> combined_vec = Teuchos::rcp(new Tpetra_Vector(combined_map)); 
+  
+  LO counter_local = 0; 
+  for (int m = 0; m < n_vecs; ++m) {
+    int disc_local_elements_m = vecs[m]->getMap()->getNodeNumElements(); 
+    Teuchos::RCP<Tpetra_Vector> temp = combined_vec->offsetViewNonConst(vecs[m]->getMap(), counter_local);
+    Teuchos::Array<ST> array(vecs[m]->getMap()->getNodeNumElements());
+    vecs[m]->get1dCopy(array);
+    for (std::size_t i=0; i<vecs[m]->getMap()->getNodeNumElements(); ++i)
+     temp->replaceLocalValue(i, array[i]);
+    counter_local += disc_local_elements_m; 
+  }
+
+  return combined_vec;
+}
+
+// Overridden from Thyra::ModelEvaluator<ST>
+
+//IKT: Similar to tpetraFromThyra but for thyra product vectors (currently only used in Coupled Schwarz problems). 
+void tpetraFromThyraProdVec(
+  const Teuchos::Array<Teuchos::RCP<const Thyra::VectorBase<ST> > > &thyraResponses,
+  const Teuchos::Array<Teuchos::Array<Teuchos::RCP<const Thyra::MultiVectorBase<ST> > > > &thyraSensitivities,
+  Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > &responses,
+  Teuchos::Array<Teuchos::Array<Teuchos::RCP<const Tpetra_MultiVector> > > &sensitivities)
+{
+  responses.clear();
+  responses.reserve(thyraResponses.size());
+  typedef Teuchos::Array<Teuchos::RCP<const Thyra::VectorBase<ST> > > ThyraResponseArray;
+  for (ThyraResponseArray::const_iterator it_begin = thyraResponses.begin(),
+      it_end = thyraResponses.end(),
+      it = it_begin;
+      it != it_end;
+      ++it) 
+      {
+      Teuchos::RCP<const Thyra::ProductVectorBase<ST> > r_prod =
+           Teuchos::nonnull(*it) ?
+           Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<ST> >(*it,true) :
+           Teuchos::null;
+      if (r_prod != Teuchos::null) {
+        //FIXME!  r_vec should be all the product vectors concatenated not just the first one
+        Teuchos::RCP<const Thyra::ProductVectorSpaceBase<ST> > prod_space  = r_prod->productSpace();
+        int nProdVecs = prod_space->numBlocks(); 
+        //create Teuchos array of spaces / vectors, to be populated from the product vector
+        Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > rs(nProdVecs); 
+        for (int i=0; i<nProdVecs; i++) {
+          rs[i] =  ConverterT::getConstTpetraVector(r_prod->getVectorBlock(i)); 
+        }
+        Teuchos::RCP<Tpetra_Vector> r_vec = createCombinedTpetraVector(rs); 
+        //Teuchos::RCP<const Tpetra_Vector> r_vec = 
+        //     Teuchos::rcp_dynamic_cast<const ThyraVector>(r_prod->getVectorBlock(0),true)->getConstTpetraVector();
+        responses.push_back(r_vec);
+      }
+    }
+
+  sensitivities.clear();
+  sensitivities.reserve(thyraSensitivities.size());
+  typedef Teuchos::Array<Teuchos::Array<Teuchos::RCP<const Thyra::MultiVectorBase<ST> > > > ThyraSensitivityArray;
+  for (ThyraSensitivityArray::const_iterator it_begin = thyraSensitivities.begin(),
+      it_end = thyraSensitivities.end(),
+      it = it_begin;
+      it != it_end;
+      ++it) {
+    ThyraSensitivityArray::const_reference sens_thyra = *it;
+    Teuchos::Array<Teuchos::RCP<const Tpetra_MultiVector> > sens;
+    sens.reserve(sens_thyra.size());
+    for (ThyraSensitivityArray::value_type::const_iterator jt = sens_thyra.begin(),
+        jt_end = sens_thyra.end();
+        jt != jt_end;
+        ++jt) {
+        Teuchos::RCP<const Thyra::ProductVectorBase<ST> > s_prod =
+           Teuchos::nonnull(*jt) ?
+           Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<ST> >(*jt,true) :
+           Teuchos::null;
+        if (s_prod != Teuchos::null) {
+          //FIXME!  s_vec should be all the product vectors concatenated not just the first one
+          Teuchos::RCP<const Tpetra_Vector> s_vec = 
+             Teuchos::rcp_dynamic_cast<const ThyraVector>(s_prod->getVectorBlock(0),true)->getConstTpetraVector();
+          sens.push_back(s_vec);
+        }
+    }
+    sensitivities.push_back(sens);
+  }
+}
+
+
 void tpetraFromThyra(
   const Teuchos::Array<Teuchos::RCP<const Thyra::VectorBase<ST> > > &thyraResponses,
   const Teuchos::Array<Teuchos::Array<Teuchos::RCP<const Thyra::MultiVectorBase<ST> > > > &thyraSensitivities,
@@ -58,7 +195,7 @@ void tpetraFromThyra(
       it != it_end;
       ++it) {
     responses.push_back(Teuchos::nonnull(*it) ? ConverterT::getConstTpetraVector(*it) : Teuchos::null);
-  }
+    }
 
   sensitivities.clear();
   sensitivities.reserve(thyraSensitivities.size());
@@ -160,7 +297,16 @@ int main(int argc, char *argv[]) {
 
     Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > responses;
     Teuchos::Array<Teuchos::Array<Teuchos::RCP<const Tpetra_MultiVector> > > sensitivities;
-    tpetraFromThyra(thyraResponses, thyraSensitivities, responses, sensitivities);
+
+    //Check if thyraResponses are product vectors or regular vectors
+    Teuchos::RCP<const Thyra::ProductVectorBase<ST> > r_prod; 
+    if (thyraResponses.size() > 0) {
+      r_prod = Teuchos::nonnull(thyraResponses[0]) ?
+           Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<ST> >(thyraResponses[0],false) :
+           Teuchos::null;
+    }
+    if (r_prod == Teuchos::null) tpetraFromThyra(thyraResponses, thyraSensitivities, responses, sensitivities);
+    else tpetraFromThyraProdVec(thyraResponses, thyraSensitivities, responses, sensitivities);
 
     const int num_p = solver->Np(); // Number of *vectors* of parameters
     const int num_g = solver->Ng(); // Number of *vectors* of responses
@@ -304,14 +450,14 @@ int main(int argc, char *argv[]) {
     if (writeToMatrixMarketSoln == true) { 
 
       //create serial map that puts the whole solution on processor 0
-      int numMyElements = (xfinal->getMap()->getComm()->getRank() == 0) ? app->getDiscretization()->getMapT()->getGlobalNumElements() : 0;
+      int numMyElements = (xfinal->getMap()->getComm()->getRank() == 0) ? xfinal->getMap()->getGlobalNumElements() : 0;
 
      Teuchos::RCP<const Tpetra_Map> serial_map = Teuchos::rcp(new const Tpetra_Map(INVALID, numMyElements, 0, comm));
  
       //create importer from parallel map to serial map and populate serial solution xfinal_serial
-      Teuchos::RCP<Tpetra_Import> importOperator = Teuchos::rcp(new Tpetra_Import(app->getDiscretization()->getMapT(), serial_map)); 
+      Teuchos::RCP<Tpetra_Import> importOperator = Teuchos::rcp(new Tpetra_Import(xfinal->getMap(), serial_map)); 
       Teuchos::RCP<Tpetra_Vector> xfinal_serial = Teuchos::rcp(new Tpetra_Vector(serial_map)); 
-      xfinal_serial->doImport(*app->getDiscretization()->getSolutionFieldT(), *importOperator, Tpetra::INSERT);
+      xfinal_serial->doImport(*xfinal, *importOperator, Tpetra::INSERT);
 
       //writing to MatrixMarket file
        Tpetra_MatrixMarket_Writer::writeDenseFile("xfinal.mm", xfinal_serial);
@@ -319,7 +465,7 @@ int main(int argc, char *argv[]) {
     if (writeToMatrixMarketDistrSolnMap == true) {
       //writing to MatrixMarket file
       Tpetra_MatrixMarket_Writer::writeDenseFile("xfinal_distributed.mm", *xfinal);
-      Tpetra_MatrixMarket_Writer::writeMapFile("xfinal_distributed_map.mm", *app->getDiscretization()->getMapT());
+      Tpetra_MatrixMarket_Writer::writeMapFile("xfinal_distributed_map.mm", *xfinal->getMap());
     }
   }
   TEUCHOS_STANDARD_CATCH_STATEMENTS(true, std::cerr, success);
