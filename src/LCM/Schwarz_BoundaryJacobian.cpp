ef5bd4e9326bfe39e7b5713feb3037378b633248
Alejandro Mota <amota@sandia.gov>
2015-04-21 11:05:33 -0700

    LCM: Set the domain map to the coupled application one and the range map to the current application map.

--- src/LCM/Schwarz_BoundaryJacobian.cpp
+++ src/LCM/Schwarz_BoundaryJacobian.cpp
@@ -34,9 +34,8 @@ LCM::Schwarz_BoundaryJacobian::Schwarz_BoundaryJacobian(
         b_initialized_(false),
         n_models_(0)
 {
-  //FIXME, IKT: are range_map_ = domain_map_ for the boundary Jacobian operators? 
-  range_map_ = ca[this_app_index_*n_models_ + coupled_app_index_]->getMapT();
-  domain_map_ = ca[this_app_index_*n_models_ + coupled_app_index_]->getMapT();
+  range_map_ = ca[coupled_app_index_]->getMapT();
+  domain_map_ = ca[this_app_index_]->getMapT();
   assert(0 <= this_app_index && this_app_index < ca.size());
   assert(0 <= coupled_app_index && coupled_app_index < ca.size());
 }
