7a793dc7d61d0420f3b02ca6fa2e9b0b9717412a
Irina K. Tezaur <ikalash@sandia.gov>
2015-05-21 09:51:46 -0700

    QCAD: beginning conversion of QCAD coupled Poisson to Thyra/Tpetra.
    In QCAD::CoupledPSJacobianT class a thyra blocked operator will be
    constructed as the Jacobian for the coupled system.  This will enable using
    Teko for preconditioners; hence the QCAD::CoupledPSPreconditioner class will
    not be needed. Only the (0,0) block is filled in for now.
    
    QCAD::ImplicitPSJacobianT is a tentative class that would implement a Tpetra_Operator
    apply method for parts of the coupled Jacobian that do not have an explicit form as
    a CrsMatrix.  I suspect this may not be necessary, but need to check with Erik Nielsen.
    If it's necessary, it needs to be filled in.

---
+++ src/QCAD/QCAD_CoupledPSJacobianT.hpp
@@ -0,0 +1,48 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#ifndef QCAD_COUPLEDPSJACOBIANT_H
+#define QCAD_COUPLEDPSJACOBIANT_H
+
+#include <iostream>
+#include "Teuchos_Comm.hpp"
+#include "Tpetra_Map.hpp"
+#include "Tpetra_Vector.hpp"
+#include "Tpetra_Operator.hpp"
+#include "Tpetra_CrsMatrix.hpp"
+#include "Tpetra_Import.hpp"
+
+#include "Albany_DataTypes.hpp"
+
+#include "Teuchos_RCP.hpp"
+
+#include "Thyra_BlockedLinearOpBase.hpp"
+#include "Thyra_PhysicallyBlockedLinearOpBase.hpp"
+#include "QCAD_ImplicitPSJacobianT.hpp"
+
+namespace QCAD {
+
+/** 
+ *  \brief A class that evaluates the Jacobian of a
+ *  QCAD coupled Poisson-Schrodinger problem
+ */
+
+class CoupledPSJacobianT {
+public:
+  CoupledPSJacobianT(Teuchos::RCP<Teuchos_Comm const> const & commT);
+
+  ~CoupledPSJacobianT();
+
+  Teuchos::RCP<Thyra::LinearOpBase<ST>> getThyraCoupledJacobian(Teuchos::RCP<Tpetra_CrsMatrix>& Jac_Poisson) const;
+
+private:
+
+  Teuchos::RCP<Teuchos_Comm const>
+  commT_;
+};
+
+}
+#endif  
