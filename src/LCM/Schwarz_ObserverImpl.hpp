46827a0017a889a95b4b5fb9233fdb6dbf075fc7
Alejandro Mota <amota@sandia.gov>
2015-05-28 20:07:09 -0700

    LCM: Minor changes.

--- src/LCM/Schwarz_ObserverImpl.hpp
+++ src/LCM/Schwarz_ObserverImpl.hpp
@@ -3,29 +3,31 @@
 //    This Software is released under the BSD license detailed     //
 //    in the file "license.txt" in the top-level Albany directory  //
 //*****************************************************************//
-#ifndef SCHWARZ_OBSERVERIMPL_HPP
-#define SCHWARZ_OBSERVERIMPL_HPP
+#if !defined(LCM_Schwarz_ObserverImpl_hpp)
+#define LCM_Schwarz_ObserverImpl_hpp
 
 #include "Schwarz_StatelessObserverImpl.hpp"
 
 namespace LCM {
 
-class ObserverImpl : public StatelessObserverImpl {
+class ObserverImpl: public StatelessObserverImpl {
 public:
-  explicit ObserverImpl(Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application> > &apps);
+  explicit ObserverImpl(
+      Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application> > &apps);
 
   virtual void observeSolutionT(
-    double stamp, Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solutionT,
-    Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solution_dotT);
+      double stamp,
+      Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solutionT,
+      Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solution_dotT);
 
 private:
   ObserverImpl(const ObserverImpl&);
   ObserverImpl& operator=(const ObserverImpl&);
-protected:
-  int n_models_; 
+  protected:
+  int n_models_;
   Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application> > apps_;
 };
 
 } // namespace LCM
 
-#endif // SCHWARZ_OBSERVERIMPL_HPP
+#endif // LCM_Schwarz_ObserverImpl_hpp
