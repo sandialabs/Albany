b335a175969ea3d87c56eb973df9d05fb84c9a0f
Alejandro Mota <amota@sandia.gov>
2015-05-28 20:07:41 -0700

    LCM: Minor changes.

--- src/LCM/Schwarz_ObserverImpl.cpp
+++ src/LCM/Schwarz_ObserverImpl.cpp
@@ -17,32 +17,35 @@
 namespace LCM {
 
 ObserverImpl::
-ObserverImpl (Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application> > &apps) :
-  StatelessObserverImpl(apps) 
+ObserverImpl(Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application> > &apps) :
+    StatelessObserverImpl(apps)
 {
 #ifdef OUTPUT_TO_SCREEN
   std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
 #endif
-  n_models_  = apps.size();
-  apps_ = apps;  
+  n_models_ = apps.size();
+  apps_ = apps;
 }
 
-
 void ObserverImpl::observeSolutionT(
-  double stamp, Teuchos::Array<Teuchos::RCP<const Tpetra_Vector > > non_overlapped_solutionT,
-  Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solution_dotT)
+    double stamp,
+    Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solutionT,
+    Teuchos::Array<Teuchos::RCP<const Tpetra_Vector> > non_overlapped_solution_dotT)
 {
 #ifdef OUTPUT_TO_SCREEN
   std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
 #endif
-  for (int m=0; m<n_models_; m++) { 
-    apps_[m]->evaluateStateFieldManagerT(stamp, non_overlapped_solution_dotT[m].ptr(), 
-                                         Teuchos::null, *non_overlapped_solutionT[m]); 
-    apps_[m]->getStateMgr().updateStates(); 
+  for (int m = 0; m < n_models_; m++) {
+    apps_[m]->evaluateStateFieldManagerT(
+        stamp,
+        non_overlapped_solution_dotT[m].ptr(),
+        Teuchos::null,
+        *non_overlapped_solutionT[m]);
+    apps_[m]->getStateMgr().updateStates();
   }
   StatelessObserverImpl::observeSolutionT(stamp, non_overlapped_solutionT,
-                                          non_overlapped_solution_dotT);
-  
+      non_overlapped_solution_dotT);
+
 }
 
 } // namespace LCM
