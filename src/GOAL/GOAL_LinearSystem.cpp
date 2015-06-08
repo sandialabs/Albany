368e99a4c76b03a23b052029dbea9c2c78b30c86
Brian Granzow <brian.granzow@gmail.com>
2015-06-07 23:06:31 -0400

    GOAL: adding a linear system class
    responsible for managing tpetra vectors / matrices
    used in the linear solve for the adjoint problem.

---
+++ src/GOAL/GOAL_LinearSystem.cpp
@@ -0,0 +1,71 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include "GOAL_LinearSystem.hpp"
+#include "GOAL_Discretization.hpp"
+#include "PHAL_Workset.hpp"
+#include "Albany_AbstractPUMIDiscretization.hpp"
+
+using Teuchos::rcp;
+using Teuchos::RCP;
+
+namespace GOAL {
+
+LinearSystem::LinearSystem(RCP<Discretization>& d) :
+  disc(d)
+{
+  init();
+  fillSolution();
+}
+
+LinearSystem::~LinearSystem()
+{
+}
+
+void LinearSystem::init()
+{
+  RCP<Albany::AbstractPUMIDiscretization> d = disc->getPUMIDisc();
+  RCP<const Tpetra_Map> m = d->getMapT();
+  RCP<const Tpetra_Map> om = d->getOverlapMapT();
+  RCP<const Tpetra_CrsGraph> g = d->getJacobianGraphT();
+  RCP<const Tpetra_CrsGraph> og = d->getOverlapJacobianGraphT();
+  x = rcp( new Tpetra_Vector(m) );
+  xdot = rcp( new Tpetra_Vector(m) );
+  xdotdot = rcp( new Tpetra_Vector(m) );
+  rhs = rcp( new Tpetra_Vector(m) );
+  jac = rcp( new Tpetra_CrsMatrix(g) );
+  overlapX = rcp( new Tpetra_Vector(om) );
+  overlapXdot = rcp( new Tpetra_Vector(om) );
+  overlapXdotdot = rcp( new Tpetra_Vector(om) );
+  overlapRhs = rcp( new Tpetra_Vector(om) );
+  overlapJac = rcp( new Tpetra_CrsMatrix(og) );
+  importer = rcp(new Tpetra_Import(m,om));
+  exporter = rcp(new Tpetra_Export(om,m));
+}
+
+void LinearSystem::fillSolution()
+{
+  disc->fillSolution(x);
+  overlapX->doImport(*x,*importer,Tpetra::INSERT);
+}
+
+void LinearSystem::setWorksetSolutionInfo(PHAL::Workset& workset)
+{
+  workset.xT = overlapX;
+  workset.xdotT = overlapXdot;
+  workset.xdotdotT = overlapXdotdot;
+  workset.JacT = overlapJac;
+  workset.x_importerT = importer;
+  workset.comm = x->getMap()->getComm();
+}
+
+void LinearSystem::completeJacobianFill()
+{
+  jac->doExport(*(overlapJac),*(exporter),Tpetra::ADD);
+  jac->fillComplete();
+}
+
+}
