1a8eddf79056ed85a863c37b51ff51937253e3e7
Brian Granzow <brian.granzow@gmail.com>
2015-06-07 23:07:32 -0400

    GOAL: updating response function
    to reflect the changes brought by the addition
    of the the three classes:
    GOAL::Discretization
    GOAL::LinearSystem
    GOAL::FieldManagerBundle

--- src/GOAL/responses/GOAL_MechAdjResponse.hpp
+++ src/GOAL/responses/GOAL_MechAdjResponse.hpp
@@ -17,27 +17,19 @@
 
 namespace GOAL {
 
-using Teuchos::rcp;
-using Teuchos::RCP;
-using Teuchos::ArrayRCP;
-using Teuchos::rcpFromRef;
-using PHX::FieldManager;
-using PHAL::AlbanyTraits;
+struct ProblemBundle;
+class FieldManagerBundle;
 
-/** \brief residual only response function for mechanics adjoints
-  * \details solves an auxiliary adjoint problem based on a specified
-  * quantity of interest. this response function will only work with
-  * the AlbanyT executable */
 class MechAdjResponse :
   public Albany::ScalarResponseFunction
 {
   public:
 
     MechAdjResponse(
-        const RCP<Albany::Application>& app,
-        const RCP<Albany::AbstractProblem>& prob,
-        const RCP<Albany::StateManager>& sm,
-        const ArrayRCP<RCP<Albany::MeshSpecsStruct> >&  ms,
+        const Teuchos::RCP<Albany::Application>& app,
+        const Teuchos::RCP<Albany::AbstractProblem>& prob,
+        const Teuchos::RCP<Albany::StateManager>& sm,
+        const Teuchos::ArrayRCP<Teuchos::RCP<Albany::MeshSpecsStruct> >&  ms,
         Teuchos::ParameterList& rp);
 
     virtual ~MechAdjResponse();
@@ -54,47 +46,18 @@ class MechAdjResponse :
 
   private:
 
-    /* private to prohibit copying */
     MechAdjResponse(const MechAdjResponse&);
     MechAdjResponse& operator=(const MechAdjResponse&);
 
-    void createFieldManagers();
-    void initializeLinearSystem();
-    void setupWorkset(PHAL::Workset& workset);
-    void evaluateJac(PHAL::Workset& workset);
+    Teuchos::RCP<ProblemBundle> pb;
+    Teuchos::RCP<FieldManagerBundle> fmb;
 
-    Teuchos::ParameterList& params_;
-    RCP<Albany::Application> app_;
-    RCP<Albany::AbstractProblem> problem_;
-    RCP<Albany::StateManager> stateMgr_;
-    ArrayRCP<RCP<Albany::MeshSpecsStruct> > meshSpecs_;
-
-    ArrayRCP<RCP<FieldManager<AlbanyTraits> > > jfm_;
-    RCP<FieldManager<AlbanyTraits> > dfm_;
-
-    RCP<Tpetra_Export> exporter_;
-
-    RCP<const Tpetra_Map> map_;
-    RCP<const Tpetra_Map> overlapMap_;
-    RCP<const Tpetra_CrsGraph> graph_;
-    RCP<const Tpetra_CrsGraph> overlapGraph_;
-
-    RCP<Tpetra_Vector> qoi_;
-    RCP<Tpetra_Vector> overlapQoi_;
-    RCP<Tpetra_CrsMatrix> jac_;
-    RCP<Tpetra_CrsMatrix> overlapJac_;
-
-    RCP<Teuchos::FancyOStream> out_;
+    int evalCtr;
+    bool enrich;
+    bool writePHXGraphs;
+    bool writeLinearSystem;
 
   public:
-
-    /* notes:
-       (1) the following methods are meaningless for this response,
-       but must be implemented since they are pure virtual members
-       of the ScalarResponseFunction.
-       (2) trying to use the methods below would be a bad idea
-       (3) disregarding any sane style standard for the code below
-       so that this file isn't 8 million lines long */
 
     virtual void evaluateTangentT(const double alpha, const double beta, const double omega, const double current_time, bool sum_derivs, const Tpetra_Vector* xdot, const Tpetra_Vector* xdotdot, const Tpetra_Vector& x, const Teuchos::Array<ParamVec>& p, ParamVec* deriv_p, const Tpetra_MultiVector* Vxdot, const Tpetra_MultiVector* Vxdotdot, const Tpetra_MultiVector* Vx, const Tpetra_MultiVector* Vp, Tpetra_Vector* g, Tpetra_MultiVector* gx, Tpetra_MultiVector* gp) {}
     virtual void evaluateGradientT(const double current_time, const Tpetra_Vector* xdotT, const Tpetra_Vector* xdotdotT, const Tpetra_Vector& xT, const Teuchos::Array<ParamVec>& p, ParamVec* deriv_p, Tpetra_Vector* gT, Tpetra_MultiVector* dg_dxT, Tpetra_MultiVector* dg_dxdotT, Tpetra_MultiVector* dg_dxdotdotT, Tpetra_MultiVector* dg_dpT) {}
@@ -111,6 +74,7 @@ class MechAdjResponse :
     virtual void evaluateMPTangent(const double alpha, const double beta, const double omega, const double current_time, bool sum_derivs, const Stokhos::ProductEpetraVector* mp_xdot, const Stokhos::ProductEpetraVector* mp_xdotdot, const Stokhos::ProductEpetraVector& mp_x, const Teuchos::Array<ParamVec>& p, const Teuchos::Array<int>& mp_p_index, const Teuchos::Array< Teuchos::Array<MPType> >& mp_p_vals, ParamVec* deriv_p, const Epetra_MultiVector* Vx, const Epetra_MultiVector* Vxdot, const Epetra_MultiVector* Vxdotdot, const Epetra_MultiVector* Vp, Stokhos::ProductEpetraVector* mp_g, Stokhos::ProductEpetraMultiVector* mp_JV, Stokhos::ProductEpetraMultiVector* mp_gp) {}
     virtual void evaluateMPGradient( const double current_time, const Stokhos::ProductEpetraVector* mp_xdot, const Stokhos::ProductEpetraVector* mp_xdotdot, const Stokhos::ProductEpetraVector& mp_x, const Teuchos::Array<ParamVec>& p, const Teuchos::Array<int>& mp_p_index, const Teuchos::Array< Teuchos::Array<MPType> >& mp_p_vals, ParamVec* deriv_p, Stokhos::ProductEpetraVector* mp_g, Stokhos::ProductEpetraMultiVector* mp_dg_dx, Stokhos::ProductEpetraMultiVector* mp_dg_dxdot, Stokhos::ProductEpetraMultiVector* mp_dg_dxdotdot, Stokhos::ProductEpetraMultiVector* mp_dg_dp) {}
 #endif
+
 
 };
 
