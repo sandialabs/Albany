62497a3d408e861d55484c467f66b0e519f66137
Luca <luca.bertagna84@gmail.com>
2015-06-15 13:27:28 -0400

    Bug fix in FELIX::HydrologyMelting
    Wrong sign for the nonlinear term.

--- src/FELIX/evaluators/FELIX_HydrologyMelting_Def.hpp
+++ src/FELIX/evaluators/FELIX_HydrologyMelting_Def.hpp
@@ -13,12 +13,12 @@ namespace FELIX {
 template<typename EvalT, typename Traits>
 HydrologyMelting<EvalT, Traits>::HydrologyMelting (const Teuchos::ParameterList& p,
                                                    const Teuchos::RCP<Albany::Layouts>& dl) :
-  q         (p.get<std::string> ("Discharge QP Variable Name"), dl->qp_gradient),
-  gradPhi   (p.get<std::string> ("Hydraulic Potential Gradient QP Variable Name"), dl->qp_gradient),
-  u_b       (p.get<std::string> ("Sliding Velocity Norm QP Variable Name"), dl->qp_scalar),
-  beta      (p.get<std::string> ("Basal Friction Coefficient QP Variable Name"), dl->qp_scalar),
-  G         (p.get<std::string> ("Geothermal Heat Source QP Variable Name"), dl->qp_scalar),
-  m         (p.get<std::string> ("Melting Rate QP Variable Name"),dl->qp_scalar)
+  q       (p.get<std::string> ("Discharge QP Variable Name"), dl->qp_gradient),
+  gradPhi (p.get<std::string> ("Hydraulic Potential Gradient QP Variable Name"), dl->qp_gradient),
+  u_b     (p.get<std::string> ("Sliding Velocity Norm QP Variable Name"), dl->qp_scalar),
+  beta    (p.get<std::string> ("Basal Friction Coefficient QP Variable Name"), dl->qp_scalar),
+  G       (p.get<std::string> ("Geothermal Heat Source QP Variable Name"), dl->qp_scalar),
+  m       (p.get<std::string> ("Melting Rate QP Variable Name"),dl->qp_scalar)
 {
   this->addDependentField(q);
   this->addDependentField(gradPhi);
@@ -36,7 +36,11 @@ HydrologyMelting<EvalT, Traits>::HydrologyMelting (const Teuchos::ParameterList&
 
   mu_w = physical_params.get<double>("Water Viscosity");
   L    = physical_params.get<double>("Ice Latent Heat");
-
+/*
+  std::cout << "HydrologyMelting:\n"
+            << "    mu_w : " << mu_w << "\n"
+            << "    L    : " << L << "\n";
+*/
   std::vector<PHX::DataLayout::size_type> dims;
   dl->qp_gradient->dimensions(dims);
   numQPs = dims[1];
@@ -72,7 +76,7 @@ void HydrologyMelting<EvalT, Traits>::evaluateFields (typename Traits::EvalData 
       for (int dim(0); dim<numDim; ++dim)
         prod += q(cell,qp,dim)*gradPhi(cell,qp,dim);
 
-      m(cell,qp) = (G(cell,qp) - beta(cell,qp) * u_b(cell,qp)*u_b(cell,qp) + nonlin_coeff * prod) / L;
+      m(cell,qp) = (G(cell,qp) - beta(cell,qp) * u_b(cell,qp)*u_b(cell,qp) - nonlin_coeff * prod) / L;
     }
   }
 }
