80959d55c20f3e91b199a814c0a59a1acbbaef29
Corbett Battaile <Corbett Battaile>
2015-06-25 15:15:38 -0600

    LCM: CrystalPlasticity: Minor tweaks and informational messages added to ForceDispl_StressStrain.py script in examples/LCM/CrystalPlasticity/.

--- examples/LCM/CrystalPlasticity/ForceDispl_StressStrain.py
+++ examples/LCM/CrystalPlasticity/ForceDispl_StressStrain.py
@@ -23,6 +23,7 @@ sys.path.append(path_home + "/LCM/trilinos-install-gcc-release/bin")
 import exodus
 import string
 from math import log
+from math import log10
 
 if __name__ == "__main__":
 
@@ -62,90 +63,107 @@ if __name__ == "__main__":
         # print "\nERROR:  Failed to extract force_x data\n"
         # sys.exit(1)
     print
+
+    sys.stdout.write("Looking for displacement node variables...\n")
     displ_var_name = ["<NULL>" for x in range(3)]
     if 'displacement_x' in nodeVariableNames:
         displ_var_name[0] = "displacement_x"
         displ_var_name[1] = "displacement_y"
         displ_var_name[2] = "displacement_z"
-	stres_var_base    = "Cauchy_Stress_"
-	eqps__var_base    = "eqps_"
+        stres_var_base    = "Cauchy_Stress_"
+        eqps__var_base    = "eqps_"
         fem_code = "LCM"
     elif 'DISPLACEMENT_X' in nodeVariableNames:
         displ_var_name[0] = "DISPLACEMENT_X"
         displ_var_name[1] = "DISPLACEMENT_Y"
         displ_var_name[2] = "DISPLACEMENT_Z"
-	stres_var_base    = "CAUCHY_STRESS_"
-	eqps__var_base    = "EQPS_"
+        stres_var_base    = "CAUCHY_STRESS_"
+        eqps__var_base    = "EQPS_"
         fem_code = "LCM"
     elif 'DISPLX' in nodeVariableNames:
         displ_var_name[0] = "DISPLX"
         displ_var_name[1] = "DISPLY"
         displ_var_name[2] = "DISPLZ"
-	stres_var_base    = "SIG"
-	eqps__var_base    = "EQPS"
+        stres_var_base    = "SIG"
+        eqps__var_base    = "EQPS"
         fem_code = "Jas3D"
     else:
-        print "ERROR: Couldn't find displacement variable displacement_x or DISPLACEMENT_X or DISPLX."
+        print "  ERROR: Couldn't find displacement variable displacement_x or DISPLACEMENT_X or DISPLX."
+        print "  ERROR: ABORTING."
+        sys.stdout.write("Done.\n")
         sys.exit(1)
-    print "Found displacement variables " + displ_var_name[0] + ", " + displ_var_name[1] + ", " + displ_var_name[2]
+    print "  Found displacement variables " + displ_var_name[0] + ", " + displ_var_name[1] + ", " + displ_var_name[2]
+    sys.stdout.write("Done.\n")
     print "Assuming this output was created by " + fem_code
 
+    sys.stdout.write("Looking for force node variables...\n")
     force_var_name = ["<NULL>" for x in range(3)]
     if 'force_x' in nodeVariableNames:
         force_var_name[0] = "force_x"
         force_var_name[1] = "force_y"
         force_var_name[2] = "force_z"
-	flag_found_force = 1
+        flag_found_force = 1
     if 'FORCE_X' in nodeVariableNames:
         force_var_name[0] = "FORCE_X"
         force_var_name[1] = "FORCE_Y"
         force_var_name[2] = "FORCE_Z"
-	flag_found_force = 1
+        flag_found_force = 1
     elif 'FINTX' in nodeVariableNames:
         # Specifying FINT restricts us to displacement-controlled I think...
-	#    Load control would probably need to use FEXT.
+        #    Load control would probably need to use FEXT.
         force_var_name[0] = "FINTX"
         force_var_name[1] = "FINTY"
         force_var_name[2] = "FINTZ"
-	flag_found_force = 1
+        flag_found_force = 1
     else:
-        print "WARNING: Couldn't find force variables force_x or FORCE_X or FINTX."
+        print "  WARNING: Couldn't find force variables force_x or FORCE_X or FINTX."
         # sys.exit(1)
-	flag_found_force = 0
+        flag_found_force = 0
     if flag_found_force:
-        print "Found force variables " + force_var_name[0] + ", " + force_var_name[1] + ", " + force_var_name[2]
+        print "  Found force variables " + force_var_name[0] + ", " + force_var_name[1] + ", " + force_var_name[2]
+    sys.stdout.write("Done.\n")
 
+    sys.stdout.write("Looking for EQPS element variable(s)...\n")
     if 'eqps_1' in elemVariableNames:
-	flag_found_eqps = 1
+        flag_found_eqps = 1
+        print "  Found EQPS variable eqps_1"
     if 'EQPS_1' in elemVariableNames:
-	flag_found_eqps = 1
+        flag_found_eqps = 1
+        print "  Found EQPS variable EQPS_1"
     elif 'EQPS' in elemVariableNames:
-	flag_found_eqps = 1
+        flag_found_eqps = 1
+        print "  Found EQPS variable EQPS"
     else:
-        print "WARNING: Couldn't find EQPS variable(s)."
+        print "  WARNING: Couldn't find EQPS variable(s)."
         # sys.exit(1)
-	flag_found_eqps = 0
+        flag_found_eqps = 0
+    sys.stdout.write("Done.\n")
 
     # Read time
     timeVals = inFile.get_times()
 
     # Read node sets
+    sys.stdout.write("Loading node sets...")
     nodeSetIds = inFile.get_node_set_ids()
     nodeSetNodes = {}
     for nodeSetId in nodeSetIds:
         nodeIDs = inFile.get_node_set_nodes(nodeSetId)
         nodeSetNodes[nodeSetId] = nodeIDs[:]
+    sys.stdout.write("done.\n")
 
     # node_id_map = inFile.get_node_id_map()
     # for nodeID in range(numNodes):
     #     print str(node_id_map[nodeID])
+    sys.stdout.write("Loading node coordinates...")
     node_coord = {}
     for nodeNo in range(numNodes):
         nc = inFile.get_coord(nodeNo+1)
         node_coord[(nodeNo*3)+0] = nc[0]
         node_coord[(nodeNo*3)+1] = nc[1]
         node_coord[(nodeNo*3)+2] = nc[2]
+    sys.stdout.write("done.\n")
 
+    sys.stdout.write("Finding opposing nodeset faces...\n")
     # Matrix = [[0 for x in range(15)] for x in range(5)]
     #   THIS MAKES Matrix[5][15], ***NOT*** Matrix[15][5]!!!
     nodeset_delta_DIR = {}
@@ -154,9 +172,13 @@ if __name__ == "__main__":
     for nodeSetId in nodeSetIds:
         nodeSet = nodeSetNodes[nodeSetId]
         for d in range(0, 3):
-            nodeset_delta_DIR[d] = 0.0 
-        for nodeID_1 in nodeSet:
-            for nodeID_2 in nodeSet:
+            nodeset_delta_DIR[d] = 0.0
+        temp = int(log10(len(nodeSet)) - 3.0)
+        if temp < 0:
+            temp = 0
+        inc = 10**temp
+        for nodeID_1 in nodeSet[::inc]:
+            for nodeID_2 in nodeSet[::inc]:
                 for d in range(0, 3):
                     temp = node_coord[((nodeID_1-1)*3)+d] - node_coord[((nodeID_2-1)*3)+d]
                     nodeset_delta_DIR[d] += temp*temp
@@ -171,23 +193,25 @@ if __name__ == "__main__":
             dir_text = "Y"
         else:
             dir_text = "Z"
-        sys.stdout.write("Found nodeset IDs for " + dir_text + " direction:  ")
+        sys.stdout.write("  Found nodeset IDs for " + dir_text + " direction:  ")
         for i in range(num_nsets_DIR[d]):
             sys.stdout.write(str(nodeset_id_DIR_SID[d][i]) + " ")
         sys.stdout.write("\n")
         if num_nsets_DIR[d] > 2:
-            print "  WARNING: Will only use the first two nodesets in " + str(d) + "-direction!"
+            print "    WARNING: Will only use the first two nodesets in " + str(d) + "-direction!"
     flag_found_nsets_DIR = [1 for x in range(3)]
     for d in range(0, 3):
         if num_nsets_DIR[d] < 2:
-	    flag_found_nsets_DIR[d] = 0
+            flag_found_nsets_DIR[d] = 0
     print
     temp = 0
     for d in range(0, 3):
         temp += flag_found_nsets_DIR[d]
     if not temp:
-        print "ERROR: Couldn't find any opposing nodesets."
-	sys.exit(2)
+        print "  ERROR: Couldn't find any opposing nodesets."
+        sys.stdout.write("Done.\n")
+        sys.exit(2)
+    sys.stdout.write("Done.\n")
 
     # In this particular case, we want to plot force-displacement curves
     # where the forces and displacements are on a specific node set
@@ -196,18 +220,21 @@ if __name__ == "__main__":
 
     # outFileName = outFileLabel + 'force_displacement.txt'
     if flag_found_eqps:
-	eqps_header_text = "EQPS"
+        eqps_header_text = "EQPS"
     else:
         eqps_header_text = "<NULL>"
     if flag_found_nsets_DIR[0]:
-	dataFile_X = open(outFileName + "_X-dir.txt", 'w')
-	dataFile_X.write("# [1]Step  [2]Time  [3]" + str(displ_var_name[0]) + "  [4]" + str(force_var_name[0]) + "  [5]TrueStrain_Elastic  [6]TrueStrain_Plastic  [7]TrueStrain_Total  [8]AvgCauchyStress  [9]" + eqps_header_text + "  [10]" + eqps_header_text + "/TrueStrain_Plastic\n")
+        dataFile_X = open(outFileName + "_X-dir.txt", 'w')
+        dataFile_X.write("# [1]Step  [2]Time  [3]" + str(displ_var_name[0]) + "  [4]" + str(force_var_name[0]) + "  [5]TrueStrain_Elastic  [6]TrueStrain_Plastic  [7]TrueStrain_Total  [8]AvgCauchyStress  [9]" + eqps_header_text + "  [10]" + eqps_header_text + "/TrueStrain_Plastic\n")
+        print "Created file " + outFileName + "_X-dir.txt"
     if flag_found_nsets_DIR[1]:
-	dataFile_Y = open(outFileName + "_Y-dir.txt", 'w')
-	dataFile_Y.write("# [1]Step  [2]Time  [3]" + str(displ_var_name[1]) + "  [4]" + str(force_var_name[1]) + "  [5]TrueStrain_Elastic  [6]TrueStrain_Plastic  [7]TrueStrain_Total  [8]AvgCauchyStress  [9]" + eqps_header_text + "  [10]" + eqps_header_text + "/TrueStrain_Plastic\n")
+        dataFile_Y = open(outFileName + "_Y-dir.txt", 'w')
+        dataFile_Y.write("# [1]Step  [2]Time  [3]" + str(displ_var_name[1]) + "  [4]" + str(force_var_name[1]) + "  [5]TrueStrain_Elastic  [6]TrueStrain_Plastic  [7]TrueStrain_Total  [8]AvgCauchyStress  [9]" + eqps_header_text + "  [10]" + eqps_header_text + "/TrueStrain_Plastic\n")
+        print "Created file " + outFileName + "_Y-dir.txt"
     if flag_found_nsets_DIR[2]:
-	dataFile_Z = open(outFileName + "_Z-dir.txt", 'w')
-	dataFile_Z.write("# [1]Step  [2]Time  [3]" + str(displ_var_name[2]) + "  [4]" + str(force_var_name[2]) + "  [5]TrueStrain_Elastic  [6]TrueStrain_Plastic  [7]TrueStrain_Total  [8]AvgCauchyStress  [9]" + eqps_header_text + "  [10]" + eqps_header_text + "/TrueStrain_Plastic\n")
+        dataFile_Z = open(outFileName + "_Z-dir.txt", 'w')
+        dataFile_Z.write("# [1]Step  [2]Time  [3]" + str(displ_var_name[2]) + "  [4]" + str(force_var_name[2]) + "  [5]TrueStrain_Elastic  [6]TrueStrain_Plastic  [7]TrueStrain_Total  [8]AvgCauchyStress  [9]" + eqps_header_text + "  [10]" + eqps_header_text + "/TrueStrain_Plastic\n")
+        print "Created file " + outFileName + "_Z-dir.txt"
 
     elastic_modulus_DIR = [0.0 for x in range(3)]
     displ_DIR = [[0.0 for x in range(numNodes)] for x in range(3)]
@@ -215,29 +242,31 @@ if __name__ == "__main__":
 
     for timeStep in range(numTimeSteps):
 
+        sys.stdout.write("Computing and writing data for step " + str(timeStep+1) + " = time " + str(timeVals[timeStep]) + "...")
+
         eqps = 0.0
-	if flag_found_eqps:
+        if flag_found_eqps:
             if fem_code == "Jas3D":
-        	for elemNo in range(numElems):
+                for elemNo in range(numElems):
                     varName = eqps__var_base
                     temp = inFile.get_element_variable_values(elemNo+1, varName, timeStep+1)
                     eqps += temp[0]
-        	eqps /= numElems
+                eqps /= numElems
             elif fem_code == "LCM":
-        	for elemNo in range(numElems):
+                for elemNo in range(numElems):
                     for gaussPt in range(1, 9):
-                	varName = eqps__var_base + str(gaussPt)
-                	temp = inFile.get_element_variable_values(elemNo+1, varName, timeStep+1)
-                	eqps += temp[0]
-        	eqps /= 8.0*numElems
+                        varName = eqps__var_base + str(gaussPt)
+                        temp = inFile.get_element_variable_values(elemNo+1, varName, timeStep+1)
+                        eqps += temp[0]
+                eqps /= 8.0*numElems
 
         for d in range(0, 3):
 
             if not flag_found_nsets_DIR[d]:
-	        continue
+                continue
 
             displ_DIR[d] = inFile.get_node_variable_values(displ_var_name[d], timeStep+1)
-	    if flag_found_force:
+            if flag_found_force:
                 force_DIR[d] = inFile.get_node_variable_values(force_var_name[d], timeStep+1)
 
             for nodeNo in range(numNodes):
@@ -316,7 +345,9 @@ if __name__ == "__main__":
                 elastic_strain_2 = strain
                 elastic_stress_2 = stress
                 elastic_modulus_DIR[d]  = (elastic_stress_2 - elastic_stress_1)/(elastic_strain_2 - elastic_strain_1)
-                print "Found " + dir_text + "-direction elastic modulus = " + str(elastic_modulus_DIR[d])
+                sys.stdout.write("\n  Found " + dir_text + "-direction elastic modulus = " + str(elastic_modulus_DIR[d]))
+		if d == 2:
+		    sys.stdout.write("\n")
                 strain_elastic = stress/elastic_modulus_DIR[d]
                 strain_plastic = strain - strain_elastic
             else:
@@ -337,12 +368,17 @@ if __name__ == "__main__":
               dataFile = dataFile_Z
             dataFile.write(str(timeStep+1) + " " + str(timeVals[timeStep]) + " " + str(displ) + "  " + str(force) + "  " + str(strain_elastic) + "  " + str(strain_plastic) + "  " + str(strain) + "  " + str(stress) + "  " + str(eqps) + "  " + str(eqps_strain_ratio) + "\n")
 
+        if timeStep == 1:
+            sys.stdout.write("Done.\n")
+	else:
+            sys.stdout.write("done.\n")
+
     if flag_found_nsets_DIR[0]:
-	dataFile_X.close()
+        dataFile_X.close()
     if flag_found_nsets_DIR[1]:
-	dataFile_Y.close()
+        dataFile_Y.close()
     if flag_found_nsets_DIR[2]:
-	dataFile_Z.close()
+        dataFile_Z.close()
 
     print
     inFile.close()
