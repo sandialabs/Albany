468182056e2f2f253e6d3f07c567862d375111d4
David Littlewood <djlittl@sandia.gov>
2015-07-01 15:52:08 -0600

    Fixed typo that broke the build.

--- src/LCM/models/CrystalPlasticityModel.hpp
+++ src/LCM/models/CrystalPlasticityModel.hpp
@@ -34,7 +34,7 @@ public:
   // typedef for automatic differentiation type used in internal Newton loop
   // options are:  DFad (dynamically sized), SFad (static size), SLFad (bounded)
 //   typedef typename Sacado::Fad::DFad<ScalarT> Fad;
-  typedef typename Sacado::Fad::SLFad<ScalarT, 12> SLFad;
+  typedef typename Sacado::Fad::SLFad<ScalarT, 12> Fad;
 
   using ConstitutiveModel<EvalT, Traits>::num_dims_;
   using ConstitutiveModel<EvalT, Traits>::num_pts_;
