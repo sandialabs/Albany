45f43ceb6741b1b4528deb9c6c4d29484e3d1eeb
Irina K. Tezaur <ikalash@sandia.gov>
2015-07-02 13:50:41 -0700

    Adding CDash scripts.

---
+++ doc/dashboards/camobap.ca.sandia.gov/nightly_cron_script_albany32bit.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+cd /home/ikalash/Desktop/nightlyCDash
+
+rm -rf /home/ikalash/Desktop/nightlyCDash/repos
+rm -rf /home/ikalash/Desktop/nightlyCDash/build
+rm -rf /home/ikalash/Desktop/nightlyCDash/ctest_nightly.cmake.work
+rm -rf /home/ikalash/Desktop/nightlyCDash/nightly_log*
+rm -rf /home/ikalash/Desktop/nightlyCDash/results*
+
+cat albany32bit ctest_nightly.cmake.frag >& ctest_nightly.cmake  
+
+export PATH=$PATH:/usr/lib64/openmpi/bin:/home/ikalash/Install/ParaView-4.3.1-Linux-64bit/bin:/home/ikalash/Install:/home/ikalash/Install/Cubit:/home/ikalash/Install/R2015a/bin:/home/ikalash/Desktop/nightlyAlbanyTests/Results/Trilinos/build/install
+
+export LD_LIBRARY_PATH=/usr/lib64:/usr/lib64/openmpi/lib
+
+now=$(date +"%m_%d_%Y-%H_%M")
+#LOG_FILE=/projects/AppComp/nightly/cee-compute011/nightly_$now
+LOG_FILE=/home/ikalash/Desktop/nightlyCDash/nightly_log_32bit.txt
+
+eval "env  TEST_DIRECTORY=/home/ikalash/Desktop/nightlyCDash SCRIPT_DIRECTORY=/home/ikalash/Desktop/nightlyCDash ctest -VV -S /home/ikalash/Desktop/nightlyCDash/ctest_nightly.cmake" > $LOG_FILE 2>&1
+
+# Copy a basic installation to /projects/albany for those who like a nightly
+# build.
+#cp -r build/TrilinosInstall/* /projects/albany/trilinos/nightly/;
+#chmod -R a+X /projects/albany/trilinos/nightly;
+#chmod -R a+r /projects/albany/trilinos/nightly;
