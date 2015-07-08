998c786c72c01c2a0ba1524fd844f50e2c6f0c1f
Mauro Perego <mperego@sandia.gov>
2015-07-08 10:17:25 -0600

    FELIX, MPAS interface, make sure that "Time Step" is in Problem parameter list, or set it if it is not there.

--- src/FELIX/interface_with_mpas/Interface.cpp
+++ src/FELIX/interface_with_mpas/Interface.cpp
@@ -286,8 +286,10 @@ void velocity_solver_extrude_3d_grid(int nLayers, int nGlobalTriangles,
   paramList->sublist("Problem").sublist("Physical Parameters").set("Ice Density", MPAS_rho_ice);
   paramList->sublist("Problem").sublist("Physical Parameters").set("Water Density", MPAS_rho_seawater);
 
-  if (paramList->sublist("Problem").get<std::string>("Name") == "FELIX Coupled FO H 3D")
+  if (paramList->sublist("Problem").get<std::string>("Name") == "FELIX Coupled FO H 3D") {
     paramList->sublist("Problem").sublist("Parameter Fields").set("Register Surface Mass Balance", 1);
+    paramList->sublist("Problem").set("Time Step", paramList->sublist("Problem").get("Time Step", 0)); //if it is not there set it to zero.
+  }
 
   Teuchos::RCP<Teuchos::Array<double> >inputArrayBasal = Teuchos::rcp(new Teuchos::Array<double> (1, 1.0));
   paramList->sublist("Problem").sublist("Neumann BCs").set("Cubature Degree", 3);
