ea5f1ffefea2dc541c7e9978bad3b0b575d20ab2
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-07-08 18:43:23 -0400

    start interface to MeshSim Adapt

---
+++ src/adapt/AAdapt_SimAdapt.hpp
@@ -0,0 +1,19 @@
+#include "AAdapt_AbstractAdapterT.hpp"
+
+namespace AAdapt {
+
+struct SimAdaptImpl;
+
+class SimAdapt : public AbstractAdapterT {
+public:
+  SimAdapt(const Teuchos::RCP<Teuchos::ParameterList>& params_,
+           const Teuchos::RCP<ParamLib>& paramLib_,
+           const Albany::StateManager& StateMgr_,
+           const Teuchos::RCP<const Teuchos_Comm>& commT_);
+  virtual bool queryAdaptationCriteria(int iteration);
+  virtual bool adaptMesh(const Teuchos::RCP<const Tpetra_Vector>& solution,
+                         const Teuchos::RCP<const Tpetra_Vector>& ovlp_solution);
+  virtual Teuchos::RCP<const Teuchos::ParameterList> getValidAdapterParameters();
+};
+
+}
