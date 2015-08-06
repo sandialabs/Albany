54bfcbf799999a6748f9d22df39d622152766985
Alejandro Mota <amota@sandia.gov>
2015-08-06 16:42:12 -0700

    LCM: Skeleton for MiniNonlinearSolver framework. This is intended for the solution of **small** nonlinear systems, and therefore will depend heavily on MiniTensor so that it is amenable to Kokkoization. It is intended as the framework of choice in constitutive models and other situations such as optimization for bifurcation in material models. To start it will have Newton and a version of preconditioned conjugated gradient that we implemented in the Caltech code. It is initially introduced in Albany, but most probably will end up moving to MiniTensor once it is reasonably implemented, tested and debugged.

---
+++ src/LCM/utils/MiniNonlinearSolver.cc
@@ -0,0 +1,12 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include "MiniNonlinearSolver.h"
+
+namespace Intrepid
+{
+
+} // namespace Intrepid
---
+++ src/LCM/utils/MiniNonlinearSolver.h
@@ -0,0 +1,17 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#if !defined(LCM_MiniNonlinearSolver_h)
+#define LCM_MiniNonlinearSolver_h
+
+#include <Intrepid_MiniTensor.h>
+
+namespace Intrepid
+{
+
+} // namespace Intrepid
+
+#endif // LCM_MiniNonlinearSolver_h
