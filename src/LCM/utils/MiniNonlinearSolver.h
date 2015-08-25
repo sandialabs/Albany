02cfdac9f7c7683abf35edba14ce3e5fda1a1585
Alejandro Mota <amota@sandia.gov>
2015-08-24 18:30:46 -0700

    LCM: Working on mini trust-region solver.

--- src/LCM/utils/MiniNonlinearSolver.h
+++ src/LCM/utils/MiniNonlinearSolver.h
@@ -257,6 +257,265 @@ class NewtonSolver<PHAL::AlbanyTraits::MPTangent, Residual, N> :
 };
 #endif
 
+///
+/// TrustRegion Solver Base class. See Nocedal's algorithm 4.1.
+///
+template<typename EvalT, typename Residual,
+Intrepid::Index N = Intrepid::DYNAMIC>
+class TrustRegionSolver_Base
+{
+public:
+  using ScalarT = typename EvalT::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+
+  virtual
+  ~TrustRegionSolver_Base() {}
+
+  virtual
+  void
+  solve(Residual & residual, Intrepid::Vector<FadT, N> & x) {}
+
+  template <typename T>
+  void setMaximumNumberIterations(T && mni)
+  {max_num_iter_ = std::forward<T>(mni);}
+
+  Intrepid::Index
+  getMaximumNumberIterations()
+  {return max_num_iter_;}
+
+  Intrepid::Index
+  getNumberIterations()
+  {return num_iter_;}
+
+  template <typename T>
+  void setMaximumStepLength(T && msl)
+  {max_step_length_ = std::forward<T>(msl);}
+
+  ValueT
+  getMaximumStepLength() const
+  {return max_step_length_;}
+
+  template <typename T>
+  void setInitialStepLength(T && isl)
+  {initial_step_length_ = std::forward<T>(isl);}
+
+  ValueT
+  getInitialStepLength() const
+  {return initial_step_length_;}
+
+  template <typename T>
+  void setMinimumReduction(T && mr)
+  {min_reduction_ = std::forward<T>(mr);}
+
+  ValueT
+  getMinumumReduction() const
+  {return min_reduction_;}
+
+  template <typename T>
+  void setRelativeTolerance(T && rt)
+  {rel_tol_ = std::forward<T>(rt);}
+
+  ValueT
+  getRelativeTolerance() const
+  {return rel_tol_;}
+
+  ValueT
+  getRelativeError() const
+  {return rel_error_;}
+
+  template <typename T>
+  void setAbsoluteTolerance(T && at)
+  {abs_tol_ = std::forward<T>(at);}
+
+  ValueT
+  getAbsoluteTolerance() const
+  {return abs_tol_;}
+
+  ValueT
+  getAbsoluteError() const
+  {return abs_error_;}
+
+  bool
+  isConverged() const
+  {return converged_;}
+
+protected:
+  Intrepid::Index
+  max_num_iter_{128};
+
+  Intrepid::Index
+  num_iter_{0};
+
+  ValueT
+  max_step_length_{1.0};
+
+  ValueT
+  initial_step_length_{1.0};
+
+  ValueT
+  min_reduction_{0.25};
+
+  ValueT
+  rel_tol_{1.0e-10};
+
+  ValueT
+  rel_error_{1.0};
+
+  ValueT
+  abs_tol_{1.0e-10};
+
+  ValueT
+  abs_error_{1.0};
+
+  bool
+  converged_{false};
+};
+
+//
+// Specializations
+//
+template<typename EvalT, typename Residual,
+Intrepid::Index N = Intrepid::DYNAMIC>
+class TrustRegionSolver;
+
+//
+// Residual
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::Residual, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::Residual, Residual, N>
+{
+public:
+  using ScalarT = typename PHAL::AlbanyTraits::Residual::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+
+  void
+  solve(
+      Residual & residual,
+      Intrepid::Vector<FadT, N> & x) override;
+};
+
+//
+// Jacobian
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::Jacobian, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::Jacobian, Residual, N>
+{
+public:
+  using ScalarT = typename PHAL::AlbanyTraits::Jacobian::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+//
+// Tangent
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::Tangent, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::Tangent, Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::Tangent::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+//
+// Distribured Parameter Derivative
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::DistParamDeriv, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::DistParamDeriv,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::DistParamDeriv::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+#ifdef ALBANY_SG
+//
+// SGResidual
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::SGResidual, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::SGResidual,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::SGResidual::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+//
+// SGJacobian
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::SGJacobian, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::SGJacobian,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::SGJacobian::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+//
+// SGTangent
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::SGTangent, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::SGTangent,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::SGTangent::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+#endif
+
+#ifdef ALBANY_ENSEMBLE
+//
+// MPResidual
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::MPResidual, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::MPResidual,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::MPResidual::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+//
+// MPJacobian
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::MPJacobian, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::MPJacobian,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::MPJacobian::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+
+//
+// MPTangent
+//
+template<typename Residual, Intrepid::Index N>
+class TrustRegionSolver<PHAL::AlbanyTraits::MPTangent, Residual, N> :
+    public TrustRegionSolver_Base<PHAL::AlbanyTraits::MPTangent,
+    Residual, N>
+{
+  using ScalarT = typename PHAL::AlbanyTraits::MPTangent::ScalarT;
+  using ValueT = typename Sacado::ValueType<ScalarT>::type;
+  using FadT = typename Sacado::Fad::DFad<ValueT>;
+};
+#endif
 } //namesapce LCM
 
 #include "MiniNonlinearSolver.t.h"
--- src/LCM/utils/MiniNonlinearSolver.t.h
+++ src/LCM/utils/MiniNonlinearSolver.t.h
@@ -8,7 +8,7 @@ namespace LCM
 {
 
 //
-// Specializations
+// Specializations for Newton solver
 //
 
 //
@@ -26,9 +26,6 @@ solve(
 
   Intrepid::Vector<FadT, N>
   r = residual.compute(x);
-
-  Intrepid::Vector<ValueT, N>
-  x_val = Sacado::Value<Intrepid::Vector<FadT, N>>::eval(x);
 
   Intrepid::Vector<ValueT, N>
   r_val = Sacado::Value<Intrepid::Vector<FadT, N>>::eval(r);
@@ -127,4 +124,139 @@ solve(
 
 #endif // ALBANY_ENSEMBLE
 
+//
+// Specializations for trust-region solver
+//
+
+//
+// Residual
+//
+template<typename Residual, Intrepid::Index N>
+void
+TrustRegionSolver<PHAL::AlbanyTraits::Residual, Residual, N>::
+solve(
+    Residual & residual,
+    Intrepid::Vector<FadT, N> & x)
+{
+  Intrepid::Index const
+  dimension = x.get_dimension();
+
+  Intrepid::Vector<FadT, N>
+  r = residual.compute(x);
+
+  Intrepid::Vector<ValueT, N>
+  r_val = Sacado::Value<Intrepid::Vector<FadT, N>>::eval(r);
+
+  ValueT const
+  initial_norm = Intrepid::norm(r_val);
+
+  this->num_iter_ = 0;
+
+  this->converged_ = initial_norm <= this->abs_tol_;
+
+  while (this->converged_ == false) {
+
+    r = residual.compute(x);
+
+    r_val = Sacado::Value<Intrepid::Vector<FadT, N>>::eval(r);
+
+    this->abs_error_ = Intrepid::norm(r_val);
+
+    this->rel_error_ = this->abs_error_ / initial_norm;
+
+    bool const
+    converged_relative = this->rel_error_ <= this->rel_tol_;
+
+    bool const
+    converged_absolute = this->abs_error_ <= this->abs_tol_;
+
+    this->converged_ = converged_relative || converged_absolute;
+
+    bool const
+    is_max_iter = this->num_iter_ >= this->max_num_iter_;
+
+    bool const
+    end_solve = this->converged_ || is_max_iter;
+
+    if (end_solve == true) break;
+
+    Intrepid::Tensor<ValueT, N>
+    DrDx(dimension);
+
+    for (Intrepid::Index i{0}; i < dimension; ++i) {
+      for (Intrepid::Index j{0}; j < dimension; ++j) {
+        DrDx(i, j) = r(i).dx(j);
+      }
+    }
+
+    Intrepid::Vector<ValueT, N> const
+    p = Intrepid::solve(DrDx, r_val);
+
+    Intrepid::Vector<ValueT, N>
+    x_val = Sacado::Value<Intrepid::Vector<FadT, N>>::eval(x);
+
+    ValueT const
+    f_x = this->abs_error_;
+
+    Intrepid::Vector<ValueT, N> const
+    xp_val = x_val + p;
+
+    Intrepid::Vector<ValueT, N> const
+    rp_val = residual.compute(xp_val);
+
+    ValueT const
+    f_xp = Intrepid::norm(rp_val);
+
+    ValueT const
+    m_0 = f_x;
+
+    ValueT const
+    m_p = f_x;
+
+    ++this->num_iter_;
+  }
+
+  return;
+}
+
+//
+// Jacobian
+//
+
+//
+// Tangent
+//
+
+//
+// DistParamDeriv
+//
+
+#ifdef ALBANY_SG
+//
+// SGResidual
+//
+
+//
+// SGJacobian
+//
+
+//
+// SGTangent
+//
+#endif // ALBANY_SG
+
+#ifdef ALBANY_ENSEMBLE
+//
+// MPResidual
+//
+
+//
+// MPJacobian
+//
+
+//
+// MPTangent
+//
+
+#endif // ALBANY_ENSEMBLE
 } // namespace LCM
