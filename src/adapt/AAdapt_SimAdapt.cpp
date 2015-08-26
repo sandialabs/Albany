6aaa6b032eef477f5359a46343f7c51e8a30a1b0
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-08-26 09:34:24 -0400

    AMP: use SPR error estimator for adapt

--- src/adapt/AAdapt_SimAdapt.cpp
+++ src/adapt/AAdapt_SimAdapt.cpp
@@ -2,6 +2,7 @@
 #include "Albany_SimDiscretization.hpp"
 #include <MeshSimAdapt.h>
 #include <apfSIM.h>
+#include <spr.h>
 
 namespace AAdapt {
 
@@ -11,6 +12,7 @@ SimAdapt::SimAdapt(const Teuchos::RCP<Teuchos::ParameterList>& params_,
                    const Teuchos::RCP<const Teuchos_Comm>& commT_):
   AbstractAdapterT(params_, paramLib_, StateMgr_, commT_)
 {
+  errorBound = params_->get<double>("Error Bound", 0.1);
 }
 
 bool SimAdapt::queryAdaptationCriteria(int iteration)
@@ -21,7 +23,8 @@ bool SimAdapt::queryAdaptationCriteria(int iteration)
 bool SimAdapt::adaptMesh(const Teuchos::RCP<const Tpetra_Vector>& solution,
                          const Teuchos::RCP<const Tpetra_Vector>& ovlp_solution)
 {
-  Teuchos::RCP<Albany::AbstractDiscretization> disc = state_mgr_.getDiscretization();
+  Teuchos::RCP<Albany::AbstractDiscretization> disc =
+    state_mgr_.getDiscretization();
   Teuchos::RCP<Albany::SimDiscretization> sim_disc =
     Teuchos::rcp_dynamic_cast<Albany::SimDiscretization>(disc);
   Teuchos::RCP<Albany::APFMeshStruct> apf_ms =
@@ -31,9 +34,21 @@ bool SimAdapt::adaptMesh(const Teuchos::RCP<const Tpetra_Vector>& solution,
   bool should_transfer_ip_data = adapt_params_->get<bool>("Transfer IP Data", false);
   if (should_transfer_ip_data)
     sim_disc->attachQPData();
+  apf::Field* sol_fld = apf_m->findField(Albany::APFMeshStruct::solution_name);
+  apf::Field* grad_ip_fld = spr::getGradIPField(sol_fld, "grad_sol",
+      apf_ms->cubatureDegree);
+  apf::Field* size_fld = spr::getSPRSizeField(grad_ip_fld, errorBound);
+  apf::destroyField(grad_ip_fld);
   pParMesh sim_pm = apf_msim->getMesh();
   pMSAdapt adapter = MSA_new(sim_pm, 1);
-  /* TODO: set size field here */
+  apf::MeshEntity* v;
+  apf::MeshIterator* it = apf_m->begin(0);
+  while ((v = apf_m->iterate(it))) {
+    double size = apf::getScalar(size_fld, v, 0);
+    MSA_setVertexSize(adapter, (pVertex) v, size);
+  }
+  apf_m->end(it);
+  apf::destroyField(size_fld);
   MSA_adapt(adapter, NULL);
   MSA_delete(adapter);
   apf_m->verify();
@@ -48,6 +63,7 @@ Teuchos::RCP<const Teuchos::ParameterList> SimAdapt::getValidAdapterParameters()
   Teuchos::RCP<Teuchos::ParameterList> validPL =
     this->getGenericAdapterParams("ValidSimAdaptParams");
   validPL->set<bool>("Transfer IP Data", false, "Turn on solution transfer of integration point data");
+  validPL->set<double>("Error Bound", 0.1, "Max relative error for error-based adaptivity");
   return validPL;
 }
 
