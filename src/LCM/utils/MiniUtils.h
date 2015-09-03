ad28a6670bf314794b81ea23ffa3d64c1d909a1d
Alejandro Mota <amota@sandia.gov>
2015-09-03 13:42:30 -0700

    LCM: Working on seprating MiniTensor and Albany code from trust region and conjugate gradient methods.

--- src/LCM/utils/MiniUtils.h
+++ src/LCM/utils/MiniUtils.h
@@ -50,8 +50,7 @@ public:
   }
 
   virtual
-  ~NonlinearMethod_Base()
-  {}
+  ~NonlinearMethod_Base() {}
 
   virtual
   void
@@ -75,15 +74,15 @@ class NewtonMethod : public NonlinearMethod_Base<Residual, T, N>
 public:
 
   virtual
-  ~NewtonMethod()
-  {}
+  ~NewtonMethod() {}
 
   virtual
   void
   solve(Residual & residual, Intrepid::Vector<T, N> & x) override;
 
-  void setMaximumNumberIterations(T && mni)
-  {max_num_iter_ = std::forward<T>(mni);}
+  void
+  setMaximumNumberIterations(Intrepid::Index const mni)
+  {max_num_iter_ = mni;}
 
   Intrepid::Index
   getMaximumNumberIterations()
@@ -93,8 +92,9 @@ public:
   getNumberIterations()
   {return num_iter_;}
 
-  void setRelativeTolerance(T && rt)
-  {rel_tol_ = std::forward<T>(rt);}
+  void
+  setRelativeTolerance(T const rt)
+  {rel_tol_ = rt;}
 
   T
   getRelativeTolerance() const
@@ -104,8 +104,9 @@ public:
   getRelativeError() const
   {return rel_error_;}
 
-  void setAbsoluteTolerance(T && at)
-  {abs_tol_ = std::forward<T>(at);}
+  void
+  setAbsoluteTolerance(T const at)
+  {abs_tol_ = at;}
 
   T
   getAbsoluteTolerance() const
@@ -119,7 +120,7 @@ public:
   isConverged() const
   {return converged_;}
 
-protected:
+private:
   Intrepid::Index
   max_num_iter_{128};
 
@@ -142,12 +143,260 @@ protected:
   converged_{false};
 };
 
-//
-// Deal with derivative information for all the mini solvers.
-// Call this when a converged solution is obtained on a system that is
-// typed on a FAD type.
-// Assuming that T is a FAD type and S is a simple type.
-//
+///
+/// Trust Region method class.  See Nocedal's algorithm 11.5.
+///
+template<typename Residual, typename T, Intrepid::Index N = Intrepid::DYNAMIC>
+class TrustRegionMethod : public NonlinearMethod_Base<Residual, T, N>
+{
+public:
+
+  virtual
+  ~TrustRegionMethod() {}
+
+  virtual
+  void
+  solve(Residual & residual, Intrepid::Vector<T, N> & x) override;
+
+  void
+  setMaximumNumberIterations(Intrepid::Index const mni)
+  {max_num_iter_ = mni;}
+
+  Intrepid::Index
+  getMaximumNumberIterations()
+  {return max_num_iter_;}
+
+  void
+  setMaximumNumberRestrictIterations(T const mntri)
+  {max_num_restrict_iter_ = mntri;}
+
+  Intrepid::Index
+  getMaximumNumberRestrictIterations()
+  {return max_num_restrict_iter_;}
+
+  Intrepid::Index
+  getNumberIterations()
+  {return num_iter_;}
+
+  void
+  setMaximumStepLength(T const msl)
+  {max_step_length_ = msl;}
+
+  T
+  getMaximumStepLength() const
+  {return max_step_length_;}
+
+  void
+  setInitialStepLength(T const isl)
+  {initial_step_length_ = isl;}
+
+  T
+  getInitialStepLength() const
+  {return initial_step_length_;}
+
+  void
+  setMinimumReduction(T const mr)
+  {min_reduction_ = mr;}
+
+  T
+  getMinumumReduction() const
+  {return min_reduction_;}
+
+  void
+  setRelativeTolerance(T const rt)
+  {rel_tol_ = rt;}
+
+  T
+  getRelativeTolerance() const
+  {return rel_tol_;}
+
+  T
+  getRelativeError() const
+  {return rel_error_;}
+
+  void
+  setAbsoluteTolerance(T const at)
+  {abs_tol_ = at;}
+
+  T
+  getAbsoluteTolerance() const
+  {return abs_tol_;}
+
+  T
+  getAbsoluteError() const
+  {return abs_error_;}
+
+  bool
+  isConverged() const
+  {return converged_;}
+
+private:
+  Intrepid::Index
+  max_num_iter_{128};
+
+  Intrepid::Index
+  max_num_restrict_iter_{4};
+
+  Intrepid::Index
+  num_iter_{0};
+
+  T
+  max_step_length_{1.0};
+
+  T
+  initial_step_length_{1.0};
+
+  T
+  min_reduction_{0.25};
+
+  T
+  rel_tol_{1.0e-10};
+
+  T
+  rel_error_{1.0};
+
+  T
+  abs_tol_{1.0e-10};
+
+  T
+  abs_error_{1.0};
+
+  bool
+  converged_{false};
+};
+
+///
+/// Conjugate Gradient Method class.
+/// For now the Gram-Schmidt method is fixed to Polak-Ribiere
+/// and preconditioning with the Hessian.
+/// This is taken from J.R. Shewchuck "painless" conjugate gradient
+/// manuscript that is all over the place on the net.
+///
+template<typename Residual, typename T, Intrepid::Index N = Intrepid::DYNAMIC>
+class ConjugateGradientMethod : public NonlinearMethod_Base<Residual, T, N>
+{
+public:
+
+  virtual
+  ~ConjugateGradientMethod() {}
+
+  virtual
+  void
+  solve(Residual & residual, Intrepid::Vector<T, N> & x) override;
+
+  void
+  setMaximumNumberIterations(Intrepid::Index const mni)
+  {max_num_iter_ = mni;}
+
+  Intrepid::Index
+  getMaximumNumberIterations()
+  {return max_num_iter_;}
+
+  T
+  getInitialSecantStepLength() const
+  {return initial_secant_step_length_;}
+
+  void
+  setInitialSecantStepLength(T const issl)
+  {initial_secant_step_length_ = issl;}
+
+  void
+  setMaximumNumberSecantIterations(T const mntri)
+  {max_num_secant_iter_ = mntri;}
+
+  Intrepid::Index
+  getMaximumNumberSecantIterations()
+  {return max_num_secant_iter_;}
+
+  void
+  setSecantTolerance(T const st)
+  {secant_tol_ = st;}
+
+  T
+  getSecantTolerance() const
+  {return secant_tol_;}
+
+  Intrepid::Index
+  getNumberIterations()
+  {return num_iter_;}
+
+  void
+  setRestartDirectionsInterval(Intrepid::Index const rdi)
+  {restart_directions_interval_ = rdi;}
+
+  Intrepid::Index
+  getRestartDirectionsInterval() const
+  {return restart_directions_interval_;}
+
+  void
+  setRelativeTolerance(T const rt)
+  {rel_tol_ = rt;}
+
+  T
+  getRelativeTolerance() const
+  {return rel_tol_;}
+
+  T
+  getRelativeError() const
+  {return rel_error_;}
+
+  void
+  setAbsoluteTolerance(T const at)
+  {abs_tol_ = at;}
+
+  T
+  getAbsoluteTolerance() const
+  {return abs_tol_;}
+
+  T
+  getAbsoluteError() const
+  {return abs_error_;}
+
+  bool
+  isConverged() const
+  {return converged_;}
+
+private:
+  Intrepid::Index
+  max_num_iter_{128};
+
+  Intrepid::Index
+  max_num_secant_iter_{16};
+
+  Intrepid::Index
+  num_iter_{0};
+
+  Intrepid::Index
+  restart_directions_interval_{32};
+
+  T
+  initial_secant_step_length_{0.9};
+
+  T
+  secant_tol_{1.0e-6};
+
+  T
+  rel_tol_{1.0e-10};
+
+  T
+  rel_error_{1.0};
+
+  T
+  abs_tol_{1.0e-10};
+
+  T
+  abs_error_{1.0};
+
+  bool
+  converged_{false};
+};
+
+///
+/// Deal with derivative information for all the mini solvers.
+/// Call this when a converged solution is obtained on a system that is
+/// typed on a FAD type.
+/// Assuming that T is a FAD type and S is a simple type.
+///
 template<typename T, typename S, Intrepid::Index N>
 void
 computeFADInfo(
