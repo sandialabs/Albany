125b44374dedf5396a27bfe53b902ead7c43b070
Irina K. Tezaur <ikalash@sandia.gov>
2015-10-27 10:05:34 -0700

    Modifying some nightly test scripts for Irina T.'s machine.

--- doc/dashboards/camobap.ca.sandia.gov/send_email_64bit.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_64bit.sh
@@ -2,10 +2,12 @@
 
 #source $1 
 
-TTT=`grep "tests failed" /home/ikalash/Desktop/nightlyCDash/nightly_log_64bit.txt`
+TTT=`grep "(Failed)" /home/ikalash/nightlyCDash/nightly_log_64bit.txt -c`
+TTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_log_64bit.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "Albany (master, 64bit): $TTT" "ikalash@sandia.gov" < /home/ikalash/Desktop/nightlyCDash/results_64bit
-/bin/mail -s "Albany (master, 64bit): $TTT" "agsalin@sandia.gov" < /home/ikalash/Desktop/nightlyCDash/results_64bit
-/bin/mail -s "Albany (master, 64bit): $TTT" "gahanse@sandia.gov" < /home/ikalash/Desktop/nightlyCDash/results_64bit
-/bin/mail -s "Albany (master, 64bit): $TTT" "ambradl@sandia.gov" < /home/ikalash/Desktop/nightlyCDash/results_64bit
+/bin/mail -s "Albany (master, 64bit): $TTT tests failed, $TTTT tests not run" "ikalash@sandia.gov" < /home/ikalash/nightlyCDash/results_64bit
+/bin/mail -s "Albany (master, 64bit): $TTT tests failed, $TTTT tests not run" "agsalin@sandia.gov" < /home/ikalash/nightlyCDash/results_64bit
+/bin/mail -s "Albany (master, 64bit): $TTT tests failed, $TTTT tests not run" "gahanse@sandia.gov" < /home/ikalash/nightlyCDash/results_64bit
+/bin/mail -s "Albany (master, 64bit): $TTT tests failed, $TTTT tests not run" "ambradl@sandia.gov" < /home/ikalash/nightlyCDash/results_64bit
+/bin/mail -s "Albany (master, 64bit): $TTT tests failed, $TTTT tests not run" "ipdemes@sandia.gov" < /home/ikalash/nightlyCDash/results_64bit
