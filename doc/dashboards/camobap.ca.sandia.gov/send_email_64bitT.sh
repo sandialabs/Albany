62e0b7489d4c83cd538821301d0c4d26ea24f9e2
Irina K. Tezaur <ikalash@sandia.gov>
2015-10-28 10:36:22 -0700

    Fixing send_email scripts for Irina T.'s nightly tests.

--- doc/dashboards/camobap.ca.sandia.gov/send_email_64bitT.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_64bitT.sh
@@ -3,7 +3,7 @@
 #source $1 
 
 TTT=`grep "(Failed)" /home/ikalash/nightlyCDash/nightly_log_64bitT.txt -c`
-TTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_log_64bitT.txt -c`
+TTTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_log_64bitT.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
 /bin/mail -s "Albany (master, 64bit, AlbanyT only): $TTT tests failed, $TTTT tests not run" "ikalash@sandia.gov" < /home/ikalash/nightlyCDash/results_64bitT
