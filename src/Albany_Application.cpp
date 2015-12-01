53f0509a4572328252de7480f5c1fcd067a4ddd9
ipdemes <ipdemes@sandia.gov>
2015-12-01 15:42:21 -0700

    Added Jacobian_deriv_dims and Tangen_deriv_dims arres to the Workset in purpose to store derivwtive dimention information there.

--- src/Albany_Application.cpp
+++ src/Albany_Application.cpp
@@ -1271,6 +1271,13 @@ computeGlobalJacobianImplT(const double alpha,
     workset.JacT      = overlapped_jacT;
     loadWorksetJacobianInfo(workset, alpha, beta, omega);
 
+   //fill Jacobian derivative dimensions:
+   for (int ps=0; ps < fm.size(); ps++){
+      (workset.Jacobian_deriv_dims).push_back(
+        PHAL::getDerivativeDimensions<PHAL::AlbanyTraits::Jacobian>(this, ps));
+   }
+
+
     for (int ws=0; ws < numWorksets; ws++) {
       loadWorksetBucketInfo<PHAL::AlbanyTraits::Jacobian>(workset, ws);
 
@@ -1782,6 +1789,12 @@ for (unsigned int i=0; i<shapeParams.size(); i++) *out << shapeParams[i] << "  "
       if (nfm!=Teuchos::null)
         deref_nfm(nfm, wsPhysIndex, ws)->evaluateFields<PHAL::AlbanyTraits::Tangent>(workset);
     }
+
+   //fill Tangent derivative dimensions
+   for (int ps=0; ps < fm.size(); ps++){
+      (workset.Tangent_deriv_dims).push_back(
+        PHAL::getDerivativeDimensions<PHAL::AlbanyTraits::Tangent>(this, ps));
+   }
   }
 
   params = Teuchos::null;
