871ef983b8d82b4d2d701f8368793eaad83947ab
Brian Granzow <brian.granzow@gmail.com>
2015-12-04 18:35:10 -0500

    GOAL: draft of advection diffusion problem

---
+++ src/GOAL/evaluators/GOAL_AdvDiffResidual.hpp
@@ -0,0 +1,62 @@
+//*****************************************************************//
+//    Albany 2.0:  Copyright 2012 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#ifndef GOAL_ADVDIFFRESIDUAL_HPP
+#define GOAL_ADVDIFFRESIDUAL_HPP
+
+#include "Phalanx_config.hpp"
+#include "Phalanx_Evaluator_WithBaseImpl.hpp"
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
+
+#include "Albany_Layouts.hpp"
+
+namespace GOAL {
+
+template<typename EvalT, typename Traits>
+class AdvDiffResidual :
+  public PHX::EvaluatorWithBaseImpl<Traits>,
+  public PHX::EvaluatorDerived<EvalT, Traits>
+{
+
+  public:
+
+    AdvDiffResidual(
+        const Teuchos::ParameterList& p,
+        const Teuchos::RCP<Albany::Layouts>& dl);
+
+    void postRegistrationSetup(
+        typename Traits::SetupData d,
+        PHX::FieldManager<Traits>& vm);
+
+    void evaluateFields(typename Traits::EvalData d);
+
+  private:
+
+    typedef typename EvalT::ScalarT ScalarT;
+    typedef typename EvalT::MeshScalarT MeshScalarT;
+
+    double k;
+    double a[3];
+
+    int numDims;
+    int numNodes;
+    int numQPs;
+
+    // Input
+    PHX::MDField<ScalarT,Cell,QuadPoint> u;
+    PHX::MDField<ScalarT,Cell,QuadPoint,Dim> gradU;
+    PHX::MDField<MeshScalarT,Cell,Node,QuadPoint> wBF;
+    PHX::MDField<MeshScalarT,Cell,Node,QuadPoint,Dim> wGradBF;
+
+    // Output
+    PHX::MDField<MeshScalarT,Cell,Node> residual;
+
+};
+
+}
+
+#endif
