77e30ef10ca5e9bb20388a86dc3f1f6446873f64
Coleman Alleman <callema@sandia.gov>
2015-12-08 11:14:09 -0800

    LCM: Added stress calulcation to postprocessor

--- src/LCM/utils/python/LCM_postprocess.py
+++ src/LCM/utils/python/LCM_postprocess.py
@@ -15,59 +15,79 @@ import matplotlib.pyplot as plt
 inFileName = sys.argv[1]
 outFileName = sys.argv[2]
 
+
+# set i/o units ------------------------------------------------------------------------------------
+
 inFile = exodus(inFileName,"r")
 if os.path.isfile(outFileName):
   cmdLine = "rm %s" % outFileName
   os.system(cmdLine)
 outFile = copy_mesh(inFileName, outFileName)
 
-# get number of dimensions
+
+# get number of dimensions -------------------------------------------------------------------------
 num_dims = inFile.num_dimensions()
 
-print "Dimensions?"
+print "Dimensions"
 print num_dims
 
-# get times
+
+# get times ----------------------------------------------------------------------------------------
 times = inFile.get_times()
 print "Print the times"
-for time in times:
-  print time
+print times[0], times[-1]
 
-# get list of nodal variables
+
+# get list of nodal variables ----------------------------------------------------------------------
 node_var_names= inFile.get_node_variable_names()
 print "Printing the names of the nodal variables"
-for name in node_var_names:
-    print name
+print node_var_names
 
-# get list of element variables
-elem_var_names= inFile.get_element_variable_names()
+
+# get list of element variables --------------------------------------------------------------------
+elem_var_names = inFile.get_element_variable_names()
+num_variables_unique = 0
+names_variable_unique = []
 print "Printing the names of the element variables"
 for name in elem_var_names:
-    print name
+    new_name = True
+    for name_unique in names_variable_unique:
+      if (name.startswith(name_unique+"_")):
+        new_name = False
+    if (new_name == True):
+      indices = [int(s) for s in name if s.isdigit()]
+      names_variable_unique.append(name[0:name.find(str(indices[0]))-1])
+print names_variable_unique
 
-# create any global variables
+
+# get number of element blocks and bloc ids --------------------------------------------------------
+block_ids = inFile.get_elem_blk_ids()
+num_blocks = inFile.num_blks()
 
 
-# figure out number of integration points
-#print dir(elem_var_names[0])
-# check that "Weights" exist as an element variable
 
+
+# create any global variables ----------------------------------------------------------------------
+
+
+# figure out number of integration points ----------------------------------------------------------
 num_points = 0
 for name in elem_var_names:
   if (name.startswith("Weights_")):
       num_points += 1
 
+# check that "Weights" exist as an element variable ------------------------------------------------
 if (num_points == 0):
   raise Exception("The weights field is not available...try again.")
       
-print "Number of Integration points?"
+print "Number of Integration points"
 print num_points
 
-# write times to outFile
+# write times to outFile ---------------------------------------------------------------------------
 for step in range(len(times)):
   outFile.put_time(step+1,times[step])
 
-# write out displacement vector
+# write out displacement vector --------------------------------------------------------------------
 dx = []
 dy = []
 dz = []
@@ -78,13 +98,39 @@ outFile.put_node_variable_name('displacement_y', 2)
 outFile.put_node_variable_name('displacement_z', 3)
 
 for step in range(len(times)):
-  dx = inFile.get_node_variable_values('disp_x',step+1)
-  dy = inFile.get_node_variable_values('disp_y',step+1)
-  dz = inFile.get_node_variable_values('disp_z',step+1)
+
+  dx = inFile.get_node_variable_values('displacement_x',step+1)
+  dy = inFile.get_node_variable_values('displacement_y',step+1)
+  dz = inFile.get_node_variable_values('displacement_z',step+1)
+
   outFile.put_node_variable_values('displacement_x',step+1,dx)
   outFile.put_node_variable_values('displacement_y',step+1,dy)
   outFile.put_node_variable_values('displacement_z',step+1,dz)
-#
+
+
+# outFile.close()
+# sys.exit()
+
+outFile.set_element_variable_number(int(1))
+outFile.put_element_variable_name('stress_cauchy_11', 1)
+
+for name in names_variable_unique:
+  if (name == 'Cauchy_Stress'):
+    for step in range(len(times)):
+      for block in block_ids:
+        stress_cauchy_11 = [x*y for (x,y) in 
+          zip(inFile.get_element_variable_values(block,'Cauchy_Stress_01',step+1), 
+            inFile.get_element_variable_values(block,'Weights_1',step+1))]
+
+        for point in range(1,num_points):
+
+          stress_cauchy_11 = [x+y*z for (x,y,z) in 
+            zip(
+              stress_cauchy_11,
+              inFile.get_element_variable_values(block,'Cauchy_Stress_' + str(point*num_dims**2+1),step+1), 
+              inFile.get_element_variable_values(block,'Weights_'+str(point),step+1))]
+        
+        outFile.put_element_variable_values(block,'stress_cauchy_11',step+1,stress_cauchy_11)
 
 # 
 
