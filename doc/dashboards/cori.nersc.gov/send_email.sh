56c53e36aa06d60ae1b0b1ddcb6c4c2516fc3798
Irina K. Tezaur <ikalash@sandia.gov>
2016-01-07 13:47:42 -0800

    FELIX: checking in cronjob script and fixing path in send_email.sh script for Cori FELIX nightly test cronjob.

--- doc/dashboards/cori.nersc.gov/send_email.sh
+++ doc/dashboards/cori.nersc.gov/send_email.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 
-BASE_DIR=/global/homes/i/ikalash/nightlyCoriCDash
+BASE_DIR=/project/projectdirs/piscees/nightlyCoriCDash
 
-/bin/mail -s "Cori nightly test results" "ikalash@sandia.gov, agsalin@sandia.gov" -F "Irina Tezaur"  < $BASE_DIR/test_summary.txt
+/usr/bin/mail -s "Cori nightly test results" "ikalash@sandia.gov, agsalin@sandia.gov" -F "Irina Tezaur"  < $BASE_DIR/test_summary.txt
