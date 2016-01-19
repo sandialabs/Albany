c2914f4ed96fbf99bbaf942c5ac1d82e3dd2eeee
Jay Foulk <jwfoulk@sandia.gov>
2016-01-19 06:49:43 -0800

    LCM: Cleaning up plotting script

--- examples/LCM/EquilibriumConcentrationBC/plotEqConcentration.py
+++ examples/LCM/EquilibriumConcentrationBC/plotEqConcentration.py
@@ -3,7 +3,7 @@ import exodus
 import numpy
 import matplotlib.pyplot as plt
 
-file_name = "test.e"
+file_name = "EqConcentrationBC.e"
 exo_file = exodus.exodus(file_name,"r")
 inp_var_name = "tauH"
 dep_var_name = "CL"
@@ -21,6 +21,8 @@ dep_var = numpy.zeros(shape=(n_steps,1))
 check = numpy.zeros(shape=(n_steps,1))
 error = numpy.zeros(shape=(n_steps,1))
 
+## Given the pressure, calculate the concentration
+## C_{L} = C_{L,app} * exp(V_{H}*\tau_{h}/R/T)
 ## Material parameters for steel
 partial_molar_volume = 2.0
 ideal_gas_constant = 0.008314
@@ -34,7 +36,11 @@ for i in range(n_steps):
     dep_var[i] = dep_values[node_id-1]
     check[i] = cl_applied*numpy.exp(partial_molar_volume*ind_var[i]
                /ideal_gas_constant/temperature)
-    error[i] = numpy.log10(numpy.abs(check[i]-dep_var[i]))
+    tmp = numpy.abs(check[i]-dep_var[i])
+    if tmp > 0.0:
+        error[i] = numpy.log10(numpy.abs(check[i]-dep_var[i]))
+    else:
+        error[i] = 0.0
 
 fig, ax = plt.subplots()
 ax.plot(ind_var[:],dep_var[:],color='blue',marker='o',label=file_name)
@@ -48,7 +54,7 @@ plt.show()
 fig.savefig(output_file_name)
 
 fig, ax = plt.subplots()
-ax.plot(ind_var[:],error[:],color='blue',marker='o',label='error')
+ax.plot(ind_var[1:],error[1:],color='blue',marker='o',label='error')
 plt.xlabel(inp_var_name)
 plt.ylabel('log10(|CL - analytical_solution|)')
 lg = plt.legend(loc = 4)
