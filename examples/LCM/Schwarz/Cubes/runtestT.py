1d6f7f07fabadc3fe49a9d23662d042c1326887c
Irina K. Tezaur <ikalash@sandia.gov>
2016-01-29 13:56:19 -0800

    LCM: fixing BC specification in Cubes problem.

--- examples/LCM/Schwarz/Cubes/runtestT.py
+++ examples/LCM/Schwarz/Cubes/runtestT.py
@@ -20,7 +20,7 @@ logfile = open(log_file_name, 'w')
 
 #specify tolerance to determine test failure / passing
 tolerance = 1.0e-9; 
-meanvalue = 0.00118324368593;
+meanvalue = 0.000600364739979;
 
 # run AlbanyT 
 command = ["./AlbanyT", "cubes.xml"]
