30957323176a53bca2813bdcaeac8d9d1f3229ee
Bill Spotz <wfspotz@sandia.gov>
2016-03-09 11:20:48 -0700

    Update Gaia CDash scripts
    ctest_nightly.cmake.frag
        Changed references to edison to gaia; commented out PISCES
        instructions; MPI_DIR, BOOST_DIR and NETCDF_DIR set to the new
        Modules location; added HDF5_DIR, set to Modules location; turned
        off Fortran support; moved Trilinos_ENABLE_ROL to Trilinos package
        enable section.
    
    gaia_modules.sh
        Changed references to edison to gaia; utilize new Modules
        locations
    
    nightly_cron_script_trilinos_gaia.sh
        Added -j 12 to invocation of ctest (doesn't propagate to
        invocation of make)

--- doc/dashboards/gaia.sandia.gov/nightly_cron_script_trilinos_gaia.sh
+++ doc/dashboards/gaia.sandia.gov/nightly_cron_script_trilinos_gaia.sh
@@ -17,5 +17,7 @@ cat trilinosAeras ctest_nightly.cmake.frag >& ctest_nightly.cmake
 now=$(date +"%m_%d_%Y-%H_%M")
 LOG_FILE=$BASE_DIR/nightly_log_gaiaTrilinos.txt
 
-eval "env  TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR ctest -VV -S $BASE_DIR/ctest_nightly.cmake" > $LOG_FILE 2>&1
+JOBS=12
+
+eval "env  TEST_DIRECTORY=$BASE_DIR SCRIPT_DIRECTORY=$BASE_DIR ctest -VV -j $JOBS -S $BASE_DIR/ctest_nightly.cmake" > $LOG_FILE 2>&1
 
