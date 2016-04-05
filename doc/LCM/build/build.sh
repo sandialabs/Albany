fe2b0339326ff6847e4a4d9885645a4e092213ea
Alejandro Mota <amota@sandia.gov>
2016-04-04 17:40:41 -0700

    LCM: Add NDEBUG flag to more builds. Clean up in SolidMechanics (WIP).
    Pair programming with Coleman.

--- doc/LCM/build/build.sh
+++ doc/LCM/build/build.sh
@@ -130,11 +130,11 @@ case "$SCRIPT_NAME" in
 		;;
 	    profile)
 		sed -i -e "s|lcm_fpe_switch|OFF|g;" "$CONFIG_FILE"
-		sed -i -e "s|lcm_cxx_flags||g;" "$CONFIG_FILE"
+		sed -i -e "s|lcm_cxx_flags|-DNDEBUG|g;" "$CONFIG_FILE"
 		;;
 	    small)
 		sed -i -e "s|lcm_fpe_switch|OFF|g;" "$CONFIG_FILE"
-		sed -i -e "s|lcm_cxx_flags||g;" "$CONFIG_FILE"
+		sed -i -e "s|lcm_cxx_flags|-DNDEBUG|g;" "$CONFIG_FILE"
 		;;
             mixed)
 		sed -i -e "s|lcm_fpe_switch|ON|g;" "$CONFIG_FILE"
