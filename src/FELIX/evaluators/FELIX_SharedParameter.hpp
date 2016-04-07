faf46309c3d37bdb4690bd83fbcdbabe34488f91
Luca Bertagna <luca.bertagna84@gmail.com>
2016-04-07 12:20:52 -0400

    Mod in FELIX SharedParameter
    The method to set the nominal value for the parameter now takes 2 arguments: a parameter list
    and a double. Before resorting to the input double, the method scans the input list. This is
    supposed to be the 'Parameter' sublist of the problem section in the input file. Therefore,
    the method looks for the parameter vector with the right name; if found and if there are
    nominal values for that parameter it loads the values, otherwise uses the input default value.

--- src/FELIX/evaluators/FELIX_SharedParameter.hpp
+++ src/FELIX/evaluators/FELIX_SharedParameter.hpp
@@ -5,6 +5,8 @@
 #include "Phalanx_Evaluator_Derived.hpp"
 #include "Sacado_ParameterAccessor.hpp"
 
+#include "Albany_Utils.hpp"
+
 namespace FELIX
 {
 
@@ -37,11 +39,7 @@ public:
     this->utils.setFieldData(param_as_field,fm);
   }
 
-  static void setNominalValue (double val)
-  {
-    value = val;
-    dummy = 0;
-  }
+  static void setNominalValue (const Teuchos::ParameterList& p, double default_value);
 
   static ScalarT getValue ()
   {
@@ -64,16 +62,72 @@ protected:
 
   static ScalarT              value;
   static ScalarT              dummy;
+  static std::string          param_name;
 
-  std::string                 param_name;
   PHX::MDField<ScalarT,Dim>   param_as_field;
 };
+
+template<typename EvalT, typename Traits, typename ParamNameEnum, ParamNameEnum ParamName>
+void SharedParameter<EvalT,Traits,ParamNameEnum,ParamName>::
+setNominalValue (const Teuchos::ParameterList& p, double default_value)
+{
+  // First we scan the Parameter list to see if this parameter is listed in it,
+  // in which case we use the nominal value.
+  bool found = false;
+  if (p.isParameter("Number of Parameter Vectors"))
+  {
+    int n = p.get<int>("Number of Parameter Vectors");
+    for (int i=0; (found==false) && i<n; ++i)
+    {
+      const Teuchos::ParameterList& pvi = p.sublist(Albany::strint("Parameter Vector",i));
+      if (!pvi.isParameter("Nominal Values"))
+        continue; // Pointless to check the parameter names, since we don't have nominal values
+
+      int m = pvi.get<int>("Number");
+      for (int j=0; j<m; ++j)
+      {
+        if (pvi.get<std::string>(Albany::strint("Parameter",j))==param_name)
+        {
+          Teuchos::Array<double> nom_vals = pvi.get<Teuchos::Array<double>>("Nominal Values");
+          value = nom_vals[j];
+          found = true;
+          break;
+        }
+      }
+    }
+  }
+  else if (p.isParameter("Number") && !p.isParameter("Nominal Values"))
+  {
+    int m = p.get<int>("Number");
+    for (int j=0; j<m; ++j)
+    {
+      if (p.get<std::string>(Albany::strint("Parameter",j))==param_name)
+      {
+        Teuchos::Array<double> nom_vals = p.get<Teuchos::Array<double>>("Nominal Values");
+        value = nom_vals[j];
+        found = true;
+        break;
+      }
+    }
+  }
+
+  if (!found)
+  {
+    value = default_value;
+  }
+
+  dummy = 0;
+}
 
 template<typename EvalT, typename Traits, typename ParamNameEnum, ParamNameEnum ParamName>
 typename EvalT::ScalarT SharedParameter<EvalT,Traits,ParamNameEnum,ParamName>::value;
 
 template<typename EvalT, typename Traits, typename ParamNameEnum, ParamNameEnum ParamName>
 typename EvalT::ScalarT SharedParameter<EvalT,Traits,ParamNameEnum,ParamName>::dummy;
+
+template<typename EvalT, typename Traits, typename ParamNameEnum, ParamNameEnum ParamName>
+std::string SharedParameter<EvalT,Traits,ParamNameEnum,ParamName>::param_name;
+
 } // Namespace FELIX
 
 #endif // FELIX_SHARED_PARAMETER_HPP
