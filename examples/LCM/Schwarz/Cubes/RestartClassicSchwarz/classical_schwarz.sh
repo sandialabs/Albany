09fa95ac2a8c7c9ddc833422e7961c4fbd9a8944
Irina K. Tezaur <ikalash@sandia.gov>
2016-04-15 15:09:13 -0700

    LCM: continuing work on classical Schwarz implementation wrapped around Albany. All that needs to be put in now is while loop and convergence criterion (need to talk to Alejandro about the latter).

--- examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/classical_schwarz.sh
+++ examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/classical_schwarz.sh
@@ -27,6 +27,17 @@ do
    sed -i -e 's#<Parameter  name="Initial Value" type="double" value="0.0"/>#<Parameter  name="Initial Value" type="double" value="'$load_value'"/>#g' cube0_restart_"$step".xml
    sed -i -e 's#<Parameter  name="Min Value" type="double" value="0.0"/>#<Parameter  name="Min Value" type="double" value="'$load_value'"/>#g' cube0_restart_"$step".xml
    sed -i -e 's#<Parameter  name="Max Value" type="double" value="0.0"/>#<Parameter  name="Max Value" type="double" value="'$load_value'"/>#g' cube0_restart_"$step".xml
+   #create step^th input file for DTK run
+   cp input_schwarz_cube0_target.xml input_schwarz_cube0_target_"$step".xml 
+   #Replace name of exodus source file for step^th DTK input file
+   exo_src_name=cube1_restart_out_"$step".exo 
+   sed -i -e "s/cube1_restart_out.exo/$exo_src_name/g" input_schwarz_cube0_target_"$step".xml
+   #Replace name of exodus target input file for step^th DTK input file
+   exo_tgt_in_name=cube0_in_"$step".exo 
+   sed -i -e "s/cube0_in.exo/$exo_tgt_in_name/g" input_schwarz_cube0_target_"$step".xml
+   #Replace name of exodus target output file for step^th DTK input file
+   exo_tgt_out_name=target_cube0_out_"$step".exo 
+   sed -i -e "s/target_cube0_out.exo/$exo_tgt_out_name/g" input_schwarz_cube0_target_"$step".xml
    
    #################  Cube 1  ######################################
    #Extract step^th snapshot from cube1_in.exo file
@@ -75,6 +86,22 @@ do
    #################  ALBANY RUN FOR CUBE1  ##############################
    #run Albany with cube1_restart_"$step".xml input file
    ./AlbanyT cube1_restart_"$step".xml
+
+   #################  DTK RUN FOR TRANSFERING CUBE1 SOLN TO CUBE0  ##############################
+   #run DTK_Interp_Volume_to_NS executable with input_schwarz_cube0_target_"$step".xml
+   ./DTK_Interp_Volume_to_NS --xml-in-file=input_schwarz_cube0_target_"$step".xml
+   
+   #################  POST-PROCESSING AFTER DTK RUN ##############################
+   #change time stamp in cube0_in_$step.exo to 0 to be consistent with DTK output target file which will have a time stamp of 0
+   ncap2 -s 'time_whole=0*time_whole' cube0_in_"$step".exo tmp.exo
+   mv tmp.exo cube0_in_"$step".exo 
+   #merge target_cube0_out_$step.exo file with cube0_in_$step.exo
+   ejoin target_cube0_out_"$step".exo cube0_in_"$step".exo #this produces a file ejoin-out.e
+   mv ejoin-out.e cube0_restart_"$step".exo
+ 
+   #TODO: check convergence
+   #if (converged)
+   #   exit while loop
 
    #TODO: end while loop
    echo "...done!"
