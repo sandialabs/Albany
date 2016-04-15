d085be46d2f52301cf4f4263afaae4f03434844b
Irina K. Tezaur <ikalash@sandia.gov>
2016-04-15 14:54:53 -0700

    LCM: continuing work on classical Schwarz implementation wrapped around Albany.

--- examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/classical_schwarz.sh
+++ examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/classical_schwarz.sh
@@ -27,6 +27,21 @@ do
    sed -i -e 's#<Parameter  name="Initial Value" type="double" value="0.0"/>#<Parameter  name="Initial Value" type="double" value="'$load_value'"/>#g' cube0_restart_"$step".xml
    sed -i -e 's#<Parameter  name="Min Value" type="double" value="0.0"/>#<Parameter  name="Min Value" type="double" value="'$load_value'"/>#g' cube0_restart_"$step".xml
    sed -i -e 's#<Parameter  name="Max Value" type="double" value="0.0"/>#<Parameter  name="Max Value" type="double" value="'$load_value'"/>#g' cube0_restart_"$step".xml
+   
+   #################  Cube 1  ######################################
+   #Extract step^th snapshot from cube1_in.exo file
+   ncks -d time_step,$step cube1_in.exo cube1_in_"$step".exo
+   #Create step^th input file for Albany run
+   cp cube1_restart.xml cube1_restart_"$step".xml 
+   #Replace name of exodus input file to restart from in step^th input file
+   exo_in_name=cube1_in_"$step".exo
+   sed -i -e "s/cube1_in.exo/$exo_in_name/g" cube1_restart_"$step".xml
+   #Replace name of ouput file to restart from in step^th input file
+   exo_out_name=cube1_restart_out_"$step".exo
+   sed -i -e "s/cube1_restart_out.exo/$exo_out_name/g" cube1_restart_"$step".xml
+   sed -i -e 's#<Parameter  name="Initial Value" type="double" value="0.0"/>#<Parameter  name="Initial Value" type="double" value="'$load_value'"/>#g' cube1_restart_"$step".xml
+   sed -i -e 's#<Parameter  name="Min Value" type="double" value="0.0"/>#<Parameter  name="Min Value" type="double" value="'$load_value'"/>#g' cube1_restart_"$step".xml
+   sed -i -e 's#<Parameter  name="Max Value" type="double" value="0.0"/>#<Parameter  name="Max Value" type="double" value="'$load_value'"/>#g' cube1_restart_"$step".xml
    #create step^th input file for DTK run
    cp input_schwarz_cube1_target.xml input_schwarz_cube1_target_"$step".xml 
    #Replace name of exodus source file for step^th DTK input file
@@ -38,10 +53,6 @@ do
    #Replace name of exodus target output file for step^th DTK input file
    exo_tgt_out_name=target_cube1_out_"$step".exo 
    sed -i -e "s/target_cube1_out.exo/$exo_tgt_out_name/g" input_schwarz_cube1_target_"$step".xml
-   
-   #################  Cube 1  ######################################
-   #Extract step^th snapshot from cube1_in.exo file
-   ncks -d time_step,$step cube1_in.exo cube1_in_"$step".exo
    
    #TODO: add while loop here (while Schwarz unconverged)
 
@@ -60,6 +71,10 @@ do
    #merge target_cube1_out_$step.exo file with cube1_in_$step.exo
    ejoin target_cube1_out_"$step".exo cube1_in_"$step".exo #this produces a file ejoin-out.e
    mv ejoin-out.e cube1_restart_"$step".exo
+
+   #################  ALBANY RUN FOR CUBE1  ##############################
+   #run Albany with cube1_restart_"$step".xml input file
+   ./AlbanyT cube1_restart_"$step".xml
 
    #TODO: end while loop
    echo "...done!"
