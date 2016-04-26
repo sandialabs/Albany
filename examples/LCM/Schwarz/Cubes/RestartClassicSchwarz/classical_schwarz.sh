307d49d81bbeef6752682af966ea229809749b53
Irina K. Tezaur <ikalash@sandia.gov>
2016-04-26 10:10:01 -0700

    LCM: more work on Full Schwarz wrapped around Albany.  Loop logic should be correct now in classical_schwarz.sh.  Now need to modify the code to propagate incremented load step into *.xml files for Albany runs.

--- examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/classical_schwarz.sh
+++ examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/classical_schwarz.sh
@@ -11,6 +11,10 @@
 #cube1_restart.xml  
 #norm_displacements.m
 
+if [ ! $1 ] ; then
+    echo "This function requires 1 argument: # load steps (int)";
+    exit
+fi
 
 echo "Initial cleanup..."
 #initial cleanup: remove all old *exo files
@@ -26,8 +30,8 @@ rm -rf cube1_in_*.exo
 rm -rf cube0_restart_*.xml 
 rm -rf cube1_restart_*.xml
 #initial cleanup: remove other old files
-rm -rf disp0_old 
-rm -rf disp1_old
+rm -rf displ0_old 
+rm -rf displ1_old
 rm -rf error  
 echo "...cleanup done."
 
@@ -35,14 +39,12 @@ echo "...cleanup done."
 tol_schwarz=0.001
 echo "Schwarz convergence tolerance = $tol_schwarz"
 
-iterate_schwarz=1 #flag to tell code to continue Schwarz; if =0, Schwarz will stop 
-step=0 #start with Schwarz step 0 
 
 #FIXME: set load correctly
 
-while [ $iterate_schwarz -eq 1 ]; do 
- 
-   echo "Starting step $step of classical Schwarz with load = $step..."
+#load step loop
+for (( step=0; step<$1; step++ )); do 
+   echo "Starting load step = $step..."
    echo "   Pre-processing..."
    #################  PRE-PROCESSING  ##############################
    #################  Cube 0  ######################################
@@ -100,67 +102,74 @@ while [ $iterate_schwarz -eq 1 ]; do
    sed -i -e "s/target_cube1_out.exo/$exo_tgt_out_name/g" input_schwarz_cube1_target_"$step".xml
    echo "   ...pre-processing done."
    
-   echo "   Running Albany on subdomain 0..."
-   #################  ALBANY RUN FOR CUBE0  ##############################
-   #run Albany with cube0_restart_"$step".xml input file
-   ./AlbanyT cube0_restart_"$step".xml #this will write out an Exodus file with the name cube0_restart_out_$step.exo 
-   echo "   ...Albany run on subdomain 0 done."
+   iterate_schwarz=1 #flag to tell code to continue Schwarz; if =0, Schwarz will stop
+                     #this parameter should be reset to 1 in each load step 
+   schwarz_iter=0 #Schwarz iteration number 
+   while [ $iterate_schwarz -eq 1 ]; do 
+     echo "   Running Albany on subdomain 0..."
+     #################  ALBANY RUN FOR CUBE0  ##############################
+     #run Albany with cube0_restart_"$step".xml input file
+     ./AlbanyT cube0_restart_"$step".xml #this will write out an Exodus file with the name cube0_restart_out_$step.exo 
+     echo "   ...Albany run on subdomain 0 done."
 
-   echo "   Transferring solution in cube0 onto cube1 using DTK..."
-   #################  DTK RUN FOR TRANSFERING CUBE0 SOLN TO CUBE1  ##############################
-   #run DTK_Interp_Volume_to_NS executable with input_schwarz_cube1_target_"$step".xml
-   ./DTK_Interp_Volume_to_NS --xml-in-file=input_schwarz_cube1_target_"$step".xml #this will write out an Exodus file with the name 
+     echo "   Transferring solution in cube0 onto cube1 using DTK..."
+     #################  DTK RUN FOR TRANSFERING CUBE0 SOLN TO CUBE1  ##############################
+     #run DTK_Interp_Volume_to_NS executable with input_schwarz_cube1_target_"$step".xml
+     ./DTK_Interp_Volume_to_NS --xml-in-file=input_schwarz_cube1_target_"$step".xml #this will write out an Exodus file with the name 
                                                                                   #target_cube0_out_$step.exo
-   echo "   ...solution transfer from cube0 onto cube1 done."
+     echo "   ...solution transfer from cube0 onto cube1 done."
   
-   echo "   Post-processing of cube1 exo file..."
-   #################  POST-PROCESSING AFTER DTK RUN ##############################
-   #change time stamp in cube1_in_$step.exo to 0 to be consistent with DTK output target file which will have a time stamp of 0
-   ncap2 -s 'time_whole=0*time_whole' cube1_in_"$step".exo tmp.exo
-   mv tmp.exo cube1_in_"$step".exo 
-   #merge target_cube1_out_$step.exo file with cube1_in_$step.exo
-   #FIXME? do we want to merge cube1_in_"step".exo or cube1_out_$step".exo??
-   ejoin target_cube1_out_"$step".exo cube1_in_"$step".exo #this produces a file ejoin-out.e
-   mv ejoin-out.e cube1_restart_"$step".exo #copy ejoin-out.e to cube1_restart_$step.exo
-   echo "   ...post-processing of cube1 exo file done."
+     echo "   Post-processing of cube1 exo file..."
+     #################  POST-PROCESSING AFTER DTK RUN ##############################
+     #change time stamp in cube1_in_$step.exo to 0 to be consistent with DTK output target file which will have a time stamp of 0
+     ncap2 -s 'time_whole=0*time_whole' cube1_in_"$step".exo tmp.exo
+     mv tmp.exo cube1_in_"$step".exo 
+     #merge target_cube1_out_$step.exo file with cube1_in_$step.exo
+     #FIXME? do we want to merge cube1_in_"step".exo or cube1_out_$step".exo??
+     ejoin target_cube1_out_"$step".exo cube1_in_"$step".exo #this produces a file ejoin-out.e
+     mv ejoin-out.e cube1_restart_"$step".exo #copy ejoin-out.e to cube1_restart_$step.exo
+     echo "   ...post-processing of cube1 exo file done."
 
-   echo "   Running Albany on subdomain 1..."
-   #################  ALBANY RUN FOR CUBE1  ##############################
-   #run Albany with cube1_restart_"$step".xml input file
-   ./AlbanyT cube1_restart_"$step".xml #this will write out an Exodus file with the name cube1_restart_out_$step.exo
-   echo "   ...Albany run on subdomain 1 done."
+     echo "   Running Albany on subdomain 1..."
+     #################  ALBANY RUN FOR CUBE1  ##############################
+     #run Albany with cube1_restart_"$step".xml input file
+     ./AlbanyT cube1_restart_"$step".xml #this will write out an Exodus file with the name cube1_restart_out_$step.exo
+     echo "   ...Albany run on subdomain 1 done."
 
-   echo "   Transferring solution in cube1 onto cube0 using DTK..."
-   #################  DTK RUN FOR TRANSFERING CUBE1 SOLN TO CUBE0  ##############################
-   #run DTK_Interp_Volume_to_NS executable with input_schwarz_cube0_target_"$step".xml
-   ./DTK_Interp_Volume_to_NS --xml-in-file=input_schwarz_cube0_target_"$step".xml #this will write out an Exodus file with the name 
+     echo "   Transferring solution in cube1 onto cube0 using DTK..."
+     #################  DTK RUN FOR TRANSFERING CUBE1 SOLN TO CUBE0  ##############################
+     #run DTK_Interp_Volume_to_NS executable with input_schwarz_cube0_target_"$step".xml
+     ./DTK_Interp_Volume_to_NS --xml-in-file=input_schwarz_cube0_target_"$step".xml #this will write out an Exodus file with the name 
                                                                                   #target_cube1_out_$step.exo
-   echo "   ...solution transfer from cube1 onto cube0 done."
+     echo "   ...solution transfer from cube1 onto cube0 done."
 
-   echo "   Post-processing of cube0 exo file..."
-   #################  POST-PROCESSING AFTER DTK RUN ##############################
-   #change time stamp in cube0_in_$step.exo to 0 to be consistent with DTK output target file which will have a time stamp of 0
-   ncap2 -s 'time_whole=0*time_whole' cube0_in_"$step".exo tmp.exo
-   mv tmp.exo cube0_in_"$step".exo 
-   #merge target_cube0_out_$step.exo file with cube0_in_$step.exo
-   #FIXME? do we want to merge cube0_in_"step".exo or cube0_out_$step".exo??
-   ejoin target_cube0_out_"$step".exo cube0_in_"$step".exo #this produces a file ejoin-out.e
-   mv ejoin-out.e cube0_restart_"$step".exo #copy ejoin-out.e to cube1_restart_$step.exo
-   echo "   ...post-processing of cube0 exo file done."
+     echo "   Post-processing of cube0 exo file..."
+     #################  POST-PROCESSING AFTER DTK RUN ##############################
+     #change time stamp in cube0_in_$step.exo to 0 to be consistent with DTK output target file which will have a time stamp of 0
+     ncap2 -s 'time_whole=0*time_whole' cube0_in_"$step".exo tmp.exo
+     mv tmp.exo cube0_in_"$step".exo 
+     #merge target_cube0_out_$step.exo file with cube0_in_$step.exo
+     #FIXME? do we want to merge cube0_in_"step".exo or cube0_out_$step".exo??
+     ejoin target_cube0_out_"$step".exo cube0_in_"$step".exo #this produces a file ejoin-out.e
+     mv ejoin-out.e cube0_restart_"$step".exo #copy ejoin-out.e to cube1_restart_$step.exo
+     echo "   ...post-processing of cube0 exo file done."
 
-   echo "   Checking convergence..."
-   #################  CHECK CONVERGENCE  #########################################
-   matlab -nodesktop -nosplash -r "norm_displacements($step);quit;" #this will create an ascii file error containing the value of the error
-   #read error from error file
-   err=$(sed "${1}q;d" error)
-   #check if error < tol_schwarz; if it is, the method is converged 
-   if (($(echo $err '<=' $tol_schwarz | bc -l))); then 
-     echo "...classical Schwarz converged after $step steps!"
-     iterate_schwarz=0 
-   else
-     echo "   error = $err > tol_schwarz = $tol_schwarz"
-     echo "...Schwarz failed to converge.  Continuing."
-     let "step=step+1"
-   fi
-done
-
+     echo "   Checking convergence..."
+     #################  CHECK CONVERGENCE  #########################################
+     matlab -nodesktop -nosplash -r "norm_displacements($schwarz_iter);quit;" #this will create an ascii file error containing the value of the error
+     #read error from error file
+     err=$(head -n 1 error)
+     echo "  error = $err" 
+     #check if error < tol_schwarz; if it is, the method is converged 
+     if (($(echo $err '<=' $tol_schwarz | bc -l))); then 
+       echo "  ...classical Schwarz converged after $schwarz_iter Schwarz iterations!"
+       iterate_schwarz=0 
+     else
+       echo "   error = $err > tol_schwarz = $tol_schwarz"
+       echo "  ...Schwarz failed to converge.  Continuing."
+       #increment Schwarz iteration 
+       let "schwarz_iter=schwarz_iter+1"
+     fi
+   done #while loop
+   echo "...finished load step $step run!" 
+done #load step loop 
--- examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/norm_displacements.m
+++ examples/LCM/Schwarz/Cubes/RestartClassicSchwarz/norm_displacements.m
@@ -6,48 +6,46 @@
 %vals_nod_var1, vals_nod_var2, and vals_nod_var3 in the *exo file,
 %respectively.  If they are not, code needs to be modified.
 
-%Input: Schwarz step number, step_no (int) 
-function[] = norm_displlacements(step_no) 
+%Input: Schwarz step number, schwarz_no (int) 
+function[] = norm_displacements(schwarz_no) 
 
-file0_exo_name = strcat('cube0_restart_',num2str(step_no),'.exo');
-file1_exo_name = strcat('cube1_restart_',num2str(step_no),'.exo');
+file0_exo_name = strcat('cube0_restart_',num2str(schwarz_no),'.exo');
+file1_exo_name = strcat('cube1_restart_',num2str(schwarz_no),'.exo');
 
-step_no
-file0_exo_name
-file1_exo_name
+disp(['      schwarz_no = ',num2str(schwarz_no)]); 
 
 %Here we hard-code 2-norm.  norm_type could be made an input argument.
 norm_type = 2; 
 
 %cube0
-%x-displlacement
+%x-displacement
 displ0_x = ncread(file0_exo_name, 'vals_nod_var1'); 
-%y-displlacement
+%y-displacement
 displ0_y = ncread(file0_exo_name, 'vals_nod_var2'); 
-%z-displlacement
+%z-displacement
 displ0_z = ncread(file0_exo_name, 'vals_nod_var3'); 
 %get last snapshot
 displ0_x = displ0_x(:,end); 
 displ0_y = displ0_y(:,end); 
 displ0_z = displ0_z(:,end); 
-%concatenate into a single displlacement vector
+%concatenate into a single displacement vector
 displ0 = zeros(3*length(displ0_x),1); 
 displ0(1:3:end) = displ0_x; 
 displ0(2:3:end) = displ0_y; 
 displ0(3:3:end) = displ0_z; 
 
 %cube1
-%x-displlacement
+%x-displacement
 displ1_x = ncread(file1_exo_name, 'vals_nod_var1'); 
-%y-displlacement
+%y-displacement
 displ1_y = ncread(file1_exo_name, 'vals_nod_var2'); 
-%z-displlacement
+%z-displacement
 displ1_z = ncread(file1_exo_name, 'vals_nod_var3'); 
 %get last snapshot
 displ1_x = displ1_x(:,end); 
 displ1_y = displ1_y(:,end); 
 displ1_z = displ1_z(:,end); 
-%concatenate into a single displlacement vector
+%concatenate into a single displacement vector
 displ1 = zeros(3*length(displ1_x),1); 
 displ1(1:3:end) = displ1_x; 
 displ1(2:3:end) = displ1_y; 
@@ -56,40 +54,41 @@ displ1(3:3:end) = displ1_z;
 displ{1} = displ0; 
 displ{2} = displ1; 
 
-if (step_no == 0)
-  %if it's the first step, set error = 1 so that code continues
-  %TODO: check with Alejandro what he does.
-  error = 1;  
+if (schwarz_no == 0)
+  displ_old{1} = zeros(length(displ{1}),1); 
+  displ_old{2} = zeros(length(displ{2}),1); 
 else
-  %The following is based on Alejandro's file FullSchwarz.m 
-  %specific case of 2 domains
   %read displ_old from file
   displ_old{1} = dlmread('displ0_old'); 
-  displ_old{2} = dlmread('displ1_old'); 
-  for i=1:2
-    displlacement_norms(i) = norm(displ{i}); 
-    diff = displ{i} - displ_old{i}; 
-    difference_norms(i) = norm(diff); 
-  end
-
-  norm_displ = norm(displlacement_norms, norm_type); 
-  norm_difference = norm(difference_norms, norm_type); 
-
-  %compute error which will be used to determine if Schwarz has converged.
-  norm_displ
-  if (norm_displ > 0.0)
-    error = norm_difference / norm_displ; 
-  else
-    error = norm_difference;
-  end
+  displ_old{2} = dlmread('displ1_old');
+end
+ 
+%The following is based on Alejandro's file FullSchwarz.m 
+%specific case of 2 domains
+for i=1:2
+  displacement_norms(i) = norm(displ{i}); 
+  diff = displ{i} - displ_old{i}; 
+  difference_norms(i) = norm(diff); 
 end
 
-disp(['error = ', num2str(error)]); 
-%write new displlacements to displ*_old files.
-dlmwrite('displ0_old', displ{1}); 
-dlmwrite('displ1_old', displ{2}); 
+norm_displ = norm(displacement_norms, norm_type); 
+norm_difference = norm(difference_norms, norm_type); 
+
+%compute error which will be used to determine if Schwarz has converged.
+disp(['      norm_displ = ', num2str(norm_displ)]); 
+if (norm_displ > 0.0)
+  error = norm_difference / norm_displ; 
+else
+  error = norm_difference;
+end
+
+disp(['      error = ', num2str(error)]);
+format long g 
+%write new displacements to displ*_old files.
+dlmwrite('displ0_old', displ{1}, 'precision', 10); 
+dlmwrite('displ1_old', displ{2}, 'precision', 10); 
 %write error to file
-dlmwrite('error', error); 
+dlmwrite('error', error, 'precision', 10); 
 
 
 
