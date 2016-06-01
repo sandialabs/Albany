e6ce0befe78fddc54a5d80b6e2723ebfba1d41ce
Irina K. Tezaur <ikalash@sandia.gov>
2016-06-01 11:15:09 -0700

    Modifying cdash scripts so they process timeouts correctly.

--- doc/dashboards/camobap.ca.sandia.gov/send_email_functor_openMP.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_functor_openMP.sh
@@ -4,9 +4,10 @@
 
 TTT=`grep "(Failed)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
 TTTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
+TTTTT=`grep "Timeouts" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run" "ikalash@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
-/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run" "agsalin@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
-/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run" "gahanse@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
-/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run" "onguba@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
+/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
+/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "agsalin@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
+/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "gahanse@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
+/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "onguba@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
