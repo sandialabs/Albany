0f8b3506dfa5788c3210cdb1173da203d0d234f9
Jay Foulk <jwfoulk@sandia.gov>
2016-06-04 18:14:03 -0700

    LCM: Changing extension of output file

--- src/LCM/utils/python/PostProcess.py
+++ src/LCM/utils/python/PostProcess.py
@@ -83,7 +83,7 @@ if __name__ == "__main__":
 
     inFile.close()
 
-    outFileName = outFileLabel + 'force_displacement.txt'
+    outFileName = outFileLabel + 'force_displacement.dat'
     dataFile = open(outFileName, 'w')
     for timeStep in range(numTimeSteps):
         dataFile.write(str(nodeset_displacement[timeStep]) + "  " + str(nodeset_force[timeStep]) + "\n")
