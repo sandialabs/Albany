sandialabs/Albany

commit bacd37aac6f306c716f9fbeaf866ae1566b01d81

Progress on reading lattice orientations from a genesis file for use with the CrystalPlasticity model.
Lattice orientations from the mesh file are now available in the CrystalPlasticityModel, but are not
currently used.  Some model refactoring is required before this can happen (e.g., basis vectors must
be state data, not material properties).

src/disc/pumi/Albany_APFDiscretization.cpp
@@ -261,6 +261,12 @@ Albany::APFDiscretization::getSphereVolume() const
   return sphereVolume;
 }
 
+const Albany::WorksetArray<Teuchos::ArrayRCP<double*> >::type&
+Albany::APFDiscretization::getLatticeOrientation() const
+{
+  return latticeOrientation;
+}
+
 double mean (const double* x, const int n,
              const Teuchos::RCP<const Tpetra_Map>& map) {
   Teuchos::ArrayView<const double> xav = Teuchos::arrayView(x, n);

examples/LCM/CrystalPlasticity/OrientationOnMesh/OrientationOnMesh.xml
@@ -1,0 +1,199 @@
+<ParameterList>
+  <!-- Dumping Matrix Market to file
+         <ParameterList name="Debug Output">
+     <Parameter name="Write Jacobian to MatrixMarket" type="int" value="-1"/>
+  </ParameterList>
+  -->
+  <ParameterList name="Scaling">
+    <Parameter name="Scale" type="double" value="1.0e4"/>
+    <Parameter name="Scale BC Dofs" type="bool" value="true"/>
+  </ParameterList>
+  <!-- Replace the above scaling sublist with the following to use diagonal scaling
+  <ParameterList name="Scaling">
+    <Parameter name="Scale BC Dofs" type="bool" value="false"/>
+    <Parameter name="Type" type="string" value="Diagonal"/>
+  </ParameterList>
+  -->
+  <!-- Replace the above scaling sublist with the following to use absolute value row sum scaling
+  <ParameterList name="Scaling">
+    <Parameter name="Scale BC Dofs" type="bool" value="false"/>
+    <Parameter name="Type" type="string" value="Abs Row Sum"/>
+  </ParameterList>
+  -->
+  <ParameterList name="Problem">
+    <Parameter name="Name" type="string" value="Mechanics 3D"/>
+    <Parameter name="Solution Method" type="string" value="Continuation"/>
+    <Parameter name="Phalanx Graph Visualization Detail" type="int" value="0"/>
+    <Parameter name="MaterialDB Filename" type="string" value="OrientationOnMesh_Material_Implicit.xml"/> -->
+    <ParameterList name="Dirichlet BCs">
+      <ParameterList name="Time Dependent DBC on NS nodelist_2 for DOF X">
+        <Parameter name="Number of points" type="int" value="4"/>
+        <Parameter name="Time Values" type="Array(double)" value="{ 0.0, 1.0, 2.0,  3.0}"/>
+        <Parameter name="BC Values"   type="Array(double)" value="{ 0.0, 0.5, 1.0,  1.5}"/>
+      </ParameterList>
+      <Parameter name="DBC on NS nodelist_1 for DOF X" type="double" value="0.0"/>
+      <Parameter name="DBC on NS nodelist_1 for DOF Y" type="double" value="0.0"/>
+      <Parameter name="DBC on NS nodelist_1 for DOF Z" type="double" value="0.0"/>
+    </ParameterList>
+    <ParameterList name="Parameters">
+      <Parameter name="Number" type="int" value="1"/>
+      <Parameter name="Parameter 0" type="string" value="Time"/>
+    </ParameterList>
+    <ParameterList name="Response Functions">
+      <Parameter name="Number" type="int" value="1"/>
+      <Parameter name="Response 0" type="string" value="Solution Average"/>
+    </ParameterList>
+  </ParameterList>
+  <ParameterList name="Discretization">
+    <Parameter name="Method" type="string" value="Exodus"/>
+    <Parameter name="Exodus Input File Name" type="string" value="OrientationOnMesh_orientation.g"/>
+    <Parameter name="Exodus Output File Name" type="string" value="OrientationOnMesh.exo"/>
+    <Parameter name="Cubature Degree" type="int" value="2"/>
+    <Parameter name="Separate Evaluators by Element Block" type="bool" value="true"/>
+    <Parameter name="Solution Vector Components" type="Array(string)" value="{displacement, V}"/>
+    <Parameter name="Residual Vector Components" type="Array(string)" value="{force, V}"/>
+  </ParameterList>
+  <!-- The regression test examines "Response 0", as defined above. -->
+<!--
+  <ParameterList name="Regression Results">
+    <Parameter  name="Number of Comparisons" type="int" value="1"/>
+    <Parameter  name="Test Values" type="Array(double)" value="{0.025}"/>
+    <Parameter  name="Relative Tolerance" type="double" value="1.0e-12"/>
+  </ParameterList>
+-->
+  <ParameterList name="Piro">
+    <ParameterList name="LOCA">
+      <ParameterList name="Bifurcation"/>
+      <ParameterList name="Constraints"/>
+      <ParameterList name="Predictor">
+        <Parameter name="Method" type="string" value="Constant"/>
+      </ParameterList>
+      <ParameterList name="Stepper">
+        <Parameter  name="Continuation Method" type="string" value="Natural"/>
+        <Parameter name="Initial Value" type="double" value="0.0"/>
+        <Parameter name="Continuation Parameter" type="string" value="Time"/>
+        <Parameter name="Max Steps" type="int" value="5000"/>
+        <Parameter name="Max Value" type="double" value="0.03"/>
+        <Parameter name="Min Value" type="double" value="0.0"/>
+        <Parameter name="Compute Eigenvalues" type="bool" value="0"/>
+        <ParameterList name="Eigensolver">
+          <Parameter name="Method" type="string" value="Anasazi"/>
+          <Parameter name="Operator" type="string" value="Jacobian Inverse"/>
+          <Parameter name="Num Eigenvalues" type="int" value="0"/>
+        </ParameterList>
+      </ParameterList>
+      <ParameterList name="Step Size">
+       <Parameter name="Method" type="string" value="Adaptive"/>
+       <Parameter name="Initial Step Size" type="double" value="0.0005"/>
+       <Parameter name="Max Step Size" type="double" value="0.02"/>
+       <Parameter name="Min Step Size" type="double" value="0.00001"/>
+       <Parameter name="Failed Step Reduction Factor" type="double" value="0.5"/>
+       <Parameter name="Aggressiveness" type="double" value="0.1"/>
+      </ParameterList>
+    </ParameterList>
+    <ParameterList name="NOX">
+      <ParameterList name="Direction">
+        <Parameter name="Method" type="string" value="Newton"/>
+        <ParameterList name="Newton">
+          <ParameterList name="Linear Solver">
+            <Parameter name="Tolerance" type="double" value="1.0e-9"/>
+          </ParameterList>
+          <Parameter name="Forcing Term Method" type="string" value="Constant"/>
+          <Parameter name="Rescue Bad Newton Solve" type="bool" value="1"/>
+          <ParameterList name="Stratimikos Linear Solver">
+            <ParameterList name="NOX Stratimikos Options"> 	  
+            </ParameterList>
+            <ParameterList name="Stratimikos">
+              <Parameter name="Linear Solver Type" type="string" value="Belos"/>
+              <ParameterList name="Linear Solver Types">
+                <ParameterList name="Belos">
+                  <ParameterList name="VerboseObject">
+                    <Parameter name="Verbosity Level" type="string" value="high"/>
+                  </ParameterList>
+                  <Parameter name="Solver Type" type="string" value="Block GMRES"/>
+                  <ParameterList name="Solver Types">
+                    <ParameterList name="Block GMRES">
+                      <Parameter name="Output Frequency" type="int" value="1"/>
+                      <Parameter name="Output Style" type="int" value="1"/>
+                      <Parameter name="Verbosity" type="int" value="33"/>
+                      <Parameter name="Maximum Iterations" type="int" value="500"/>
+                      <Parameter name="Block Size" type="int" value="1"/>
+                      <Parameter name="Num Blocks" type="int" value="24"/>
+                      <Parameter name="Flexible Gmres" type="bool" value="0"/>
+                    </ParameterList>
+                  </ParameterList>
+                </ParameterList>
+              </ParameterList>
+              <Parameter name="Preconditioner Type" type="string" value="Ifpack2"/>
+              <ParameterList name="Preconditioner Types">
+                <ParameterList name="Ifpack2">
+                  <Parameter name="Overlap" type="int" value="2"/>
+                  <Parameter name="Prec Type" type="string" value="ILUT"/>
+                  <ParameterList name="Ifpack2 Settings">
+                    <Parameter name="fact: drop tolerance" type="double" value="0"/>
+                    <Parameter name="fact: ilut level-of-fill" type="double" value="1"/>
+                    <Parameter name="fact: level-of-fill" type="int" value="1"/>
+                  </ParameterList>
+                </ParameterList>
+              </ParameterList>
+            </ParameterList>
+          </ParameterList>
+        </ParameterList>
+      </ParameterList>
+      <ParameterList name="Line Search">
+        <ParameterList name="Full Step">
+        </ParameterList>
+        <Parameter name="Method" type="string" value="Full Step"/>
+      </ParameterList>
+      <Parameter name="Nonlinear Solver" type="string" value="Line Search Based"/>
+      <ParameterList name="Printing">
+        <Parameter name="Output Precision" type="int" value="3"/>
+        <Parameter name="Output Processor" type="int" value="0"/>
+        <!-- set the output information -->
+        <ParameterList name="Output Information">
+          <Parameter name="Error" type="bool" value="1"/>
+          <Parameter name="Warning" type="bool" value="0"/>
+          <Parameter name="Outer Iteration" type="bool" value="1"/>
+          <Parameter name="Parameters" type="bool" value="0"/>
+          <Parameter name="Details" type="bool" value="0"/>
+          <Parameter name="Linear Solver Details" type="bool" value="0"/>
+          <Parameter name="Stepper Iteration" type="bool" value="0"/>
+          <Parameter name="Stepper Details" type="bool" value="0"/>
+          <Parameter name="Stepper Parameters" type="bool" value="0"/>
+        </ParameterList>
+      </ParameterList>
+      <ParameterList name="Solver Options">
+        <Parameter name="Status Test Check Type" type="string" value="Complete"/>
+      </ParameterList>
+      <ParameterList name="Status Tests">
+       <Parameter name="Test Type" type="string" value="Combo"/>
+       <Parameter name="Combo Type" type="string" value="OR"/>
+       <Parameter name="Number of Tests" type="int" value="4"/>
+        <ParameterList name="Test 0">
+         <Parameter name="Test Type" type="string" value="RelativeNormF"/>
+         <Parameter name="Tolerance" type="double" value="1.0e-10"/>
+        </ParameterList>
+        <ParameterList name="Test 1">
+         <Parameter name="Test Type" type="string" value="MaxIters"/>
+         <Parameter name="Maximum Iterations" type="int" value="10"/>
+        </ParameterList>
+        <ParameterList name="Test 2">
+          <Parameter name="Test Type" type="string" value="Combo"/>
+          <Parameter name="Combo Type" type="string" value="AND"/>
+          <Parameter name="Number of Tests" type="int" value="2"/>
+          <ParameterList name="Test 0">
+            <Parameter name="Test Type" type="string" value="NStep"/>
+            <Parameter name="Number of Nonlinear Iterations" type="int" value="1"/>
+          </ParameterList>
+          <ParameterList name="Test 1">
+            <Parameter name="Test Type" type="string" value="NormF"/>
+            <Parameter name="Tolerance" type="double" value="1.0e-10"/>
+          </ParameterList>
+        </ParameterList>
+        <ParameterList name="Test 3">
+          <Parameter name="Test Type" type="string" value="FiniteValue"/>
+        </ParameterList>
+      </ParameterList>
+    </ParameterList>
+  </ParameterList>
+</ParameterList>

examples/LCM/CrystalPlasticity/OrientationOnMesh/OrientationOnMesh_Material_Implicit.xml
@@ -1,0 +1,444 @@
+<ParameterList>
+
+     <ParameterList name="ElementBlocks">
+
+          <ParameterList name="block_1">
+
+               <Parameter name="material" 
+                          type="string" 
+                          value="FCC" />
+
+               <Parameter name="Weighted Volume Average J" 
+                          type="bool" 
+                          value="true" />
+
+               <Parameter name="Volume Average Pressure" 
+                          type="bool" 
+                          value="true" />
+
+          </ParameterList>
+
+     </ParameterList>
+
+     <ParameterList name="Materials">
+
+          <ParameterList name="FCC">
+
+               <ParameterList name="Material Model"> 
+
+                    <Parameter name="Model Name" 
+                               type="string" 
+                               value="CrystalPlasticity"/>
+
+               </ParameterList>
+
+<!-- ~~~~~~~~~ Crystal Elasticity moduli and orientation -->
+
+               <ParameterList name="Crystal Elasticity">
+
+                    <Parameter name="C11" 
+                               type="double" 
+                               value="204.6e3"/>
+
+                    <Parameter name="C12" 
+                               type="double" 
+                               value="137.7e3"/>
+
+                    <Parameter name="C44" 
+                               type="double" 
+                               value="126.2e3"/>
+
+                    <Parameter name="Basis Vector 1" 
+                               type="Array(double)" 
+                               value="{1.0, 0.0, 0.0}"/>
+
+                    <Parameter name="Basis Vector 2" 
+                               type="Array(double)" 
+                               value="{0.0, 1.0, 0.0}"/>
+
+                    <Parameter name="Basis Vector 3" 
+                               type="Array(double)" 
+                               value="{0.0, 0.0, 1.0}"/>
+
+               </ParameterList>
+
+               <Parameter name="Read Lattice Orientation From Mesh"
+                          type="bool" 
+                          value="true"/>
+
+               <Parameter name="Integration Scheme" 
+                          type="string" 
+                          value="Implicit"/>
+
+               <Parameter name="Implicit Integration Relative Tolerance" 
+                          type="double" 
+                          value="1.0e-35"/>
+
+               <Parameter name="Implicit Integration Absolute Tolerance" 
+                          type="double" 
+                          value="1.0e-12"/>
+
+               <Parameter name="Implicit Integration Max Iterations" 
+                          type="int" 
+                          value="100"/>
+
+               <Parameter name="Output CP_Residual" 
+                          type="bool" 
+                          value="true"/>
+
+               <ParameterList name="Flow Rule">
+
+                    <Parameter name="Type" 
+                               type="string" 
+                               value="Power Law"/>
+
+                    <Parameter name="Gamma Dot" 
+                               type="double" 
+                               value="1.0e00"/>
+
+                    <Parameter name="Gamma Exponent" 
+                               type="double" 
+                               value="2.0e01"/>
+
+               </ParameterList>
+
+               <ParameterList name="Hardening Law">
+
+                    <Parameter name="Type" 
+                               type="string" 
+                               value="Exponential"/>
+
+                    <Parameter name="Hardening" 
+                               type="double" 
+                               value="3.55e02"/>
+
+                    <Parameter name="Hardening Exponent" 
+                               type="double" 
+                               value="2.9e00"/>
+
+                    <Parameter name="Tau Critical"
+                               type="double"
+                               value="1.22e02"/>
+
+               </ParameterList>
+
+      <!-- Crystal Plasticity Slip System -->
+      <!-- Convention taken from Plasticity by Nemat-Nasser, p. 454 - 455 -->
+
+               <Parameter name="Number of Slip Systems" 
+                          type="int" 
+                          value="12"/>
+
+               <ParameterList name="Slip System 1">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{-1.0,1.0,0.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{1.0,1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 2">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{0.0,-1.0,1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{1.0,1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 3">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{1.0,0.0,-1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{1.0,1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 4">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{-1.0,-1.0,0.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{-1.0,1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 5">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{1.0,0.0,1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{-1.0,1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 6">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{0.0,1.0,-1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{-1.0,1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 7">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{1.0,-1.0,0.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{-1.0,-1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 8">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{0.0,1.0,1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{-1.0,-1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/>
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 9">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{-1.0,0.0,-1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{-1.0,-1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 10">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{1.0,1.0,0.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{1.0,-1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/>
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 11">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{-1.0,0.0,1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{1.0,-1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+
+               <ParameterList name="Slip System 12">
+
+                    <Parameter name="Slip Direction" 
+                               type="Array(double)" 
+                               value="{0.0,-1.0,-1.0}"/>
+
+                    <Parameter name="Slip Normal" 
+                               type="Array(double)"    
+                               value="{1.0,-1.0,1.0}"/>
+
+                    <Parameter name="Flow Rule" 
+                               type="string" 
+                               value="Flow Rule"/>
+
+                    <Parameter name="Hardening Law" 
+                               type="string" 
+                               value="Hardening Law"/> 
+
+               </ParameterList>
+
+      <!-- Specify output to Exodus -->
+      <Parameter name="Output Cauchy Stress" type="bool" value="true"/>
+      <!-- <Parameter name="Output Fp" type="bool" value="true"/> -->
+      <!-- <Parameter name="Output L" type="bool" value="true"/> -->
+      <Parameter name="Output eqps" type="bool" value="true"/>
+      <Parameter name="Output gamma_1" type="bool" value="true"/>
+      <Parameter name="Output gamma_2" type="bool" value="true"/>
+      <Parameter name="Output gamma_3" type="bool" value="true"/>
+      <Parameter name="Output gamma_4" type="bool" value="true"/>
+      <Parameter name="Output gamma_5" type="bool" value="true"/>
+      <Parameter name="Output gamma_6" type="bool" value="true"/>
+      <Parameter name="Output gamma_7" type="bool" value="true"/>
+      <Parameter name="Output gamma_8" type="bool" value="true"/>
+      <Parameter name="Output gamma_9" type="bool" value="true"/>
+      <Parameter name="Output gamma_10" type="bool" value="true"/>
+      <Parameter name="Output gamma_11" type="bool" value="true"/>
+      <Parameter name="Output gamma_12" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_1" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_2" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_3" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_4" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_5" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_6" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_7" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_8" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_9" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_10" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_11" type="bool" value="true"/>
+      <Parameter name="Output gamma_dot_12" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_1" type="bool" value="true"/>      
+      <Parameter name="Output tau_hard_1" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_2" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_3" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_4" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_5" type="bool" value="true"/> 
+      <Parameter name="Output tau_hard_6" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_7" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_8" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_9" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_10" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_11" type="bool" value="true"/>
+      <Parameter name="Output tau_hard_12" type="bool" value="true"/>
+      <Parameter name="Output tau_1" type="bool" value="true"/>
+      <Parameter name="Output tau_1" type="bool" value="true"/>
+      <Parameter name="Output tau_2" type="bool" value="true"/>
+      <Parameter name="Output tau_3" type="bool" value="true"/>
+      <Parameter name="Output tau_4" type="bool" value="true"/>
+      <Parameter name="Output tau_5" type="bool" value="true"/>
+      <Parameter name="Output tau_6" type="bool" value="true"/>
+      <Parameter name="Output tau_7" type="bool" value="true"/>
+      <Parameter name="Output tau_8" type="bool" value="true"/>
+      <Parameter name="Output tau_9" type="bool" value="true"/>
+      <Parameter name="Output tau_10" type="bool" value="true"/>
+      <Parameter name="Output tau_11" type="bool" value="true"/>
+      <Parameter name="Output tau_12" type="bool" value="true"/>
+      <Parameter name="Output CP_Residual" type="bool" value="true"/>
+
+    </ParameterList>
+
+  </ParameterList>
+
+</ParameterList>

commit c17af0c0a053de88487267a3dbf77786b83fdddf

resize lattice orientations in PUMI Discretization
this fixes a crash of most SCOREC tests introduced
by bacd37a, which introduced lattice orientations
and required them to have numBuckets entries even
if they are not being used, meaning all discretizations
must resize this in all cases.

src/disc/pumi/Albany_APFDiscretization.cpp
@@ -1009,6 +1009,7 @@ void Albany::APFDiscretization::computeWorksetInfoBase(
   wsElNodeID.resize(numBuckets);
   coords.resize(numBuckets);
   sphereVolume.resize(numBuckets);
+  latticeOrientation.resize(numBuckets);
 
   // Clear map if remeshing
   if(!elemGIDws.empty()) elemGIDws.clear();