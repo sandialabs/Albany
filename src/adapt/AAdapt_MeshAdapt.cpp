e97b867a548c1759b668e264bc887919efd6d89e
Dan Ibanez <dan.a.ibanez@gmail.com>
2016-08-08 15:32:02 -0400

    MeshAdapt: clarify comment
    if the original were true we wouldn't be having
    so many problem with latent resized data.

--- src/adapt/AAdapt_MeshAdapt.cpp
+++ src/adapt/AAdapt_MeshAdapt.cpp
@@ -263,7 +263,7 @@ void AAdapt::MeshAdapt::afterAdapt()
     mesh->writeNative(outFile.c_str());
   }
 
-  // Throw away all the Albany data structures and re-build them from the mesh
+  // Throw away some Albany data structures and re-build them from the mesh
   // Note that the solution transfer for the QP fields happens in this call
   pumi_discretization->updateMesh(should_transfer_ip_data);
 
