a2fefc680e95ca616e67b406b27c727b5137e0d9
Luca Bertagna <luca.bertagna84@gmail.com>
2016-08-09 15:42:57 -0400

    Bug fix in ResponseSquaredL2Error
    I used the wrong index to fetch a dimension from a layout, probably
    a residue from a copy&paste from the side version of it.

--- src/evaluators/PHAL_ResponseSquaredL2Error_Def.hpp
+++ src/evaluators/PHAL_ResponseSquaredL2Error_Def.hpp
@@ -18,7 +18,7 @@ ResponseSquaredL2ErrorBase(Teuchos::ParameterList& p, const Teuchos::RCP<Albany:
   Teuchos::ParameterList* plist = p.get<Teuchos::ParameterList*>("Parameter List");
 
   // Gathering dimensions
-  numQPs   = dl->qp_scalar->dimension(2);
+  numQPs   = dl->qp_scalar->dimension(1);
 
   Teuchos::RCP<PHX::DataLayout> layout;
   std::string rank,fname,target_fname;
