0bc5086ede745649258385c6eef2358c894fcc65
Alejandro Mota <amota@sandia.gov>
2016-08-30 11:07:22 -0700

    LCM: Add ROL's tests. Disabled by default.

--- doc/LCM/build/trilinos-config.sh
+++ doc/LCM/build/trilinos-config.sh
@@ -65,6 +65,7 @@ cmake \
  -D EpetraExt_USING_HDF5:BOOL=OFF \
  -D Intrepid_ENABLE_TESTS:BOOL=OFF \
  -D Intrepid2_ENABLE_TESTS:BOOL=OFF \
+ -D ROL_ENABLE_TESTS:BOOL=OFF \
  -D Phalanx_INDEX_SIZE_TYPE:STRING="lcm_phalanx_index_type" \
  -D Phalanx_KOKKOS_DEVICE_TYPE:STRING="lcm_kokkos_device" \
  -D Phalanx_SHOW_DEPRECATED_WARNINGS:BOOL=OFF \
