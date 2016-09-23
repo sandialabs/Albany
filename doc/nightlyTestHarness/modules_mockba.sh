4992f4b169935b285ec18aa6fcfa9b86c14f6b1c
Irina K. Tezaur <ikalash@sandia.gov>
2016-09-22 17:26:55 -0700

    Adding modules file.

---
+++ doc/nightlyTestHarness/modules_mockba.sh
@@ -0,0 +1,7 @@
+
+module purge
+module load gcc/4.9.2/openmpi/1.8.7
+module load netcdf/4.3.2/gcc/4.9.2/openmpi/1.8.7
+module load hdf5/1.8.12/gcc/4.9.2/openmpi/1.8.7
+module load boost/1.55.0/gcc/4.9.2/base
+ 
