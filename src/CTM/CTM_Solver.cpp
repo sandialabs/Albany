decc6fe2b61a28f1a56d0a12356eb56e1343a710
Mario Juha <mariojesusjuha@gmail.com>
2016-10-10 11:09:56 -0400

    CTM: Adding input parameters related with linear algebra and time stepping algorithm.

--- src/CTM/CTM_Solver.cpp
+++ src/CTM/CTM_Solver.cpp
@@ -2,72 +2,97 @@
 #include "CTM_ThermalProblem.hpp"
 #include <Albany_DiscretizationFactory.hpp>
 #include <Albany_AbstractDiscretization.hpp>
+#include "CTM_SolutionInfo.hpp"
 
 namespace CTM {
 
-static RCP<ParameterList> get_valid_params() {
-  auto p = rcp(new ParameterList);
-  p->sublist("Discretization");
-  p->sublist("Temperature Problem");
-//  p->sublist("Mechanics");
-  p->sublist("Linear Algebra");
-  p->sublist("Time");
-}
+    static RCP<ParameterList> get_valid_params() {
+        auto p = rcp(new ParameterList);
+        p->sublist("Discretization");
+        p->sublist("Temperature Problem");
+        //  p->sublist("Mechanics");
+        p->sublist("Linear Algebra");
+        p->sublist("Time");
+    }
 
-static void validate_params(RCP<const ParameterList> p) {
-  assert(p->isSublist("Discretization"));
-  assert(p->isSublist("Temperature Problem"));
-//  assert(p->isSublist("Mechanics"));
-  assert(p->isSublist("Linear Algebra"));
-  assert(p->isSublist("Time"));
-}
+    static void validate_params(RCP<const ParameterList> p) {
+        assert(p->isSublist("Discretization"));
+        assert(p->isSublist("Temperature Problem"));
+        //  assert(p->isSublist("Mechanics"));
+        assert(p->isSublist("Linear Algebra"));
+        assert(p->isSublist("Time"));
+        //
+        const Teuchos::ParameterList &time_list = p->sublist("Time");
+        assert(time_list.isType<double>("Initial Time"));
+        assert(time_list.isType<double>("Step Size"));
+        assert(time_list.isType<int>("Number of Steps"));
+        //
+        const Teuchos::ParameterList &la_list = p->sublist("Linear Algebra");
+        assert(la_list.isType<double>("Linear Tolerance"));
+        assert(la_list.isType<int>("Linear Max. Iterations"));
+        assert(la_list.isType<int>("Linear Krylov Size"));
+        assert(la_list.isType<double>("Nonlinear Tolerance"));
+        assert(la_list.isType<int>("Nonlinear Max. Iterations"));     
+    }
 
-Solver::Solver(
-    RCP<const Teuchos_Comm> c,
-    RCP<ParameterList> p) :
-  comm(c),
-  params(p) {
+    Solver::Solver(
+            RCP<const Teuchos_Comm> c,
+            RCP<ParameterList> p) :
+    comm(c),
+    params(p) {
 
-    validate_params(params);
-    temp_params = rcpFromRef(params->sublist("Temperature Problem", true));
+        validate_params(params);
+        temp_params = rcpFromRef(params->sublist("Temperature Problem", true));
+        //
+        Teuchos::ParameterList &time_list = params->sublist("Time");
+        t_old = time_list.get<double>("Initial Time");
+        dt = time_list.get<double>("Step Size");
+        num_steps = time_list.get<int>("Number of Steps");
+        //
+        t_current = t_old + dt;
 
-    initial_setup();
-}
+        initial_setup();
+    }
 
-void Solver::initial_setup() {
+    void Solver::initial_setup() {
 
-  // create parameter libraries
-  // note: we never intend to use these objects, we create them because they
-  // are inputs to constructors for various other objects.
-  param_lib = rcp(new ParamLib);
-  dist_param_lib = rcp(new DistParamLib);
+        // create parameter libraries
+        // note: we never intend to use these objects, we create them because they
+        // are inputs to constructors for various other objects.
+        param_lib = rcp(new ParamLib);
+        dist_param_lib = rcp(new DistParamLib);
 
-  // create the mesh specs struct
-  bool explicit_scheme = false;
-  disc_factory = rcp(new Albany::DiscretizationFactory(params, comm, false));
-  mesh_specs = disc_factory->createMeshSpecs();
+        // create the mesh specs struct
+        bool explicit_scheme = false;
+        disc_factory = rcp(new Albany::DiscretizationFactory(params, comm, false));
+        mesh_specs = disc_factory->createMeshSpecs();
 
-  // create the problem objects
-  auto dim = mesh_specs[0]->numDim;
-  t_problem = rcp(new ThermalProblem(temp_params, param_lib, dim, comm));
+        // create the problem objects
+        auto dim = mesh_specs[0]->numDim;
+        t_problem = rcp(new ThermalProblem(temp_params, param_lib, dim, comm));
 
-  temp_params->validateParameters(*(t_problem->getValidProblemParameters()),0);
-  t_problem->buildProblem(mesh_specs, state_mgr);
+        temp_params->validateParameters(*(t_problem->getValidProblemParameters()), 0);
+        t_problem->buildProblem(mesh_specs, state_mgr);
 
-  // create the initial discretization object
-  auto neq = t_problem->numEquations();
-  disc = disc_factory->createDiscretization(
-      neq,
-      t_problem->getSideSetEquations(),
-      state_mgr.getStateInfoStruct(),
-      state_mgr.getSideSetStateInfoStruct(),
-      t_problem->getFieldRequirements(),
-      t_problem->getSideSetFieldRequirements(),
-      t_problem->getNullSpace());
+        // create the initial discretization object
+        auto neq = t_problem->numEquations();
+        disc = disc_factory->createDiscretization(
+                neq,
+                t_problem->getSideSetEquations(),
+                state_mgr.getStateInfoStruct(),
+                state_mgr.getSideSetStateInfoStruct(),
+                t_problem->getFieldRequirements(),
+                t_problem->getSideSetFieldRequirements(),
+                t_problem->getNullSpace());
 
-}
+        sol_info = Teuchos::rcp(new SolutionInfo());
+        //
+        sol_info->resize(disc,true);
 
-void Solver::solve() {
-}
+    }
+
+    void Solver::solve() {
+        
+    }
 
 }
--- src/CTM/CTM_Solver.hpp
+++ src/CTM/CTM_Solver.hpp
@@ -10,74 +10,81 @@
 #include <PHAL_Workset.hpp>
 
 namespace Albany {
-class AbstractProblem;
-class AbstractDiscretization;
-class DiscretizationFactory;
-class StateManager;
-struct MeshSpecsStruct;
+    class AbstractProblem;
+    class AbstractDiscretization;
+    class DiscretizationFactory;
+    class StateManager;
+    struct MeshSpecsStruct;
+}
+
+namespace CTM {
+    class SolutionInfo;
 }
 
 /// \brief All CTM symbols are contained in this namespace.
 namespace CTM {
 
-/// \brief The coupled thermomechanics solver.
-/// \details The intent of this solver is to provide an interface to solve
-/// adaptive one-way coupled thermomechanics problems by performing
-/// successive iterations of the following steps:
-///
-/// - Build all data structures for the temperature problem.
-/// - Solve the temperature problem at time t_n.
-/// - Attach the temperature field T to the discretization.
-/// - Destroy all data structures for the temperature problem.
-/// - Build all data structures for the mechanics problem.
-/// - Solve the mechnaics problem at time t_n using the temperature field T.
-/// - Attach the mechanics fields u to the discretization.
-/// - Destroy all data structures for the mechanics problem.
-/// - Adapt the mesh if desired, performing solution transfer for T and u.
-class Solver {
-
-  public:
-
-    /// \brief Constructor.
-    /// \param comm The global Teuchos communicator object.
-    /// \param params The global CTM input file parameters:
+    /// \brief The coupled thermomechanics solver.
+    /// \details The intent of this solver is to provide an interface to solve
+    /// adaptive one-way coupled thermomechanics problems by performing
+    /// successive iterations of the following steps:
     ///
-    /// Parameter Name    | Parameter Type
-    /// --------------    | --------------
-    /// "Discretization"  | sublist
-    /// "Temperature"     | sublist
-    /// "Mechanics"       | sublist
-    /// "Linear Algebra"  | sublist
-    /// "Time"            | sublist
-    Solver(RCP<const Teuchos_Comm> comm, RCP<ParameterList> params);
+    /// - Build all data structures for the temperature problem.
+    /// - Solve the temperature problem at time t_n.
+    /// - Attach the temperature field T to the discretization.
+    /// - Destroy all data structures for the temperature problem.
+    /// - Build all data structures for the mechanics problem.
+    /// - Solve the mechnaics problem at time t_n using the temperature field T.
+    /// - Attach the mechanics fields u to the discretization.
+    /// - Destroy all data structures for the mechanics problem.
+    /// - Adapt the mesh if desired, performing solution transfer for T and u.
 
-    /// \brief Solve the problem for the entire specified time domain.
-    void solve();
+    class Solver {
+    public:
 
-  private:
+        /// \brief Constructor.
+        /// \param comm The global Teuchos communicator object.
+        /// \param params The global CTM input file parameters:
+        ///
+        /// Parameter Name    | Parameter Type
+        /// --------------    | --------------
+        /// "Discretization"  | sublist
+        /// "Temperature"     | sublist
+        /// "Mechanics"       | sublist
+        /// "Linear Algebra"  | sublist
+        /// "Time"            | sublist
+        Solver(RCP<const Teuchos_Comm> comm, RCP<ParameterList> params);
 
-    RCP<const Teuchos_Comm> comm;
-    RCP<ParameterList> params;
-    RCP<ParameterList> temp_params;
-    RCP<ParameterList> mech_params;
+        /// \brief Solve the problem for the entire specified time domain.
+        void solve();
 
-    RCP<ParamLib> param_lib;
-    RCP<DistParamLib> dist_param_lib;
+    private:
 
-    RCP<Albany::DiscretizationFactory> disc_factory;
-    RCP<Albany::AbstractDiscretization> disc;
-    ArrayRCP<RCP<Albany::MeshSpecsStruct> > mesh_specs;
-    Albany::StateManager state_mgr;
+        RCP<const Teuchos_Comm> comm;
+        RCP<ParameterList> params;
+        RCP<ParameterList> temp_params;
+        RCP<ParameterList> mech_params;
 
-    RCP<Albany::AbstractProblem> t_problem;
-    RCP<Albany::AbstractProblem> m_problem;
+        RCP<ParamLib> param_lib;
+        RCP<DistParamLib> dist_param_lib;
 
-    double t_current;
-    double t_previous;
+        RCP<Albany::DiscretizationFactory> disc_factory;
+        RCP<Albany::AbstractDiscretization> disc;
+        ArrayRCP<RCP<Albany::MeshSpecsStruct> > mesh_specs;
+        Albany::StateManager state_mgr;
+        Teuchos::RCP<SolutionInfo> sol_info;
 
-    void initial_setup();
+        RCP<Albany::AbstractProblem> t_problem;
+        RCP<Albany::AbstractProblem> m_problem;
 
-};
+        double t_old;
+        double t_current;
+        double dt;
+        int num_steps;
+
+        void initial_setup();
+
+    };
 
 }
 
