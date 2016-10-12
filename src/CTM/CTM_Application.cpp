d18847931c1d820abdedadffe5c329e324e9ea02
Mario Juha <mariojesusjuha@gmail.com>
2016-10-12 12:14:59 -0400

    CTM: Work in progress. Adeed post registration setup function in CTM_Application.

--- src/CTM/CTM_Application.cpp
+++ src/CTM/CTM_Application.cpp
@@ -28,6 +28,11 @@ namespace CTM {
 
         nfm = problem->getNeumannFieldManager();
 
+        meshSpecs = disc->getMeshStruct()->getMeshSpecs();
+        
+        //
+        explicit_scheme = disc->isExplicitScheme();
+
         // get initial conditions
         Teuchos::ArrayRCP<
                 Teuchos::ArrayRCP<Teuchos::ArrayRCP<Teuchos::ArrayRCP<int> > > > wsElNodeEqID =
@@ -35,8 +40,8 @@ namespace CTM {
         Teuchos::ArrayRCP<Teuchos::ArrayRCP<Teuchos::ArrayRCP<double*> > > coords =
                 disc->getCoords();
         Teuchos::ArrayRCP<std::string> wsEBNames = disc->getWsEBNames();
-        const int numDim = disc->getNumDim();
-        const int neq = disc->getNumEq();
+        numDim = disc->getNumDim();
+        neq = disc->getNumEq();
 
         // Create problem PL
         Teuchos::RCP<Teuchos::ParameterList> problemParams =
@@ -64,14 +69,14 @@ namespace CTM {
             }
         }
 #endif
-//        // get mesh
-//	Teuchos::RCP<Albany::SimDiscretization> sim_disc =
-//	  Teuchos::rcp_dynamic_cast<Albany::SimDiscretization>(disc);
-//        Teuchos::RCP<Albany::APFMeshStruct> apf_ms = sim_disc->getAPFMeshStruct();
-//        
-//        apf::Mesh* apf_m = apf_ms->getMesh();
-//        
-//        apf::writeVtkFiles("out",apf_m);
+        //        // get mesh
+        //	Teuchos::RCP<Albany::SimDiscretization> sim_disc =
+        //	  Teuchos::rcp_dynamic_cast<Albany::SimDiscretization>(disc);
+        //        Teuchos::RCP<Albany::APFMeshStruct> apf_ms = sim_disc->getAPFMeshStruct();
+        //        
+        //        apf::Mesh* apf_m = apf_ms->getMesh();
+        //        
+        //        apf::writeVtkFiles("out",apf_m);
 
     }
 
@@ -79,4 +84,100 @@ namespace CTM {
     ~Application() {
         //
     }
+
+    Teuchos::RCP<Albany::AbstractDiscretization>
+    Application::getDiscretization() const {
+        return disc;
+    }
+
+    Teuchos::RCP<Albany::AbstractProblem>
+    Application::getProblem() const {
+        return problem;
+    }
+
+    void Application::loadWorksetSidesetInfo(PHAL::Workset& workset, const int ws) {
+
+        workset.sideSets = Teuchos::rcpFromRef(disc->getSideSets(ws));
+
+    }
+
+    void Application::postRegSetup(std::string eval) {
+        if (setupSet.find(eval) != setupSet.end()) return;
+
+        setupSet.insert(eval);
+
+        if (eval == "Residual") {
+            for (int ps = 0; ps < fm.size(); ps++)
+                fm[ps]->postRegistrationSetupForType<PHAL::AlbanyTraits::Residual>(eval);
+            if (dfm != Teuchos::null)
+                dfm->postRegistrationSetupForType<PHAL::AlbanyTraits::Residual>(eval);
+            if (nfm != Teuchos::null)
+                for (int ps = 0; ps < nfm.size(); ps++)
+                    nfm[ps]->postRegistrationSetupForType<PHAL::AlbanyTraits::Residual>(eval);
+        } else if (eval == "Jacobian") {
+            for (int ps = 0; ps < fm.size(); ps++) {
+                std::vector<PHX::index_size_type> derivative_dimensions;
+                // get derivative dimension
+                int node_count = this->getEnrichedMeshSpecs()[ps].get()->ctd.node_count;
+                int derivDimenensions = neq * node_count;
+                derivative_dimensions.push_back(derivDimenensions);
+                fm[ps]->setKokkosExtendedDataTypeDimensions<PHAL::AlbanyTraits::Jacobian>(derivative_dimensions);
+                fm[ps]->postRegistrationSetupForType<PHAL::AlbanyTraits::Jacobian>(eval);
+                if (nfm != Teuchos::null && ps < nfm.size()) {
+                    nfm[ps]->setKokkosExtendedDataTypeDimensions<PHAL::AlbanyTraits::Jacobian>(derivative_dimensions);
+                    nfm[ps]->postRegistrationSetupForType<PHAL::AlbanyTraits::Jacobian>(eval);
+                }
+            }
+            if (dfm != Teuchos::null) {
+                //amb Need to look into this. What happens with DBCs in meshes having
+                // different element types?
+                std::vector<PHX::index_size_type> derivative_dimensions;
+                // get derivative dimension
+                int node_count = this->getEnrichedMeshSpecs()[0].get()->ctd.node_count;
+                int derivDimenensions = neq * node_count;
+                derivative_dimensions.push_back(derivDimenensions);
+                dfm->setKokkosExtendedDataTypeDimensions<PHAL::AlbanyTraits::Jacobian>(derivative_dimensions);
+                dfm->postRegistrationSetupForType<PHAL::AlbanyTraits::Jacobian>(eval);
+            }
+        } else
+            TEUCHOS_TEST_FOR_EXCEPTION(eval != "Known Evaluation Name", std::logic_error,
+                "Error in setup call \n" << " Unrecognized name: " << eval << std::endl);
+
+
+        // Write out Phalanx Graph if requested, on Proc 0, for Resid and Jacobian
+        bool alreadyWroteResidPhxGraph = false;
+        bool alreadyWroteJacPhxGraph = false;
+
+        if (phxGraphVisDetail > 0) {
+            bool detail = false;
+            if (phxGraphVisDetail > 1) detail = true;
+
+            if ((eval == "Residual") && (alreadyWroteResidPhxGraph == false)) {
+                *out << "Phalanx writing graphviz file for graph of Residual fill (detail ="
+                        << phxGraphVisDetail << ")" << std::endl;
+                *out << "Process using 'dot -Tpng -O phalanx_graph' \n" << std::endl;
+                for (int ps = 0; ps < fm.size(); ps++) {
+                    std::stringstream pg;
+                    pg << "phalanx_graph_" << ps;
+                    fm[ps]->writeGraphvizFile<PHAL::AlbanyTraits::Residual>(pg.str(), detail, detail);
+                }
+                alreadyWroteResidPhxGraph = true;
+                //      phxGraphVisDetail = -1;
+            } else if ((eval == "Jacobian") && (alreadyWroteJacPhxGraph == false)) {
+                *out << "Phalanx writing graphviz file for graph of Jacobian fill (detail ="
+                        << phxGraphVisDetail << ")" << std::endl;
+                *out << "Process using 'dot -Tpng -O phalanx_graph' \n" << std::endl;
+                for (int ps = 0; ps < fm.size(); ps++) {
+                    std::stringstream pg;
+                    pg << "phalanx_graph_jac_" << ps;
+                    fm[ps]->writeGraphvizFile<PHAL::AlbanyTraits::Jacobian>(pg.str(), detail, detail);
+                }
+                alreadyWroteJacPhxGraph = true;
+            }
+            //Stop writing out phalanx graphs only when a Jacobian and a Residual graph has been written out
+            if ((alreadyWroteResidPhxGraph == true) && (alreadyWroteJacPhxGraph == true))
+                phxGraphVisDetail = -2;
+        }
+    }
+
 }
--- src/CTM/CTM_Application.hpp
+++ src/CTM/CTM_Application.hpp
@@ -1,14 +1,18 @@
 #ifndef CTM_APPLICATION_HPP
 #define CTM_APPLICATION_HPP
 
+#include <vector>
+//
 #include "Teuchos_RCP.hpp"
 #include "Teuchos_ParameterList.hpp"
 //
 #include "PHAL_AlbanyTraits.hpp"
 #include "PHAL_Workset.hpp"
 #include "Phalanx.hpp"
+#include "PHAL_Utilities.hpp"
 //
 #include "Albany_AbstractProblem.hpp"
+#include "Albany_AbstractDiscretization.hpp"
 
 namespace CTM {
 
@@ -17,29 +21,64 @@ namespace CTM {
     class Application {
     public:
         Application(Teuchos::RCP<Teuchos::ParameterList> p,
-                Teuchos::RCP<SolutionInfo> sinfo, 
+                Teuchos::RCP<SolutionInfo> sinfo,
                 Teuchos::RCP<Albany::AbstractProblem> prob,
                 Teuchos::RCP<Albany::AbstractDiscretization> d);
         // prohibit copy constructor
         Application(const Application& app) = delete;
         //! Destructor
         ~Application();
+
+        //! Get underlying abstract discretization
+        Teuchos::RCP<Albany::AbstractDiscretization> getDiscretization() const;
+
+        //! Get problem object
+        Teuchos::RCP<Albany::AbstractProblem> getProblem() const;
+
+        Teuchos::ArrayRCP<Teuchos::RCP<Albany::MeshSpecsStruct> >
+        getEnrichedMeshSpecs() const {
+            return meshSpecs;
+        }
+
+        //
+
+        int getNumEquations() const {
+            return neq;
+        }
+        //
+
+        int getSpatialDimension() const {
+            return numDim;
+        }
+
+        //! Routine to get workset (bucket) size info needed by all Evaluation types
+        template <typename EvalT>
+        void loadWorksetBucketInfo(PHAL::Workset& workset, const int& ws);
+
+        //! Routine to load common sideset info into workset
+        void loadWorksetSidesetInfo(PHAL::Workset& workset, const int ws);
+
+        void postRegSetup(std::string eval);
+
     private:
         // Problem parameter list
         Teuchos::RCP<Teuchos::ParameterList> params;
-        
+
         // solution info
         Teuchos::RCP<SolutionInfo> solution_info;
 
         //! Output stream, defaults to pronting just Proc 0
         Teuchos::RCP<Teuchos::FancyOStream> out;
-        
+
         // Problem to be solved
         Teuchos::RCP<Albany::AbstractProblem> problem;
-        
+
         // discretization
         Teuchos::RCP<Albany::AbstractDiscretization> disc;
 
+        //! mesh specs
+        Teuchos::ArrayRCP<Teuchos::RCP<Albany::MeshSpecsStruct> > meshSpecs;
+        
         //! Phalanx Field Manager for volumetric fills
         Teuchos::ArrayRCP<Teuchos::RCP<PHX::FieldManager<PHAL::AlbanyTraits> > > fm;
 
@@ -49,7 +88,49 @@ namespace CTM {
         //! Phalanx Field Manager for Neumann Conditions
         Teuchos::ArrayRCP<Teuchos::RCP<PHX::FieldManager<PHAL::AlbanyTraits> > > nfm;
 
+        std::set<std::string> setupSet;
+        mutable int phxGraphVisDetail;
+        mutable int stateGraphVisDetail;
+        bool explicit_scheme;
+
+        unsigned int neq, numDim;
+
     };
+
+    template <typename EvalT>
+    void Application::loadWorksetBucketInfo(PHAL::Workset& workset,
+            const int& ws) {
+
+        const Albany::WorksetArray<Teuchos::ArrayRCP<Teuchos::ArrayRCP<Teuchos::ArrayRCP<LO> > > >::type&
+                wsElNodeEqID = disc->getWsElNodeEqID();
+        const Albany::WorksetArray<Teuchos::ArrayRCP<Teuchos::ArrayRCP<GO> > >::type&
+                wsElNodeID = disc->getWsElNodeID();
+        const Albany::WorksetArray<Teuchos::ArrayRCP<Teuchos::ArrayRCP<double*> > >::type&
+                coords = disc->getCoords();
+        const Albany::WorksetArray<std::string>::type& wsEBNames = disc->getWsEBNames();
+
+        workset.numCells = wsElNodeEqID[ws].size();
+        workset.wsElNodeEqID = wsElNodeEqID[ws];
+        workset.wsElNodeID = wsElNodeID[ws];
+        workset.wsCoords = coords[ws];
+        workset.EBName = wsEBNames[ws];
+        workset.wsIndex = ws;
+
+        workset.local_Vp.resize(workset.numCells);
+
+        // Sidesets are integrated within the Cells
+        loadWorksetSidesetInfo(workset, ws);
+        //        workset.stateArrayPtr = &stateMgr.getStateArray(Albany::StateManager::ELEM, ws);
+
+
+        //  workset.wsElNodeEqID_kokkos =
+        Kokkos::View<int***, PHX::Device> wsElNodeEqID_kokkos("wsElNodeEqID_kokkos", workset.numCells, wsElNodeEqID[ws][0].size(), wsElNodeEqID[ws][0][0].size());
+        workset.wsElNodeEqID_kokkos = wsElNodeEqID_kokkos;
+        for (int i = 0; i < workset.numCells; i++)
+            for (int j = 0; j < wsElNodeEqID[ws][0].size(); j++)
+                for (int k = 0; k < wsElNodeEqID[ws][0][0].size(); k++)
+                    workset.wsElNodeEqID_kokkos(i, j, k) = workset.wsElNodeEqID[i][j][k];
+    }
 
 }
 
