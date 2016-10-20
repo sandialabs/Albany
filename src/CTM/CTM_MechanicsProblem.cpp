f654240fd1db02b317d1a09a662721f17f1e826e
Mario Juha <mariojesusjuha@gmail.com>
2016-10-20 17:31:09 -0400

    CTM: Work in progress. Continue implementation of CTM_Mechanics problem.

--- src/CTM/CTM_MechanicsProblem.cpp
+++ src/CTM/CTM_MechanicsProblem.cpp
@@ -16,7 +16,7 @@ namespace CTM {
     isTransient_(false) {
 
         *out << "Problem name = Mechanics Problem \n";
-        this->setNumEquations(1);
+        this->setNumEquations(num_dims);
         material_db_ = LCM::createMaterialDatabase(params, comm);
 
         /*This is needed because right now the function constructBCEvaluators
@@ -30,6 +30,8 @@ namespace CTM {
         // Is it a transient analysis?
         if (params->isParameter("Transient")) {
             isTransient_ = params->get<bool>("Transient", true);
+            TEUCHOS_TEST_FOR_EXCEPTION(isTransient_,std::out_of_range,
+                    "We apologize but transient analysis is not implemented yet for CTM_MechanicsProblem");
             *out << "Solving a transient analysis" << std::endl;
         }
     }
@@ -104,55 +106,89 @@ namespace CTM {
     void MechanicsProblem::constructNeumannEvaluators(
             const RCP<Albany::MeshSpecsStruct>& mesh_specs) {
 
-        Albany::BCUtils<Albany::NeumannTraits> bcUtils;
+        // Note: we only enter this function if sidesets are defined in the mesh file
+        // i.e. meshSpecs.ssNames.size() > 0
 
-        if (!bcUtils.haveBCSpecified(this->params))
+        Albany::BCUtils<Albany::NeumannTraits> neuUtils;
+
+        // Check to make sure that Neumann BCs are given in the input file
+        if (!neuUtils.haveBCSpecified(this->params)) {
             return;
+        }
 
-        std::vector<std::string> bcNames(neq);
-        Teuchos::ArrayRCP<std::string> dof_names(neq);
-        Teuchos::Array<Teuchos::Array<int> > offsets;
-        offsets.resize(neq);
+        // Construct BC evaluators for all side sets and names
+        // Note that the string index sets up the equation offset,
+        // so ordering is important
 
-        bcNames[0] = "T";
-        dof_names[0] = "Temperature";
-        offsets[0].resize(1);
-        offsets[0][0] = 0;
+        std::vector<std::string> neumannNames(neq + 1);
+        Teuchos::Array<Teuchos::Array<int>> offsets;
+        offsets.resize(neq + 1);
+
+        int index = 0;
+
+
+        neumannNames[index] = "sig_x";
+        offsets[index].resize(1);
+        offsets[index++][0] = 0;
+
+        // The Neumann BC code uses offsets[neq].size() as num dim, so use num_dims_
+        // here rather than neq.
+        offsets[neq].resize(num_dims);
+        offsets[neq][0] = 0;
+
+        if (num_dims > 1) {
+            neumannNames[index] = "sig_y";
+            offsets[index].resize(1);
+            offsets[index++][0] = 1;
+            offsets[neq][1] = 1;
+        }
+
+        if (num_dims > 2) {
+            neumannNames[index] = "sig_z";
+            offsets[index].resize(1);
+            offsets[index++][0] = 2;
+            offsets[neq][2] = 2;
+        }
+
+
+        neumannNames[neq] = "all";
 
         // Construct BC evaluators for all possible names of conditions
-        // Should only specify flux vector components (dudx, dudy, dudz), or dudn, not both
-        std::vector<std::string> condNames(4);
-        //dudx, dudy, dudz, dudn, scaled jump (internal surface), or robin (like DBC plus scaled jump)
+        // Should only specify flux vector components (dudx, dudy, dudz),
+        // or dudn, not both
+        std::vector<std::string> condNames(3); //dudx, dudy, dudz, dudn, P
+        Teuchos::ArrayRCP<std::string> dof_names(1);
+        dof_names[0] = "Displacement";
 
         // Note that sidesets are only supported for two and 3D currently
         if (num_dims == 2)
-            condNames[0] = "(dudx, dudy)";
+            condNames[0] = "(t_x, t_y)";
         else if (num_dims == 3)
-            condNames[0] = "(dudx, dudy, dudz)";
+            condNames[0] = "(t_x, t_y, t_z)";
         else
             TEUCHOS_TEST_FOR_EXCEPTION(true, Teuchos::Exceptions::InvalidParameter,
-                std::endl << "Error: Sidesets only supported in 2 and 3D." << std::endl);
+                '\n' << "Error: Sidesets only supported in 2 and 3D." << '\n');
 
         condNames[1] = "dudn";
+        condNames[2] = "P";
 
-        condNames[2] = "scaled jump";
+        // The resize below is not appropriate for MechanicsProblem
+        // not sure what the right thing to do is
+        nfm.resize(1); // Elasticity problem only has one element block
 
-        condNames[3] = "robin";
-
-        /*This is needed because right now the function constructBCEvaluators
-         require a QCAD::MaterialDataBase instead of LCM::MaterialDataBase
-         this need to be corrected.*/
-        Teuchos::RCP<QCAD::MaterialDatabase> materialDB
-                = Teuchos::rcp(new QCAD::MaterialDatabase(materialFileName_, comm_));
-        nfm.resize(1); // Heat problem only has one physics set   
-        nfm[0] = bcUtils.constructBCEvaluators(mesh_specs, bcNames, dof_names, false, 0,
-                condNames, offsets, dl, this->params, Teuchos::null, materialDB);
-
-        // release temporal material data base
-        materialDB = Teuchos::null;
-
+        nfm[0] = neuUtils.constructBCEvaluators(
+                mesh_specs,
+                neumannNames,
+                dof_names,
+                true, // isVectorField
+                0, // offsetToFirstDOF
+                condNames,
+                offsets,
+                dl,
+                this->params,
+                this->paramLib);
     }
-
+        
     //    void MechanicsProblem::getAllocatedStates(
     //            ArrayRCP<ArrayRCP<RCP<FC> > > old_state,
     //            ArrayRCP<ArrayRCP<RCP<FC> > > new_state) const {
