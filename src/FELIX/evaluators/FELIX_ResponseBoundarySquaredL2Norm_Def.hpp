b78b4625a8ddd7d812966e5715f2f13e5bc3c0a7
Mauro Perego <mperego@sandia.gov>
2016-10-23 14:34:48 -0600

    FELIX: Add Response to compute Squared L2 Norm on Boundary (using lumped mass matrix)

---
+++ src/FELIX/evaluators/FELIX_ResponseBoundarySquaredL2Norm_Def.hpp
@@ -0,0 +1,177 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include <fstream>
+#include "Teuchos_TestForException.hpp"
+#include "Phalanx_DataLayout.hpp"
+#include "Teuchos_CommHelpers.hpp"
+#include "Phalanx.hpp"
+#include "PHAL_Utilities.hpp"
+
+template<typename EvalT, typename Traits>
+FELIX::ResponseBoundarySquaredL2Norm<EvalT, Traits>::
+ResponseBoundarySquaredL2Norm(Teuchos::ParameterList& p, const Teuchos::RCP<Albany::Layouts>& dl)
+{
+  // get and validate Response parameter list
+  Teuchos::ParameterList* plist = p.get<Teuchos::ParameterList*>("Parameter List");
+  Teuchos::RCP<const Teuchos::ParameterList> reflist = this->getValidResponseParameters();
+  plist->validateParameters(*reflist, 0);
+
+  Teuchos::RCP<Teuchos::ParameterList> paramList = p.get<Teuchos::RCP<Teuchos::ParameterList> >("Parameters From Problem");
+  Teuchos::RCP<ParamLib> paramLib = paramList->get< Teuchos::RCP<ParamLib> > ("Parameter Library");
+
+
+  // Setting up the fields required by the regularizations
+  sideName = paramList->get<std::string> ("Basal Side Name");
+
+  TEUCHOS_TEST_FOR_EXCEPTION (dl->side_layouts.find(sideName)==dl->side_layouts.end(), std::runtime_error,
+                              "Error! Side data layout not found.\n");
+  Teuchos::RCP<Albany::Layouts> dl_side = dl->side_layouts.at(sideName);
+
+  const std::string& w_side_measure_name = paramList->get<std::string>("Weighted Measure 2D Name");
+  const std::string& solution_name       = plist->get<std::string>("Field Name");
+
+  solution        = PHX::MDField<ScalarT,Cell,Side,Node>(solution_name, dl_side->node_scalar);
+  w_side_measure  = PHX::MDField<MeshScalarT,Cell,Side,QuadPoint>(w_side_measure_name, dl_side->qp_scalar);
+
+  scaling = plist->get<double>("Scaling Coefficient", 1.0);
+
+
+  // Get Dimensions
+  numSideNodes  = dl_side->node_scalar->dimension(2);
+  numSideDims   = dl_side->node_gradient->dimension(3);
+  numSideQPs = dl_side->qp_scalar->dimension(2);
+
+  this->addDependentField(w_side_measure);
+  this->addDependentField(solution);
+
+  this->setName("Response Boundary Squared L2 Norm" + PHX::typeAsString<EvalT>());
+
+  using PHX::MDALayout;
+
+  // Setup scatter evaluator
+  p.set("Stand-alone Evaluator", false);
+  std::string local_response_name = "Local Response Boundary Laplacian Regularization";
+  std::string global_response_name = "Global Response Boundary Laplacian Regularization";
+  int worksetSize = dl->qp_scalar->dimension(0);
+  int responseSize = 1;
+  Teuchos::RCP<PHX::DataLayout> local_response_layout = Teuchos::rcp(new MDALayout<Cell, Dim>(worksetSize, responseSize));
+  Teuchos::RCP<PHX::DataLayout> global_response_layout = Teuchos::rcp(new MDALayout<Dim>(responseSize));
+  PHX::Tag<ScalarT> local_response_tag(local_response_name, local_response_layout);
+  PHX::Tag<ScalarT> global_response_tag(global_response_name, global_response_layout);
+  p.set("Local Response Field Tag", local_response_tag);
+  p.set("Global Response Field Tag", global_response_tag);
+  PHAL::SeparableScatterScalarResponse<EvalT, Traits>::setup(p, dl);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+void FELIX::ResponseBoundarySquaredL2Norm<EvalT, Traits>::
+postRegistrationSetup(typename Traits::SetupData d, PHX::FieldManager<Traits>& fm)
+{
+  this->utils.setFieldData(solution, fm);
+  this->utils.setFieldData(w_side_measure, fm);
+
+  PHAL::SeparableScatterScalarResponse<EvalT, Traits>::postRegistrationSetup(d, fm);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+void FELIX::ResponseBoundarySquaredL2Norm<EvalT, Traits>::preEvaluate(typename Traits::PreEvalData workset)
+{
+  PHAL::set(this->global_response, 0.0);
+
+  p_reg = 0;
+
+  // Do global initialization
+  PHAL::SeparableScatterScalarResponse<EvalT, Traits>::preEvaluate(workset);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+void FELIX::ResponseBoundarySquaredL2Norm<EvalT, Traits>::evaluateFields(typename Traits::EvalData workset)
+{
+  TEUCHOS_TEST_FOR_EXCEPTION (workset.sideSets==Teuchos::null, std::logic_error,
+                              "Side sets defined in input file but not properly specified on the mesh" << std::endl);
+
+  // Zero out local response
+  PHAL::set(this->local_response, 0.0);
+
+  // ----------------- Surface side ---------------- //
+
+
+
+  // --------------- Regularization term on the basal side ----------------- //
+
+  if (workset.sideSets->find(sideName) != workset.sideSets->end())
+  {
+    const std::vector<Albany::SideStruct>& sideSet = workset.sideSets->at(sideName);
+    for (auto const& it_side : sideSet)
+    {
+      // Get the local data of side and cell
+      const int cell = it_side.elem_LID;
+      const int side = it_side.side_local_id;
+
+
+      MeshScalarT trapezoid_weight = 0;
+      for (int qp=0; qp<numSideQPs; ++qp)
+        trapezoid_weight += w_side_measure(cell,side, qp);
+      trapezoid_weight /= numSideNodes;
+
+      ScalarT t = 0;
+      for (int inode=0; inode<numSideNodes; ++inode) {
+        //using trapezoidal rule to get diagonal mass matrix
+        t += std::pow(solution(cell,side,inode,offset),2)* trapezoid_weight;
+      }
+      this->local_response(cell, 0) += t*scaling;
+      this->global_response(0) += t*scaling;
+      p_reg += t*scaling;
+    }
+  }
+
+  // Do any local-scattering necessary
+  PHAL::SeparableScatterScalarResponse<EvalT, Traits>::evaluateFields(workset);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+void FELIX::ResponseBoundarySquaredL2Norm<EvalT, Traits>::postEvaluate(typename Traits::PostEvalData workset) {
+
+  //amb Deal with op[], pointers, and reduceAll.
+  PHAL::reduceAll<ScalarT>(*workset.comm, Teuchos::REDUCE_SUM,
+                           this->global_response);
+  PHAL::reduceAll<ScalarT>(*workset.comm, Teuchos::REDUCE_SUM, p_reg);
+  reg = p_reg;
+
+
+  if(workset.comm->getRank()   ==0)
+    std::cout << "Boundary Squared L2 Norm: "<< Sacado::ScalarValue<ScalarT>::eval(reg) <<std::endl;
+
+
+  // Do global scattering
+  PHAL::SeparableScatterScalarResponse<EvalT, Traits>::postEvaluate(workset);
+}
+
+// **********************************************************************
+template<typename EvalT, typename Traits>
+Teuchos::RCP<const Teuchos::ParameterList> FELIX::ResponseBoundarySquaredL2Norm<EvalT, Traits>::getValidResponseParameters() const
+{
+  Teuchos::RCP<Teuchos::ParameterList> validPL = rcp(new Teuchos::ParameterList("Valid ResponseBoundarySquaredL2Norm Params"));
+  Teuchos::RCP<const Teuchos::ParameterList> baseValidPL = PHAL::SeparableScatterScalarResponse<EvalT, Traits>::getValidResponseParameters();
+  validPL->setParameters(*baseValidPL);
+
+  validPL->set<std::string>("Name", "", "Name of response function");
+  validPL->set<std::string>("Field Name", "Solution", "Not used");
+  validPL->set<double>("Scaling Coefficient", 1.0, "Coefficient that scales the response");
+  validPL->set<int>("Phalanx Graph Visualization Detail", 0, "Make dot file to visualize phalanx graph");
+  validPL->set<std::string>("Description", "", "Description of this response used by post processors");
+
+  validPL->set<std::string> ("Basal Side Name", "", "Name of the side set correspongint to the ice-bedrock interface");
+
+  return validPL;
+}
+// **********************************************************************
+
