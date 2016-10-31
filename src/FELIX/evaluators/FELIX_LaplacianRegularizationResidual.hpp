11ca3431985d26e67a28db29f8f2a162ee047616
Mauro Perego <mperego@sandia.gov>
2016-10-31 16:46:17 -0600

    FELIX: Added problem "LaplacianSampling" and needed evaluator to sample from a Gaussian distribution with covariance C = L^{-2} where L is the laplace operator plus a reaction term for well-posedness. The problem takes samples from a weighted normal distribution Y (Y= M^{-1/2} X, X ~ N(0,I) ), and return a sample for the Gaussian distribution of zero mean and covariance C. Here M is a lumped mass matrix.
    Added input file examples/FELIX/FO_GIS/input_sampling.xml.

---
+++ src/FELIX/evaluators/FELIX_LaplacianRegularizationResidual.hpp
@@ -0,0 +1,61 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#ifndef FELIX_LAPLACIAN_REGULAIZATION_RESIDUAL_HPP
+#define FELIX_LAPLACIAN_REGULAIZATION_RESIDUAL_HPP
+
+//#include "FELIX_MeshRegion.hpp"
+#include "PHAL_SeparableScatterScalarResponse.hpp"
+
+namespace FELIX {
+/**
+ * \brief Response Description
+ */
+  template<typename EvalT, typename Traits>
+  class LaplacianRegularizationResidual : public PHX::EvaluatorWithBaseImpl<Traits>,
+        public PHX::EvaluatorDerived<EvalT, Traits>
+  {
+  public:
+    typedef typename EvalT::ScalarT ScalarT;
+    typedef typename EvalT::MeshScalarT MeshScalarT;
+    typedef typename EvalT::ParamScalarT ParamScalarT;
+
+    LaplacianRegularizationResidual(Teuchos::ParameterList& p,
+       const Teuchos::RCP<Albany::Layouts>& dl);
+
+    void postRegistrationSetup(typename Traits::SetupData d,
+             PHX::FieldManager<Traits>& vm);
+
+    void evaluateFields(typename Traits::EvalData d);
+
+
+  private:
+
+ //   std::string sideName;
+ //   std::vector<std::vector<int> >  sideNodes;
+    
+    int numCells;
+    int numNodes;
+    int numQPs;
+    int cellDim;
+ //   int sideDim;
+
+    PHX::MDField<ScalarT>  field;
+    PHX::MDField<ScalarT>       gradField;
+    PHX::MDField<ParamScalarT>  forcing;
+    PHX::MDField<MeshScalarT>   gradBF;
+    PHX::MDField<MeshScalarT>   w_measure;
+
+    PHX::MDField<ScalarT> residual;
+
+    ScalarT p_reg, reg;
+    double laplacian_coeff, mass_coeff;
+    int offset;
+  };
+
+} // Namespace FELIX
+
+#endif // FELIX_RESPONSE_SURFACE_VELOCITY_MISMATCH_HPP
