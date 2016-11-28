164292fa5b454bceb2d94bb413392d721d20511c
Mauro Perego <mperego@sandia.gov>
2016-11-27 22:55:04 -0700

    FELIX, Enthalpy problem, added two evaluators for computing 1. the residual of the vertical velocity (to compute directly the vertical velocity instead of its z derivative) and   2. the basal residual which include a Robin term to "force" the enthalpy to be the one at the melting point, when ice is melting at the bed but still cold inside.

---
+++ src/FELIX/evaluators/FELIX_EnthalpyBasalResid.hpp
@@ -0,0 +1,77 @@
+/*
+ * FELIX_EnthalpyBasalResid.hpp
+ *
+ *  Created on: May 31, 2016
+ *      Author: abarone
+ */
+
+#ifndef FELIX_ENTHALPY_BASAL_RESID_HPP_
+#define FELIX_ENTHALPY_BASAL_RESID_HPP_
+
+
+#include "Phalanx_config.hpp"
+#include "Phalanx_Evaluator_WithBaseImpl.hpp"
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
+#include "Albany_Layouts.hpp"
+
+namespace FELIX
+{
+
+  /** \brief Geotermal Flux Heat Evaluator
+
+    This evaluator evaluates the production of heat coming from the earth
+   */
+
+  template<typename EvalT, typename Traits, typename Type>
+  class EnthalpyBasalResid : public PHX::EvaluatorWithBaseImpl<Traits>,
+  public PHX::EvaluatorDerived<EvalT, Traits>
+  {
+  public:
+    EnthalpyBasalResid(const Teuchos::ParameterList& p, const Teuchos::RCP<Albany::Layouts>& dl);
+
+    void postRegistrationSetup (typename Traits::SetupData d, PHX::FieldManager<Traits>& fm);
+
+    void evaluateFields(typename Traits::EvalData d);
+
+  private:
+    typedef typename EvalT::ScalarT ScalarT;
+    typedef typename EvalT::MeshScalarT MeshScalarT;
+    typedef typename EvalT::ParamScalarT ParamScalarT;
+
+    // Input:
+    PHX::MDField<ParamScalarT,Cell,Side,QuadPoint>    	 geoFlux;     // [W m^{-2}] = [Pa m s^{-1}]
+    PHX::MDField<ParamScalarT,Cell,Side,QuadPoint>       beta; // [kPa m / yr]
+    PHX::MDField<ScalarT,Cell,Side,QuadPoint>            basal_dTdz; // [K  km^{-1}]
+    PHX::MDField<ScalarT,Cell,Side, QuadPoint>           enthalpy;  //[MW s m^{-3}]
+    PHX::MDField<ParamScalarT,Cell, Side, QuadPoint>     enthalpyHs;  //[MW s m^{-3}]
+    PHX::MDField<Type,Cell,Side,QuadPoint,VecDim>        velocity; // [m yr^{-1}
+    PHX::MDField<ScalarT,Cell,Side,QuadPoint>            verticalVel; // [m y^{-1}]
+    PHX::MDField<RealType,Cell,Side,Node,QuadPoint>   	 BF;          // []
+    PHX::MDField<RealType,Cell,Side,Node,QuadPoint,Dim>  GradBF;      // [km^{-1}
+    PHX::MDField<MeshScalarT,Cell,Side,QuadPoint>     	 w_measure;   // [km^2]
+    PHX::MDField<ScalarT,Cell,Node>                      diffEnth;  //[MW s m^{-3}]
+    PHX::MDField<ScalarT,Dim>                            homotopy;
+
+    // Output:
+    PHX::MDField<ScalarT,Cell,Node> enthalpyBasalResid;      // [MW] = [k^{-2} kPa s^{-1} km^3]
+    PHX::MDField<ScalarT,Cell,Node> enthalpyBasalResidSUPG;  // [MW s^{-1}] = [k^{-2} kPa s^{-2} km^3]
+
+    std::vector<std::vector<int> >  sideNodes;
+    std::string                     basalSideName;
+
+    int numCellNodes;
+    int numSideNodes;
+    int numSideQPs;
+    int sideDim;
+    int vecDimFO;
+
+    double a;
+
+    bool haveSUPG;
+    bool isGeoFluxConst;
+  };
+
+}	// end namespace FELIX
+
+#endif /* FELIX_ENTHALPY_BASAL_RESID_HPP_ */
---
+++ src/FELIX/evaluators/FELIX_w_Resid.hpp
@@ -0,0 +1,59 @@
+/*
+ * FELIX_w_Resid.hpp
+ *
+ *  Created on: Jun 7, 2016
+ *      Author: abarone
+ */
+
+#ifndef FELIX_W_RESID_HPP_
+#define FELIX_W_RESID_HPP_
+
+#include "Phalanx_config.hpp"
+#include "Phalanx_Evaluator_WithBaseImpl.hpp"
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
+#include "Albany_Layouts.hpp"
+
+namespace FELIX
+{
+
+  template<typename EvalT, typename Traits, typename VelocityType>
+  class w_Resid : public PHX::EvaluatorWithBaseImpl<Traits>,
+  public PHX::EvaluatorDerived<EvalT, Traits>
+  {
+  public:
+    w_Resid (const Teuchos::ParameterList& p, const Teuchos::RCP<Albany::Layouts>& dl);
+
+    void postRegistrationSetup (typename Traits::SetupData d, PHX::FieldManager<Traits>& fm);
+
+    void evaluateFields(typename Traits::EvalData d);
+
+  private:
+    typedef typename EvalT::ScalarT ScalarT;
+    typedef typename EvalT::MeshScalarT MeshScalarT;
+    typedef typename EvalT::ParamScalarT ParamScalarT;
+
+    // Input:
+    PHX::MDField<MeshScalarT,Cell,Node,QuadPoint> wBF;  // [km^3]
+    PHX::MDField<ScalarT,Cell,Node> basalMeltRate; // [m yr^{-1}]
+    PHX::MDField<VelocityType,Cell,QuadPoint,VecDim,Dim>  GradVelocity; // [k^{-1} yr^{-1}]
+    PHX::MDField<ScalarT,Cell,QuadPoint, Dim> w_z;  // [k^{-1} yr^{-1}]
+    PHX::MDField<ScalarT,Cell,Node> w; // [m yr^{-1}]
+
+    // Output
+    PHX::MDField<ScalarT,Cell,Node> Residual;
+
+    std::string sideName;
+    std::vector<std::vector<int> >  sideNodes;
+    int numNodes;
+    int numSideNodes;
+    int numQPs;
+
+  };
+
+}	// Namespace FELIX
+
+
+
+
+#endif /* FELIX_VELOCITYZ_HPP_ */
