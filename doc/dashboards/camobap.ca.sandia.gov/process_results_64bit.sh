c59a6569f671bbc5b7380db42cf4747d958f535c
Irina K. Tezaur <ikalash@sandia.gov>
2016-12-14 08:00:01 -0800

    Removing obsolete CDash scripts.  Adding print statement showing path to CDash site to end of results files that are emailed out daily.

--- doc/dashboards/camobap.ca.sandia.gov/process_results_64bit.sh
+++ doc/dashboards/camobap.ca.sandia.gov/process_results_64bit.sh
@@ -1,5 +1,5 @@
 
-cd /home/ikalash/Desktop/nightlyCDash
+cd /home/ikalash/nightlyCDash
 grep "Test   #" nightly_log_64bit.txt >& results0
 grep "Test  #" nightly_log_64bit.txt >& results1 
 cat results0 results1 >& results11
@@ -11,4 +11,7 @@ grep "Total Test" nightly_log_64bit.txt >& results4
 cat results3 results4 >& results5
 grep "(Failed)" nightly_log_64bit.txt >& results6 
 cat results5 results6 >& results_64bit
+echo "" >> results_64bit 
+echo "The Albany CDash site can be accessed here: http://cdash.sandia.gov/CDash-2-3-0/index.php?project=Albany" >> results_64bit 
+echo "" >> results_64bit 
 rm results0 results1 results11 results2 results3 results4 results5 results6
--- doc/dashboards/camobap.ca.sandia.gov/process_results_functor_openMP.sh
+++ doc/dashboards/camobap.ca.sandia.gov/process_results_functor_openMP.sh
@@ -11,4 +11,7 @@ grep "Total Test" nightly_log_functor_openMP.txt >& results4
 cat results3 results4 >& results5
 grep "(Failed)" nightly_log_functor_openMP.txt >& results6 
 cat results5 results6 >& results_functor_openMP
+echo "" >> results_functor_openMP 
+echo "The Albany CDash site can be accessed here: http://cdash.sandia.gov/CDash-2-3-0/index.php?project=Albany" >> results_functor_openMP
+echo "" >> results_functor_openMP
 rm results0 results1 results11 results2 results3 results4 results5 results6
