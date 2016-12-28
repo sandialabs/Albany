b211585c4eae68570d8be40b32210d0dbbbe6483
Brian Granzow <brian.granzow@gmail.com>
2016-12-27 21:08:14 -0500

    aniso: add some groundwork for advection problem

--- src/ANISO/AdvectionProblem.cpp
+++ src/ANISO/AdvectionProblem.cpp
@@ -11,40 +11,86 @@
 #include "PHAL_FactoryTraits.hpp"
 #include "Albany_Utils.hpp"
 
-Albany::AdvectiveProblem::AdvectiveProblem(
+Albany::AdvectionProblem::AdvectionProblem(
     const Teuchos::RCP<Teuchos::ParameterList>& params_,
     const Teuchos::RCP<ParamLib>& param_lib,
     const int num_dims,
  	  Teuchos::RCP<const Teuchos::Comm<int> >& commT) :
   Albany::AbstractProblem(params_, param_lib),
   num_dims_(num_dims) {
+
+    std::string fname = params->get<std::string>("MaterialDB Filename");
+    material_db_ = Teuchos::rcp(new QCAD::MaterialDatabase(fname, commT));
+    this->setNumEquations(1);
 }
 
-Albany::AdvectiveProblem::~AdvectiveProblem() {
+Albany::AdvectionProblem::~AdvectionProblem() {
 }
 
-void Albany::AdvectiveProblem::buildProblem(
+void Albany::AdvectionProblem::buildProblem(
   Teuchos::ArrayRCP<Teuchos::RCP<Albany::MeshSpecsStruct> >  meshSpecs,
   Albany::StateManager& stateMgr) {
+
+  int phys_sets = meshSpecs.size();
+  *out << "Num MeshSpecs: " << phys_sets << std::endl;
+  fm.resize(phys_sets);
+
+  for (int ps=0; ps < phys_sets; ++ps) {
+    fm[ps] = Teuchos::rcp(new PHX::FieldManager<PHAL::AlbanyTraits>);
+    buildEvaluators(
+        *fm[ps],
+        *meshSpecs[ps],
+        stateMgr,
+        BUILD_RESID_FM,
+        Teuchos::null);
+  }
+
+  if (meshSpecs[0]->nsNames.size() > 0)
+    constructDirichletEvaluators(meshSpecs[0]->nsNames);
+
+  if (meshSpecs[0]->ssNames.size() > 0)
+    constructNeumannEvaluators(meshSpecs[0]);
+
 }
 
 Teuchos::Array<Teuchos::RCP<const PHX::FieldTag> >
-Albany::AdvectiveProblem::buildEvaluators(
+Albany::AdvectionProblem::buildEvaluators(
   PHX::FieldManager<PHAL::AlbanyTraits>& fm0,
   const Albany::MeshSpecsStruct& meshSpecs,
   Albany::StateManager& stateMgr,
   Albany::FieldManagerChoice fmchoice,
   const Teuchos::RCP<Teuchos::ParameterList>& responseList) {
+
+  ConstructEvaluatorsOp<AdvectionProblem> op(
+      *this, fm0, meshSpecs, stateMgr, fmchoice, responseList);
+  Sacado::mpl::for_each<PHAL::AlbanyTraits::BEvalTypes> fe(op);
+  return *op.tags;
 }
 
-void Albany::AdvectiveProblem::constructDirichletEvaluators(
+void Albany::AdvectionProblem::constructDirichletEvaluators(
     const std::vector<std::string>& nodeSetIDs) {
+
+  std::vector<std::string> bcNames(neq);
+  bcNames[0] = "Phi";
+  Albany::BCUtils<Albany::DirichletTraits> bcUtils;
+  dfm = bcUtils.constructBCEvaluators(
+      nodeSetIDs, bcNames, this->params, this->paramLib);
+  offsets_ = bcUtils.getOffsets();
 }
 
-void Albany::AdvectiveProblem::constructNeumannEvaluators(
+void Albany::AdvectionProblem::constructNeumannEvaluators(
     const Teuchos::RCP<Albany::MeshSpecsStruct>& meshSpecs) {
+
+  *out << "unsupported neumann boundary conditions\n";
+  abort();
 }
 
 Teuchos::RCP<const Teuchos::ParameterList>
-Albany::AdvectiveProblem::getValidProblemParameters() const {
+Albany::AdvectionProblem::getValidProblemParameters() const {
+
+  Teuchos::RCP<Teuchos::ParameterList> validPL =
+    this->getGenericProblemParams("ValidAdvectionProblemParams");
+  validPL->set<std::string>(
+      "MaterialDB FileName", "materials.xml", "XML file name");
+  return validPL;
 }
--- src/ANISO/AdvectionProblem.hpp
+++ src/ANISO/AdvectionProblem.hpp
@@ -21,17 +21,17 @@
 
 namespace Albany {
 
-class AdvectiveProblem : public AbstractProblem {
+class AdvectionProblem : public AbstractProblem {
 
   public:
  
-    AdvectiveProblem(
+    AdvectionProblem(
       const Teuchos::RCP<Teuchos::ParameterList>& params,
       const Teuchos::RCP<ParamLib>& param_lib,
       const int num_dims,
       Teuchos::RCP<const Teuchos::Comm<int> >& commT);
 
-    ~AdvectiveProblem();
+    ~AdvectionProblem();
 
     int spatialDimension() const { return num_dims_; }
 
@@ -51,9 +51,9 @@ class AdvectiveProblem : public AbstractProblem {
 
   private:
 
-    AdvectiveProblem(const AdvectiveProblem&);
+    AdvectionProblem(const AdvectionProblem&);
     
-    AdvectiveProblem& operator=(const AdvectiveProblem&);
+    AdvectionProblem& operator=(const AdvectionProblem&);
 
   public:
 
@@ -94,7 +94,7 @@ class AdvectiveProblem : public AbstractProblem {
 
 template <typename EvalT>
 Teuchos::RCP<const PHX::FieldTag>
-Albany::AdvectiveProblem::constructEvaluators(
+Albany::AdvectionProblem::constructEvaluators(
   PHX::FieldManager<PHAL::AlbanyTraits>& fm0,
   const Albany::MeshSpecsStruct& meshSpecs,
   Albany::StateManager& stateMgr,
@@ -109,6 +109,69 @@ Albany::AdvectiveProblem::constructEvaluators(
   using std::vector;
   using std::string;
   using PHAL::AlbanyTraits;
+
+  const CellTopologyData* const elem_top = &meshSpecs.ctd;
+  std::string eb_name = meshSpecs.ebName;
+  std::string material_name;
+  material_name = material_db_->getElementBlockParam<std::string>(
+      eb_name, "material");
+
+  RCP<Intrepid2::Basis<PHX::Device, RealType, RealType> >
+    intrepid_basis = Albany::getIntrepid2Basis(*elem_top);
+
+  RCP<shards::CellTopology> elem_type =
+    rcp(new shards::CellTopology(elem_top));
+
+  Intrepid2::DefaultCubatureFactory cub_factory;
+
+  RCP<Intrepid2::Cubature<PHX::Device> > elem_cubature =
+    cub_factory.create<PHX::Device, RealType, RealType>(
+        *elem_type, meshSpecs.cubatureDegree);
+
+
+  const int workset_size = meshSpecs.worksetSize;
+  const int num_vertices = elem_type->getNodeCount();
+  const int num_nodes = intrepid_basis->getCardinality();
+  const int num_qps = elem_cubature->getNumPoints();
+
+  *out  << "Field Dimensions: Workset=" << workset_size
+        << ", Vertices= " << num_vertices
+        << ", Nodes= " << num_nodes
+        << ", QuadPts= " << num_qps
+        << ", Dim= " << num_dims_
+        << std::endl;
+
+  dl_ = rcp(new Albany::Layouts(
+        workset_size, num_vertices, num_nodes, num_qps, num_dims_));
+
+  Teuchos::ArrayRCP<std::string> dof_names(1);
+  Teuchos::ArrayRCP<std::string> resid_names(1);
+  dof_names[0] = "Phi";
+  resid_names[0] = "Phi Residual";
+
+  Albany::EvaluatorUtils<EvalT, PHAL::AlbanyTraits> eval_utils(dl_);
+  Teuchos::RCP<PHX::Evaluator<AlbanyTraits> > ev;
+
+  fm0.template registerEvaluator<EvalT>(
+      eval_utils.constructGatherSolutionEvaluator_noTransient(
+        false, dof_names));
+
+  fm0.template registerEvaluator<EvalT>(
+      eval_utils.constructScatterResidualEvaluator(
+        false, resid_names));
+
+  fm0.template registerEvaluator<EvalT>(
+      eval_utils.constructDOFInterpolationEvaluator(dof_names[0]));
+
+  fm0.template registerEvaluator<EvalT>(
+      eval_utils.constructDOFGradInterpolationEvaluator(dof_names[0]));
+
+  fm0.template registerEvaluator<EvalT>(
+      eval_utils.constructGatherCoordinateVectorEvaluator());
+
+  fm0.template registerEvaluator<EvalT>(
+      eval_utils.constructComputeBasisFunctionsEvaluator(
+        elem_type, intrepid_basis, elem_cubature));
 
 }
 
