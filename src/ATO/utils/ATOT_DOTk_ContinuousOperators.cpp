615f5f38dc89a068bb93bae0fabf246ae0cb298a
Irina K. Tezaur <ikalash@sandia.gov>
2017-01-07 15:20:56 -0800

    ATO: creating ATOT version of DOTk_Optimizer and DOTk_ContinuousOperators classes.

---
+++ src/ATO/utils/ATOT_DOTk_ContinuousOperators.cpp
@@ -0,0 +1,71 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+
+#include "DOTk/DOTk_Types.hpp"
+#include "DOTk/vector.hpp"
+#include "ATOT_DOTk_ContinuousOperators.hpp"
+#include "ATOT_Solver.hpp"
+
+namespace ATOT {
+
+ATO_DOTk_ContinuousOperators::ATO_DOTk_ContinuousOperators(
+  OptInterface* interface,
+  Teuchos::RCP<const Teuchos_Comm> comm) :
+  dotk::DOTk_ContinuousOperators::DOTk_ContinuousOperators(),
+  solverInterface( interface ),
+  myComm( comm )
+{
+  nTopologyUpdates = 0;
+  current_dfdz = new ATO::vector(solverInterface->GetNumOptDofs(), comm);
+}
+ATO_DOTk_ContinuousOperators::~ATO_DOTk_ContinuousOperators()
+{
+  if(current_dfdz) delete current_dfdz;
+}
+
+Real 
+ATO_DOTk_ContinuousOperators::Fval(const ATO::dotk::vector<Real> & z_)
+{
+  // if(z_.nUpdates != nTopologyUpdates){
+  //   nTopologyUpdates = z_.nUpdates;
+       solverInterface->ComputeObjective(&(z_[0]), current_f, NULL);
+  // }
+  return current_f;
+}
+void 
+ATO_DOTk_ContinuousOperators::Fval(
+  const std::vector<std::map<dotk::types::variable_t, std::tr1::shared_ptr<ATO::dotk::vector<Real> > > > & z_,
+  const std::map<dotk::types::variable_t, std::tr1::shared_ptr<ATO::dotk::vector<Real> > > & fval_)
+{
+}
+void 
+ATO_DOTk_ContinuousOperators::Fval(
+  const std::vector< std::map<dotk::types::variable_t, std::tr1::shared_ptr< ATO::dotk::vector<Real> > > > & z_plus_,
+  const std::vector< std::map<dotk::types::variable_t, std::tr1::shared_ptr< ATO::dotk::vector<Real> > > > & z_minus_,
+  const std::map<dotk::types::variable_t, std::tr1::shared_ptr< ATO::dotk::vector<Real> > > & fval_plus_,
+  const std::map<dotk::types::variable_t, std::tr1::shared_ptr< ATO::dotk::vector<Real> > > & fval_minus_)
+{
+}
+void 
+ATO_DOTk_ContinuousOperators::F_z(
+  const ATO::dotk::vector<Real> & z_, ATO::dotk::vector<Real> & f_z_)
+{
+  // if(z_.nUpdates != nTopologyUpdates){
+  //   nTopologyUpdates = z_.nUpdates;
+       solverInterface->ComputeObjective(&(z_[0]), current_f, &(current_dfdz->operator[](0)));
+  // }
+  f_z_.copy(*current_dfdz);
+}
+void 
+ATO_DOTk_ContinuousOperators::F_zz(
+  const ATO::dotk::vector<Real> & z_, 
+  const ATO::dotk::vector<Real> & dz_, ATO::dotk::vector<Real> & f_zz_dz_)
+{
+}
+
+
+}
