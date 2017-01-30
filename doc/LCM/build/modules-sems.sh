0b74c87ce9f62905ec27bfa243a628e6c8ff3a02
Coleman Alleman <callema@sandia.gov>
2017-01-30 09:51:36 -0700

    LCM: Changed cee module load script to load most recent sems modules

--- doc/LCM/build/modules-sems.sh
+++ doc/LCM/build/modules-sems.sh
@@ -1,6 +1,6 @@
 module load sems-env
-module load sems-gcc/5.1.0
-module load sems-openmpi/1.6.5
+module load sems-gcc/6.1.0
+module load sems-openmpi/1.8.7
 module load sems-netcdf/4.3.2/parallel
 module load sems-boost/1.59.0/base
 module load sems-hdf5/1.8.12/parallel
