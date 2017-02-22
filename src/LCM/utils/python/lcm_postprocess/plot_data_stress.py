cd26680f14618f89fddc6e1011484172de5555e3
Coleman Alleman <callema@sandia.gov>
2017-02-22 15:27:47 -0800

    LCM: Various fixes and minor enhancements to lcm_postprocess

--- src/LCM/utils/python/lcm_postprocess/plot_data_stress.py
+++ src/LCM/utils/python/lcm_postprocess/plot_data_stress.py
@@ -4,22 +4,34 @@ import cPickle as pickle
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from matplotlib import rcParams
+import numpy as np
 
 
 # Plot ij-components of Cauchy stress vs ij-components of Logarithmic strain 
 # @profile
-def plot_data_stress(domain = None, filename = None):
+def plot_data_stress(domain = None, filename = None, truncate_legend = False):
 
     if filename != None:
         domain = pickle.load(open(filename, 'rb'))
 
     num_dims = domain.num_dims
     times = domain.times
+    num_blocks = len(domain.blocks)
 
 #    rc_params.update({'figure.autolayout': True})
 
+    string_legend = ['Domain']
+    if truncate_legend is True:
+        num_blocks_plot = 13
+    else:
+        num_blocks_plot = num_blocks    
+
+    string_legend.extend(['Block ' + str(key_block) for key_block in sorted(list(domain.blocks))[:num_blocks_plot]])
+    if truncate_legend is True:
+        string_legend.extend(['...', 'Block ' + str(sorted(list(domain.blocks))[-1])])
+
     # plt.rc('text', usetex=True)
-    plt.rc('font', family='serif', size=22)
+    plt.rc('font', family = 'serif', size = 22)
 
     fig = plt.figure()
 
@@ -32,23 +44,18 @@ def plot_data_stress(domain = None, filename = None):
             ax.hold(True)
 
             ax.plot(
-                [domain.variables['Log_Strain'][key_step][(dim_i, dim_j)] for key_step in times],
-                [domain.variables['Cauchy_Stress'][key_step][(dim_i, dim_j)] for key_step in times],
+                [int(domain.variables['Log_Strain'][key_step][(dim_i, dim_j)]*1e8)/1e8 for key_step in times],
+                [int(domain.variables['Cauchy_Stress'][key_step][(dim_i, dim_j)]*1e8)/1e8 for key_step in times],
                 marker = 'o')
-
-            str_legend = ['Domain']
 
             for key_block in domain.blocks:
 
                 block = domain.blocks[key_block]
 
                 ax.plot(
-                    [block.variables['Log_Strain'][key_step][(dim_i, dim_j)] for key_step in times],
-                    [block.variables['Cauchy_Stress'][key_step][(dim_i, dim_j)] for key_step in times],
+                    [int(block.variables['Log_Strain'][key_step][(dim_i, dim_j)]*1e8)/1e8 for key_step in times],
+                    [int(block.variables['Cauchy_Stress'][key_step][(dim_i, dim_j)]*1e8)/1e8 for key_step in times],
                     linestyle = ':')
-
-                str_legend.append('Block ' + str(key_block))
-
 
             plt.xlabel('Logarithmic Strain $\epsilon_{'+ str(dim_i + 1) + str(dim_j + 1) +'}$')
             plt.ylabel('Cauchy Stress $\sigma_{'+ str(dim_i + 1) + str(dim_j + 1) +'}$ (MPa)')
@@ -57,7 +64,7 @@ def plot_data_stress(domain = None, filename = None):
             plt.locator_params(axis='x',nbins=4)
 
             legend = plt.legend(
-                str_legend,
+                string_legend,
                 bbox_to_anchor = (1.05, 1), 
                 loc = 2, 
                 borderaxespad = 0.,
@@ -84,6 +91,14 @@ if __name__ == '__main__':
     except:
         raise
 
-    plot_data_stress(filename = name_file_input)
+    try:
+        truncate_legend = sys.argv[2]
+    except:
+        truncate_legend = False
+        pass
+
+    plot_data_stress(
+        filename = name_file_input,
+        truncate_legend = truncate_legend)
 
 # end if __name__ == '__main__':
