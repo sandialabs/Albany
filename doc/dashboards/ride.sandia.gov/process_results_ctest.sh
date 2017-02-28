e8b3bf2f1e9e4a3f55b5dfe7da9c3699d2d0386f
Irina K. Tezaur <ikalash@sandia.gov>
2017-02-28 09:39:04 -0800

    Fixing ride Jenkins cdash scripts.

--- doc/dashboards/ride.sandia.gov/process_results_ctest.sh
+++ doc/dashboards/ride.sandia.gov/process_results_ctest.sh
@@ -1,5 +1,4 @@
 
-cd ${WORKSPACE}
 grep "Test   #" nightly_log_rideAlbany.txt >& results0
 grep "Test  #" nightly_log_rideAlbany.txt >& results1
 cat results0 results1 >& results11
--- doc/dashboards/ride.sandia.gov/send_email_ctest.sh
+++ doc/dashboards/ride.sandia.gov/send_email_ctest.sh
@@ -2,10 +2,10 @@
 
 #source $1 
 
-TTT=`grep "(Failed)" ${WORKSPACE}/nightly_log_rideAlbany.txt -c`
-TTTT=`grep "(Not Run)" ${WORKSPACE}/nightly_log_rideAlbany.txt -c`
-TTTTT=`grep "(Timeout)" ${WORKSPACE}/nightly_log_rideAlbany.txt -c`
-TT=`grep "...   Passed" ${WORKSPACE}/nightly_log_rideAlbany.txt -c`
+TTT=`grep "(Failed)" nightly_log_rideAlbany.txt -c`
+TTTT=`grep "(Not Run)" nightly_log_rideAlbany.txt -c`
+TTTTT=`grep "(Timeout)" nightly_log_rideAlbany.txt -c`
+TT=`grep "...   Passed" nightly_log_rideAlbany.txt -c`
 
-/bin/mail -s "Albany (master, CUDA, KOKKOS_UNDER_DEVELOPMENT): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, agsalin@sandia.gov, jwatkin@sandia.gov, amota@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov" < ${WORKSPACE}/results_cuda
+/bin/mail -s "Albany (master, CUDA, KOKKOS_UNDER_DEVELOPMENT): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, agsalin@sandia.gov, jwatkin@sandia.gov, amota@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov" < results_cuda
 
