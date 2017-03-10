693f79819f5501218537672622f633425f19af10
Brian Granzow <brian.granzow@gmail.com>
2017-03-10 11:36:00 -0500

    CTM: begin solution info clean up

--- src/CTM/CTM_SolutionInfo.hpp
+++ src/CTM/CTM_SolutionInfo.hpp
@@ -11,69 +11,56 @@ namespace CTM {
 
 using Teuchos::RCP;
 
-    class SolutionInfo {
-    public:
+class SolutionInfo {
 
-        // constructor
-        SolutionInfo();
-        // do not use copy constructor
-        SolutionInfo(const SolutionInfo&) = delete;
-        // do not use assignment operator
-        SolutionInfo& operator=(const SolutionInfo&) = delete;
+  public:
 
-        // get solution vectors
-        Teuchos::RCP<Tpetra_MultiVector> getOwnedMV();
-        //
-        Teuchos::RCP<Tpetra_MultiVector> getGhostMV();
+    SolutionInfo();
 
-        // get exporter
-        Teuchos::RCP<Tpetra_Export> getExporter();
-        // get importer
-        Teuchos::RCP<Tpetra_Import> getImporter();
+    SolutionInfo(const SolutionInfo&) = delete;
+    SolutionInfo& operator=(const SolutionInfo&) = delete;
 
-        // scatter
-        void scatter_x(
-                const Tpetra_Vector& xT, /* note that none are overlapped */
-                const Tpetra_Vector* x_dotT,
-                const Tpetra_Vector* x_dotdotT);
+    Teuchos::RCP<Tpetra_MultiVector> getOwnedMV();
+    Teuchos::RCP<Tpetra_MultiVector> getGhostMV();
 
-        // get residual vectors
-        Teuchos::RCP<Tpetra_Vector> getOwnedResidual();
-        //
-        Teuchos::RCP<Tpetra_Vector> getGhostResidual();
+    Teuchos::RCP<Tpetra_Export> getExporter();
+    Teuchos::RCP<Tpetra_Import> getImporter();
 
-        // get Jacobian
-        Teuchos::RCP<Tpetra_CrsMatrix> getOwnedJacobian();
-        //
-        Teuchos::RCP<Tpetra_CrsMatrix> getGhostJacobian();
+    void scatter_x(
+        const Tpetra_Vector& xT,
+        const Tpetra_Vector* x_dotT,
+        const Tpetra_Vector* x_dotdotT);
 
-        void gather_x();
-        void scatter_x();
+    Teuchos::RCP<Tpetra_Vector> getOwnedResidual();
+    Teuchos::RCP<Tpetra_Vector> getGhostResidual();
 
-        void gather_f();
-        void scatter_f();
+    Teuchos::RCP<Tpetra_CrsMatrix> getOwnedJacobian();
+    Teuchos::RCP<Tpetra_CrsMatrix> getGhostJacobian();
 
-        void gather_J();
-        void scatter_J();
+    void gather_x();
+    void scatter_x();
 
-        void resize(RCP<Albany::AbstractDiscretization> disc, bool have_x_dot);
-    private:
-        // solution multi vector. First column corresponds to main variable.
-        // second column to time derivative of main variable.
-        RCP<Tpetra_MultiVector> owned_x;
-        RCP<Tpetra_MultiVector> ghost_x;
-        // Residual vector.
-        RCP<Tpetra_Vector> owned_f;
-        RCP<Tpetra_Vector> ghost_f;
-        // Jacobians
-        RCP<Tpetra_CrsMatrix> owned_J;
-        RCP<Tpetra_CrsMatrix> ghost_J;
+    void gather_f();
+    void scatter_f();
 
-        RCP<Tpetra_Export> exporter;
-        RCP<Tpetra_Import> importer;
+    void gather_J();
+    void scatter_J();
 
-    };
+    void resize(RCP<Albany::AbstractDiscretization> disc, bool have_x_dot);
 
-}
+  private:
+    
+    RCP<Tpetra_MultiVector> owned_x;
+    RCP<Tpetra_MultiVector> ghost_x;
+    RCP<Tpetra_Vector> owned_f;
+    RCP<Tpetra_Vector> ghost_f;
+    RCP<Tpetra_CrsMatrix> owned_J;
+    RCP<Tpetra_CrsMatrix> ghost_J;
+    RCP<Tpetra_Export> exporter;
+    RCP<Tpetra_Import> importer;
+
+};
+
+} // namespace CTM
 
 #endif
