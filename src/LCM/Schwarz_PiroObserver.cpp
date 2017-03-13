950064d880d8ab2d782ec2871c84e5f2234bcdaa
Alejandro Mota <amota@sandia.gov>
2017-03-13 14:54:09 -0700

    LCM: WIP removing T suffix to denote Tpetra in identifiers as this clutters this code that does not support Epetra (#22).

---
+++ src/LCM/Schwarz_PiroObserver.cpp
@@ -0,0 +1,143 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include "Schwarz_PiroObserver.hpp"
+
+#include "PHAL_AlbanyTraits.hpp"
+#include "Teuchos_ScalarTraits.hpp"
+
+#include <cstddef>
+
+//#define OUTPUT_TO_SCREEN 
+
+LCM::Schwarz_PiroObserver::Schwarz_PiroObserver(
+    const Teuchos::RCP<SchwarzCoupled>& cs_model)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  apps_ = cs_model->getApps();
+  n_models_ = apps_.size();
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "# models seen by Schwarz_PiroObserver: " << n_models_ << '\n';
+#endif 
+  impl_ = Teuchos::rcp(new ObserverImpl(apps_));
+}
+
+void
+LCM::Schwarz_PiroObserver::observeSolution(
+    const Thyra::VectorBase<ST> &solution)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  this->observeSolutionImpl(solution, Teuchos::ScalarTraits<ST>::zero());
+}
+
+void
+LCM::Schwarz_PiroObserver::observeSolution(
+    const Thyra::VectorBase<ST> &solution,
+    const ST stamp)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  this->observeSolutionImpl(solution, stamp);
+}
+
+void
+LCM::Schwarz_PiroObserver::observeSolution(
+    const Thyra::VectorBase<ST> &solution,
+    const Thyra::VectorBase<ST> &solution_dot,
+    const ST stamp)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  this->observeSolutionImpl(solution, solution_dot, stamp);
+}
+
+namespace { // anonymous
+
+Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>>
+tpetraFromThyra(const Thyra::VectorBase<double> &v, int n_models)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+
+  const Teuchos::RCP<const Thyra::ProductVectorBase<ST>> v_nonowning_rcp =
+      Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<ST>>(
+          Teuchos::rcpFromRef(v));
+
+  //Create a Teuchos array of the vs for each model.
+  Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> vs(n_models);
+  for (int m = 0; m < n_models; ++m) {
+    //Get each Tpetra vector
+    vs[m] = Teuchos::rcp_dynamic_cast<const ThyraVector>(
+        v_nonowning_rcp->getVectorBlock(m), true)->getConstTpetraVector();
+  }
+  return vs;
+}
+
+} // anonymous namespace
+
+void
+LCM::Schwarz_PiroObserver::observeSolutionImpl(
+    const Thyra::VectorBase<ST> &solution,
+    const ST default_stamp)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions_tpetra =
+      tpetraFromThyra(solution, n_models_);
+  Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> null_array;
+  null_array.resize(n_models_);
+  for (int m = 0; m < n_models_; m++)
+    null_array[m] = Teuchos::null;
+
+  this->observeTpetraSolutionImpl(
+      solutions_tpetra,
+      null_array,
+      default_stamp);
+}
+
+void
+LCM::Schwarz_PiroObserver::observeSolutionImpl(
+    const Thyra::VectorBase<ST> &solution,
+    const Thyra::VectorBase<ST> &solution_dot,
+    const ST default_stamp)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions_tpetra =
+      tpetraFromThyra(solution, n_models_);
+  Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions_dot_tpetra =
+      tpetraFromThyra(solution_dot, n_models_);
+
+  this->observeTpetraSolutionImpl(
+      solutions_tpetra,
+      solutions_dot_tpetra,
+      default_stamp);
+}
+
+void
+LCM::Schwarz_PiroObserver::observeTpetraSolutionImpl(
+    Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions,
+    Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions_dot,
+    const ST default_stamp)
+{
+#ifdef OUTPUT_TO_SCREEN
+  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  // Determine the stamp associated with the snapshot
+  const ST stamp = impl_->getTimeParamValueOrDefault(default_stamp);
+
+  //FIXME: change arguments to take in arrays
+  impl_->observeSolutionT(stamp, solutions, solutions_dot);
+}
---
+++ src/LCM/Schwarz_PiroObserver.hpp
@@ -0,0 +1,67 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+//IK, 9/12/14: no Epetra!
+
+#if !defined(Schwarz_PiroObserver_hpp)
+#define Schwarz_PiroObserver_hpp
+
+#include "Piro_ObserverBase.hpp"
+
+#include "Albany_DataTypes.hpp"
+#include "Albany_Application.hpp"
+#include "Schwarz_Coupled.hpp"
+#include "Schwarz_ObserverImpl.hpp"
+#include "Teuchos_RCP.hpp"
+
+namespace LCM {
+
+class Schwarz_PiroObserver: public Piro::ObserverBase<ST> {
+public:
+  explicit Schwarz_PiroObserver(
+      Teuchos::RCP<SchwarzCoupled> const & cs_model);
+
+  virtual void observeSolution(
+      Thyra::VectorBase<ST> const & solution);
+
+  virtual void observeSolution(
+      Thyra::VectorBase<ST> const & solution,
+      ST const stamp);
+
+  virtual void observeSolution(
+      Thyra::VectorBase<ST> const & solution,
+      Thyra::VectorBase<ST> const & solution_dot,
+      ST const stamp);
+
+protected:
+  int n_models_;
+
+private:
+  void observeSolutionImpl(
+      Thyra::VectorBase<ST> const & solution,
+      ST const default_stamp);
+
+  void observeSolutionImpl(
+      Thyra::VectorBase<ST> const & solution,
+      Thyra::VectorBase<ST> const & solution_dot,
+      ST const default_stamp);
+
+  void observeTpetraSolutionImpl(
+      Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions,
+      Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> solutions_dot,
+      ST const default_stamp);
+
+  Teuchos::RCP<ObserverImpl>
+  impl_;
+
+  Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application>>
+  apps_;
+
+};
+
+} // namespace Albany
+
+#endif // Schwarz_PiroObserver_hpp
