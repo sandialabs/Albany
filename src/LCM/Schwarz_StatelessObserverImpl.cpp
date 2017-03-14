5664d99968047822cacc1442b4c8758cbc68d268
Alejandro Mota <amota@sandia.gov>
2017-03-13 17:51:59 -0700

    LCM: Add missing virtual destructor. Remove stale code. Make protected members private since no derived classes exist or are planned. (#22)

--- src/LCM/Schwarz_StatelessObserverImpl.cpp
+++ src/LCM/Schwarz_StatelessObserverImpl.cpp
@@ -5,60 +5,72 @@
 //*****************************************************************//
 #include "Schwarz_StatelessObserverImpl.hpp"
 
-#include "Albany_AbstractDiscretization.hpp"
-
-#include "Teuchos_TimeMonitor.hpp"
-
-#include <string>
-
-//#define OUTPUT_TO_SCREEN 
-
 namespace LCM {
 
+//
+//
+//
 StatelessObserverImpl::
 StatelessObserverImpl(
     Teuchos::ArrayRCP<Teuchos::RCP<Albany::Application>> &apps)
 {
-#ifdef OUTPUT_TO_SCREEN
-  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
-#endif
   apps_ = apps;
   n_models_ = apps.size();
   sol_out_time_ = Teuchos::TimeMonitor::getNewTimer("Albany: Output to File");
 }
 
-RealType StatelessObserverImpl::
+//
+//
+//
+StatelessObserverImpl::
+~StatelessObserverImpl()
+{
+  return;
+}
+
+//
+//
+//
+RealType
+StatelessObserverImpl::
 getTimeParamValueOrDefault(RealType default_value) const
 {
-#ifdef OUTPUT_TO_SCREEN
-  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
-#endif
   // FIXME, IKT, : We may want to change the logic here at some point.
   // I am assuming all the models have the same parameters,
   // so we only pull the time-label from the 0th model.
-  const std::string label("Time");
-  return
-      (apps_[0]->getParamLib()->isParameter(label)) ?
-          apps_[0]->getParamLib()->getRealValue<PHAL::AlbanyTraits::Residual>(
-              label) :
-          default_value;
+  std::string const
+  label("Time");
+
+  bool const
+  have_time = apps_[0]->getParamLib()->isParameter(label);
+
+  return have_time == true ?
+    apps_[0]->getParamLib()->getRealValue<PHAL::AlbanyTraits::Residual>(label) :
+    default_value;
 }
 
-void StatelessObserverImpl::observeSolutionT(
+//
+//
+//
+void
+StatelessObserverImpl::
+observeSolutionT(
     double stamp,
-    Teuchos::Array<Teuchos::RCP<const Tpetra_Vector>> non_overlapped_solutionT,
-    Teuchos::Array<Teuchos::RCP<const Tpetra_Vector>> non_overlapped_solution_dotT)
+    Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> non_overlapped_solution,
+    Teuchos::Array<Teuchos::RCP<Tpetra_Vector const>> non_overlapped_solution_dot)
 {
-#ifdef OUTPUT_TO_SCREEN
-  std::cout << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
-#endif
-  Teuchos::TimeMonitor timer(*sol_out_time_);
+  Teuchos::TimeMonitor
+  timer(*sol_out_time_);
+
   for (int m = 0; m < n_models_; m++) {
-    const Teuchos::RCP<const Tpetra_Vector> overlapped_solutionT =
-        apps_[m]->getAdaptSolMgrT()->updateAndReturnOverlapSolutionT(*non_overlapped_solutionT[m]);
-    apps_[m]->getDiscretization()->writeSolutionT(
-        *overlapped_solutionT, stamp, /*overlapped =*/true);
+    Teuchos::RCP<Tpetra_Vector const> const
+    overlapped_solution = apps_[m]->getAdaptSolMgrT()->
+      updateAndReturnOverlapSolutionT(*non_overlapped_solution[m]);
+
+    apps_[m]->getDiscretization()->
+      writeSolutionT(*overlapped_solution, stamp, true);
   }
+  return;
 }
 
 } // namespace LCM
