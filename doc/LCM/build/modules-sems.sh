024052ddef3535ec602e91dbc3320bc7d66bafbc
Alejandro Mota <amota@sandia.gov>
2017-03-16 16:48:14 -0700

    LCM: Add modules to SEMS module-loading script to support yaml-cpp, git and cmake in preparation for switching LCM to YAML input.
    [#21, #22]

--- doc/LCM/build/modules-sems.sh
+++ doc/LCM/build/modules-sems.sh
@@ -4,3 +4,6 @@ module load sems-openmpi/1.8.7
 module load sems-netcdf/4.3.2/parallel
 module load sems-boost/1.59.0/base
 module load sems-hdf5/1.8.12/parallel
+module load sems-yaml_cpp/0.5.3/base
+module load sems-cmake/3.3.2
+module load sems-git/2.10.1
