2f620386f1fa2b2cc80afec477bfd9865f9a445b
Alejandro Mota <amota@sandia.gov>
2017-03-16 17:19:48 -0700

    LCM: Remove explicit loading of Boost, which seems to cause conflicts on the SEMS environment when YAML is specified. Boost is loaded automatically as it is a dependency.
    [#21, #22]

--- doc/LCM/build/modules-sems.sh
+++ doc/LCM/build/modules-sems.sh
@@ -2,7 +2,6 @@ module load sems-env
 module load sems-gcc/6.1.0
 module load sems-openmpi/1.8.7
 module load sems-netcdf/4.3.2/parallel
-module load sems-boost/1.59.0/base
 module load sems-hdf5/1.8.12/parallel
 module load sems-yaml_cpp/0.5.3/base
 module load sems-cmake/3.3.2
