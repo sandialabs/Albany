3f06c740e464fc5bc2c51fd1ab403a84b6d0ae35
Alejandro Mota <amota@sandia.gov>
2017-03-25 21:23:40 -0700

    LCM: Add missing file.

---
+++ tests/small/LCM/HeliumODEs/runtest.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+from subprocess import Popen
+
+result = 0
+
+######################
+# Test 1 - HeBubbles
+######################
+print "test 1 - HeBubbles"
+name = "HeBubbles"
+log_file_name = name + ".log"
+if os.path.exists(log_file_name):
+    os.remove(log_file_name)
+logfile = open(log_file_name, 'w')
+
+# run AlbanyT
+command = ["./AlbanyT", name + ".yaml"]
+p = Popen(command, stdout=logfile, stderr=logfile)
+return_code = p.wait()
+if return_code != 0:
+    result = return_code
+
+# run exodiff"
+command = ["./exodiff", "-file", \
+           name + ".exodiff", \
+           name + ".gold.e", \
+           name + ".e"]
+p = Popen(command, stdout=logfile, stderr=logfile)
+return_code = p.wait()
+if return_code != 0:
+    result = return_code
+
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+    sys.exit(result)
+
+######################
+# Test 2 - HeBubblesDecay
+######################
+print "test 2 - HeBubblesDecay"
+name = "HeBubblesDecay"
+log_file_name = name + ".log"
+if os.path.exists(log_file_name):
+    os.remove(log_file_name)
+logfile = open(log_file_name, 'w')
+
+# run AlbanyT
+command = ["./AlbanyT", name + ".yaml"]
+p = Popen(command, stdout=logfile, stderr=logfile)
+return_code = p.wait()
+if return_code != 0:
+    result = return_code
+
+# run exodiff"
+command = ["./exodiff", "-file", \
+           name + ".exodiff", \
+           name + ".gold.e", \
+           name + ".e"]
+p = Popen(command, stdout=logfile, stderr=logfile)
+return_code = p.wait()
+if return_code != 0:
+    result = return_code
+
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+    sys.exit(result)
+
+sys.exit(result)
