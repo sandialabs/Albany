6a318a9f124a417212b4469809e9f6bd8b4e6204
Mauro Perego <mperego@sandia.gov>
2017-04-12 14:36:01 -0600

    FELIX: Added evaluator to compute pressure corrected temperature on elements

---
+++ src/FELIX/evaluators/FELIX_PressureCorrectedTemperature.cpp
@@ -0,0 +1,15 @@
+/*
+ * FELIX_PressureCorrectedTemperature.cpp
+ *
+ *  Created on: Jun 6, 2016
+ *      Author: abarone
+ */
+
+#include "PHAL_AlbanyTraits.hpp"
+
+#include "FELIX_PressureCorrectedTemperature.hpp"
+#include "FELIX_PressureCorrectedTemperature_Def.hpp"
+
+PHAL_INSTANTIATE_TEMPLATE_CLASS_WITH_ONE_SCALAR_TYPE(FELIX::PressureCorrectedTemperature)
+
+
