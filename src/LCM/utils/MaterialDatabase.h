6096e8f22a0259f777a0ac5d9c855faeeab45b05
Dan Ibanez <daibane@sandia.gov>
2017-04-16 10:10:03 -0600

    consistent LCM and QCAD MaterialDatabase headers
    now a simple text diff shows the main difference
    is the QCAD one supports node sets and side sets
    
    part of #112

--- src/LCM/utils/MaterialDatabase.h
+++ src/LCM/utils/MaterialDatabase.h
@@ -12,129 +12,84 @@
 
 namespace LCM {
 
-///
-/// Centralized collection of material parameters
-///
+/*!
+ * \brief Centralized collection of material parameters
+ */
 class MaterialDatabase
 {
 public:
 
-  /// Default constructor
+  //! Default constructor
   MaterialDatabase(
-      std::string const & input_file,
-      Teuchos::RCP<Teuchos::Comm<int> const> & tcomm);
+      std::string const& input_file,
+      Teuchos::RCP<Teuchos::Comm<int> const>& tcomm);
 
-  /// Destructor
+  //! Destructor
   ~MaterialDatabase();
 
-  /// No copying
+  //! No copying
   MaterialDatabase(MaterialDatabase const &) = delete;
 
-  /// No copying
+  //! No copying
   MaterialDatabase & operator=(MaterialDatabase const &) = delete;
 
-  /// Get a parameter / check existence
-  bool isParam(std::string const & param_name);
+  //! Get a parameter / check existence
+  bool isParam(std::string const& param_name);
 
   template<typename T>
-  T
-  getParam(std::string const & param_name);
+  T getParam(std::string const& param_name);
 
   template<typename T>
-  T
-  getParam(std::string const & param_name, T def_val);
+  T getParam(std::string const& param_name, T def_val);
 
-  /// Get a parameter for a particular material
-  bool
-  isMaterialParam(
-      std::string const & material_name,
-      std::string const & param_name);
+  //! Get a parameter for a particular material
+  bool isMaterialParam(std::string const& material_name, std::string const& param_name);
 
   template<typename T>
-  T
-  getMaterialParam(
-      std::string const & material_name,
-      std::string const & param_name);
+  T getMaterialParam(std::string const& material_name, std::string const& param_name);
 
   template<typename T>
-  T
-  getMaterialParam(
-      std::string const & material_name,
-      std::string const & param_name,
-      T def_val);
+  T getMaterialParam(std::string const& material_name, std::string const& param_name, T def_val);
 
-  /// Get a parameter for a particular element block
-  ///(or assoc. material if param_name is not in element block)
-  bool
-  isElementBlockParam(
-      std::string const & eb_name,
-      std::string const & param_name);
+  //! Get a parameter for a particular element block (or assoc. material if param_name is not in element bloc)
+  bool isElementBlockParam(std::string const& eb_name, std::string const& param_name);
 
   template<typename T>
-  T
-  getElementBlockParam(
-      std::string const & eb_name,
-      std::string const & param_name);
+  T getElementBlockParam(std::string const& eb_name, std::string const& param_name);
 
   template<typename T>
-  T
-  getElementBlockParam(
-      std::string const & eb_name,
-      std::string const & param_name,
-      T def_val);
+  T getElementBlockParam(std::string const& eb_name, std::string const& param_name, T def_val);
 
-  /// Get a sublist from a particular element block
-  bool
-  isElementBlockSublist(
-      std::string const & eb_name,
-      std::string const & sublist_name);
+  //! Get a sublist from a particular element block
+  bool isElementBlockSublist(std::string const& eb_name, std::string const& sublist_name);
 
-  Teuchos::ParameterList &
-  getElementBlockSublist(
-      std::string const & eb_name,
-      std::string const & sublist_name);
+  Teuchos::ParameterList&
+  getElementBlockSublist(std::string const& eb_name, std::string const& sublist_name);
 
-  /// Get a vector of the value of all parameters in the entire list
-  /// with name == param_name
+  //! Get a vector of the value of all parameters in the entire list with name == param_name
   template<typename T>
-  std::vector<T>
-  getAllMatchingParams(
-      std::string const & param_name);
+  std::vector<T> getAllMatchingParams(std::string const& param_name);
+
+private:
+  template<typename T>
+  void getAllMatchingParams_helper(std::string const& param_name,
+           std::vector<T>& results, Teuchos::ParameterList& param_list);
+
+  std::string translateDBSublistName(Teuchos::ParameterList* param_list, std::string const& list_name);
 
 private:
 
-  template<typename T>
-  void
-  getAllMatchingParams_helper(
-      std::string const & param_name,
-      std::vector<T> & results,
-      Teuchos::ParameterList & param_list);
-
-  std::string
-  translateDBSublistName(
-      Teuchos::ParameterList * p_list,
-      std::string const & list_name);
-
-private:
-
-  /// Encapsulated parameter list which holds all the data
-  Teuchos::ParameterList
-  data_{"Material Parameters"};
-
-  Teuchos::ParameterList *
-  p_materials_list_{nullptr};
-
-  Teuchos::ParameterList *
-  p_eb_list_{nullptr};
+  //! Encapsulated parameter list which holds all the data
+  Teuchos::ParameterList data_{"Material Parameters"};
+  Teuchos::ParameterList* p_materials_list_{nullptr};
+  Teuchos::ParameterList* p_eb_list_{nullptr};
 };
 
-//
-//
-//
 Teuchos::RCP<MaterialDatabase>
 createMaterialDatabase(
-    Teuchos::RCP<Teuchos::ParameterList> const & params,
-    Teuchos::RCP<Teuchos_Comm const> & commT);
-}
+    Teuchos::RCP<Teuchos::ParameterList> const& params,
+    Teuchos::RCP<Teuchos_Comm const>& commT);
 
-#endif // LCM_MaterialDatabase_h
+}  // namespace LCM
+
+#endif
--- src/problems/QCAD_MaterialDatabase.hpp
+++ src/problems/QCAD_MaterialDatabase.hpp
@@ -12,90 +12,103 @@
 
 namespace QCAD {
 
-  /*!
-   * \brief Centralized collection of material parameters
-   */
-  class MaterialDatabase
-  {
-  public:
-  
-    //! Default constructor
-    MaterialDatabase(const std::string& inputFile,
-                     Teuchos::RCP<const Teuchos::Comm<int> >& tcomm);  
+/*!
+ * \brief Centralized collection of material parameters
+ */
+class MaterialDatabase
+{
+public:
 
-    //! Destructor
-    ~MaterialDatabase();
+  //! Default constructor
+  MaterialDatabase(
+      std::string const& input_file,
+      Teuchos::RCP<Teuchos::Comm<int> const>& tcomm);
 
-    //! Get a parameter / check existence
-    bool isParam(const std::string& paramName);
-    template<typename T>
-    T getParam(const std::string& paramName);
-    template<typename T>
-    T getParam(const std::string& paramName, T def_val);
+  //! Destructor
+  ~MaterialDatabase();
 
-    //! Get a parameter for a particular material
-    bool isMaterialParam(const std::string& materialName, const std::string& paramName);
-    template<typename T>
-    T getMaterialParam(const std::string& materialName, const std::string& paramName);
-    template<typename T>
-    T getMaterialParam(const std::string& materialName, const std::string& paramName, T def_val);
+  //! No copying
+  MaterialDatabase(MaterialDatabase const &) = delete;
+
+  //! No copying
+  MaterialDatabase & operator=(MaterialDatabase const &) = delete;
+
+  //! Get a parameter / check existence
+  bool isParam(std::string const& param_name);
+
+  template<typename T>
+  T getParam(std::string const& param_name);
+
+  template<typename T>
+  T getParam(std::string const& param_name, T def_val);
+
+  //! Get a parameter for a particular material
+  bool isMaterialParam(std::string const& material_name, std::string const& param_name);
+
+  template<typename T>
+  T getMaterialParam(std::string const& material_name, std::string const& param_name);
+
+  template<typename T>
+  T getMaterialParam(std::string const& material_name, std::string const& param_name, T def_val);
+
+  //! Get a parameter for a particular element block (or assoc. material if param_name is not in element bloc)
+  bool isElementBlockParam(std::string const& eb_name, std::string const& param_name);
+
+  template<typename T>
+  T getElementBlockParam(std::string const& eb_name, std::string const& param_name);
+
+  template<typename T>
+  T getElementBlockParam(std::string const& eb_name, std::string const& param_name, T def_val);
+
+  //! Get a sublist from a particular element block
+  bool isElementBlockSublist(std::string const& eb_name, std::string const& sublist_name);
+
+  Teuchos::ParameterList&
+  getElementBlockSublist(std::string const& eb_name, std::string const& sublist_name);
+
+  //! Get a vector of the value of all parameters in the entire list with name == param_name
+  template<typename T>
+  std::vector<T> getAllMatchingParams(std::string const& param_name);
+
+  //! Get a parameter for a particular node set
+  bool isNodeSetParam(std::string const& material_name, std::string const& param_name);
 
 
-    //! Get a parameter for a particular node set
-    bool isNodeSetParam(const std::string& materialName, const std::string& paramName);
-    template<typename T>
-    T getNodeSetParam(const std::string& materialName, const std::string& paramName);
-    template<typename T>
-    T getNodeSetParam(const std::string& materialName, const std::string& paramName, T def_val);
+  template<typename T>
+  T getNodeSetParam(std::string const& material_name, std::string const& param_name);
 
-    //! Get a parameter for a particular side set
-    bool isSideSetParam(const std::string& materialName, const std::string& paramName);
-    template<typename T>
-    T getSideSetParam(const std::string& materialName, const std::string& paramName);
-    template<typename T>
-    T getSideSetParam(const std::string& materialName, const std::string& paramName, T def_val);
+  template<typename T>
+  T getNodeSetParam(std::string const& material_name, std::string const& param_name, T def_val);
 
-    //! Get a parameter for a particular element block (or assoc. material if paramName is not in element bloc)
-    bool isElementBlockParam(const std::string& ebName, const std::string& paramName);
-    template<typename T>
-    T getElementBlockParam(const std::string& ebName, const std::string& paramName);
-    template<typename T>
-    T getElementBlockParam(const std::string& ebName, const std::string& paramName, T def_val);
+  //! Get a parameter for a particular side set
+  bool isSideSetParam(std::string const& material_name, std::string const& param_name);
+  template<typename T>
+  T getSideSetParam(std::string const& material_name, std::string const& param_name);
+  template<typename T>
+  T getSideSetParam(std::string const& material_name, std::string const& param_name, T def_val);
 
-    //! Get a sublist from a particular element block
-    bool isElementBlockSublist(const std::string& ebName, const std::string& subListName);
-    Teuchos::ParameterList&
-    getElementBlockSublist(const std::string& ebName, const std::string& subListName);
+private:
+  template<typename T>
+  void getAllMatchingParams_helper(std::string const& param_name,
+           std::vector<T>& results, Teuchos::ParameterList& param_list);
 
-    //! Get a vector of the value of all parameters in the entire list with name == paramName
-    template<typename T> 
-    std::vector<T> getAllMatchingParams(const std::string& paramName);
+  std::string translateDBSublistName(Teuchos::ParameterList* param_list, std::string const& list_name);
 
-  private:
-    template<typename T> 
-    void getAllMatchingParams_helper(const std::string& paramName, 
-				     std::vector<T>& results, Teuchos::ParameterList& pList);
+private:
 
-    std::string translateDBSublistName(Teuchos::ParameterList*, const std::string&);
+  //! Encapsulated parameter list which holds all the data
+  Teuchos::ParameterList data_{"Material Parameters"};
+  Teuchos::ParameterList* p_materials_list_{nullptr};
+  Teuchos::ParameterList* p_eb_list_{nullptr};
+  Teuchos::ParameterList* p_ns_list{nullptr};
+  Teuchos::ParameterList* p_ss_list{nullptr};
+};
 
-    
+Teuchos::RCP<MaterialDatabase>
+createMaterialDatabase(
+    Teuchos::RCP<Teuchos::ParameterList> const& params,
+    Teuchos::RCP<Teuchos_Comm const>& commT);
 
-  private:
-
-    //! Private to prohibit copying
-    MaterialDatabase(const MaterialDatabase&);
-    
-    //! Private to prohibit copying
-    MaterialDatabase& operator=(const MaterialDatabase&);
-
-    //! Encapsulated parameter list which holds all the data
-    Teuchos::ParameterList data_;
-    Teuchos::ParameterList* pMaterialsList_;
-    Teuchos::ParameterList* pEBList_;
-    Teuchos::ParameterList* pNSList_;
-    Teuchos::ParameterList* pSSList_;
-  };
-
-}
+}  // namespace QCAD
 
 #endif
