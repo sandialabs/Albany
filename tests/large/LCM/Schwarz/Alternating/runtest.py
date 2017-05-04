b864f2d2d59ca4b711e01c0cb9ef817d2433d230
Alejandro Mota <amota@sandia.gov>
2017-05-03 18:01:59 -0700

    LCM: Make Schwarz alternating test more strict.
    If the convergence criterion is satisfied, the following string
    is output:
    
    Schwarz Alternating Method converged: YES
    
    otherwise the string becomes:
    
    Schwarz Alternating Method converged: NO
    
    Change the failure test criterion from the presence of the second
    string to the absence of the first.

--- tests/large/LCM/Schwarz/Alternating/runtest.py
+++ tests/large/LCM/Schwarz/Alternating/runtest.py
@@ -1,37 +1,40 @@
-
 #! /usr/bin/env python
-
 import sys
 import os
 import re
+
 from subprocess import Popen
 
-result = 0
-
-######################
-# Test 1 
-######################
-print "test 1 - Schwarz Alternating"
 name = "cuboid"
 log_file_name = name + ".log"
+result = 0
+
+print "test 1 - Schwarz Alternating"
+
 if os.path.exists(log_file_name):
     os.remove(log_file_name)
+    
 logfile = open(log_file_name, 'w')
 
 # run Albany
 command = ["./AlbanyT", "cuboids.yaml"]
 p = Popen(command, stdout=logfile, stderr=logfile)
 return_code = p.wait()
+
 if return_code != 0:
     result = return_code
 
+converged = False
+
 for line in open(log_file_name):
-  if "Schwarz Alternating Method converged: NO" in line:
-    result = result+1 
+  if "Schwarz Alternating Method converged: YES" in line:
+    converged = True
+
+if converged == False:    
+  result = result + 1
 
 if result != 0:
     print "result is %s" % result
     print "%s test has failed" % name
-    sys.exit(result)
 
 sys.exit(result)
