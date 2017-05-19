f7615995982feb2d929b1194cbaefaee3608aec0
Dan Ibanez <daibane@sandia.gov>
2017-05-19 11:01:10 -0600

    point to internal CDash instead
    did manage to post to public one,
    but it has reached upload quota

--- doc/LCM/build/CTestConfig.cmake
+++ doc/LCM/build/CTestConfig.cmake
@@ -1,6 +1,6 @@
 set(CTEST_PROJECT_NAME "Albany")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 MDT")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=Albany")
+set(CTEST_DROP_SITE "cdash.sandia.gov")
+set(CTEST_DROP_LOCATION "/CDash-2-3-0/submit.php?project=Albany")
 set(CTEST_DROP_SITE_CDASH TRUE)
 set(CTEST_DROP_METHOD "http")
