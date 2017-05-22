c231f8a02a2c9a14726cfa6876dbde6262ec9662
Dan Ibanez <daibane@sandia.gov>
2017-05-22 08:14:13 -0600

    update albany-lcm-symlinks
    remove "mail" variants, add "dash" variants

--- doc/LCM/install/albany-lcm-symlinks.sh
+++ doc/LCM/install/albany-lcm-symlinks.sh
@@ -5,18 +5,15 @@ ln -s Albany/doc/LCM/build/trilinos-config.sh .
 ln -s Albany/doc/LCM/build/albany-config.sh .
 ln -s Albany/doc/LCM/build/build.sh .
 ln -s Albany/doc/LCM/build/build-all.sh .
-ln -s build.sh clean-config-build-test-mail.sh
-ln -s build.sh config-build-test-mail.sh
-ln -s build.sh build-test-mail.sh
-ln -s build.sh test-mail.sh
-ln -s build.sh mail.sh
+ln -s build.sh clean-config-build-test-dash.sh
 ln -s build.sh clean-config-build-test.sh
 ln -s build.sh config-build-test.sh
 ln -s build.sh build-test.sh
 ln -s build.sh test.sh
+ln -s build.sh clean-config-build-dash.sh
 ln -s build.sh clean-config-build.sh
 ln -s build.sh config-build.sh
+ln -s build.sh clean-config-dash.sh
 ln -s build.sh clean-config.sh
 ln -s build.sh config.sh
 ln -s build.sh clean.sh
-
