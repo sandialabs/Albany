2ea6156f513e2e3615247135de3dc6fe46f839c8
Alejandro Mota <amota@sandia.gov>
2017-05-22 18:19:12 -0700

    Reduce ctest verbosity from -VV to -V so that users only see a couple of lines per tests instead of the whole output for each test.

--- doc/LCM/build/build.sh
+++ doc/LCM/build/build.sh
@@ -7,7 +7,7 @@ BUILD_STRING=$ARCH-$TOOL_CHAIN-$BUILD_TYPE
 PREFIX=$PACKAGE-$BUILD_STRING
 LOG_FILE="$LCM_DIR/${PREFIX}.log"
 
-ctest -VV -S $LCM_DIR/Albany/doc/LCM/build/lcm_build.cmake \
+ctest -V -S $LCM_DIR/Albany/doc/LCM/build/lcm_build.cmake \
 -DSCRIPT_NAME:STRING=`basename $0` \
 -DPACKAGE:STRING=$PACKAGE \
 -DBUILD_THREADS:STRING=$2 \
