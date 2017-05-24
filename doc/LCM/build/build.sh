a5deec83fdbf4a1437935c035e41ab601fedd61e
Alejandro Mota <amota@sandia.gov>
2017-05-24 13:57:43 -0700

    LCM: Finalizing testing and minor tweaking of new cmake-based build system. Updating install doc.

--- doc/LCM/build/build.sh
+++ doc/LCM/build/build.sh
@@ -9,11 +9,24 @@ BUILD_ID_STRING=$ARCH-$TOOL_CHAIN-$BUILD_TYPE
 PREFIX=$PACKAGE-$BUILD_ID_STRING
 LOG_FILE="$LCM_DIR/${PREFIX}.log"
 
+case "$THREADS" in
+    -V)
+        VERBOSITY="-V"
+        THREADS=""
+    ;;
+    -VV)
+        VERBOSITY="-VV"
+        THREADS=""
+    ;;
+    *)
+    ;;
+esac
+
 if [ -z $VERBOSITY ]; then
     VERBOSITY="-VV"
 fi
 
-ctest $VERBOSITY --timeout 60 -S $LCM_DIR/Albany/doc/LCM/build/lcm_build.cmake \
+ctest $VERBOSITY --timeout 180 -S $LCM_DIR/Albany/doc/LCM/build/lcm_build.cmake \
 -DSCRIPT_NAME:STRING=`basename $0` \
 -DPACKAGE:STRING=$PACKAGE \
 -DBUILD_THREADS:STRING=$THREADS \
