ea96a9c65b6b1852dde0cc1f2a529a322a90045a
Alejandro Mota <amota@sandia.gov>
2017-06-06 15:18:44 -0700

    LCM #145: WIP implementation of strong DBCs.

--- src/LCM/evaluators/bc/StrongDBC_Def.hpp
+++ src/LCM/evaluators/bc/StrongDBC_Def.hpp
@@ -44,6 +44,30 @@ void
 StrongDBC<PHAL::AlbanyTraits::Residual, Traits>::
 evaluateFields(typename Traits::EvalData dirichlet_workset)
 {
+  Teuchos::RCP<Tpetra_Vector>
+  f = dirichlet_workset.fT;
+
+  Teuchos::ArrayRCP<ST>
+  f_view = f->get1dViewNonConst();
+
+  // Grab the vector of node GIDs for this Node Set ID
+  std::vector<std::vector<int>> const &
+  ns_nodes = dirichlet_workset.nodeSets->find(this->nodeSetID)->second;
+
+  for (size_t node = 0; node < ns_nodes.size(); node++) {
+
+    int const
+    dof = ns_nodes[node][this->offset];
+
+    f_view[dof] = 0.0;
+
+#if defined(ALBANY_LCM)
+    // Record DOFs to avoid setting Schwarz BCs on them.
+    dirichlet_workset.fixed_dofs_.insert(dof);
+#endif
+
+  }
+
   return;
 }
 
