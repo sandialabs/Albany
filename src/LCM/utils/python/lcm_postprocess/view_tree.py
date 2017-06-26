17923069df857e81f0d634036af2d5724f48b4a3
Coleman Alleman <callema@sandia.gov>
2017-06-26 08:38:26 -0700

    LCM: Removed automatic loading of tk dependent functions from lcm_postprocess

--- src/LCM/utils/python/lcm_postprocess/view_tree.py
+++ src/LCM/utils/python/lcm_postprocess/view_tree.py
@@ -3,13 +3,13 @@
 if __name__ == '__main__':
 
     import sys
-    from _core import view_tree
+    from _core import _view_tree
 
     try:
         name_file_input = sys.argv[1]
     except:
         raise
 
-    view_tree(filename = name_file_input)
+    _view_tree(filename = name_file_input)
 
 # end if __name__ == '__main__':
