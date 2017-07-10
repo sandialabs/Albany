62ff34b1da407edb561f61b54ea96d66f82a41f9
Alejandro Mota <amota@sandia.gov>
2017-07-10 11:15:36 -0700

    LCM: Fix failing Schwarz test.

--- src/LCM/solvers/Schwarz_Alternating.cpp
+++ src/LCM/solvers/Schwarz_Alternating.cpp
@@ -642,16 +642,16 @@ SchwarzLoop() const
         in_args = sub_inargs_[subdomain];
 
         // Propagate previous solution if this is not the first step
-        if (stop > 0) {
-          in_args.set_x(solutions_[subdomain]);
-        }
+        //if (stop > 0) {
+        //  in_args.set_x(solutions_[subdomain]);
+        //}
 
         Thyra::ModelEvaluatorBase::OutArgs<ST> &
         out_args = sub_outargs_[subdomain];
 
         solver.evalModel(in_args, out_args);
 
-        solutions_[subdomain] = in_args.get_x();
+        //solutions_[subdomain] = in_args.get_x();
 
         // After solve, get info to check convergence
         Teuchos::RCP<SolutionSniffer>
