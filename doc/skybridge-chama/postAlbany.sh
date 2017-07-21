d840b420e7e25d33f7cf6930a3cf762a5df8a651
Jay Foulk <jwfoulk@sandia.gov>
2017-07-21 08:47:09 -0600

    Updating shell script to include needed python paths for robustness suite
    Need to do a few of these things with the modules

--- doc/skybridge-chama/postAlbany.sh
+++ doc/skybridge-chama/postAlbany.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-source ~/bin/runAlbany
+module load lcm/sems
+module load serial-gcc-release
+module unload sems-python/2.7.9
+module load canopy
 
-module unload python/2.7
-module load canopy/1.6.2
+# path to robustness suite
+export ROBUSTNESS_SUITE=/ascldap/users/jwfoulk/SolidMechanics/SolidMechanicsExamples/LCM/CrystalPlasticity/robustnessTests
+
+# must have includes for exodusII
+PATH=$LCM_DIR/trilinos-install-serial-gcc-release/include:$PATH
+
+## Python tools in robustness suite
+export PYTHONPATH=$ROBUSTNESS_SUITE/Tools:$PYTHONPATH
+export PATH=$ROBUSTNESS_SUITE/Tools:$PATH
+
+## Account information needed for robustness suite
+export MYWCID=fy150167
