380902abc3bf9dd311568bfcd274a19ab8abc8af
Alejandro Mota <amota@sandia.gov>
2017-10-11 15:26:19 -0700

    LCM: Remove LCM dependency on yaml-cpp since now Teuchos has native yaml parsing.

--- doc/LCM/modulefiles/lcm-tpls.lua
+++ doc/LCM/modulefiles/lcm-tpls.lua
@@ -7,8 +7,6 @@ local mpi_root = os.getenv("MPI_ROOT")
 local netcdf_root = "/ascldap/users/daibane/LCM/TPL_toss3/install/netcdf"
 local netcdf_inc = netcdf_root .. "/include"
 local netcdf_lib = netcdf_root .. "/lib"
-local yaml_cpp_inc = "/ascldap/users/daibane/LCM/TPL_toss3/install/yaml-cpp/include"
-local yaml_cpp_lib = "/ascldap/users/daibane/LCM/TPL_toss3/install/yaml-cpp/lib"
 setenv("BOOST_INC", boost_inc)
 setenv("BOOST_LIB", boost_lib)
 setenv("BOOSTLIB_INC", boost_inc)
@@ -20,7 +18,5 @@ setenv("NETCDF", netcdf_root)
 setenv("NETCDF_INC", netcdf_inc)
 setenv("NETCDF_LIB", netcdf_lib)
 setenv("LCM_NETCDF_PARALLEL", "ON")
-setenv("YAML_CPP_INC", yaml_cpp_inc)
-setenv("YAML_CPP_LIB", yaml_cpp_lib)
 -- PATHs for MPI are set by the modules
 prepend_path("LD_LIBRARY_PATH", netcdf_lib)
