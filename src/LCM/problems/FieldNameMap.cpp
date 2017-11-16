564653145b7525beff90a0403cbeb1473a54d347
Coleman Alleman <callema@sandia.gov>
2017-11-16 10:56:13 -0800

    LCM: Fixed a problem with transient heat conduction

--- src/LCM/problems/FieldNameMap.cpp
+++ src/LCM/problems/FieldNameMap.cpp
@@ -39,45 +39,75 @@ namespace LCM {
     name_map->insert( std::make_pair("J","J") );
     name_map->insert( std::make_pair("Velocity_Gradient","Velocity_Gradient") );
     name_map->insert( std::make_pair("Velocity_Gradient_Plastic","Velocity_Gradient_Plastic") );
+
+    //
     // Crystal plasticity model
-    const int max_slip_systems = 48;
+    //
+    int const
+    max_slip_systems = 48;
+
     // field names for slip on each slip system
-    for (int i=0; i < max_slip_systems; ++i) {
-      std::string g = Albany::strint("gamma", i+1,'_');
-      name_map->insert( std::make_pair(g,g) );
+    for (int sys{0}; sys < max_slip_systems; ++sys) {
+      
+      std::string const 
+      g = Albany::strint("gamma", sys + 1, '_');
+
+      name_map->insert(std::make_pair(g, g));
     }
+
     // field names for slip rate on each slip system
-    for (int i=0; i < max_slip_systems; ++i) {
-          std::string g_dot = Albany::strint("gamma_dot", i+1,'_');
-          name_map->insert( std::make_pair(g_dot,g_dot) );
+    for (int sys{0}; sys < max_slip_systems; ++sys) {
+
+      std::string const
+      g_dot = Albany::strint("gamma_dot", sys + 1,'_');
+
+      name_map->insert(std::make_pair(g_dot, g_dot));
     }
+
     // field names for the hardening on each slip system
-    for (int i=0; i < max_slip_systems; ++i) {
-          std::string t_h = Albany::strint("tau_hard", i+1,'_');
-          name_map->insert( std::make_pair(t_h,t_h) );
+    for (int sys{0}; sys < max_slip_systems; ++sys) {
+
+      std::string const
+      t_h = Albany::strint("tau_hard", sys + 1, '_');
+
+      name_map->insert(std::make_pair(t_h, t_h));
     }
+
     // field names for shear stress on each slip system
-    for (int i=0; i < max_slip_systems; ++i) {
-      std::string t = Albany::strint("tau", i+1,'_');
-      name_map->insert( std::make_pair(t,t) );
+    for (int sys{0}; sys < max_slip_systems; ++sys) {
+
+      std::string const
+      tau = Albany::strint("tau", sys + 1, '_');
+
+      name_map->insert(std::make_pair(tau, tau));
     }
-    name_map->insert( std::make_pair("CP_Residual","CP_Residual") );
-    name_map->insert( std::make_pair("CP_Residual_Iter","CP_Residual_Iter") );
+
+    name_map->insert(std::make_pair("CP_Residual","CP_Residual"));
+    name_map->insert(std::make_pair("CP_Residual_Iter","CP_Residual_Iter"));
     // field name for crystallographic rotation tensor
-    name_map->insert( std::make_pair("Re","Re") );
-    // field name for ViscoElastic model
-    name_map->insert( std::make_pair("H_1","H_1") );
-    name_map->insert( std::make_pair("H_2","H_2") );
-    name_map->insert( std::make_pair("H_3","H_3") );
-    name_map->insert( std::make_pair("Instantaneous Stress","Instantaneous Stress") );
+    name_map->insert(std::make_pair("Re","Re"));
+
+    //
+    // ViscoElastic model
+    //
+    name_map->insert(std::make_pair("H_1","H_1"));
+    name_map->insert(std::make_pair("H_2","H_2"));
+    name_map->insert(std::make_pair("H_3","H_3"));
+    name_map->insert(std::make_pair("Instantaneous Stress","Instantaneous Stress"));
+
+    //
     // Poroplasticity model
+    //
     name_map->insert( std::make_pair("Total_Stress","Total_Stress") );
     name_map->insert( std::make_pair("KCPermeability","KCPermeability") );
     name_map->insert( std::make_pair("Biot_Modulus","Biot_Modulus") );
     name_map->insert( std::make_pair("Biot_Coefficient","Biot_Coefficient") );
     name_map->insert( std::make_pair("Porosity","Porosity") );
     name_map->insert( std::make_pair("Pore_Pressure","Pore_Pressure") );
-    // hydrogen transport model
+
+    //
+    // Hydrogen transport model
+    //
     name_map->insert( std::make_pair("Transport","Transport") );
     name_map->insert( std::make_pair("HydroStress","HydroStress") );
     name_map->insert( std::make_pair("Diffusion_Coefficient","Diffusion_Coefficient") );
@@ -91,23 +121,33 @@ namespace LCM {
     	"Concentration_Equilibrium_Parameter") );
     name_map->insert( std::make_pair("Gradient_Element_Length",
     	"Gradient_Element_Length") );
-    // helium ODEs
+
+    //
+    // Helium ODEs
+    //
     name_map->insert( std::make_pair("He_Concentration","He_Concentration") );
     name_map->insert( std::make_pair("Total_Bubble_Density","Total_Bubble_Density") );
     name_map->insert( std::make_pair("Bubble_Volume_Fraction","Bubble_Volume_Fraction") );
-    // geo-models
+
+    //
+    // Geo-models
+    //
     name_map->insert( std::make_pair("Back_Stress","Back_Stress") );
     name_map->insert( std::make_pair("Cap_Parameter","Cap_Parameter") );
     name_map->insert( std::make_pair("volPlastic_Strain","volPlastic_Strain") );
     name_map->insert( std::make_pair("Strain","Strain") );
     name_map->insert( std::make_pair("Friction_Parameter","Friction_Parameter") );
 
-    if ( surface_flag ) {
-      std::map<std::string, std::string>::iterator it;
+    if (surface_flag) {
+
+      std::map<std::string, std::string>::iterator
+      it;
+      
       for (it = name_map->begin(); it != name_map->end(); ++it) {
-        it->second = "surf_"+it->second;
+        it->second = "surf_" + it->second;
       }
     }
+
     field_name_map_ = name_map;
   }
 
