9290a0770d67deca0cb0a0ceb8ce1e9387ecc125
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-05 14:21:06 -0800

    Modifying scripts for camobap to get rid of separate 32bit and 64bit builds.
    Now all builds are 64bit by default.

--- doc/dashboards/camobap.ca.sandia.gov/send_email_functor_openMP.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_functor_openMP.sh
@@ -7,4 +7,4 @@ TTTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt
 TTTTT=`grep "(Timeout)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "Albany (master, OpenMP, KOKKOS_UNDER_DEVELOPMENT): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, gahanse@sandia.gov, jwatkin@sandia.gov, daibane@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
+/bin/mail -s "Albany (master, OpenMP): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, gahanse@sandia.gov, jwatkin@sandia.gov, daibane@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
