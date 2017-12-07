c7c5221cf3da0cf1a2665585d5894a04e3addab3
Irina K. Tezaur <ikalash@sandia.gov>
2017-12-07 12:03:58 -0800

    Modifying email scripts for camobap.

--- doc/dashboards/camobap.ca.sandia.gov/send_emailT.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_emailT.sh
@@ -5,6 +5,7 @@
 TTT=`grep "(Failed)" /home/ikalash/nightlyCDash/nightly_logT.txt -c`
 TTTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_logT.txt -c`
 TTTTT=`grep "(Timeout)" /home/ikalash/nightlyCDash/nightly_logT.txt -c`
+TT=`grep "...   Passed" /home/ikalash/nightlyCDash/nightly_logT.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "Albany (master, AlbanyT only): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov" < /home/ikalash/nightlyCDash/resultsT
+/bin/mail -s "Albany (master, AlbanyT only): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov" < /home/ikalash/nightlyCDash/resultsT
--- doc/dashboards/camobap.ca.sandia.gov/send_email_functor_openMP.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_functor_openMP.sh
@@ -5,6 +5,7 @@
 TTT=`grep "(Failed)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
 TTTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
 TTTTT=`grep "(Timeout)" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
+TT=`grep "...   Passed" /home/ikalash/nightlyCDash/nightly_log_functor_openMP.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "Albany (master, OpenMP): $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, gahanse@sandia.gov, jwatkin@sandia.gov, daibane@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
+/bin/mail -s "Albany (master, OpenMP): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov, gahanse@sandia.gov, jwatkin@sandia.gov, daibane@sandia.gov" < /home/ikalash/nightlyCDash/results_functor_openMP
