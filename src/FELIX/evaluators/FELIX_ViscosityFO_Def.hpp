sandialabs/Albany

commit 23008140256bf5f7a12ed4171e009c457137b60a

FELIX CUDA: Replacing std::max in flowRate kernel

src/FELIX/evaluators/FELIX_ViscosityFO_Def.hpp
@@ -205,7 +205,8 @@ template<typename EvalT,typename Traits,typename VelT, typename TemprT>
 template<typename TemperatureT>
 KOKKOS_INLINE_FUNCTION
 TemperatureT ViscosityFO<EvalT,Traits,VelT,TemprT>::flowRate (const TemperatureT& T) const {
-  return (T < switchingT) ? arrml / exp (actenl / gascon / std::max(T,TemperatureT(150))) : arrmh / exp (actenh / gascon / T);
+  return (T < switchingT) ? arrml / exp (actenl / gascon / ((T < TemperatureT(150)) ? T : TemperatureT(150))) : 
+    arrmh / exp (actenh / gascon / T);
 }
 
 //**********************************************************************

commit 35ec6dcf38e6a9f0f224cc38510be2560dd91732

Fix trivial bug introduced in commit 23008140256bf5f7a12. This should fix issue #249

src/FELIX/evaluators/FELIX_ViscosityFO_Def.hpp
@@ -205,7 +205,7 @@ template<typename EvalT,typename Traits,typename VelT, typename TemprT>
 template<typename TemperatureT>
 KOKKOS_INLINE_FUNCTION
 TemperatureT ViscosityFO<EvalT,Traits,VelT,TemprT>::flowRate (const TemperatureT& T) const {
-  return (T < switchingT) ? arrml / exp (actenl / gascon / ((T < TemperatureT(150)) ? T : TemperatureT(150))) : 
+  return (T < switchingT) ? arrml / exp (actenl / gascon / ((T > TemperatureT(150)) ? T : TemperatureT(150))) : 
     arrmh / exp (actenh / gascon / T);
 }
 