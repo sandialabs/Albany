4f4ef398b019c7ae4903f0dbdd05b80a63b25b1c
Jenn Frederick <jmfrede@sandia.gov>
2018-02-12 09:13:34 -0700

    LCM: Rearranged thermal variable ordering, again.
    Reordered variables to match Residual_Def ordering.

--- src/LCM/evaluators/residuals/HeatEqnResidual.hpp
+++ src/LCM/evaluators/residuals/HeatEqnResidual.hpp
@@ -43,11 +43,11 @@ public:
 private:
   // Input:
   PHX::MDField<const MeshScalarT, Cell, Node, QuadPoint> wBF;
+  PHX::MDField<const MeshScalarT, Cell, Node, QuadPoint, Dim> wGradBF;
   PHX::MDField<const ScalarT, Cell, QuadPoint> Temperature;
   PHX::MDField<const ScalarT, Cell, QuadPoint> Tdot;
-  PHX::MDField<const ScalarT, Cell, QuadPoint> thermal_conductivity_;
-  PHX::MDField<const MeshScalarT, Cell, Node, QuadPoint, Dim> wGradBF;
   PHX::MDField<const ScalarT, Cell, QuadPoint, Dim> TGrad;
+  PHX::MDField<const ScalarT, Cell, QuadPoint> thermal_conductivity_;
   PHX::MDField<const ScalarT, Cell, QuadPoint> density_;
   PHX::MDField<const ScalarT, Cell, QuadPoint> specific_heat_;
   PHX::MDField<const ScalarT, Cell, QuadPoint> pressure_;
