296774b4bfc19ffb761747c27dba0c697ed328f8
Irina K. Tezaur <ikalash@sandia.gov>
2018-03-01 13:47:03 -0800

    Per his request, removing Andy from list of recipients of Albany test results for builds that I own.

--- doc/dashboards/camobap.ca.sandia.gov/send_email_cismAlbanyEpetra.sh
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_cismAlbanyEpetra.sh
@@ -9,4 +9,4 @@ TT=`grep "...   Passed" /home/ikalash/nightlyCDash/nightly_log_cismAlbanyEpetra.
 
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "cism-piscees (felix_interface, epetra): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, agsalin@sandia.gov" < /home/ikalash/nightlyCDash/results_cismAlbanyEpetra
+/bin/mail -s "cism-piscees (felix_interface, epetra): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /home/ikalash/nightlyCDash/results_cismAlbanyEpetra
