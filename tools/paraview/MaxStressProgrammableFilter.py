d1b96963db341306f4f76f21d345a5eb0787775e
Glen Hansen <gahanse@sandia.gov>
2018-05-17 15:54:23 -0600

    Tweak the paraview README.

--- tools/paraview/MaxStressProgrammableFilter.py
+++ tools/paraview/MaxStressProgrammableFilter.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 cauchy_stress = []
 for i in range(1,45):
     cauchy_stress.append(inputs[0].CellData['Cauchy_Stress_%02d' % (i,)])
-dataArray = max(cauchy_stress)
+dataArray = np.squeeze(np.asarray(np.matrix(cauchy_stress).max(axis=0)))
 output.CellData.append(dataArray, 'Cauchy_Stress_Max')
 
 displacement = inputs[0].PointData["solution_"]
