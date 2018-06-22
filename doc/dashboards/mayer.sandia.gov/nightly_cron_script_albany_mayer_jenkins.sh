d95919f4c8c783bfd38a43a09816396b5b5bb7b9
Irina Tezaur <ikalash@mayer.sandia.gov>
2018-06-21 22:05:32 -0600

    Updating mayer scripts per discussion with Jerry.

--- doc/dashboards/mayer.sandia.gov/nightly_cron_script_albany_mayer_jenkins.sh
+++ doc/dashboards/mayer.sandia.gov/nightly_cron_script_albany_mayer_jenkins.sh
@@ -8,6 +8,10 @@ unset https_proxy
 
 cat albany_jenkins ctest_nightly.cmake.frag >& ctest_nightly.cmake  
 
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
 now=$(date +"%m_%d_%Y-%H_%M")
 LOG_FILE=$BASE_DIR/nightly_log_mayerAlbany.txt
 
--- doc/dashboards/mayer.sandia.gov/nightly_cron_script_trilinos_mayer_jenkins.sh
+++ doc/dashboards/mayer.sandia.gov/nightly_cron_script_trilinos_mayer_jenkins.sh
@@ -6,6 +6,10 @@ cd $BASE_DIR
 unset http_proxy
 unset https_proxy
 
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
 cat trilinos_jenkins ctest_nightly_jenkins.cmake.frag >& ctest_nightly.cmake  
 
 now=$(date +"%m_%d_%Y-%H_%M")
