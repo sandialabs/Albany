b97923cc03e9f995c659c61feef1c0ee8b0660cc
Irina K. Tezaur <ikalash@sandia.gov>
2018-07-05 09:09:16 -0700

    LCM: fixing failing Crystal Plasticity Orientation on Mesh tests.

--- tests/large/LCM/CrystalPlasticity/runtest.cmake
+++ tests/large/LCM/CrystalPlasticity/runtest.cmake
@@ -1,6 +1,19 @@
 
 
-# 0. Run epu 
+
+# 1. Run the program and generate the exodus output
+
+message("Running the command:")
+message("${TEST_PROG} " " ${TEST_ARGS}")
+
+EXECUTE_PROCESS(COMMAND ${TEST_PROG} ${TEST_ARGS}
+                RESULT_VARIABLE HAD_ERROR)
+
+if(HAD_ERROR)
+	message(FATAL_ERROR "Albany didn't run: test failed")
+endif()
+
+# 2. Run epu 
 
 if (DEFINED MPIMNP AND ${MPIMNP} GREATER 1)
   if (NOT SEACAS_EPU)
@@ -22,20 +35,7 @@ if (DEFINED MPIMNP AND ${MPIMNP} GREATER 1)
 endif()
 
 
-# 1. Run the program and generate the exodus output
-
-message("Running the command:")
-message("${TEST_PROG} " " ${TEST_ARGS}")
-
-EXECUTE_PROCESS(COMMAND ${TEST_PROG} ${TEST_ARGS}
-                RESULT_VARIABLE HAD_ERROR)
-
-if(HAD_ERROR)
-	message(FATAL_ERROR "Albany didn't run: test failed")
-endif()
-
-
-# 2. Find and run exodiff
+# 3. Find and run exodiff
 
 if (NOT SEACAS_EXODIFF)
   message(FATAL_ERROR "Cannot find exodiff")
