6a6c010b45618ef1c3c073eef90bdc0dd9ff05db
Irina K. Tezaur <ikalash@sandia.gov>
2018-07-06 17:04:45 -0700

    LCM: progress towards issue #278.

---
+++ tests/large/LCM/Schwarz/Alternating/Dynamics/Torsion/check_convergence.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import sys
+import os
+import re
+
+from subprocess import Popen
+
+name = "torsion_parallel"
+log_file_name = name + ".log"
+result = 0
+
+with open(log_file_name, 'r') as log_file:
+    print log_file.read()
+
+converged = False
+
+for line in open(log_file_name):
+  if "Schwarz Alternating Method converged: YES" in line:
+    converged = True
+
+for line in open(log_file_name):
+  if "Schwarz Alternating Method converged: NO" in line:
+    converged = False
+
+if converged == False:
+  result = result + 1
+
+with open(log_file_name, 'r') as log_file:
+    print log_file.read()
+
+if result != 0:
+    print "result is %s" % result
+    print "%s test has failed" % name
+
+
+sys.exit(result)
