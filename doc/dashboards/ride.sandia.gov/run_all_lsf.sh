b9638f07b4b11616aec80f825330daa8aedd8820
Irina K. Tezaur <ikalash@sandia.gov>
2018-07-18 11:58:08 -0700

    Changing ride Jenkins job script to send just 1 email rather than 2.

--- doc/dashboards/ride.sandia.gov/run_all_lsf.sh
+++ doc/dashboards/ride.sandia.gov/run_all_lsf.sh
@@ -15,5 +15,5 @@ bash -c -l "source ride_modules_cuda.sh >& modules_trilinos.out; bash nightly_cr
 cat albany ctest_nightly.cmake.frag >& ctest_nightly.cmake
 bash -c -l "source ride_modules_cuda.sh >& modules_albany.out; bash nightly_cron_script_albany_ride.sh"
 bash process_results_ctest.sh 
-bash send_email_ctest.sh  
+#bash send_email_ctest.sh  
 
