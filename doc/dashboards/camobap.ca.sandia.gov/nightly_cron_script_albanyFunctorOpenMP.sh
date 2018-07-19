6ac76d09952bea3b7fb9a53f0f4ab21ad430fe1e
Irina K. Tezaur <ikalash@sandia.gov>
2018-07-19 13:53:23 -0700

    Updating OpenMP build settings on camobap.

--- doc/dashboards/camobap.ca.sandia.gov/nightly_cron_script_albanyFunctorOpenMP.sh
+++ doc/dashboards/camobap.ca.sandia.gov/nightly_cron_script_albanyFunctorOpenMP.sh
@@ -7,10 +7,9 @@ cat albanyFunctorOpenMP ctest_nightly.cmake.frag >& ctest_nightly.cmake
 export PATH=$PATH:/usr/lib64/openmpi/bin:/home/ikalash/Install/ParaView-4.3.1-Linux-64bit/bin:/home/ikalash/Install:/home/ikalash/Install/Cubit:/home/ikalash/Install/R2015a/bin:/home/ikalash/nightlyAlbanyTests/Results/Trilinos/build-openmp/install
 
 export LD_LIBRARY_PATH=/usr/lib64:/usr/lib64/openmpi/lib
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
 export OMP_PLACES=threads
 export OMP_PROC_BIND=spread
-
 
 now=$(date +"%m_%d_%Y-%H_%M")
 #LOG_FILE=/projects/AppComp/nightly/cee-compute011/nightly_$now
