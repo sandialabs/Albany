d8e73532461f832df82736e6b7024ab51ddc1581
Irina K. Tezaur <ikalash@sandia.gov>
2018-09-24 11:09:30 -0700

    Minor mod to email script.

--- doc/dashboards/ride.sandia.gov/send_email_ctest.sh
+++ doc/dashboards/ride.sandia.gov/send_email_ctest.sh
@@ -7,5 +7,5 @@ TTTT=`grep "(Not Run)" nightly_log_rideAlbany.txt -c`
 TTTTT=`grep "(Timeout)" nightly_log_rideAlbany.txt -c`
 TT=`grep "...   Passed" nightly_log_rideAlbany.txt -c`
 
-/bin/mail -s "Albany (master, CUDA, KOKKOS_UNDER_DEVELOPMENT): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, jwatkin@sandia.gov, amota@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov" < results_cuda
+/bin/mail -s "Albany (master, Ride CUDA+P100+P8, KOKKOS_UNDER_DEVELOPMENT): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, jwatkin@sandia.gov, amota@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov" < results_cuda
 
