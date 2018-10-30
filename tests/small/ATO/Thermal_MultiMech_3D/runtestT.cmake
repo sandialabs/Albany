83d534f92b0a00c13e14c3613db6174d436472d1
Irina K. Tezaur <ikalash@sandia.gov>
2018-10-29 20:57:53 -0700

    Progress towards issue #327: converting ATO to yaml. Starting on large tests subdirs.

--- tests/small/ATO/Thermal_MultiMech_3D/runtestT.cmake
+++ tests/small/ATO/Thermal_MultiMech_3D/runtestT.cmake
@@ -1,6 +1,6 @@
 # 1. Run the program and generate the exodus output
 
-EXECUTE_PROCESS(COMMAND ${TEST_PROG} nodalT.xml RESULT_VARIABLE HAD_ERROR)
+EXECUTE_PROCESS(COMMAND ${TEST_PROG} nodalT.yaml RESULT_VARIABLE HAD_ERROR)
 
 if(HAD_ERROR)
 	message(FATAL_ERROR "Albany: test failed")
