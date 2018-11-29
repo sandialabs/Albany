72646efc42c03ccd9f888fab469263dc9e2c9bd1
Mauro Perego <mperego@sandia.gov>
2018-11-28 18:36:36 -0700

    LandIce: Modified enum ParamEnum, adding MuPowerLaw, and          renaming Mu with MuCoulomb

--- src/LandIce/problems/LandIce_Hydrology.hpp
+++ src/LandIce/problems/LandIce_Hydrology.hpp
@@ -892,12 +892,12 @@ Hydrology::constructEvaluators (PHX::FieldManager<PHAL::AlbanyTraits>& fm0,
   //--- Shared Parameter for basal friction coefficient: mu ---//
   p = Teuchos::rcp(new Teuchos::ParameterList("Basal Friction Coefficient: mu"));
 
-  param_name = ParamEnumName::Mu;
+  param_name = ParamEnumName::MuCoulomb;
   p->set<std::string>("Parameter Name", param_name);
   p->set< Teuchos::RCP<ParamLib> >("Parameter Library", paramLib);
 
-  Teuchos::RCP<LandIce::SharedParameter<EvalT,PHAL::AlbanyTraits,ParamEnum,ParamEnum::Mu>> ptr_mu;
-  ptr_mu = Teuchos::rcp(new LandIce::SharedParameter<EvalT,PHAL::AlbanyTraits,ParamEnum,ParamEnum::Mu>(*p,dl));
+  Teuchos::RCP<LandIce::SharedParameter<EvalT,PHAL::AlbanyTraits,ParamEnum,ParamEnum::MuCoulomb>> ptr_mu;
+  ptr_mu = Teuchos::rcp(new LandIce::SharedParameter<EvalT,PHAL::AlbanyTraits,ParamEnum,ParamEnum::MuCoulomb>(*p,dl));
   ptr_mu->setNominalValue(params->sublist("Parameters"),params->sublist("LandIce Basal Friction Coefficient").get<double>(param_name,-1.0));
   fm0.template registerEvaluator<EvalT>(ptr_mu);
 
--- src/LandIce/problems/LandIce_ParamEnum.hpp
+++ src/LandIce/problems/LandIce_ParamEnum.hpp
@@ -10,16 +10,18 @@ enum class ParamEnum
 {
   Alpha        = 0,
   Lambda       = 1,
-  Mu           = 2,
-  Power        = 3,
-  Homotopy     = 4
+  MuCoulomb    = 2,
+  MuPowerLaw   = 3,
+  Power        = 4,
+  Homotopy     = 5
 };
 
 namespace ParamEnumName
 {
   static const std::string Alpha         = "Hydraulic-Over-Hydrostatic Potential Ratio";
   static const std::string Lambda        = "Bed Roughness";
-  static const std::string Mu            = "Coulomb Friction Coefficient";
+  static const std::string MuCoulomb     = "Coulomb Friction Coefficient";
+  static const std::string MuPowerLaw    = "Power Law Coefficient";
   static const std::string Power         = "Power Exponent";
   static const std::string HomotopyParam = "Homotopy Parameter";
 } // ParamEnum
