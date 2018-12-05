f5f5f9b757905d69f9ea312b13e98a1a9e24bc50
Irina K. Tezaur <ikalash@sandia.gov>
2018-12-05 08:05:23 -0800

    Adding scripts for nightly testing on camobap with FPE turned on.

---
+++ doc/dashboards/camobap.ca.sandia.gov/send_email_fpe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#source $1 
+
+TTT=`grep "(Failed)" /home/ikalash/nightlyCDash/nightly_logFPE.txt -c`
+TTTT=`grep "(Not Run)" /home/ikalash/nightlyCDash/nightly_logFPE.txt -c`
+TTTTT=`grep "(Timeout)" /home/ikalash/nightlyCDash/nightly_logFPE.txt -c`
+TT=`grep "...   Passed" /home/ikalash/nightlyCDash/nightly_logFPE.txt -c`
+
+#/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
+/bin/mail -s "Albany (master, FPE check on): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, gahanse@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /home/ikalash/nightlyCDash/results
