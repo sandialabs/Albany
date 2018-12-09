56f838b9149d221d892f55fa9261835d3a959b09
Irina K. Tezaur <ikalash@sandia.gov>
2018-12-08 19:33:54 -0800

    Fixing syntax error in nightly tests on camobap.

--- doc/nightlyTestHarness/trilinos_build_openmp.sh
+++ doc/nightlyTestHarness/trilinos_build_openmp.sh
@@ -13,7 +13,7 @@ echo "    Starting Trilinos cmake" ; date
 if [ $MPI_BUILD ] ; then
   cp $ALBDIR/doc/nightlyTestHarness/do-cmake-trilinos-mpi-openmp-camobap .
   source ./do-cmake-trilinos-mpi-openmp-camobap > $TRILOUTDIR/trilinos_openmp_cmake.out 2>&1
-else
+#else
 #  cp $ALBDIR/doc/nightlyTestHarness/do-cmake-trilinos-tpetra .
 #  source ./do-cmake-trilinos-tpetra-no-scorec > $TRILOUTDIR/trilinos_openmp_cmake.out 2>&1
 fi
