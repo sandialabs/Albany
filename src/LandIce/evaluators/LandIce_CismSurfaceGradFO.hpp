877bdbf00e0a678969bd6064b1d573c00507c170
Luca Bertagna <lbertag@sandia.gov>
2019-01-31 09:27:13 -0700

    LandIce: fix to CISM surface grad evaluator

--- src/LandIce/evaluators/LandIce_CismSurfaceGradFO.hpp
+++ src/LandIce/evaluators/LandIce_CismSurfaceGradFO.hpp
@@ -4,64 +4,57 @@
 //    in the file "license.txt" in the top-level Albany directory  //
 //*****************************************************************//
 
-#ifndef LANDICE_CISMSURFACEGRADFO_HPP
-#define LANDICE_CISMSURFACEGRADFO_HPP
+#ifndef LANDICE_CISM_SURFACE_GRAD_FO_HPP
+#define LANDICE_CISM_SURFACE_GRAD_FO_HPP
 
 #include "Phalanx_config.hpp"
 #include "Phalanx_Evaluator_WithBaseImpl.hpp"
 #include "Phalanx_Evaluator_Derived.hpp"
 #include "Phalanx_MDField.hpp"
-#include "Sacado_ParameterAccessor.hpp" 
+
 #include "Albany_Layouts.hpp"
+#include "Albany_ScalarOrdinalTypes.hpp"
+#include "PHAL_Dimension.hpp"
 
 namespace LandIce {
-/** \brief Finite Element Interpolation Evaluator
 
-    This evaluator interpolates nodal DOF values to quad points.
+/** \brief Construct surface gradient from CISM components
 
+    Given nodal values for dsdx and dsdy, this evaluator computes
+    the vector quantity grad(ds) at the quadrature points
 */
 
 template<typename EvalT, typename Traits>
 class CismSurfaceGradFO : public PHX::EvaluatorWithBaseImpl<Traits>,
-		    public PHX::EvaluatorDerived<EvalT, Traits>,
-		    public Sacado::ParameterAccessor<EvalT, SPL_Traits> {
-
+		    public PHX::EvaluatorDerived<EvalT, Traits> {
 public:
 
   typedef typename EvalT::ScalarT ScalarT;
 
   CismSurfaceGradFO(const Teuchos::ParameterList& p,
-              const Teuchos::RCP<Albany::Layouts>& dl);
+                    const Teuchos::RCP<Albany::Layouts>& dl);
 
   void postRegistrationSetup(typename Traits::SetupData d,
-                      PHX::FieldManager<Traits>& vm);
+                             PHX::FieldManager<Traits>& vm);
 
   void evaluateFields(typename Traits::EvalData d);
-  
-  ScalarT& getValue(const std::string &n); 
 
 private:
  
   typedef typename EvalT::MeshScalarT MeshScalarT;
-  typedef typename EvalT::ParamScalarT ParamScalarT;
-  
-  ScalarT dummyParam;
 
   // Input:
   //! Values at nodes
-  PHX::MDField<const ParamScalarT,Cell,Node> dsdx_node;
-  PHX::MDField<const ParamScalarT,Cell,Node> dsdy_node;
-  //! Basis Functions
+  PHX::MDField<const MeshScalarT,Cell,Node> dsdx_node;
+  PHX::MDField<const MeshScalarT,Cell,Node> dsdy_node;
   PHX::MDField<const RealType,Cell,Node,QuadPoint> BF;
 
   // Output:
-  PHX::MDField<ParamScalarT,Cell,QuadPoint,Dim> gradS_qp;
+  PHX::MDField<MeshScalarT,Cell,QuadPoint,Dim> gradS_qp;
 
   unsigned int numQPs, numDims, numNodes;
-  
- 
 };
 
-}
+} // namespace LandIce
 
-#endif
+#endif // LANDICE_CISM_SURFACE_GRAD_FO_HPP
