e3bc31dcd408705e3e18adb58398c61b818e7d58
Irina K. Tezaur <ikalash@sandia.gov>
2019-03-07 09:07:29 -0800

    Tweaks to cee nightlies to hopefully make them more stable.

--- doc/dashboards/cee-compute011.sandia.gov/send_email_clangdbg.sh
+++ doc/dashboards/cee-compute011.sandia.gov/send_email_clangdbg.sh
@@ -8,5 +8,5 @@ TTTTT=`grep "(Timeout)" /scratch/albany/nightly_log_clangdbg.txt -c`
 TT=`grep "...   Passed" /scratch/albany/nightly_log_clangdbg.txt -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "Albany64BitClangDbg, cee-compute011: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /scratch/albany/results_clangdbg
+/bin/mail -s "Albany64BitClangDbg, cee-compute012: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /scratch/albany/results_clangdbg
 #/bin/mail -s "Albany64BitClang, cee-compute016: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /scratch/albany/results_clangdbg
