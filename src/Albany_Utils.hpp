404b99ec598e8079d5d17de10eda4e7b1e0cd5b0
Luca Bertagna <lbertag@sandia.gov>
2019-03-14 13:50:34 -0600

    Moved macros outside of Albany_Utils.hpp.
    So one can use them without including a whole lot of other headers.
    
    For backward compatiblity, Albany_Utils.hpp includes Albany_Macros.hpp.
    
    Note: ALBANY_ASSERT_IMPL has been redefined without using assert_fail, which
    is still in Albany_Util.hpp.

--- src/Albany_Utils.hpp
+++ src/Albany_Utils.hpp
@@ -10,8 +10,9 @@
 // Get Albany configuration macros
 #include "Albany_config.h"
 
-// For cudaCheckError
-#include <stdexcept>
+// Get all debug macros
+// Note: this is for backward compatibility. Prefer including Albany_Debug.hpp for debug macros
+#include "Albany_Macros.hpp"
 
 #include <sstream>
 
@@ -19,49 +20,6 @@
 #include "Albany_CommUtils.hpp"
 
 #include "Teuchos_RCP.hpp"
-
-// Checks if the previous Kokkos::Cuda kernel has failed
-#ifdef ALBANY_CUDA_ERROR_CHECK
-#define cudaCheckError() \
-  { cudaError(__FILE__, __LINE__); }
-inline void
-cudaError(const char* file, int line) {
-  cudaError_t err = cudaGetLastError();
-  if (err != cudaSuccess) {
-    fprintf(
-        stderr, "CUDA Error: %s before %s:%d\n", cudaGetErrorString(err), file,
-        line);
-    throw std::runtime_error(cudaGetErrorString(err));
-  }
-}
-#else
-#define cudaCheckError()
-#endif
-
-// NVTX Range creates a colored range which can be viewed on the nvvp timeline
-// (from Parallel Forall blog)
-#ifdef ALBANY_CUDA_NVTX
-#include "nvToolsExt.h"
-static const uint32_t nvtx_colors[] = { 0x0000ff00, 0x000000ff, 0x00ffff00,
-    0x00ff00ff, 0x0000ffff, 0x00ff0000, 0x00ffffff };
-static const int num_nvtx_colors = sizeof(nvtx_colors)/sizeof(uint32_t);
-#define PUSH_RANGE(name,cid) { \
-  int color_id = cid; \
-  color_id = color_id%num_nvtx_colors;\
-  nvtxEventAttributes_t eventAttrib = {0}; \
-  eventAttrib.version = NVTX_VERSION; \
-  eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE; \
-  eventAttrib.colorType = NVTX_COLOR_ARGB; \
-  eventAttrib.color = nvtx_colors[color_id]; \
-  eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII; \
-  eventAttrib.message.ascii = name; \
-  nvtxRangePushEx(&eventAttrib); \
-}
-#define POP_RANGE nvtxRangePop();
-#else
-#define PUSH_RANGE(name,cid)
-#define POP_RANGE
-#endif
 
 namespace Albany {
 
@@ -269,33 +227,5 @@ enum class BuildType {None, Tpetra, Epetra};
 BuildType build_type(const BuildType value=BuildType::None);
 
 }  // end namespace Albany
-
-#ifdef __CUDA_ARCH__
-#define ALBANY_ASSERT_IMPL(cond, ...) assert(cond)
-#else
-#define ALBANY_ASSERT_IMPL(cond, msg, ...)          \
-  do {                                              \
-    if (!(cond)) {                                  \
-      std::ostringstream omsg;                      \
-      omsg << #cond " failed at ";                  \
-      omsg << __FILE__ << " +" << __LINE__ << '\n'; \
-      omsg << msg << '\n';                          \
-      Albany::assert_fail(omsg.str());              \
-    }                                               \
-  } while (0)
-#endif
-
-#define ALBANY_ASSERT(...) ALBANY_ASSERT_IMPL(__VA_ARGS__, "")
-
-#ifdef NDEBUG
-#define ALBANY_EXPECT(...)
-#else
-#define ALBANY_EXPECT(...) ALBANY_ASSERT(__VA_ARGS__)
-#endif
-
-#define ALBANY_ALWAYS_ASSERT(cond) ALBANY_ASSERT(cond)
-#define ALBANY_ALWAYS_ASSERT_VERBOSE(cond, msg) ALBANY_ASSERT(cond, msg)
-#define ALBANY_DEBUG_ASSERT(cond) ALBANY_EXPECT(cond)
-#define ALBANY_DEBUG_ASSERT_VERBOSE(cond, msg) ALBANY_EXPECT(cond, msg)
 
 #endif  // ALBANY_UTILS
