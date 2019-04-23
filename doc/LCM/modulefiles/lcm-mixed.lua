119cf1916311135235835542233dfabcce7a7210
Alejandro Mota <amota@sandia.gov>
2019-04-23 14:51:09 -0700

    LCM: Enable trap FPE by default all build types.

--- doc/LCM/modulefiles/lcm-mixed.lua
+++ doc/LCM/modulefiles/lcm-mixed.lua
@@ -2,7 +2,7 @@ whatis("LCM mixed build type")
 
 setenv("BUILD_TYPE", "mixed")
 setenv("BUILD_STRING", "RELEASE")
-setenv("LCM_FPE_SWITCH", "OFF")
+setenv("LCM_FPE_SWITCH", "ON")
 setenv("LCM_DENORMAL_SWITCH", "ON")
 setenv("LCM_CXX_FLAGS", "-g -O0")
 
