b3688a3e03caf6c9824f9d7f7cac79f12765077e
Irina K. Tezaur <ikalash@sandia.gov>
2019-05-07 08:33:25 -0700

    Updating cori modules to hopefully fix cori build.

--- doc/dashboards/cori.nersc.gov/cori_modules.sh
+++ doc/dashboards/cori.nersc.gov/cori_modules.sh
@@ -7,7 +7,7 @@ module unload cmake netcdf-hdf5parallel/4.2.0 python
 module unload intel PrgEnv-intel 
 module load PrgEnv-gnu
 module load cmake/3.11.4 python cray-netcdf-hdf5parallel 
-module load boost
+module load boost/1.70.0
 module load git 
 #module unload darshan
 module list
