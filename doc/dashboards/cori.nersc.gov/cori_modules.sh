526b148fa71c7b667e5f36bc97555ae4e0a70b41
Irina K. Tezaur <ikalash@sandia.gov>
2019-05-08 09:51:40 -0700

    More tweaks to Cori modules.

--- doc/dashboards/cori.nersc.gov/cori_modules.sh
+++ doc/dashboards/cori.nersc.gov/cori_modules.sh
@@ -2,7 +2,8 @@
 
 #!/bin/bash
 
-module load boost/1.70.0
+module unload gcc/7.3.0
+module load boost/1.67.0
 module load git 
 module unload cmake netcdf-hdf5parallel/4.2.0 python
 #module swap PrgEnv-intel PrgEnv-gnu; 
