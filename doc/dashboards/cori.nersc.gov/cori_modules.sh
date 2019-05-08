bae3d192871fc7cc24e5e10ff76ced26e9d8493d
Irina K. Tezaur <ikalash@sandia.gov>
2019-05-08 09:36:59 -0700

    Updating Cori modules.

--- doc/dashboards/cori.nersc.gov/cori_modules.sh
+++ doc/dashboards/cori.nersc.gov/cori_modules.sh
@@ -2,12 +2,12 @@
 
 #!/bin/bash
 
+module load boost/1.70.0
+module load git 
 module unload cmake netcdf-hdf5parallel/4.2.0 python
 #module swap PrgEnv-intel PrgEnv-gnu; 
 module unload intel PrgEnv-intel 
 module load PrgEnv-gnu
 module load cmake/3.11.4 python cray-netcdf-hdf5parallel 
-module load boost/1.70.0
-module load git 
 #module unload darshan
 module list
