7599751112c49f23587beb2cbc2b6271d55d1bce
Irina K. Tezaur <ikalash@sandia.gov>
2019-05-09 07:59:50 -0700

    Revert "Changing Tpetra configure options to hopefully fix compilation error"
    This reverts commit e79e35d7ad0fa757694f53cbd1acaeeed9889fd0.

--- doc/dashboards/cori.nersc.gov/cori_modules.sh
+++ doc/dashboards/cori.nersc.gov/cori_modules.sh
@@ -9,7 +9,6 @@ module unload cmake netcdf-hdf5parallel/4.2.0 python
 #module swap PrgEnv-intel PrgEnv-gnu; 
 module unload intel PrgEnv-intel 
 module load PrgEnv-gnu
-module load cmake/3.11.4 python cray-netcdf-hdf5parallel
-module load gcc/7.3.0 
+module load cmake/3.11.4 python cray-netcdf-hdf5parallel 
 #module unload darshan
 module list
