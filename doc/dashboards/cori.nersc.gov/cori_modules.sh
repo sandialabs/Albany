624089831adf8208e3ea8ba60bcb31ebc72da56d
Irina K. Tezaur <ikalash@sandia.gov>
2019-05-14 21:38:18 -0700

    Fixing Cori modules, finally.  Nightlies on Cori should be clean now.

--- doc/dashboards/cori.nersc.gov/cori_modules.sh
+++ doc/dashboards/cori.nersc.gov/cori_modules.sh
@@ -2,13 +2,15 @@
 
 #!/bin/bash
 
-module unload gcc/7.3.0
-module load boost/1.67.0
-module load git 
-module unload cmake netcdf-hdf5parallel/4.2.0 python
-#module swap PrgEnv-intel PrgEnv-gnu; 
+
+module unload cmake cray-netcdf-hdf5parallel python 
 module unload intel PrgEnv-intel 
 module load PrgEnv-gnu
-module load cmake/3.11.4 python cray-netcdf-hdf5parallel 
-#module unload darshan
+module unload cray-mpich
+module load cray-mpich/7.7.6 
+module unload gcc/7.3.0 
+module load gcc/8.2.0 
+module load boost/1.67.0 
+module load git 
+module load cmake/3.11.4 python cray-netcdf-hdf5parallel
 module list
