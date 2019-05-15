5011f59bd672216118d7716868e0e28611d154ef
Irina K. Tezaur <ikalash@sandia.gov>
2019-05-15 09:31:45 -0700

    Removing old nightly testing scripts for mayer.
    Attempting to fix the nightlies there so an email gets sent every morning
    with the results.  We'll see tomorrow if it worked...

--- doc/dashboards/mayer.sandia.gov/send_email.sh
+++ doc/dashboards/mayer.sandia.gov/send_email.sh
@@ -8,12 +8,12 @@ TTTTT=`grep "\*Timeout" nightly_log_mayerAlbany.txt -c`
 TT=`grep "...   Passed" nightly_log_mayerAlbany.txt -c`
 
 
-echo "Subject: Albany, mayer (KokkosNode=Serial): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
-echo "" >& b
-cat a b >& c
-cat c results_arm >& d
-mv d results_arm
-rm a b c
-cat results_arm | /usr/lib/sendmail -F ikalash@mayer.sandia.gov -t "ikalash@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, amota@sandia.gov, jwatkin@sandia.gov"
+echo "Subject: Albany, mayer (KokkosNode=Serial): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& /mscratch/albany/a
+echo "" >& /mscratch/albany/b
+cat /mscratch/albany/a /mscratch/albany/b >& /mscratch/albany/c
+cat /mscratch/albany/c /mscratch/albany/results_arm >& /mscratch/albany/d
+mv /mscratch/albany/d /mscratch/albany/results_arm
+rm /mscratch/albany/a /mscratch/albany/b /mscratch/albany/c
+cat /mscratch/albany/results_arm | /usr/lib/sendmail -F ikalash@mayer.sandia.gov -t "ikalash@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, amota@sandia.gov, jwatkin@sandia.gov"
 #cat results_arm | /usr/lib/sendmail -F ikalash@mayer.sandia.gov -t "ikalash@sandia.gov"
 
