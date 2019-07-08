680f219913a8be184f7321a6161218ca04be9883
Jerry Watkins <jwatkin@sandia.gov>
2019-07-08 13:54:29 -0700

    Add field dependency funcs for memoization of LandIce mismip

--- src/LandIce/evaluators/LandIce_SharedParameter.hpp
+++ src/LandIce/evaluators/LandIce_SharedParameter.hpp
@@ -37,7 +37,7 @@ public:
   void postRegistrationSetup(typename Traits::SetupData d, PHX::FieldManager<Traits>& fm)
   {
     this->utils.setFieldData(param_as_field,fm);
-    d.fill_field_dependencies(this->dependentFields(),this->evaluatedFields());
+    d.fill_field_dependencies(this->dependentFields(),this->evaluatedFields(),false);
   }
 
   static void setNominalValue (const Teuchos::ParameterList& p, double default_value);
