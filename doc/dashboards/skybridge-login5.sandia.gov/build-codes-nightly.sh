d32e65988a3557d903c4287ebb0a2a2cd357ec6a
Irina K. Tezaur <ikalash@sandia.gov>
2019-07-10 14:37:02 -0600

    Correcting skybridge scripts.

--- doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
+++ doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
@@ -6,10 +6,8 @@ module load serial-intel-release
 rm -rf trilinos-build-serial-intel-release
 rm -rf trilinos-install-serial-intel-release
 rm -rf albany-build-serial-intel-release
-rm -rf Albany
-rm -rf Trilinos
 rm -rf *log 
 rm -rf slurm*
-./clean-config-build-dash.sh trilinos 8
-./clean-config-build-dash.sh albany 8
+./clean-update-config-build-dash.sh trilinos 8
+./clean-update-config-build-dash.sh albany 8
 mv albany-serial-intel-release.log albany-serial-intel-release-build.log 
