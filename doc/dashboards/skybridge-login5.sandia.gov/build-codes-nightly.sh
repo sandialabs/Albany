75ac471093794ed52b84c0a831ef9f7fe099973e
Irina K. Tezaur <ikalash@sandia.gov>
2019-08-16 11:30:12 -0700

    This should fix issue with skybridge build not being up-to-date (#514).

--- doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
+++ doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 ## Do not put any commands or blank lines before the #SBATCH lines
 export LCM_DIR=`pwd`
+rm -rf *log 
+rm -rf Albany
+rm -rf Trilinos
+git clone git@github.com:trilinos/Trilinos.git >& trilinos-clone.log
+git clone git@github.com:SNLComputation/Albany.git >& albany-clone.log 
 module use --append $LCM_DIR/Albany/doc/LCM/modulefiles
 module load serial-intel-release
 rm -rf trilinos-build-serial-intel-release
 rm -rf trilinos-install-serial-intel-release
 rm -rf albany-build-serial-intel-release
-rm -rf *log 
 rm -rf slurm*
 ./clean-update-config-build-dash.sh trilinos 8
 ./clean-update-config-build-dash.sh albany 8
