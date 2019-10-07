sandialabs/Albany

commit 77fb33cf49c88d65d222e58d2f89c6411a5abdf2

Fixes slowdown caused by excessive use of getLocalElement/getGlobalElement (#524)
Fixes #523

Also removed a few pedantic warnings, such as singed/unsinged comparisons,
lack of override keywords, unused vars, deprecated methods.

src/disc/stk/Albany_STKDiscretization.cpp
@@ -14,6 +14,7 @@
 #include "Albany_STKDiscretization.hpp"
 #include "Albany_STKNodeFieldContainer.hpp"
 #include "Albany_Utils.hpp"
+#include "Albany_GlobalLocalIndexer.hpp"
 
 #ifdef ALBANY_CONTACT
 #include "Albany_ContactManager.hpp"
@@ -542,9 +543,10 @@ STKDiscretization::getCoordinates() const
       stkMeshStruct->getCoordinatesField();
 
   const int meshDim = stkMeshStruct->numDim;
+  auto ov_node_indexer = createGlobalLocalIndexer(m_overlap_node_vs);
   for (int i = 0; i < numOverlapNodes; i++) {
     GO  node_gid = gid(overlapnodes[i]);
-    int node_lid = getLocalElement(m_overlap_node_vs, node_gid);
+    int node_lid = ov_node_indexer->getLocalElement(node_gid);
 
     double* x = stk::mesh::field_data(*coordinates_field, overlapnodes[i]);
     for (int dim = 0; dim < meshDim; ++dim) {
@@ -923,9 +925,10 @@ STKDiscretization::setupMLCoords()
   coordMV           = Thyra::createMembers(m_node_vs, numDim);
   auto coordMV_data = getNonconstLocalData(coordMV);
 
+  auto node_indexer = createGlobalLocalIndexer(m_node_vs);
   for (int i = 0; i < numOwnedNodes; i++) {
     GO      node_gid = gid(ownednodes[i]);
-    int     node_lid = getLocalElement(m_node_vs, node_gid);
+    int     node_lid = node_indexer->getLocalElement(node_gid);
     double* X        = stk::mesh::field_data(*coordinates_field, ownednodes[i]);
     for (int j = 0; j < numDim; j++) { coordMV_data[j][node_lid] = X[j]; }
   }
@@ -1955,6 +1958,7 @@ STKDiscretization::computeWorksetInfo()
     it->second.wsElNodeID_rawVec.resize(numBuckets);
   }
 
+  auto ov_node_indexer = createGlobalLocalIndexer(m_overlap_node_vs);
   for (int b = 0; b < numBuckets; b++) {
     stk::mesh::Bucket& buck = *buckets[b];
     wsElNodeID[b].resize(buck.size());
@@ -2096,6 +2100,7 @@ STKDiscretization::computeWorksetInfo()
 
       for (auto it = mapOfDOFsStructs.begin(); it != mapOfDOFsStructs.end();
            ++it) {
+        auto ov_indexer = createGlobalLocalIndexer(it->second.overlap_vs);
         IDArray&  wsElNodeEqID_array = it->second.wsElNodeEqID[b];
         GIDArray& wsElNodeID_array   = it->second.wsElNodeID[b];
         int       nComp              = it->first.second;
@@ -2105,8 +2110,7 @@ STKDiscretization::computeWorksetInfo()
           for (int k = 0; k < nComp; k++) {
             const GO node_gid = it->second.overlap_dofManager.getGlobalDOF(
                 bulkData.identifier(node) - 1, k);
-            const int node_lid =
-                getLocalElement(it->second.overlap_vs, node_gid);
+            const int node_lid = ov_indexer->getLocalElement(node_gid);
             wsElNodeEqID_array((int)i, j, k) = node_lid;
           }
         }
@@ -2137,7 +2141,7 @@ STKDiscretization::computeWorksetInfo()
       for (int j = 0; j < nodes_per_element; j++) {
         const stk::mesh::Entity rowNode  = node_rels[j];
         const GO                node_gid = gid(rowNode);
-        const LO node_lid = getLocalElement(m_overlap_node_vs, node_gid);
+        const LO node_lid = ov_node_indexer->getLocalElement(node_gid);
 
         TEUCHOS_TEST_FOR_EXCEPTION(
             node_lid < 0,
@@ -2556,6 +2560,7 @@ STKDiscretization::computeNodeSets()
   AbstractSTKFieldContainer::VectorFieldType* coordinates_field =
       stkMeshStruct->getCoordinatesField();
 
+  auto node_indexer = createGlobalLocalIndexer(m_node_vs);
   while (ns != stkMeshStruct->nsPartVec.end()) {  // Iterate over Node Sets
     // Get all owned nodes in this node set
     stk::mesh::Selector select_owned_in_nspart =
@@ -2576,7 +2581,7 @@ STKDiscretization::computeNodeSets()
          << "  on Proc 0." << std::endl;
     for (std::size_t i = 0; i < nodes.size(); i++) {
       GO  node_gid              = gid(nodes[i]);
-      int node_lid              = getLocalElement(m_node_vs, node_gid);
+      int node_lid              = node_indexer->getLocalElement(node_gid);
       nodeSetGIDs[ns->first][i] = node_gid;
       nodeSets[ns->first][i].resize(neq);
       for (std::size_t eq = 0; eq < neq; ++eq) {
@@ -2910,6 +2915,7 @@ STKDiscretization::meshToGraph()
   const stk::mesh::BucketVector& buckets =
       bulkData.get_buckets(stk::topology::ELEMENT_RANK, select_owned_in_part);
 
+  auto ov_node_indexer = createGlobalLocalIndexer(m_overlap_node_vs);
   for (size_t b = 0; b < buckets.size(); ++b) {
     const stk::mesh::Bucket& buck_cells = *buckets[b];
     // Find the surrounding elements for each node owned by this processor.
@@ -2922,7 +2928,7 @@ STKDiscretization::meshToGraph()
       for (std::size_t ncnt = 0; ncnt < num_node_rels; ++ncnt) {
         const stk::mesh::Entity rowNode = node_rels[ncnt];
         GO                      nodeGID = gid(rowNode);
-        int nodeLID = getLocalElement(m_overlap_node_vs, nodeGID);
+        int nodeLID = ov_node_indexer->getLocalElement(nodeGID);
         // In the case of degenerate elements, where a node can be entered into
         // the connect table twice, need to check to make sure that this element
         // is not already listed as surrounding this node.
@@ -2958,7 +2964,7 @@ STKDiscretization::meshToGraph()
   // loop over all the nodes owned by this PE
   for (LO ncnt = 0; ncnt < numOverlapNodes; ++ncnt) {
     Teuchos::Array<GO> adjacency;
-    GO                 globalrow = getGlobalElement(m_overlap_node_vs, ncnt);
+    GO                 globalrow = ov_node_indexer->getGlobalElement(ncnt);
     // loop over the elements surrounding node ncnt
     for (std::size_t ecnt = 0; ecnt < sur_elem[ncnt].size(); ecnt++) {
       const stk::mesh::Entity  elem          = sur_elem[ncnt][ecnt];
@@ -2990,11 +2996,12 @@ STKDiscretization::printVertexConnectivity()
 {
   if (Teuchos::is_null(nodalMatrixFactory)) { return; }
 
-  auto               dummy_op = nodalMatrixFactory->createOp();
+  auto ov_node_indexer = createGlobalLocalIndexer(m_overlap_node_vs);
+  auto dummy_op = nodalMatrixFactory->createOp();
   Teuchos::Array<LO> indices;
   Teuchos::Array<ST> vals;
   for (int i = 0; i < numOverlapNodes; ++i) {
-    GO globalvert = getGlobalElement(m_overlap_node_vs, i);
+    GO globalvert = ov_node_indexer->getGlobalElement(i);
 
     std::cout << "Center vert is : " << globalvert + 1 << std::endl;
 
@@ -3002,7 +3009,7 @@ STKDiscretization::printVertexConnectivity()
 
     for (int j = 0; j < indices.size(); j++) {
       std::cout << "                  "
-                << getGlobalElement(m_overlap_node_vs, indices[j]) + 1
+                << ov_node_indexer->getGlobalElement(indices[j]) + 1
                 << std::endl;
     }
   }

src/ATO/ATO_Solver.cpp
@@ -14,6 +14,7 @@
 #include "Albany_DistributedParameterLibrary.hpp"
 #include "Albany_SolverFactory.hpp"
 #include "Albany_Utils.hpp"
+#include "Albany_GlobalLocalIndexer.hpp"
 #ifdef ATO_USES_ISOLIB
 #include "Albany_STKDiscretization.hpp"
 #include "STKExtract.hpp"
@@ -353,6 +354,7 @@ void Solver::evalModelImpl (const Thyra_InArgs&  /* inArgs */,
     }
   }
 
+  auto indexer = Albany::createGlobalLocalIndexer(m_localNodeVS);
   for (int i=0; i<m_numPhysics; ++i) {
     Albany::StateManager& stateMgr = m_subProblems[i].app->getStateMgr();
     const auto& wsEBNames  = stateMgr.getDiscretization()->getWsEBNames();
@@ -391,8 +393,8 @@ void Solver::evalModelImpl (const Thyra_InArgs&  /* inArgs */,
             for (int cell=0; cell<numCells; ++cell) {
               for (int node=0; node<numNodes; ++node) {
                 const int gid = wsElNodeID[ws][cell][node];
-                if (Albany::locallyOwnedComponent(m_localNodeVS,gid)) {
-                  const int lid = Albany::getLocalElement(m_localNodeVS,gid);
+                if (indexer->isLocallyOwnedElement(gid)) {
+                  const int lid = indexer->getLocalElement(gid);
                   ltopo[lid] = matVal;
                 }
               }
@@ -599,6 +601,8 @@ void Solver::copyTopologyIntoParameter (const double* p, SolverSubSolver& subSol
 
   const Albany::WorksetArray<std::string>::type& wsEBNames = stateMgr.getDiscretization()->getWsEBNames();
 
+  auto indexer = Albany::createGlobalLocalIndexer(m_localNodeVS);
+
   int ntopos = m_topologyInfoStructs.size();
   for (int itopo=0; itopo<ntopos; ++itopo) {
     Teuchos::RCP<TopologyInfoStruct> topoStruct = m_topologyInfoStructs[itopo];
@@ -629,8 +633,8 @@ void Solver::copyTopologyIntoParameter (const double* p, SolverSubSolver& subSol
         for (int cell=0; cell<numCells; ++cell) {
           for (int node=0; node<numNodes; ++node) {
             const int gid = wsElNodeID[ws][cell][node];
-            if (Albany::locallyOwnedComponent(m_localNodeVS,gid)) {
-              const int lid = Albany::getLocalElement(m_localNodeVS,gid);
+            if (indexer->isLocallyOwnedElement(gid)) {
+              const int lid = indexer->getLocalElement(gid);
               ltopo[lid] = matVal;
             }
           }
@@ -665,6 +669,7 @@ void Solver::copyTopologyFromStateMgr(double* p, Albany::StateManager& stateMgr)
   const auto& wsElNodeID = stateMgr.getDiscretization()->getWsElNodeID();
 
   // copy the topology from the state manager
+  auto indexer = Albany::createGlobalLocalIndexer(m_localNodeVS);
   const int ntopos = m_topologyInfoStructs.size();
   for (int itopo=0; itopo<ntopos; ++itopo) {
     Teuchos::RCP<TopologyInfoStruct> topologyInfoStruct = m_topologyInfoStructs[itopo];
@@ -678,8 +683,8 @@ void Solver::copyTopologyFromStateMgr(double* p, Albany::StateManager& stateMgr)
       for (int cell=0; cell<numCells; ++cell) {
         for (int node=0; node<numNodes; ++node) {
           const int gid = wsElNodeID[ws][cell][node];
-          if (Albany::locallyOwnedComponent(m_localNodeVS,gid)) {
-            const int lid = Albany::getLocalElement(m_localNodeVS,gid);
+          if (indexer->isLocallyOwnedElement(gid)) {
+            const int lid = indexer->getLocalElement(gid);
             p[lid+offset] = wsTopo(cell,node);
           }
         }
@@ -760,6 +765,7 @@ void Solver::copyTopologyIntoStateMgr( const double* p, Albany::StateManager& st
   const auto& wsEBNames  = disc->getWsEBNames();
   const auto& wsElNodeID = stateMgr.getDiscretization()->getWsElNodeID();
 
+  auto indexer = Albany::createGlobalLocalIndexer(m_localNodeVS);
   const int ntopos = m_topologyInfoStructs.size();
   for (int itopo=0; itopo<ntopos; ++itopo) {
     Teuchos::RCP<TopologyInfoStruct> topoStruct = m_topologyInfoStructs[itopo];
@@ -790,7 +796,7 @@ void Solver::copyTopologyIntoStateMgr( const double* p, Albany::StateManager& st
       for (int cell=0; cell<numCells; ++cell) {
         for (int node=0; node<numNodes; ++node) {
           int gid = wsElNodeID[ws][cell][node];
-          int lid = Albany::getLocalElement(m_overlapNodeVS,gid);
+          int lid = indexer->getLocalElement(gid);
           wsTopo(cell,node) = otopo[lid];
         }
       }
@@ -809,7 +815,7 @@ void Solver::copyTopologyIntoStateMgr( const double* p, Albany::StateManager& st
         for (int cell=0; cell<numCells; ++cell) {
           for (int node=0; node<numNodes; ++node) {
             int gid = wsElNodeID[ws][cell][node];
-            int lid = Albany::getLocalElement(m_overlapNodeVS,gid);
+            int lid = indexer->getLocalElement(gid);
             fMask[lid] = 1.0;
           }
         }
@@ -832,7 +838,7 @@ void Solver::copyTopologyIntoStateMgr( const double* p, Albany::StateManager& st
       for (int cell=0; cell<numCells; ++cell) {
         for (int node=0; node<numNodes; ++node) {
           const int gid = wsElNodeID[ws][cell][node];
-          const int lid = Albany::getLocalElement(m_overlapNodeVS,gid);
+          const int lid = indexer->getLocalElement(gid);
           if (fMask[lid] != 1.0) {
             otopo[lid] = matVal;
           }
@@ -952,6 +958,7 @@ void Solver::ComputeMeasure(const std::string& measureType, const double* p,
   const int ntopos = m_topologyInfoStructs.size();
 
   std::vector<Teuchos::RCP<TopologyStruct> > topologyStructs(ntopos);
+  auto indexer = Albany::createGlobalLocalIndexer(m_localNodeVS);
 
   for (int itopo=0; itopo<ntopos; ++itopo) {
 
@@ -967,8 +974,8 @@ void Solver::ComputeMeasure(const std::string& measureType, const double* p,
       for (int cell=0; cell<numCells; ++cell) {
         for (int node=0; node<numNodes; ++node) {
           const int gid = wsElNodeID[ws][cell][node];
-          if (Albany::locallyOwnedComponent(m_localNodeVS,gid)) {
-            const int lid = Albany::getLocalElement(m_localNodeVS,gid);
+          if (indexer->isLocallyOwnedElement(gid)) {
+            const int lid = indexer->getLocalElement(gid);
             ltopo[lid] = p[lid+offset];
           }
         }

src/ATO/problems/ATO_OptimizationProblem.cpp
@@ -13,6 +13,7 @@
 #include "Albany_ProblemUtils.hpp"
 #include "Albany_AbstractDiscretization.hpp"
 #include "Adapt_NodalDataVector.hpp"
+#include "Albany_GlobalLocalIndexer.hpp"
 
 #include <Kokkos_DynRankView_Fad.hpp>
 #include "Intrepid2_FunctionSpaceTools.hpp"
@@ -143,7 +144,7 @@ ComputeVolume(double* p, const double* dfdp,
 
   const Albany::WorksetArray<int>::type& wsPhysIndex = disc->getWsPhysIndex();
 
-
+  auto indexer = Albany::createGlobalLocalIndexer(overlapNodeVs);
 
   for(int ws=0; ws<numWorksets; ws++){
 
@@ -156,7 +157,7 @@ ComputeVolume(double* p, const double* dfdp,
       double elVol = 0.0;
       for(int node=0; node<numNodes; node++){
         GO gid = wsElNodeID[ws][cell][node];
-        LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+        LO lid = indexer->getLocalElement(gid);
         if(dfdp[lid] < threshhold) {
           p[lid] = 1.0;
         } else {
@@ -166,7 +167,7 @@ ComputeVolume(double* p, const double* dfdp,
 
       for(int node=0; node<numNodes; node++){
         GO gid = wsElNodeID[ws][cell][node];
-        LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+        LO lid = indexer->getLocalElement(gid);
         for(int qp=0; qp<numQPs; qp++) {
           elVol += p[lid]*basisAtQPs[physIndex](node,qp)*weighted_measure[ws](cell,qp);
         }
@@ -747,6 +748,7 @@ computeMeasure (const std::string& measureType,
   }
 
   Teuchos::RCP<BlockMeasureMap> measureModel = measureModels.at(measureType);
+  auto indexer = Albany::createGlobalLocalIndexer(overlapNodeVs);
 
   for(int ws=0; ws<numWorksets; ws++){
 
@@ -773,7 +775,7 @@ computeMeasure (const std::string& measureType,
         for(int itopo=0; itopo<nTopologies; itopo++) pVals[itopo]=0.0;
         for(int node=0; node<numNodes; node++){
           GO gid = wsElNodeID[ws][cell][node];
-          LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+          LO lid = indexer->getLocalElement(gid);
           for(int itopo=0; itopo<nTopologies; itopo++) {
             pVals[itopo] += topoValues[itopo][lid]*basisAtQPs[physIndex](node,qp);
           }
@@ -787,7 +789,7 @@ computeMeasure (const std::string& measureType,
           blockMeasureModel->Gradient(pVals, topologies, drdz);
           for(int node=0; node<numNodes; node++){
             GO gid = wsElNodeID[ws][cell][node];
-            LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+            LO lid = indexer->getLocalElement(gid);
             for(int itopo=0; itopo<nTopologies; itopo++) {
               odmdp[itopo][lid] += drdz[itopo]
                                   *basisAtQPs[physIndex](node,qp)
@@ -853,6 +855,7 @@ computeConformalMeasure (const std::string& measureType,
   }
 
   Teuchos::RCP<BlockMeasureMap> measureModel = measureModels.at(measureType);
+  auto indexer = Albany::createGlobalLocalIndexer(overlapNodeVs);
 
   for(int ws=0; ws<numWorksets; ws++){
 
@@ -887,7 +890,7 @@ computeConformalMeasure (const std::string& measureType,
         for(int dim=0; dim<numDims; dim++)
           coordCon(node,dim) = coords[ws][cell][node][dim];
         GO gid = wsElNodeID[ws][cell][node];
-        LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+        LO lid = indexer->getLocalElement(gid);
         topoVals(node) = p[lid];
       }
 
@@ -915,7 +918,7 @@ computeConformalMeasure (const std::string& measureType,
         for(int itopo=0; itopo<nTopologies; itopo++) pVals[itopo]=0.0;
         for(int node=0; node<numNodes; node++){
           GO gid = wsElNodeID[ws][cell][node];
-          LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+          LO lid = indexer->getLocalElement(gid);
           for(int itopo=0; itopo<nTopologies; itopo++) {
             pVals[itopo] += topoValues[itopo][lid]*basisAtQPs[physIndex](node,qp);
           }
@@ -932,7 +935,7 @@ computeConformalMeasure (const std::string& measureType,
           blockMeasureModel->Gradient(pVals, topologies, drdz);
           for(int node=0; node<numNodes; node++){
             GO gid = wsElNodeID[ws][cell][node];
-            LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+            LO lid = indexer->getLocalElement(gid);
             for(int itopo=0; itopo<nTopologies; itopo++){
               if(itopo == materialTopologyIndex){
                 odmdp[itopo][lid] += drdz[itopo]*dMdtopo[node]*weightFraction
@@ -994,6 +997,7 @@ computeConformalVolume (const std::vector<Teuchos::RCP<TopologyStruct>>& topolog
     odvdp = Albany::getNonconstLocalData(overlapVec);
   }
 
+  auto indexer = Albany::createGlobalLocalIndexer(overlapNodeVs);
   for(int ws=0; ws<numWorksets; ws++){
   
     int physIndex = wsPhysIndex[ws];
@@ -1012,7 +1016,7 @@ computeConformalVolume (const std::vector<Teuchos::RCP<TopologyStruct>>& topolog
         for(int dim=0; dim<numDims; dim++)
           coordCon(node,dim) = coords[ws][cell][node][dim];
         GO gid = wsElNodeID[ws][cell][node];
-        LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+        LO lid = indexer->getLocalElement(gid);
         topoVals(node) = p[lid];
       }
 
@@ -1031,7 +1035,7 @@ computeConformalVolume (const std::vector<Teuchos::RCP<TopologyStruct>>& topolog
 
         for(int node=0; node<numNodes; node++){
           GO gid = wsElNodeID[ws][cell][node];
-          LO lid = Albany::getLocalElement(overlapNodeVs,gid);
+          LO lid = indexer->getLocalElement(gid);
           odvdp[lid] += dMdtopo(node);
         }
       }

src/ATO/utils/ATO_Aggregator.cpp
@@ -9,8 +9,9 @@
 
 #include "Albany_Application.hpp"
 #include "Albany_ThyraUtils.hpp"
+#include "Albany_GlobalLocalIndexer.hpp"
 
-#include "Teuchos_TestForException.hpp"
+#include <Teuchos_TestForException.hpp>
 
 #include <functional>
 
@@ -495,6 +496,7 @@ void Aggregator_Scaled::Evaluate()
 
     Teuchos::RCP<Thyra_Vector> deriv = derivAggregated[itopo];
     deriv->assign(0.0);
+    auto deriv_range_indexer = Albany::createGlobalLocalIndexer(deriv->range());
 
     auto deriv_data = Albany::getNonconstLocalData(deriv);
     for (int sv=0; sv<numDerivatives; ++sv) {
@@ -506,7 +508,7 @@ void Aggregator_Scaled::Evaluate()
         const int numNodes = derSrc.dimension(1);
         for (int cell=0; cell<numCells; ++cell) {
           for (int node=0; node<numNodes; ++node) {
-            const LO lid = Albany::getLocalElement(deriv->range(),wsElNodeID[ws][cell][node]);
+            const LO lid = deriv_range_indexer->getLocalElement(wsElNodeID[ws][cell][node]);
             deriv_data[lid] += scaleValueAggregated*normalize[sv]*weights[sv]*derSrc(cell,node);
           }
         }
@@ -567,6 +569,7 @@ void Aggregator_Extremum<CompareType>::Evaluate()
 
       Teuchos::RCP<Thyra_Vector> deriv = derivAggregated[itopo];
       deriv->assign(0.0);
+      auto deriv_range_indexer = Albany::createGlobalLocalIndexer(deriv->range());
       auto deriv_data = Albany::getNonconstLocalData(deriv);
 
       for (int ws=0; ws<numWorksets; ++ws) {
@@ -575,7 +578,7 @@ void Aggregator_Extremum<CompareType>::Evaluate()
         int numNodes = derSrc.dimension(1);
         for (int cell=0; cell<numCells; ++cell) {
           for (int node=0; node<numNodes; ++node) {
-            const LO lid = Albany::getLocalElement(deriv->range(),wsElNodeID[ws][cell][node]);
+            const LO lid = deriv_range_indexer->getLocalElement(wsElNodeID[ws][cell][node]);
             deriv_data[lid] += derSrc(cell,node);
           }
         }

src/Albany_StateInfoStruct.hpp
@@ -133,7 +133,7 @@ struct StateStruct
   {
     std::cout << "StateInfoStruct diagnostics for : " << name << std::endl;
     std::cout << "Dimensions : " << std::endl;
-    for (auto i = 0; i < dim.size(); ++i) {
+    for (unsigned i = 0; i < dim.size(); ++i) {
       std::cout << "    " << i << " " << dim[i] << std::endl;
     }
     std::cout << "Entity : " << entity << std::endl;

src/CMakeLists.txt
@@ -107,6 +107,7 @@ SET(SOURCES ${SOURCES}
   utility/Albany_CombineAndScatterManagerTpetra.cpp
   utility/Albany_CommUtils.cpp
   utility/Albany_Gather.cpp
+  utility/Albany_GlobalLocalIndexer.cpp
   utility/Albany_ThyraCrsMatrixFactory.cpp
   utility/Albany_ThyraUtils.cpp
   utility/Albany_TpetraThyraUtils.cpp
@@ -126,6 +127,8 @@ SET(HEADERS ${HEADERS}
   utility/Albany_CombineAndScatterManagerTpetra.hpp
   utility/Albany_CommUtils.hpp
   utility/Albany_Gather.hpp
+  utility/Albany_GlobalLocalIndexer.hpp
+  utility/Albany_GlobalLocalIndexerTpetra.hpp
   utility/Albany_ThyraCrsMatrixFactory.hpp
   utility/Albany_ThyraUtils.hpp
   utility/Albany_TpetraThyraUtils.hpp
@@ -144,7 +147,8 @@ IF (ALBANY_EPETRA)
   )
   SET(HEADERS ${HEADERS}
     utility/Albany_EpetraThyraUtils.hpp
-  utility/Albany_CombineAndScatterManagerEpetra.hpp
+    utility/Albany_CombineAndScatterManagerEpetra.hpp
+    utility/Albany_GlobalLocalIndexerEpetra.hpp
   )
 ENDIF()
 

commit df4a1582099ba1206a22c5befe65d094ed5e5384

Fixing some errors introduced by PR #524

src/Main_Solve_MPMD.cpp
@@ -14,6 +14,7 @@
 #include "Albany_SolverFactory.hpp"
 #include "Albany_Utils.hpp"
 #include "Albany_ThyraUtils.hpp"
+#include "Albany_GlobalLocalIndexer.hpp"
 #include "Albany_CommUtils.hpp"
 
 #include "Piro_PerformSolve.hpp"
@@ -433,6 +434,7 @@ void MPMD_App::copyFieldIntoState(const std::string& name, const Plato::SharedDa
   // copy the field into the state manager
   Teuchos::RCP<const Thyra_VectorSpace> overlapNodeVS = m_overlapVector->space();
   Teuchos::ArrayRCP<double> otopo = Albany::getNonconstLocalData(m_overlapVector);
+  auto ov_node_vs_indexer = Albany::createGlobalLocalIndexer(overlapNodeVS);
   for(int ws=0; ws<numWorksets; ws++){
     Albany::MDArray& wsTopo = dest[ws][name];
     const int numCells = wsTopo.dimension(0);
@@ -440,7 +442,7 @@ void MPMD_App::copyFieldIntoState(const std::string& name, const Plato::SharedDa
     for(int cell=0; cell<numCells; cell++)
       for(int node=0; node<numNodes; node++){
         int gid = wsElNodeID[ws][cell][node];
-        int lid = Albany::getLocalElement(overlapNodeVS,gid);
+        int lid = ov_node_vs_indexer->getLocalElement(gid);
         wsTopo(cell,node) = otopo[lid];
       }
   }
@@ -483,13 +485,14 @@ void MPMD_App::copyFieldFromState(const std::string& name, Plato::SharedData& sf
 
   // copy the field from the state manager
   auto data = Albany::getNonconstLocalData(m_overlapVector);
+  auto ov_vs_indexer = Albany::createGlobalLocalIndexer(m_overlapVector->space());
   for(int ws=0; ws<numWorksets; ws++){
     Albany::MDArray& wsSrc = src[ws][name];
     const int numCells = wsSrc.dimension(0);
     const int numNodes = wsSrc.dimension(1);
     for(int cell=0; cell<numCells; cell++)
       for(int node=0; node<numNodes; node++) {
-        int lid = Albany::getLocalElement(m_overlapVector->space(),wsElNodeID[ws][cell][node]);
+        int lid = ov_vs_indexer->getLocalElement(wsElNodeID[ws][cell][node]);
         data[lid] += wsSrc(cell,node);
       }
   }
@@ -530,11 +533,11 @@ void MPMD_App::exportDataMap(const Plato::data::layout_t & aDataLayout,
 
     if(aDataLayout == Plato::data::layout_t::SCALAR_FIELD)
     {
-      auto spmd_vs = Albany::getSpmdVectorSpace(m_localVector->space());
-      int numLocalVals = spmd_vs->localSubDim();
+      auto vs_indexer = Albany::createGlobalLocalIndexer(m_localVector->space());
+      int numLocalVals = vs_indexer->getNumLocalElements();
       aMyOwnedGlobalIDs.resize(numLocalVals);
       for(int lid=0; lid<numLocalVals; lid++){
-        aMyOwnedGlobalIDs[lid] = Albany::getGlobalElement(spmd_vs,lid)+1; // Albany's gids start from 0
+        aMyOwnedGlobalIDs[lid] = vs_indexer->getGlobalElement(lid)+1; // Albany's gids start from 0
       }
     }
 }

src/disc/stk/Albany_STKDiscretization.cpp
@@ -1662,6 +1662,16 @@ STKDiscretization::computeNodalVectorSpaces(bool overlapped)
             (overlapped) ? dofs_struct->overlap_vs : dofs_struct->vs;
         vs = createVectorSpace(comm, indices());
       }
+
+      // Create the Global-Local indexers
+      if (overlapped) {
+        dofs_struct->overlap_node_vs_indexer = createGlobalLocalIndexer(dofs_struct->overlap_node_vs);
+        dofs_struct->overlap_vs_indexer = createGlobalLocalIndexer(dofs_struct->overlap_vs);
+
+      } else {
+        dofs_struct->node_vs_indexer = createGlobalLocalIndexer(dofs_struct->node_vs);
+        dofs_struct->vs_indexer      = createGlobalLocalIndexer(dofs_struct->vs);
+      }
     }
   }
 }
@@ -1728,8 +1738,8 @@ void
 STKDiscretization::computeGraphsUpToFillComplete()
 {
   std::map<int, stk::mesh::Part*>::iterator pv = stkMeshStruct->partVec.begin();
-  int                                       nodes_per_element =
-      metaData.get_cell_topology(*(pv->second)).getNodeCount();
+  const auto& topo = stk::mesh::get_cell_topology(metaData.get_topology(*pv->second));
+  int nodes_per_element = topo.getNodeCount();
 
   // Loads member data:  overlap_graph, numOverlapodes, overlap_node_map,
   // coordinates, graphs
@@ -2100,7 +2110,7 @@ STKDiscretization::computeWorksetInfo()
 
       for (auto it = mapOfDOFsStructs.begin(); it != mapOfDOFsStructs.end();
            ++it) {
-        auto ov_indexer = createGlobalLocalIndexer(it->second.overlap_vs);
+        const auto& ov_indexer = it->second.overlap_vs_indexer;
         IDArray&  wsElNodeEqID_array = it->second.wsElNodeEqID[b];
         GIDArray& wsElNodeID_array   = it->second.wsElNodeID[b];
         int       nComp              = it->first.second;

src/disc/stk/Albany_STKDiscretization.hpp
@@ -33,6 +33,8 @@ namespace Albany {
 
 typedef shards::Array<GO, shards::NaturalOrder> GIDArray;
 
+class GlobalLocalIndexer;
+
 struct DOFsStruct
 {
   Teuchos::RCP<const Thyra_VectorSpace> node_vs;
@@ -45,6 +47,11 @@ struct DOFsStruct
   std::vector<IDArray>                  wsElNodeEqID;
   std::vector<std::vector<GO>>          wsElNodeID_rawVec;
   std::vector<GIDArray>                 wsElNodeID;
+
+  Teuchos::RCP<const GlobalLocalIndexer> node_vs_indexer;
+  Teuchos::RCP<const GlobalLocalIndexer> overlap_node_vs_indexer;
+  Teuchos::RCP<const GlobalLocalIndexer> vs_indexer;
+  Teuchos::RCP<const GlobalLocalIndexer> overlap_vs_indexer;
 };
 
 struct NodalDOFsStructContainer

tests/small/SteadyHeatConstrainedOpt2D/input_dirichlet_mixed_params.yaml
@@ -122,7 +122,7 @@ ANONYMOUS:
               Accept Linesearch Minimizer: false
               Accept Last Alpha: false
               Descent Method: 
-                Type: Newton-Krylov
+                Type: Quasi-Newton
                 Nonlinear CG Type: Hestenes-Stiefel
               Curvature Condition: 
                 Type: Strong Wolfe Conditions