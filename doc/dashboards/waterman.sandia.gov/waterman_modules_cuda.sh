ae3f9c56d85df23dfa38578474040ecc9492ce05
Irina Tezaur <ikalash@sandia.gov>
2019-11-23 12:23:22 -0700

    Updating waterman scripts.

--- doc/dashboards/waterman.sandia.gov/waterman_modules_cuda.sh
+++ doc/dashboards/waterman.sandia.gov/waterman_modules_cuda.sh
@@ -1,7 +1,8 @@
 
 module purge 
 module load git
-module load devpack/20180517/openmpi/2.1.2/gcc/7.2.0/cuda/9.2.88
+#module load devpack/20180517/openmpi/2.1.2/gcc/7.2.0/cuda/9.2.88
+module load devpack/20190814/openmpi/4.0.1/gcc/7.2.0/cuda/10.1.105
 module load yamlcpp/0.5.3/gcc/7.2.0
 module unload cmake 
 module load cmake/3.12.3 
