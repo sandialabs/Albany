75d31549922cc03c0a03136d51167d18f24ff222
Irina K. Tezaur <ikalash@sandia.gov>
2020-01-03 19:29:35 -0800

    Updating skybridge nightly test to use develop Trilinos instead of master.

--- doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
+++ doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
@@ -5,6 +5,8 @@ rm -rf *log
 rm -rf Albany
 rm -rf Trilinos
 git clone git@github.com:trilinos/Trilinos.git >& trilinos-clone.log
+cd Trilinos
+git checkout develop
 git clone git@github.com:SNLComputation/Albany.git >& albany-clone.log 
 module use --append $LCM_DIR/Albany/doc/LCM/modulefiles
 module load serial-intel-release
