db70f76b69efada1d1a7b948b127e43ed9d9712e
Irina K. Tezaur <ikalash@sandia.gov>
2020-01-21 20:00:40 -0800

    Updating modules to hopefully fix Cori nightly build (issue #550).

--- doc/dashboards/cori.nersc.gov/cori_modules.sh
+++ doc/dashboards/cori.nersc.gov/cori_modules.sh
@@ -9,7 +9,7 @@ module load PrgEnv-gnu
 module unload cray-mpich
 module load cray-mpich/7.7.6 
 module unload gcc/7.3.0 
-module load gcc/8.2.0 
+module load gcc/8.3.0 
 module load boost 
 module load cmake python cray-netcdf-hdf5parallel
 module list
