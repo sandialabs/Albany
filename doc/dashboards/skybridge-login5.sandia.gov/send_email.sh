b3a89d10929ea69f0f5af5325981e38c7912a792
Irina K. Tezaur <ikalash@sandia.gov>
2020-03-06 08:41:36 -0800

    Removing Alejandro from list of email recipients of nightly emails from skybridge and waterman.

--- doc/dashboards/skybridge-login5.sandia.gov/send_email.sh
+++ doc/dashboards/skybridge-login5.sandia.gov/send_email.sh
@@ -8,4 +8,4 @@ TTTTT=`grep "(Timeout)" /home/ikalash/LCM/albany-serial-intel-release.log -c`
 TT=`grep "...   Passed" /home/ikalash/LCM/albany-serial-intel-release.log -c`
 
 #/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-/bin/mail -s "albany_cluster-toss3_skybridge-login5_serial-intel-release: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov, amota@sandia.gov" < /home/ikalash/LCM/results
+/bin/mail -s "albany_cluster-toss3_skybridge-login5_serial-intel-release: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /home/ikalash/LCM/results
--- doc/dashboards/waterman.sandia.gov/send_email_ctest.sh
+++ doc/dashboards/waterman.sandia.gov/send_email_ctest.sh
@@ -7,6 +7,6 @@ TTTT=`grep "(Not Run)" nightly_log_watermanAlbany.txt -c`
 TTTTT=`grep "(Timeout)" nightly_log_watermanAlbany.txt -c`
 TT=`grep "...   Passed" nightly_log_watermanAlbany.txt -c`
 
-/bin/mail -s "Albany, waterman (KokkosNode=CUDA): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, jwatkin@sandia.gov, amota@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov" < results_cuda
+/bin/mail -s "Albany, waterman (KokkosNode=CUDA): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, jwatkin@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov" < results_cuda
 #/bin/mail -s "Albany (master, Waterman CUDA+V100+P9, KOKKOS_UNDER_DEVELOPMENT): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < results_cuda
 
