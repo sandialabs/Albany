c532303a6b80dc13766500529975cac92d1639ac
Irina K. Tezaur <ikalash@sandia.gov>
2020-03-15 16:33:41 -0700

    Fixing CEE builds.

--- doc/dashboards/cee-compute011.sandia.gov/modules_base.sh
+++ doc/dashboards/cee-compute011.sandia.gov/modules_base.sh
@@ -1,7 +1,7 @@
-  module purge
+ module purge
   module load sierra-git/2.6.1
 #  module load sierra-compiler/gcc/5.2.0
   module load sierra-devel/gcc-8.1.0-openmpi-1.10.2
 #  module load sierra-mkl/18.0-2018.1.163
   module load sierra-mkl/19.0-2019.0.117
-module load sparc-cmake
+  module load sparc-cmake
--- doc/dashboards/cee-compute011.sandia.gov/modules_clang.sh
+++ doc/dashboards/cee-compute011.sandia.gov/modules_clang.sh
@@ -1,10 +1,9 @@
+
 module purge
-module load sparc-cmake 
+module load sparc-cmake
 module load sierra-git/2.6.1
 module load sierra-devel/clang-7.0-openmpi-1.10.2
 #  module load sierra-mkl/18.0-2018.1.163
 module load sierra-mkl/19.0-2019.0.117
-module unload sierra-compiler/clang/7.0
+#module unload sierra-compiler/clang/7.0
 module unload sierra-mpi/openmpi/1.10.2
-
-
