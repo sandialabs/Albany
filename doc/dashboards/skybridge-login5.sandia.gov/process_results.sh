ec45bf757eba9ae8c1dca379e66dcdc0bef276af
Irina K. Tezaur <ikalash@sandia.gov>
2020-03-21 20:25:19 -0600

    Updating script to point to new CDash site.

--- doc/dashboards/skybridge-login5.sandia.gov/process_results.sh
+++ doc/dashboards/skybridge-login5.sandia.gov/process_results.sh
@@ -15,6 +15,6 @@ cat results5 results6 >& results7
 grep "\*Timeout" albany-serial-intel-release.log >& results8
 cat results7 results8 >& results
 echo "" >> results
-echo "The Albany CDash site can be accessed here: http://cdash.sandia.gov/CDash-2-3-0/index.php?project=Albany" >> results
+echo "The Albany CDash site can be accessed here: https://sems-cdash-son.sandia.gov/cdash/index.php?project=Albany" >> results
 echo "" >> results
 rm results0 results1 results11 results2 results3 results4 results5 results6 results7 results8
