727088d2fe7027590f6933c5d65295e088a0157d
Irina K. Tezaur <ikalash@sandia.gov>
2020-04-16 11:07:38 -0700

    Correcting case in Kokkos enables in Trilinos for nightly builds.

--- doc/LCM/build/trilinos-config.sh
+++ doc/LCM/build/trilinos-config.sh
@@ -52,11 +52,11 @@ cmake \
  \
  -D Teuchos_ENABLE_STACKTRACE:BOOL=OFF \
  -D Teuchos_ENABLE_DEFAULT_STACKTRACE:BOOL=OFF \
- -D Kokkos_ENABLE_Cuda_UVM:BOOL=lcm_enable_uvm \
+ -D Kokkos_ENABLE_CUDA_UVM:BOOL=lcm_enable_uvm \
  -D Kokkos_ENABLE_EXAMPLES:BOOL=lcm_enable_kokkos_examples \
- -D Kokkos_ENABLE_OpenMP:BOOL=lcm_enable_openmp \
- -D Kokkos_ENABLE_Pthread:BOOL=lcm_enable_pthreads \
- -D Kokkos_ENABLE_Serial:BOOL=ON \
+ -D Kokkos_ENABLE_OPENMP:BOOL=lcm_enable_openmp \
+ -D Kokkos_ENABLE_PTHREAD:BOOL=lcm_enable_pthreads \
+ -D Kokkos_ENABLE_SERIAL:BOOL=ON \
  -D Kokkos_ENABLE_TESTS:BOOL=OFF \
  -D TPL_ENABLE_CUDA:STRING=lcm_enable_cuda \
  -D TPL_ENABLE_CUSPARSE:BOOL=lcm_enable_cusparse \
