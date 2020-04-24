0008b31761c9b97118a7f92075dacbe19bccc6d4
Irina Tezaur <ikalash@sandia.gov>
2020-04-24 11:57:00 -0700

    Updating modules for mockba.

--- doc/dashboards/mockba.ca.sandia.gov/mockba_modules.sh
+++ doc/dashboards/mockba.ca.sandia.gov/mockba_modules.sh
@@ -1,8 +1,8 @@
 
 module purge
 module load sems-env
-module load sems-gcc/7.3.0
-module load sems-openmpi/1.8.7 
+module load sems-gcc/9.2.0
+module load sems-openmpi/1.10.1 
 module load sems-cmake
 module load sems-boost/1.55.0/base
 module load sems-netcdf/4.4.1/exo_parallel 
