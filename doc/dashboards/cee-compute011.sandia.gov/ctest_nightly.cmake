2b2f44c5aaf411e27751fd71a0047f99046e32de
Irina K. Tezaur <ikalash@sandia.gov>
2020-05-27 16:45:50 -0600

    Cleaning up CEE nightly test scripts.

--- doc/dashboards/cee-compute011.sandia.gov/ctest_nightly.cmake
+++ doc/dashboards/cee-compute011.sandia.gov/ctest_nightly.cmake
@@ -625,6 +625,7 @@ if (BUILD_TRILINOS OR BUILD_TRILINOSDBG OR BUILD_TRILINOSCLANG OR BUILD_TRILINOS
   if (BUILD_TRILINOS OR BUILD_TRILINOSDBG)  
     set(BOOST_DIR "/projects/albany/gcc-9.1.0")
     set(LIB_DIR "/projects/albany/gcc-9.1.0")
+    set(GCC_LIB_DIR "/projects/albany/gcc-9.1.0")
     set(MPI_BASE_DIR "/projects/albany/gcc-9.1.0")
     set(NETCDF "/projects/albany/gcc-9.1.0") 
     set(HDFDIR "/projects/albany/gcc-9.1.0")
@@ -644,7 +645,7 @@ if (BUILD_TRILINOS OR BUILD_TRILINOSDBG OR BUILD_TRILINOSCLANG OR BUILD_TRILINOS
     set(MKL_PATH "/sierra/sntools/SDK/compilers/intel/composer_xe_2019.5.281")
     set(SUPERLUDIR "/projects/albany/clang-9.0.1/SuperLU_4.3")
     set(LABLAS_LIBRARIES "-L${MKL_PATH}/lib/intel64 -Wl,--start-group ${MKL_PATH}/mkl/lib/intel64/libmkl_intel_lp64.a ${MKL_PATH}/mkl/lib/intel64/libmkl_core.a ${MKL_PATH}/mkl/lib/intel64/libmkl_sequential.a -Wl,--end-group")
-  endif (BUILD_TRILINNOSCLANG OR BUILD_TRILINOSCLANGDBG) 
+  endif (BUILD_TRILINOSCLANG OR BUILD_TRILINOSCLANGDBG) 
   set (CONF_OPTS
     "-DCMAKE_BUILD_TYPE:STRING=${BTYPE}"
     "-DCMAKE_CXX_COMPILER:STRING=${MPI_BASE_DIR}/bin/mpicxx"
@@ -776,12 +777,7 @@ endif (BUILD_TRILINOS OR BUILD_TRILINOSDBG)
     #
     "-DTPL_ENABLE_ParMETIS:STRING=OFF"
     #
-if (BUILD_TRILINOS OR BUILD_TRILINOSDBG) 
-    "-DTrilinos_EXTRA_LINK_FLAGS:STRING='-L${HDFDIR}/lib -lnetcdf -lpnetcdf -lhdf5_hl -lhdf5 -lz -lgfortran'"
-endif (BUILD_TRILINOS OR BUILD_TRILINOSDBG) 
-if (BUILD_TRILINOSCLANG OR BUILD_TRILINOSCLANGDBG) 
     "-DTrilinos_EXTRA_LINK_FLAGS:STRING='-L${LIB_DIR}/lib -L${LIB_DIR}/lib64 -lnetcdf -lpnetcdf -lhdf5_hl -lhdf5 -lz -lgfortran -Wl,-rpath,${GCC_LIB_DIR}/lib:${GCC_LIB_DIR}/lib64'"
-endif (BUILD_TRILINOSCLANG OR BUILD_TRILINOSCLANGDBG) 
     #
     "-DTrilinos_ENABLE_TriKota:BOOL=OFF"
     "-DCMAKE_INSTALL_RPATH_USE_LINK_PATH:BOOL=ON"
