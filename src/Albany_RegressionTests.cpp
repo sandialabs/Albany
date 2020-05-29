83487f5b6af26d52263a2eb1bdd4ab85b78b6b71
Irina K. Tezaur <ikalash@sandia.gov>
2020-05-29 13:48:17 -0700

    Revert "Correcting logic that was causing sensitivity comparisons to be ignored"
    This reverts commit 78998a56327ca16ec7a3abf6c1dc5f7de3bf2877.

--- src/Albany_RegressionTests.cpp
+++ src/Albany_RegressionTests.cpp
@@ -87,14 +87,12 @@ checkSolveTestResults(
         dgdp != Teuchos::null,
         "There are Sensitivity Tests but the sensitivity vector ("
             << response_index << ", " << parameter_index << ") is null!");
-    if (numSensTests != dgdp->range()->dim()) {
-      ALBANY_ASSERT(
-          false,
-          "Number of sensitivity tests ("
-              << numSensTests << ") != number of sensitivities ["
-              << response_index << "][" << parameter_index << "] ("
-              << dgdp->range()->dim() << ") !");
-    }
+    ALBANY_ASSERT(
+        numSensTests <= dgdp->range()->dim(),
+        "Number of sensitivity tests ("
+            << numSensTests << ") != number of sensitivities ["
+            << response_index << "][" << parameter_index << "] ("
+            << dgdp->range()->dim() << ") !");
   }
   for (int i = 0; i < numSensTests; i++) {
     const int numVecs = dgdp->domain()->dim();
