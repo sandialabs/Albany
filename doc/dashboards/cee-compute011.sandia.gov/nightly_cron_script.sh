2c31c23cc8d3a9e48984b6d5b9e334cc90efccc5
Irina K. Tezaur <ikalash@sandia.gov>
2020-06-15 21:52:18 -0600

    Removing comments.

--- doc/dashboards/cee-compute011.sandia.gov/nightly_cron_script.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightly_cron_script.sh
@@ -40,10 +40,8 @@ elif [ "$BUILD_OPT" = "base-trilinos" ] || [ "$BUILD_OPT" = "base-albany" ] || [
   #gcc builds
   module purge
   module load sems-env
-  #module load sems-gcc/8.3.0
   module load sems-gcc/9.2.0
   module load sems-openmpi/1.10.1
-  #module load sierra-cmake/3.12.2
   module load sems-cmake/3.12.2
   module load sems-boost/1.55.0/base
   module load sems-netcdf/4.4.1/exo_parallel
