171b727d9ab7900b1199d5699ab5803b22db4174
Irina Tezaur <ikalash@sandia.gov>
2020-10-20 17:39:01 -0700

    Moving spack nightly build to cee-compute010.

---
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/postproc_spack.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+cd /projects/albany/nightlySpackBuild
+bash process_results_spack.sh 
+bash send_email_spack.sh 
---
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/send_email_spack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#source $1 
+
+TTT=`grep "(Failed)" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
+TTTT=`grep "(Not Run)" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
+TTTTT=`grep "(Timeout)" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
+TT=`grep "...   Passed" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
+
+#/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
+#/bin/mail -s "Albany Spack Build, camobap.ca.sandia.gov: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "albany@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /projects/albany/nightlySpackBuild/results_spack
+#/bin/mail -s "Albany Spack Build (linux-rhel7-haswell, gcc-6.1.0): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov, gahanse@sandia.gov" < /projects/albany/nightlySpackBuild/results_spack
+/bin/mail -s "Albany Spack Build (linux-rhel7-haswell, gcc-10.1.1): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /projects/albany/nightlySpackBuild/results_spack
