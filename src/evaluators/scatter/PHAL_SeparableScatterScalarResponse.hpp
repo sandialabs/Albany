29e30aae1f2d2a92fe0a13ff4ef6e820db8e5ad1
kliegeois <kimliegeois@ymail.com>
2020-10-26 14:01:45 +0100

    Add documentation for Albany::Application functions related to the Hessian-vector products.

--- src/evaluators/scatter/PHAL_SeparableScatterScalarResponse.hpp
+++ src/evaluators/scatter/PHAL_SeparableScatterScalarResponse.hpp
@@ -240,27 +240,39 @@ private:
  *
  * This specialization is used to scatter the solution for the computation of:
  * <ul>
- *  <li> The @f$ H_{xx}(g)v_{x} @f$ contribution of the Hessian-vector product of the response function:
+ *  <li> The \f$\boldsymbol{H}_{\boldsymbol{x}\boldsymbol{x}}(g)\boldsymbol{v}_{\boldsymbol{x}} \f$ contribution of the Hessian-vector product of the response function:
  *       \f[
- *         H_{xx}(g)v_{x}=\left.\frac{\partial}{\partial r} \nabla_{x} g(x+ r\,v_{x},p_1, p_2)\right|_{r=0},
+ *         \boldsymbol{H}_{\boldsymbol{x}\boldsymbol{x}}(g)\boldsymbol{v}_{\boldsymbol{x}}=
+ *         \left.\frac{\partial}{\partial r} \nabla_{\boldsymbol{x}} g(\boldsymbol{x}+ r\,\boldsymbol{v}_{\boldsymbol{x}},\boldsymbol{p}_1, \boldsymbol{p}_2)\right|_{r=0},
  *       \f]
- *  <li> The @f$ H_{xp_1}(g)v_{p_1} @f$ contribution of the Hessian-vector product of the response function:
+ *  <li> The \f$\boldsymbol{H}_{\boldsymbol{x}\boldsymbol{p}_1}(g)\boldsymbol{v}_{\boldsymbol{p}_1}\f$ contribution of the Hessian-vector product of the response function:
  *       \f[
- *         H_{xp_1}(g)v_{p_1}=\left.\frac{\partial}{\partial r} \nabla_{x} g(x, p_1+ r\,v_{p_1}, p_2)\right|_{r=0},
+ *         \boldsymbol{H}_{\boldsymbol{x}\boldsymbol{p}_1}(g)\boldsymbol{v}_{\boldsymbol{p}_1}=
+ *         \left.\frac{\partial}{\partial r} \nabla_{\boldsymbol{x}} g(\boldsymbol{x}, \boldsymbol{p}_1+ r\,\boldsymbol{v}_{\boldsymbol{p}_1}, \boldsymbol{p}_2)\right|_{r=0},
  *       \f]
- *  <li> The @f$ H_{p_2x}(g)v_{x} @f$ contribution of the Hessian-vector product of the response function:
+ *  <li> The \f$\boldsymbol{H}_{\boldsymbol{p}_2\boldsymbol{x}}(g)\boldsymbol{v}_{\boldsymbol{x}} \f$ contribution of the Hessian-vector product of the response function:
  *       \f[
- *         H_{p_2x}(g)v_{x}=\left.\frac{\partial}{\partial r} \nabla_{p_2} g(x+ r\,v_{x},p_1, p_2)\right|_{r=0},
+ *         \boldsymbol{H}_{\boldsymbol{p}_2\boldsymbol{x}}(g)\boldsymbol{v}_{\boldsymbol{x}}=
+ *         \left.\frac{\partial}{\partial r} \nabla_{\boldsymbol{p}_2} g(\boldsymbol{x}+ r\,\boldsymbol{v}_{\boldsymbol{x}},\boldsymbol{p}_1,\boldsymbol{p}_2)\right|_{r=0},
  *       \f]
- *  <li> The @f$ H_{p_2p_1}(g)v_{p_1} @f$ contribution of the Hessian-vector product of the response function:
+ *  <li> The \f$\boldsymbol{H}_{\boldsymbol{p}_2\boldsymbol{p}_1}(g)\boldsymbol{v}_{\boldsymbol{p}_1} \f$ contribution of the Hessian-vector product of the response function:
  *       \f[
- *         H_{p_2p_1}(g)v_{p_1}=\left.\frac{\partial}{\partial r} \nabla_{p_2} g(x, p_1+ r\,v_{p_1}, p_2)\right|_{r=0},
+ *         \boldsymbol{H}_{\boldsymbol{p}_2\boldsymbol{p}_1}(g)\boldsymbol{v}_{\boldsymbol{p}_1}=
+ *         \left.\frac{\partial}{\partial r} \nabla_{\boldsymbol{p}_2} g(\boldsymbol{x}, \boldsymbol{p}_1+ r\,\boldsymbol{v}_{\boldsymbol{p}_1}, \boldsymbol{p}_2)\right|_{r=0},
  *       \f]
  * </ul>
  *
- *  where  @f$ x @f$  is the solution,  @f$ p_1 @f$  is a first distributed parameter,  @f$ p_2 @f$  is a potentially different second distributed parameter,
- *  @f$  g @f$  is the response function,  @f$ v_{x} @f$  is a direction vector
- *  with the same dimension as the vector  @f$ x @f$, and @f$ v_{p_1} @f$  is a direction vector with the same dimension as the vector  @f$ p_1 @f$.
+ *  where  \f$\boldsymbol{x}\f$  is the solution, \f$\boldsymbol{p}_1\f$  is a first distributed parameter, \f$\boldsymbol{p}_2\f$ is a potentially different second distributed parameter,
+ *  \f$g\f$  is the response function, \f$\boldsymbol{v}_{\boldsymbol{x}}\f$  is a direction vector
+ *  with the same dimension as the vector \f$\boldsymbol{x}\f$, and \f$\boldsymbol{v}_{\boldsymbol{p}_1}\f$  is a direction vector with the same dimension as the vector \f$\boldsymbol{p}_1\f$.
+ * 
+ * This scatter is used when calling:
+ * <ul>
+ *   <li> Albany::Application::evaluateResponseDistParamHessVecProd_xx,
+ *   <li> Albany::Application::evaluateResponseDistParamHessVecProd_xp,
+ *   <li> Albany::Application::evaluateResponseDistParamHessVecProd_px, 
+ *   <li> Albany::Application::evaluateResponseDistParamHessVecProd_pp.
+ * </ul>
  */
 
 template<typename Traits>
