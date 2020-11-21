b1f16f38aaa42cee49145ee0ab7cb73fda3b0645
Irina Tezaur <ikalash@localhost.localdomain>
2020-11-21 13:21:11 -0800

    Updating scripts for camobap post upgrade.

--- doc/nightlyTestHarness/camobap/netcdf-4.4.1-configure.sh
+++ doc/nightlyTestHarness/camobap/netcdf-4.4.1-configure.sh
@@ -1,6 +1,6 @@
-./configure CC=mpicc FC=mpifort CXX=mpicxx \
-      CXXFLAGS="-fPIC -I/nightlyCDash/albany-tpls-gcc-10.1.1/include -O3 -march=native" \
-      CFLAGS="-fPIC -I/nightlyCDash/albany-tpls-gcc-10.1.1/include -O3 -march=native" \
-      LDFLAGS="-fPIC -L/nightlyCDash/albany-tpls-gcc-10.1.1/lib -O3 -march=native" \
-      FCFLAGS="-fPIC -I/nightlyCDash/albany-tpls-gcc-10.1.1/include -O3 -march=native" \
-      --prefix=/nightlyCDash/albany-tpls-gcc-10.1.1 --disable-doxygen --enable-netcdf4 #--enable-pnetcdf
+./configure CC=/usr/lib64/openmpi/bin/mpicc FC=/usr/lib64/openmpi/bin/mpifort CXX=/usr/lib64/openmpi/bin/mpicxx \
+      CXXFLAGS="-fPIC -I/nightlyCDash/albany-tpls-gcc-10.2.1/include -O3 -march=native" \
+      CFLAGS="-fPIC -I/nightlyCDash/albany-tpls-gcc-10.2.1/include -O3 -march=native" \
+      LDFLAGS="-fPIC -L/nightlyCDash/albany-tpls-gcc-10.2.1/lib -O3 -march=native" \
+      FCFLAGS="-fPIC -I/nightlyCDash/albany-tpls-gcc-10.2.1/include -O3 -march=native" \
+      --prefix=/nightlyCDash/albany-tpls-gcc-10.2.1 --disable-doxygen --enable-netcdf4 #--enable-pnetcdf
--- doc/nightlyTestHarness/camobap/netcdf-fortran-4.4.1-configure.sh
+++ doc/nightlyTestHarness/camobap/netcdf-fortran-4.4.1-configure.sh
@@ -1,3 +1,6 @@
 
+export FCFLAGS="-w -fallow-argument-mismatch -O2"
+export FFLAGS="-w -fallow-argument-mismatch -O2"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/nightlyCDash/albany-tpls-gcc-10.2.1/lib
   
-./configure --disable-fortran-type-check --prefix=/nightlyCDash/albany-tpls-gcc-10.1.1 FLAGS=-I/nightlyCDash/albany-tpls-gcc-10.1.1/include LDFLAGS=-L/nightlyCDash/albany-tpls-gcc-10.1.1/lib
+./configure --disable-fortran-type-check --prefix=/nightlyCDash/albany-tpls-gcc-10.2.1 CPPFLAGS=-I/nightlyCDash/albany-tpls-gcc-10.2.1/include LDFLAGS=-L/nightlyCDash/albany-tpls-gcc-10.2.1/lib
