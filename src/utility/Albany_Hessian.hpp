ae7bc609351a97b51958b4943900ee902809bbba
kliegeois <kimliegeois@ymail.com>
2020-12-11 11:29:18 +0100

    Add first implementation of Hessian reconstruction

---
+++ src/utility/Albany_Hessian.hpp
@@ -0,0 +1,13 @@
+#ifndef ALBANY_HESSIAN_HPP
+#define ALBANY_HESSIAN_HPP
+
+namespace Albany
+{
+    Teuchos::RCP<Tpetra_CrsGraph> createHessianCrsGraph(
+        Teuchos::RCP<const Tpetra_Map> p_owned_map,
+        Teuchos::RCP<const Tpetra_Map> p_overlapped_map,
+        const Albany::IDArray &wsElDofs);
+
+} // namespace Albany
+
+#endif // ALBANY_HESSIAN_HPP
