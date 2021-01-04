2b770a55b779634a54d9fccfd5e6c9461f882843
Max Carlson <maxx.l.carlson@gmail.com>
2021-01-04 16:16:24 -0700

    Kokkos refactor of QuadPointsToCellInterpolation, not actually a sideset evaluator but it wasn't ported yet

--- src/evaluators/interpolation/PHAL_QuadPointsToCellInterpolation_Def.hpp
+++ src/evaluators/interpolation/PHAL_QuadPointsToCellInterpolation_Def.hpp
@@ -41,8 +41,76 @@ postRegistrationSetup(typename Traits::SetupData d,
 
   this->utils.setFieldData(field_cell,fm);
 
+  TEUCHOS_TEST_FOR_EXCEPTION (qp_dims.size() > 5, Teuchos::Exceptions::InvalidParameter, "Error! val_side has more dimensions than expected.\n");
+
+  for (int i = 0; i < qp_dims.size(); ++i)
+    dimsArray[i] = qp_dims[i];
+
   d.fill_field_dependencies(this->dependentFields(),this->evaluatedFields());
   if (d.memoizer_active()) memoizer.enable_memoizer();
+}
+
+// *********************************************************************
+// Kokkos functor
+template<typename EvalT, typename Traits, typename ScalarT>
+KOKKOS_INLINE_FUNCTION
+void QuadPointsToCellInterpolationBase<EvalT, Traits, ScalarT>::
+operator() (const Dim0_Tag& tag, const int& cell) const {
+  
+  MeshScalarT meas = 0.0;
+      
+  for (int qp(0); qp<dimsArray[1]; ++qp)
+    meas += w_measure(cell,qp);
+
+  field_cell(cell) = 0.0;
+  for (int qp(0); qp<dimsArray[1]; ++qp) {
+    field_cell(cell) += field_qp(cell,qp)*w_measure(cell,qp);
+  }
+  field_cell(cell) /= meas;
+
+}
+
+template<typename EvalT, typename Traits, typename ScalarT>
+KOKKOS_INLINE_FUNCTION
+void QuadPointsToCellInterpolationBase<EvalT, Traits, ScalarT>::
+operator() (const Dim1_Tag& tag, const int& cell) const {
+  
+  MeshScalarT meas = 0.0;
+      
+  for (size_t qp(0); qp<dimsArray[1]; ++qp)
+    meas += w_measure(cell,qp);
+
+  for (size_t i(0); i<dimsArray[2]; ++i)
+  {
+    field_cell(cell,i) = 0;
+    for (int qp(0); qp<dimsArray[1]; ++qp)
+      field_cell(cell,i) += field_qp(cell,qp,i)*w_measure(cell,qp);
+    field_cell(cell,i) /= meas;
+  }
+
+}
+
+template<typename EvalT, typename Traits, typename ScalarT>
+KOKKOS_INLINE_FUNCTION
+void QuadPointsToCellInterpolationBase<EvalT, Traits, ScalarT>::
+operator() (const Dim2_Tag& tag, const int& cell) const {
+  
+  MeshScalarT meas = 0.0;
+      
+  for (size_t qp(0); qp<dimsArray[1]; ++qp)
+    meas += w_measure(cell,qp);
+
+  for (size_t i(0); i<dimsArray[2]; ++i)
+  {
+    for (size_t j(0); j<dimsArray[3]; ++j)
+    {
+      field_cell(cell,i,j) = 0;
+      for (size_t qp(0); qp<dimsArray[1]; ++qp)
+        field_cell(cell,i,j) += field_qp(cell,qp,i,j)*w_measure(cell,qp);
+      field_cell(cell,i,j) /= meas;
+    }
+  }
+
 }
 
 //**********************************************************************
@@ -51,46 +119,21 @@ void QuadPointsToCellInterpolationBase<EvalT, Traits, ScalarT>::evaluateFields (
 {
   if (memoizer.have_saved_data(workset,this->evaluatedFields())) return;
 
-  MeshScalarT meas;
-  int numQPs = qp_dims[1];
-
-  for (size_t cell=0; cell<workset.numCells; ++cell)
+  switch(qp_dims.size())
   {
-    meas = 0.0;
-    for (int qp(0); qp<numQPs; ++qp)
-    {
-      meas += w_measure(cell,qp);
-    }
-
-    if(qp_dims.size()==2)  //scalar
-    {
-      field_cell(cell) = 0;
-      for (int qp(0); qp<numQPs; ++qp)
-        field_cell(cell) += field_qp(cell,qp)*w_measure(cell,qp);
-      field_cell(cell) /= meas;
-    }
-    else if(qp_dims.size()==3)  //vector
-    {
-      for (size_t dim(0); dim<qp_dims[2]; ++dim)
-      {
-        field_cell(cell,dim) = 0;
-        for (int qp(0); qp<numQPs; ++qp)
-          field_cell(cell,dim) += field_qp(cell,qp,dim)*w_measure(cell,qp);
-        field_cell(cell,dim) /= meas;
-      }
-    }
-    else if(qp_dims.size()==4)  //tensor
-    {
-      for (size_t dim0(0); dim0<qp_dims[2]; ++dim0)
-        for (size_t dim1(0); dim1<qp_dims[3]; ++dim1)
-      {
-        field_cell(cell,dim0, dim1) = 0;
-        for (int qp(0); qp<numQPs; ++qp)
-          field_cell(cell,dim0, dim1) += field_qp(cell,qp,dim0, dim1)*w_measure(cell,qp);
-        field_cell(cell,dim0, dim1) /= meas;
-      }
-    }
+    case 2: //scalar
+      Kokkos::parallel_for(Dim0_Policy(0, workset.numCells), *this);
+      break;
+    case 3: //vector
+      Kokkos::parallel_for(Dim1_Policy(0, workset.numCells), *this);
+      break;
+    case 4: //tensor
+      Kokkos::parallel_for(Dim2_Policy(0, workset.numCells), *this);
+      break;
+    default:
+      TEUCHOS_TEST_FOR_EXCEPTION (true, std::logic_error, "Error! Field dimension not supported (this error should have already appeared).\n");
   }
+
 }
 
 } // Namespace PHAL
