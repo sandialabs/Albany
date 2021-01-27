fee295fd4407ac094938a52419b44154a8b63846
kliegeois <kimliegeois@ymail.com>
2021-01-27 15:11:27 +0100

    Hessian: add documentation to the functions added

--- src/utility/Albany_Hessian.hpp
+++ src/utility/Albany_Hessian.hpp
@@ -3,6 +3,18 @@
 
 namespace Albany
 {
+    /**
+     * \brief createHessianCrsGraph function
+     *
+     * This function computes the Tpetra::CrsGraph associated to
+     * the Hessian w.r.t a distributed parameter.
+     *
+     * \param p_owned_map [in] Tpetra::Map which specifies the owned entries of the current distributed parameter.
+     *
+     * \param p_overlapped_map [in] Tpetra::Map which specifies the overlapped entries of the current distributed parameter.
+     *
+     * \param wsElDofs [in] Vector of IDArray associated to the mesh used.
+     */
     Teuchos::RCP<Tpetra_CrsGraph> createHessianCrsGraph(
         Teuchos::RCP<const Tpetra_Map> p_owned_map,
         Teuchos::RCP<const Tpetra_Map> p_overlapped_map,
