43073e30318624aad77e54b09449b8fa50c4b6d6
Irina K. Tezaur <ikalash@sandia.gov>
2021-02-03 13:02:20 -0700

    Updating spack nightly build script.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/run_spack.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/run_spack.sh
@@ -4,13 +4,17 @@ cd spack
 export http_proxy=http://wwwproxy.sandia.gov:80
 export https_proxy=http://wwwproxy.sandia.gov:80
 . share/spack/setup-env.sh
-spack --insecure install --dirty --keep-stage libtool >& spack_libtool.out
+module load cde/compiler/gcc/7.2.0
+spack compiler find
+spack --insecure install --dirty --keep-stage libtool%gcc@7.2.0 >& spack_libtool.out
 sleep 5m
-spack --insecure install --dirty --keep-stage diffutils >& spack_diffutils.out
+spack --insecure install --dirty --keep-stage diffutils%gcc@7.2.0 >& spack_diffutils.out
 sleep 5m
-spack --insecure install --dirty --keep-stage trilinos >& spack_trilinos.out
+spack --insecure install --dirty --keep-stage xz%gcc@7.2.0 >& spack_xz.out
 sleep 5m
-spack --insecure install --dirty --keep-stage albany >& spack_albany.out
+spack --insecure install --dirty --keep-stage trilinos%gcc@7.2.0 >& spack_trilinos.out
+sleep 5m
+spack --insecure install --dirty --keep-stage albany%gcc@7.2.0 >& spack_albany.out
 spack cd albany >& /projects/albany/nightlySpackBuild/spack_cd_albany.out
 cd ../spack-build
 ctest -V >& spack_ctest.out
