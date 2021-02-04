02cbb50d08f9d3055bbca226874e47b0e98bfd60
Irina Tezaur <ikalash@sandia.gov>
2021-02-03 19:10:46 -0700

    Updating modules on weaver.

--- doc/dashboards/weaver.sandia.gov/weaver_modules_cuda.sh
+++ doc/dashboards/weaver.sandia.gov/weaver_modules_cuda.sh
@@ -5,7 +5,7 @@ module load git
 module load devpack/20190814/openmpi/4.0.1/gcc/7.2.0/cuda/10.1.105
 module load yamlcpp/0.5.3/gcc/7.2.0
 module unload cmake 
-module load cmake/3.12.3 
+module load cmake/3.19.3 
 module list
 #FIXME: the following needs needs to be changed to point to your Trilinos!
 #export OMPI_CXX=${jenkins_trilinos_dir}/packages/kokkos/config/nvcc_wrapper
