1989145ee3c740e841842d895d01f45d07ae64f1
Irina K. Tezaur <ikalash@sandia.gov>
2021-03-10 11:44:21 -0700

    Updating CEE nightly test scripts.

--- doc/dashboards/cee-compute011.sandia.gov/sems-intel-modules.sh
+++ doc/dashboards/cee-compute011.sandia.gov/sems-intel-modules.sh
@@ -3,7 +3,7 @@ module purge
 module load sems-env
 module load sems-intel/19.0.5
 module load sems-openmpi/1.10.1 
-module load cde/cmake/3.17.1
+module load cde/dev/cmake/3.19.2
 module load sems-boost/1.55.0/base
 module load sems-netcdf/4.4.1/exo_parallel 
 module list  
