4d1cf0f407cd0107b3e4c0a29cff7bb1111acdba
Irina Kalashnikova Tezaur <ikalash@mockba.ca.sandia.gov>
2021-04-01 11:15:36 -0700

    Updating proxies on mockba.

--- doc/dashboards/mockba.ca.sandia.gov/nightly_cron_script_albany.sh
+++ doc/dashboards/mockba.ca.sandia.gov/nightly_cron_script_albany.sh
@@ -9,17 +9,14 @@ rm -rf nightly_log*
 rm -rf results*
 rm -rf *out 
 
-
-
 source mockba_modules.sh >& modules.out 
 LOG_FILE=/home/ikalash/nightlyAlbanyCDash/nightly_log.txt
 
-#unset HTTPS_PROXY
-#unset HTTP_PROXY
-#export http_proxy=wwwproxy.ca.sandia.gov:80
-#export https_proxy=wwwproxy.ca.sandia.gov:80
+unset HTTPS_PROXY
+unset HTTP_PROXY
+#export http_proxy=proxy.ca.sandia.gov:80
+#export https_proxy=proxy.ca.sandia.gov:80
 
 #env | grep -i proxy 
 
 eval "env TEST_DIRECTORY=/home/ikalash/nightlyAlbanyCDash SCRIPT_DIRECTORY=/home/ikalash/nightlyAlbanyCDash ctest -VV -S /home/ikalash/nightlyAlbanyCDash/ctest_nightly_albany.cmake" > $LOG_FILE 2>&1
-#eval "env https_proxy='https://wwwproxy.ca.sandia.gov:80' http_proxy='http://wwwproxy.ca.sandia.gov:80' HTTPS_PROXY='https://wwwproxy.ca.sandia.gov:80' HTTP_PROXY='http://wwwproxy.ca.sandia.gov' TEST_DIRECTORY=/home/ikalash/nightlyAlbanyCDash SCRIPT_DIRECTORY=/home/ikalash/nightlyAlbanyCDash ctest -VV -S /home/ikalash/nightlyAlbanyCDash/ctest_nightly_albany.cmake" > $LOG_FILE 2>&1
--- doc/dashboards/mockba.ca.sandia.gov/nightly_cron_script_cismAlbany.sh
+++ doc/dashboards/mockba.ca.sandia.gov/nightly_cron_script_cismAlbany.sh
@@ -5,16 +5,13 @@ cd /home/ikalash/nightlyAlbanyCDash
 source mockba_modules.sh >& modules.out 
 LOG_FILE=/home/ikalash/nightlyAlbanyCDash/nightly_log_cismAlbany.txt
 
-#unset HTTPS_PROXY
-#unset HTTP_PROXY
-#export http_proxy=wwwproxy.ca.sandia.gov:80
-#export https_proxy=wwwproxy.ca.sandia.gov:80
+unset HTTPS_PROXY
+unset HTTP_PROXY
+#export http_proxy=proxy.ca.sandia.gov:80
+#export https_proxy=proxy.ca.sandia.gov:80
 
 #env | grep -i proxy 
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib64:/usr/include/boost:/home/ikalash/Downloads/libiconv-1.14/lib/.libs
-export PATH=$PATH:/usr/lib64/openmpi/bin:/home/ikalash/Install/ParaView-4.4.0-Qt4-Linux-64bit/bin:/ascldap/users/ikalash/Trilinos/seacas-build/install/bin:/home/ikalash/Install/Cubit:/ascldap/users/ikalash/Install/MATLAB/R2018b/bin:/ascldap/users/ikalash/Trilinos/seacas-build/install/bin
-
 
 eval "env TEST_DIRECTORY=/home/ikalash/nightlyAlbanyCDash SCRIPT_DIRECTORY=/home/ikalash/nightlyAlbanyCDash ctest -VV -S /home/ikalash/nightlyAlbanyCDash/ctest_nightly_cismAlbany.cmake" > $LOG_FILE 2>&1
-#eval "env https_proxy='https://wwwproxy.ca.sandia.gov:80' http_proxy='http://wwwproxy.ca.sandia.gov:80' HTTPS_PROXY='https://wwwproxy.ca.sandia.gov:80' HTTP_PROXY='http://wwwproxy.ca.sandia.gov' TEST_DIRECTORY=/home/ikalash/nightlyAlbanyCDash SCRIPT_DIRECTORY=/home/ikalash/nightlyAlbanyCDash ctest -VV -S /home/ikalash/nightlyAlbanyCDash/ctest_nightly_albany.cmake" > $LOG_FILE 2>&1
+#eval "env https_proxy='https://proxy.ca.sandia.gov:80' http_proxy='http://proxy.ca.sandia.gov:80' HTTPS_PROXY='https://proxy.ca.sandia.gov:80' HTTP_PROXY='http://proxy.ca.sandia.gov' TEST_DIRECTORY=/home/ikalash/nightlyAlbanyCDash SCRIPT_DIRECTORY=/home/ikalash/nightlyAlbanyCDash ctest -VV -S /home/ikalash/nightlyAlbanyCDash/ctest_nightly_albany.cmake" > $LOG_FILE 2>&1
