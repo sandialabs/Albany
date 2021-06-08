f74740d9de7dd71a0228bf284b81ceb81f84cdf5
Luca Bertagna <lbertag@sandia.gov>
2021-06-08 13:27:02 -0600

    Minor refactor in STKFieldContainerHelper
    Use Kokkos Views instead of BucketArrays, since we are more
    familiar with those than Shards arrays. Also, we might hope
    to get kokkos views from stk directly one day.
    
    Also added a STKUtils file, in case those meta-utils can
    be useful somewhere else.

--- src/disc/stk/Albany_STKFieldContainerHelper_Def.hpp
+++ src/disc/stk/Albany_STKFieldContainerHelper_Def.hpp
@@ -4,100 +4,56 @@
 //    in the file "license.txt" in the top-level Albany directory  //
 //*****************************************************************//
 
-#include "Albany_AbstractSTKFieldContainer.hpp"
 #include "Albany_STKFieldContainerHelper.hpp"
-#include "Albany_ThyraUtils.hpp"
+#include "Albany_STKUtils.hpp"
 #include "Albany_GlobalLocalIndexer.hpp"
-#include "Albany_Macros.hpp"
+#include "Albany_ThyraUtils.hpp"
 
-#include "Albany_BucketArray.hpp"
-#include <stk_mesh/base/GetBuckets.hpp>
+#include <Kokkos_HostSpace.hpp>
+#include <Kokkos_Layout.hpp>
+#include <stk_mesh/base/FieldBase.hpp>
+#include <type_traits>
 
 namespace Albany
 {
 
-template<typename BucketArrayType>
-typename std::conditional<std::is_const<BucketArrayType>::value,const double&, double&>::type
-access (BucketArrayType& array, const int i, const int j);
-
-template<>
-const double& access<const BucketArray<AbstractSTKFieldContainer::ScalarFieldType>> (const BucketArray<AbstractSTKFieldContainer::ScalarFieldType>& array, const int j, const int i)
-{
-  ALBANY_EXPECT (j==0, "Error! Attempting to access 1d array with two indices.\n");
-  (void)j;
-  return array(i);
-}
-
-
-template<>
-double& access<BucketArray<AbstractSTKFieldContainer::ScalarFieldType>> (BucketArray<AbstractSTKFieldContainer::ScalarFieldType>& array, const int j, const int i)
-{
-  ALBANY_EXPECT (j==0, "Error! Attempting to access 1d array with two indices.\n");
-  (void) j;
-  return array(i);
-}
-
-template<>
-const double& access<const BucketArray<AbstractSTKFieldContainer::VectorFieldType>> (const BucketArray<AbstractSTKFieldContainer::VectorFieldType>& array, const int j, const int i)
-{
-  return array(j,i);
-}
-
-template<>
-double& access<BucketArray<AbstractSTKFieldContainer::VectorFieldType>> (BucketArray<AbstractSTKFieldContainer::VectorFieldType>& array, const int j, const int i)
-{
-  return array(j,i);
-}
-
-// Get the rank of a field
-template<typename FieldType>
-constexpr int getRank () {
-  return std::is_same<FieldType,AbstractSTKFieldContainer::ScalarFieldType>::value ? 0 :
-         (std::is_same<FieldType,AbstractSTKFieldContainer::VectorFieldType>::value ? 1 :
-         (std::is_same<FieldType,AbstractSTKFieldContainer::TensorFieldType>::value ? 2 : -1));
-}
-
 // Fill the result vector
 // Create a multidimensional array view of the
 // solution field data for this bucket of nodes.
-// The array is two dimensional ( Cartesian X NumberNodes )
-// and indexed by ( 0..2 , 0..NumberNodes-1 )
+// The array is two dimensional (NumberNodes, FieldDim)
+// where FieldDim=1 for scalar field, and 1+ for vector fields
 
 template<class FieldType>
 void STKFieldContainerHelper<FieldType>::
-fillVector (Thyra_Vector&    field_thyra,
+fillVector (      Thyra_Vector&    field_thyra,
             const FieldType& field_stk,
             const Teuchos::RCP<const GlobalLocalIndexer>& indexer,
             const stk::mesh::Bucket& bucket,
             const NodalDOFManager& nodalDofManager,
             const int offset)
 {
-  constexpr int rank = getRank<FieldType>();
-  TEUCHOS_TEST_FOR_EXCEPTION(rank!=0 && rank!=1, std::runtime_error,
-                             "Error! Can only handle ScalarFieldType and VectorFieldType for now.\n");
+  constexpr int rank = FieldRank<FieldType>::n;
+  static_assert(rank==0 || rank==1,
+                "Error! Can only handle ScalarFieldType and VectorFieldType for now.\n");
 
-  BucketArray<FieldType> field_array(field_stk, bucket);
+  using ScalarT = typename FieldScalar<FieldType>::type;
+  using ViewT = Kokkos::View<const ScalarT**,Kokkos::LayoutRight,Kokkos::HostSpace>;
 
-  using SFT = AbstractSTKFieldContainer::ScalarFieldType;
-  constexpr bool is_SFT = std::is_same<FieldType,SFT>::value;
-  constexpr int nodes_dim = is_SFT ? 0 : 1;
+  auto stk_data = stk::mesh::field_data(field_stk,bucket);
+  const int num_nodes_in_bucket = bucket.size();
+  const int num_vec_components = nodalDofManager.numComponents();
 
-  const int num_nodes_in_bucket = field_array.dimension(nodes_dim);
+  ViewT field_view (stk_data,num_nodes_in_bucket,num_vec_components);
 
   const stk::mesh::BulkData& mesh = field_stk.get_mesh();
   auto data = getNonconstLocalData(field_thyra);
-  int num_vec_components;
-  //IKT, FIXME: ideally nodalDofManager.numComponents() should return 1 for a SFT, I would think.
-  //Need to look into this more to come up with a better fix, hopefully.  
-  if (is_SFT == true) num_vec_components = 1; 
-  else num_vec_components = nodalDofManager.numComponents();
 
   for(int i=0; i<num_nodes_in_bucket; ++i)  {
     const GO node_gid = mesh.identifier(bucket[i]) - 1;
     const LO node_lid = indexer->getLocalElement(node_gid);
 
     for(int j=0; j<num_vec_components; ++j) {
-      data[nodalDofManager.getLocalDOF(node_lid,offset+j)] = access(field_array,j,i);
+      data[nodalDofManager.getLocalDOF(node_lid,offset+j)] = field_view(i,j);
     }
   }
 }
@@ -111,32 +67,28 @@ saveVector(const Thyra_Vector& field_thyra,
            const NodalDOFManager& nodalDofManager,
            const int offset)
 {
-  constexpr int rank = getRank<FieldType>();
-  TEUCHOS_TEST_FOR_EXCEPTION(rank!=0 && rank!=1, std::runtime_error,
-                             "Error! Can only handle ScalarFieldType and VectorFieldType for now.\n");
+  constexpr int rank = FieldRank<FieldType>::n;
+  static_assert(rank==0 || rank==1,
+                "Error! Can only handle ScalarFieldType and VectorFieldType for now.\n");
 
-  BucketArray<FieldType> field_array(field_stk, bucket);
+  using ScalarT = typename FieldScalar<FieldType>::type;
+  using ViewT = Kokkos::View<ScalarT**,Kokkos::LayoutRight,Kokkos::HostSpace>;
+  
+  auto stk_data = stk::mesh::field_data(field_stk,bucket);
+  const int num_nodes_in_bucket = bucket.size();
+  const int num_vec_components = nodalDofManager.numComponents();
 
-  using SFT = AbstractSTKFieldContainer::ScalarFieldType;
-  constexpr bool is_SFT = std::is_same<FieldType,SFT>::value;
-  constexpr int nodes_dim = is_SFT ? 0 : 1;
+  ViewT field_view (stk_data,num_nodes_in_bucket,num_vec_components);
 
-  const int num_nodes_in_bucket = field_array.dimension(nodes_dim);
-
-  const stk::mesh::BulkData& mesh = field_stk.get_mesh();
+  const auto& mesh = field_stk.get_mesh();
   auto data = getLocalData(field_thyra);
-  int num_vec_components;
-  //IKT, FIXME: ideally nodalDofManager.numComponents() should return 1 for a SFT, I would think.
-  //Need to look into this more to come up with a better fix, hopefully.  
-  if (is_SFT == true) num_vec_components = 1; 
-  else num_vec_components = nodalDofManager.numComponents();
 
   for(int i=0; i<num_nodes_in_bucket; ++i) {
     const GO node_gid = mesh.identifier(bucket[i]) - 1;
     const LO node_lid = indexer->getLocalElement(node_gid);
 
     for(int j = 0; j<num_vec_components; ++j) {
-      access(field_array,j,i) = data[nodalDofManager.getLocalDOF(node_lid,offset+j)];
+      field_view(i,j) = data[nodalDofManager.getLocalDOF(node_lid,offset+j)];
     }
   }
 }
@@ -144,43 +96,41 @@ saveVector(const Thyra_Vector& field_thyra,
 template<class FieldType>
 void STKFieldContainerHelper<FieldType>::
 copySTKField(const FieldType& source,
-             FieldType& target)
+                   FieldType& target)
 {
-  constexpr int rank = getRank<FieldType>();
-  TEUCHOS_TEST_FOR_EXCEPTION(rank!=0 && rank!=1, std::runtime_error,
-                             "Error! Can only handle ScalarFieldType and VectorFieldType for now.\n");
+  constexpr int rank = FieldRank<FieldType>::n;
+  static_assert(rank==0 || rank==1,
+                "Error! Can only handle ScalarFieldType and VectorFieldType for now.\n");
+
+  using ScalarT = typename FieldScalar<FieldType>::type;
+  using SrcViewT = Kokkos::View<const ScalarT**,Kokkos::LayoutRight,Kokkos::HostSpace>;
+  using TgtViewT = Kokkos::View<      ScalarT**,Kokkos::LayoutRight,Kokkos::HostSpace>;
 
   const stk::mesh::BulkData&     mesh = source.get_mesh();
   const stk::mesh::BucketVector& bv   = mesh.buckets(stk::topology::NODE_RANK);
 
-  using SFT = AbstractSTKFieldContainer::ScalarFieldType;
-  constexpr bool is_SFT = std::is_same<FieldType,SFT>::value;
-  constexpr int nodes_dim = is_SFT ? 0 : 1;
-
   for (auto it : bv) {
     const stk::mesh::Bucket& bucket = *it;
-
-    BucketArray<FieldType> source_array(source, bucket);
-    BucketArray<FieldType> target_array(target, bucket);
-
-    const int num_source_components = is_SFT ? 1 : source_array.dimension(0);
-    const int num_target_components = is_SFT ? 1 : target_array.dimension(0);
-    const int num_nodes_in_bucket   = source_array.dimension(nodes_dim);
+    const int num_nodes_in_bucket = bucket.size();
+    const int num_source_components = stk::mesh::field_scalars_per_entity(target, bucket);
+    const int num_target_components = stk::mesh::field_scalars_per_entity(target, bucket);
 
     const int uneven_downsampling = num_source_components % num_target_components;
+    TEUCHOS_TEST_FOR_EXCEPTION(uneven_downsampling, std::logic_error,
+        "Error in stk fields: specification of coordinate vector vs. solution layout is incorrect.\n");
 
-    TEUCHOS_TEST_FOR_EXCEPTION((uneven_downsampling) ||
-                               (num_nodes_in_bucket != target_array.dimension(nodes_dim)),
-                               std::logic_error,
-                               "Error in stk fields: specification of coordinate vector vs. solution layout is incorrect."
-                               << std::endl);
+    auto src_stk_data = stk::mesh::field_data(source,bucket);
+    auto tgt_stk_data = stk::mesh::field_data(target,bucket);
+
+    SrcViewT src_view (src_stk_data,num_nodes_in_bucket,num_source_components);
+    TgtViewT tgt_view (tgt_stk_data,num_nodes_in_bucket,num_target_components);
 
     for(int i=0; i<num_nodes_in_bucket; ++i) {
       // In source, j varies over neq (num phys vectors * numDim)
       // We want target to only vary over the first numDim components
       // Not sure how to do this generally...
       for(int j=0; j<num_target_components; ++j) {
-        access(target_array, j, i) = access(source_array, j, i);
+        tgt_view(i,j) = src_view(i,j);
       }
     }
   }
