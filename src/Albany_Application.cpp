1b3801e97b4563388acd5363401ed65306d0d249
kliegeois <kimliegeois@ymail.com>
2021-07-21 11:33:00 -0600

    Update the comments

--- src/Albany_Application.cpp
+++ src/Albany_Application.cpp
@@ -2384,9 +2384,6 @@ Application::evaluateResponseDistParamHessian_pp(
   Teuchos::ParameterList coloring_params;
   std::string matrixType = "Hessian";
   coloring_params.set("matrixType", matrixType);
-
-  // Even if the Hessian is symmetric, the distributed crs matrix
-  // is not symmetrical as the row and column map are not identical.
   coloring_params.set("symmetric", true);
 
   // Get the crs Hessian:
