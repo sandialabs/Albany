42105a5b59ca224a4285006257263f73fae85711
Luca Bertagna <lbertag@sandia.gov>
2021-08-09 11:15:21 -0600

    Minor improvement in the creation of a Tpetra fe matrix
    See #708 for a discussion on the change.

--- src/utility/Albany_ThyraCrsMatrixFactory.cpp
+++ src/utility/Albany_ThyraCrsMatrixFactory.cpp
@@ -310,10 +310,8 @@ Teuchos::RCP<Thyra_LinearOp> ThyraCrsMatrixFactory::createOp (const bool ignoreN
       // If the Thyra LinearOp is created with the matrix in assembly mode,
       // its range/domain vs's would be the overlapped ones.
       fe_matrix->beginAssembly();
-      fe_matrix->endAssembly();
-      fe_matrix->beginModify();
       fe_matrix->setAllToScalar(zero);
-      fe_matrix->endModify();
+      fe_matrix->endAssembly();
       matrix = fe_matrix;
     }
     op = createThyraLinearOp(Teuchos::rcp_implicit_cast<Tpetra_Operator>(matrix));
