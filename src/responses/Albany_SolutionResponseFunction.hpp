ec361475bfa5141296d366569838ce8c77f97010
Jerry Watkins <jwatkin@sandia.gov>
2021-08-18 15:17:35 -0600

    Match derivative dimensions with sfad size

--- src/responses/Albany_SolutionResponseFunction.hpp
+++ src/responses/Albany_SolutionResponseFunction.hpp
@@ -136,7 +136,7 @@ public:
     const Teuchos::RCP<Thyra_MultiVector>& Hv_dp) override;
   //@}
 
-  void printResponse(Teuchos::RCP<Teuchos::FancyOStream> out);
+  void printResponse(Teuchos::RCP<Teuchos::FancyOStream> out) override;
 
 protected:
   
