b3668dc2547d41b7fe0d56229e1c4003bba853c7
Irina Tezaur <ikalash@camobap.ca.sandia.gov>
2021-11-04 10:13:53 -0700

    Correcting link to cdash site in Cori nightly.

--- doc/dashboards/cori.nersc.gov/process_results_cismAlbany.sh
+++ doc/dashboards/cori.nersc.gov/process_results_cismAlbany.sh
@@ -11,6 +11,6 @@ cat results3 results4 >& results5
 grep "(Failed)" nightly_log_coriCismAlbanyRun.txt >& results6 
 cat results5 results6 >& results_coriCismAlbany
 echo "" >> results_coriCismAlbany 
-echo "The Albany CDash site can be accessed here: https://sems-cdash-son.sandia.gov/cdash/index.php?project=Albany" >> results_coriCismAlbany
+echo "The Albany CDash site can be accessed here: https://my.cdash.org/index.php?project=Albany" >> results_coriCismAlbany
 echo "" >> results_coriCismAlbany
 rm results0 results1 results11 results2 results3 results4 results5 results6
