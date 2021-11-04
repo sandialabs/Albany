4432d2690435f30f6be09e3ee358beb694f9c731
Irina K. Tezaur <ikalash@sandia.gov>
2021-11-03 23:09:46 -0600

    Updating skybridge nightly to send summary email, while build is not pushing to CDash site.

--- doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
+++ doc/dashboards/skybridge-login5.sandia.gov/build-codes-nightly.sh
@@ -4,14 +4,14 @@ export TEST_DIR=`pwd`
 rm -rf *log 
 rm -rf Albany
 rm -rf Trilinos
-rm -rf modules.out
 git clone git@github.com:trilinos/Trilinos.git >& trilinos-clone.log
 cd Trilinos
 git checkout develop
 cd ..
 git clone git@github.com:sandialabs/Albany.git >& albany-clone.log 
 module use --append $TEST_DIR/Albany/doc/LandIce/modulefiles
-module load serial-intel-release >& modules.out 
+module load serial-intel-release
+module load python/3.4
 rm -rf trilinos-build-serial-intel-release
 rm -rf trilinos-install-serial-intel-release
 rm -rf albany-build-serial-intel-release
@@ -22,3 +22,4 @@ mv trilinos-serial-intel-release.log trilinos-serial-intel-release-build.log
 ./clean-update-config-build-dash.sh albany 8
 mv albany-serial-intel-release.log albany-serial-intel-release-build.log 
 sbatch batch.openmpi.bash
+source send_email.sh
