212dbc43c866d5162676fe50ce3f5e515377fca2
Jerry Watkins <jwatkin@sandia.gov>
2021-11-17 17:51:39 -0700

    Update ALI summit build files

--- doc/summit/LandIce/summit_gcc_modules.sh
+++ doc/summit/LandIce/summit_gcc_modules.sh
@@ -3,14 +3,16 @@ module purge
 module load hsi xalt lsf-tools darshan-runtime DefApps
 module unload spectrum-mpi
 module unload xl
-module load gcc/6.4.0
-module load spectrum-mpi/10.3.0.1-20190611
-module load git/2.20.1 cmake/3.15.2
-module load boost/1.66.0
-module load hdf5/1.10.3
-module load netcdf/4.6.1
-module load parallel-netcdf/1.8.1
-module load netlib-lapack/3.8.0
-module load cuda/10.1.168
+module load gcc/9.1.0
+module load spectrum-mpi/10.4.0.3-20210112
+module load git cmake
+module load boost/1.76.0
+module load hdf5/1.10.7
+module load netcdf-c/4.8.0
+module load netcdf-fortran/4.4.5
+module load parallel-netcdf/1.12.2
+module load netlib-lapack/3.9.1
+#module load cuda/10.1.243 # doesn't work with gcc 9.1.0
+module load cuda/11.0.3
 module list
 
