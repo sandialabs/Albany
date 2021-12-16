213c1307c31ae58eaa4436d5cd3dbb553b5848b4
Irina K. Tezaur <ikalash@sandia.gov>
2021-12-15 22:11:26 -0700

    Updating spack build nightly to stage build in /scratch space instead of /tmp, which runs out of space.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/run_spack.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/run_spack.sh
@@ -1,11 +1,14 @@
 
 
 cd spack
+#export http_proxy=http://wwwproxy.sandia.gov:80
+#export https_proxy=http://wwwproxy.sandia.gov:80
 export http_proxy=http://proxy.sandia.gov:80
 export https_proxy=http://proxy.sandia.gov:80
 . share/spack/setup-env.sh
 module load cde/dev/compiler/gcc/7.2.0
 spack compiler find
+cp ../config.yaml etc/spack/defaults/config.yaml
 spack --insecure install --dirty --keep-stage libtool%gcc@7.2.0 >& spack_libtool.out
 sleep 5m
 spack --insecure install --dirty --keep-stage diffutils%gcc@7.2.0 >& spack_diffutils.out
