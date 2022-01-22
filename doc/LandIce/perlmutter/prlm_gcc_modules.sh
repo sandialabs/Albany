e2d2b73a68ccc2dda67af4c1c407a7723a2262bf
Jerry Watkins <jwatkin@sandia.gov>
2022-01-21 16:07:08 -0800

    Update perlmutter modules

--- doc/LandIce/perlmutter/prlm_gcc_modules.sh
+++ doc/LandIce/perlmutter/prlm_gcc_modules.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 module load PrgEnv-gnu
+#module load cudatoolkit/21.3_11.2
+module load cudatoolkit/21.9_11.0
+#module load cudatoolkit/21.9_11.4 # STK T0 error
 module load cpe-cuda # get correct cuda-gcc compiler
 module load cmake
 module load cray-hdf5-parallel
 module load cray-netcdf-hdf5parallel
 module load cray-parallel-netcdf
-module swap cuda/11.3.0 cuda/11.1.1
+#module swap cuda/11.3.0 cuda/11.1.1
+module load craype-accel-nvidia80
 module list
 
