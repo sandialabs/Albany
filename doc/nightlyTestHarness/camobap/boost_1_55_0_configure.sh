439036ce106df64e9716cccc89280215e980a6c2
Irina Tezaur <ikalash@sandia.gov>
2022-01-26 11:51:36 -0800

    Edits to camobap configure scripts.

--- doc/nightlyTestHarness/camobap/boost_1_55_0_configure.sh
+++ doc/nightlyTestHarness/camobap/boost_1_55_0_configure.sh
@@ -1,3 +1,3 @@
-echo "using mpi : /usr/lib64/openmpi/bin/mpicxx ;" >> ./tools/build/v2/user-config.jam
+echo "using mpi : /nightlyAlbanyTests/spack/opt/spack/linux-rhel8-haswell/gcc-11.1.0/openmpi-4.1.2-w65ftkfoqlafeb3nlcngwixdd4dwmkof/bin/mpicxx ;" >> ./tools/build/v2/user-config.jam
 #  echo "using gcc : /usr/bin/g++ ;" >> ./tools/build/v2/user-config.jam
-  ./bootstrap.sh --with-libraries=signals,regex,filesystem,system,mpi,serialization,thread,program_options,exception --prefix=/home/ikalash/nightlyAlbanyCDash/tpls-install
+  ./bootstrap.sh --with-libraries=signals,regex,filesystem,system,mpi,serialization,thread,program_options,exception --prefix=/tpls/install
--- doc/nightlyTestHarness/camobap/swig-3.0.11-configure.sh
+++ doc/nightlyTestHarness/camobap/swig-3.0.11-configure.sh
@@ -1,2 +1,2 @@
-./configure CC=/usr/lib64/openmpi/bin/mpicc FC=/usr/lib64/openmpi/bin/mpifort CXX=/usr/lib64/openmpi/bin/mpicxx \
-      --prefix=/nightlyCDash/albany-tpls-gcc-11.1.1-openmpi-4.1.0/swig --with-python3=/usr/bin/python3 --without-pcre 
+./configure CC=/tpls/install/bin/mpicc FC=/tpls/install/bin/mpifort CXX=/tpls/install/bin/mpicxx \
+      --prefix=/tpls/install --with-python3=/usr/bin/python3 --without-pcre  
