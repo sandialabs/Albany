6d5c91130790095f56986c94559fd578a8789f37
kliegeois <kimliegeois@ymail.com>
2022-01-27 14:16:51 -0700

    Change the other strides

--- src/disc/stk/Albany_ExtrudedSTKMeshStruct.cpp
+++ src/disc/stk/Albany_ExtrudedSTKMeshStruct.cpp
@@ -326,9 +326,9 @@ void Albany::ExtrudedSTKMeshStruct::setBulkData(
   }
   Teuchos::RCP<const Tpetra_Map> sides_map = Tpetra::createNonContigMapWithNode<LO, Tpetra_GO, KokkosNode>(indices(),comm);
 
-  GO globalElemStride = cells_map->getMaxAllGlobalIndex() - cells_map->getMinAllGlobalIndex() + 1;
+  GO globalElemStride     = cells_map->getMaxAllGlobalIndex() + 1;
   GO globalVerticesStride = nodes_map->getMaxAllGlobalIndex() + 1;
-  GO globalSidesStride    = sides_map->getMaxAllGlobalIndex() - sides_map->getMinAllGlobalIndex() + 1;
+  GO globalSidesStride    = sides_map->getMaxAllGlobalIndex() + 1;
 
   GO elemColumnShift     = (Ordering == COLUMN) ? 1 : globalElemStride;
   int lElemColumnShift   = (Ordering == COLUMN) ? 1 : cells2D.size();
