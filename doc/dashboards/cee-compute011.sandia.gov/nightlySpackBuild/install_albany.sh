8779b07773128163aff0a7ade9e4aeecd2646d5e
Irina K. Tezaur <ikalash@sandia.gov>
2022-03-01 21:21:47 -0700

    Edits to spack nightly test scripts.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/install_albany.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/install_albany.sh
@@ -14,11 +14,3 @@ sleep 5m
 spack --insecure install --dirty --keep-stage xz%gcc@7.2.0 >& spack_xz.out
 sleep 5m
 spack --insecure install --dirty --keep-stage albany%gcc@7.2.0+mpas >& spack_albany.out
-#spack cd albany >& /projects/albany/nightlySpackBuild/spack_cd_albany.out
-#cp cd_spack_build_dir.sh /scratch/albany/nightlySpackBuild/spack-stage/ikalash/spack-stage-albany-develop* 
-#cd /scratch/albany/nightlySpackBuild/spack-stage/ikalash/spack-stage-albany-develop* >& /projects/albany/nightlySpackBuild/spack_cd_albany.out
-#source cd_spack_build_dir.sh
-#ctest -V >& spack_ctest.out
-#mv spack_ctest.out /projects/albany/nightlySpackBuild/spack_ctest.out
-#cd /projects/albany/nightlySpackBuild
-#source /projects/albany/nightlySpackBuild/postproc_spack.sh
