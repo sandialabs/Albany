99d8154931db838424c007b1cb1d60963d00b806
Irina Tezaur <ikalash@sandia.gov>
2022-03-08 09:23:41 -0800

    Fixing camobap nightly scripts with ninja.

--- doc/nightlyTestHarness/camobap/build_trilinos_all.sh
+++ doc/nightlyTestHarness/camobap/build_trilinos_all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PATH=$PATH:/tpls/install/ninja/build-cmake
 ./run_trilinos.sh set_irinas_env.in MPI >& out #Trilinos with Serial KokkosNode
 ./run_trilinos_openmp.sh set_irinas_env.in MPI >& out_openMP  #Trilinos with OpenMP KokkosNode 
 ./run_trilinos_debug.sh set_irinas_env.in MPI >& out_Debug  #Trilinos with Serial KokkosNode + dbg build 
