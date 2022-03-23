e6c687fd7eec345f8a56f90a82cfcf6a65d7c267
Irina K. Tezaur <ikalash@sandia.gov>
2022-03-22 22:07:31 -0600

    Updates to spack nightly test scripts.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/preproc_spack.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/preproc_spack.sh
@@ -7,11 +7,11 @@ rm -rf spack_libiconv.out
 rm -rf spack 
 rm -rf spack_ctest.out 
 rm -rf results_spack 
-rm -rf /tmp/ikalash/spack*
-
+rm -rf /tmp/ikalash/spack-stage
+rm -rf a b c cb 
 echo "Cloning spack repo..."
-git clone git@github.com:SNLComputation/spack.git
+git clone git@github.com:E3SM-Project/spack.git
 cd spack
-git checkout albany
+git checkout develop
 cd ..
 echo "...done."
