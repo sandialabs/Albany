65dc66dbe3c5f868fbd16bd723fedf5fce66358a
Irina K. Tezaur <ikalash@sandia.gov>
2022-04-22 23:55:57 -0600

    Updating spack nightly build scripts.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/install_albany.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/install_albany.sh
@@ -4,13 +4,14 @@ cd spack
 export http_proxy=http://proxy.sandia.gov:80
 export https_proxy=http://proxy.sandia.gov:80
 . share/spack/setup-env.sh
-module load cde/dev/compiler/gcc/7.2.0
+module load sems-env
+module load sems-gcc/9.2.0
 spack compiler find
 cp ../config.yaml etc/spack/defaults/config.yaml
-spack --insecure install --dirty --keep-stage libtool%gcc@7.2.0 >& spack_libtool.out
+spack --insecure install --dirty --keep-stage libtool%gcc@9.2.0 >& spack_libtool.out
 sleep 5m
-spack --insecure install --dirty --keep-stage diffutils%gcc@7.2.0 >& spack_diffutils.out
+spack --insecure install --dirty --keep-stage diffutils%gcc@9.2.0 >& spack_diffutils.out
 sleep 5m
-spack --insecure install --dirty --keep-stage xz%gcc@7.2.0 >& spack_xz.out
+spack --insecure install --dirty --keep-stage xz%gcc@9.2.0 >& spack_xz.out
 sleep 5m
-spack --insecure install --dirty --keep-stage albany%gcc@7.2.0+mpas >& spack_albany.out
+spack --insecure install --dirty --keep-stage albany%gcc@9.2.0+mpas >& spack_albany.out
