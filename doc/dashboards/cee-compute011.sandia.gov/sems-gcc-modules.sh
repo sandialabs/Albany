c0020fe6e0d90853f5d9c87555156f7994d58e92
Irina K. Tezaur <ikalash@sandia.gov>
2022-04-24 12:39:55 -0600

    Fixing cmake version on CEE.

--- doc/dashboards/cee-compute011.sandia.gov/sems-gcc-modules.sh
+++ doc/dashboards/cee-compute011.sandia.gov/sems-gcc-modules.sh
@@ -3,7 +3,7 @@ module purge
 module load sems-env
 module load sems-gcc/9.2.0
 module load sems-openmpi/1.10.1 
-module load cde/dev/cmake/3.19.2
+module load sems-cmake/3.19.1
 module load sems-boost/1.55.0/base
 module load sems-netcdf/4.4.1/exo_parallel 
 module unload sems-python
