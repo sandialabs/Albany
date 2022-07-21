9f5a12bcc5cddea5f949711375a32c3f6acedc70
Irina Kalashnikova Tezaur <ikalash@mockba.ca.sandia.gov>
2022-07-20 18:10:09 -0700

    Creating Epetra-free build on mockba, which will be added to the nightly test harness.

---
+++ doc/dashboards/mockba.ca.sandia.gov/nightly_cron_script_albany_no_epetra.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd /home/ikalash/nightlyAlbanyCDash
+
+#rm -rf repos
+#rm -rf build
+#rm -rf ctest_nightly.cmake.work
+#rm -rf nightly_log*
+#rm -rf results*
+#rm -rf *out 
+
+source mockba_modules.sh >& modules.out 
+LOG_FILE=/home/ikalash/nightlyAlbanyCDash/nightly_log_no_epetra.txt
+
+unset HTTPS_PROXY
+unset HTTP_PROXY
+#export http_proxy=proxy.ca.sandia.gov:80
+#export https_proxy=proxy.ca.sandia.gov:80
+
+#env | grep -i proxy 
+
+#Hack for Albany block disc unit tests + CISM 
+alias python=/projects/sems/install/rhel7-x86_64/sems/compiler/python/2.7.9/bin/python
+alias python3=/projects/sems/install/rhel7-x86_64/sems/compiler/python/2.7.9/bin/python
+
+eval "env TEST_DIRECTORY=/home/ikalash/nightlyAlbanyCDash SCRIPT_DIRECTORY=/home/ikalash/nightlyAlbanyCDash ctest -VV -S /home/ikalash/nightlyAlbanyCDash/ctest_nightly_albany_no_epetra.cmake" > $LOG_FILE 2>&1
