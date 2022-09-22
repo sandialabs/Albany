77246c79d6139828fadc5d79b4a73e1831d9119a
kliegeois <kimliegeois@ymail.com>
2022-09-22 15:04:05 -0600

    Fix PyAlbany timers

--- PyAlbany/src/Albany_Pybind11_Timer.cpp
+++ PyAlbany/src/Albany_Pybind11_Timer.cpp
@@ -15,9 +15,13 @@ Teuchos::Time createRCPTime(const std::string name) {
 void pyalbany_time(pybind11::module &m) {
     py::class_<Teuchos::Time, Teuchos::RCP<Teuchos::Time>>(m, "Time")
         .def(py::init(&createRCPTime))
-        .def("totalElapsedTime", &Teuchos::Time::totalElapsedTime)
+        .def("totalElapsedTime",[](Teuchos::Time &m, bool readCurrentTime){
+            return m.totalElapsedTime(readCurrentTime);
+        }, py::arg("readCurrentTime") = false)
         .def("name", &Teuchos::Time::name)
-        .def("start", &Teuchos::Time::start)
+        .def("start",[](Teuchos::Time &m, bool reset){
+            return m.start(reset);
+        }, py::arg("reset") = false)
         .def("stop", &Teuchos::Time::stop);
 
     py::class_<Teuchos::StackedTimer, Teuchos::RCP<Teuchos::StackedTimer>>(m, "StackedTimer")
