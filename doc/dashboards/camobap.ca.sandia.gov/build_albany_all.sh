207c189f904dd4dd4bc73540633820d38031f0e6
Irina Tezaur <ikalash@sandia.gov>
2022-09-21 18:56:20 -0700

    Removing extended scalar builds from nightly test harness.
    I don't have the bandwidth or funding to maintain these anymore.

--- doc/dashboards/camobap.ca.sandia.gov/build_albany_all.sh
+++ doc/dashboards/camobap.ca.sandia.gov/build_albany_all.sh
@@ -9,8 +9,8 @@ bash nightly_cron_script_cismAlbany.sh; bash process_results_cismAlbany.sh #CISM
 bash nightly_cron_script_albanyT.sh; bash process_resultsT.sh #Albany, No Epetra
 bash nightly_cron_script_albanyKokkosNodeOpenMP.sh; bash process_results_functor_openMP.sh #Albany, OpenMP KokkosNode
 bash nightly_cron_script_albany_fpe.sh; bash process_results_fpe.sh #Albany FPE check on
-bash nightly_cron_script_trilinos.sh #Trilinos with extended scalar types                                         
-bash nightly_cron_script_trilinos_eti_ld.sh #Trilinos with long double and ETI 
-bash nightly_cron_script_alegra-xfem.sh; bash process_results_alegra-xfem.sh; bash send_email_alegra-xfem.sh
-bash process_results_alegra-xfem_eti.sh; bash send_email_alegra-xfem_eti.sh
+#bash nightly_cron_script_trilinos.sh #Trilinos with extended scalar types                                         
+#bash nightly_cron_script_trilinos_eti_ld.sh #Trilinos with long double and ETI 
+#bash nightly_cron_script_alegra-xfem.sh; bash process_results_alegra-xfem.sh; bash send_email_alegra-xfem.sh
+#bash process_results_alegra-xfem_eti.sh; bash send_email_alegra-xfem_eti.sh
 ~                                              
