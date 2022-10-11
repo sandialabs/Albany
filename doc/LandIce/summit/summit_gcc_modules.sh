5b57d67da3b629f62f496f70d7dfbc8e3221c28c
Jerry Watkins <watkins2@stanford.edu>
2022-10-11 13:59:26 -0400

    Update LandIce summit scripts

--- doc/LandIce/summit/summit_gcc_modules.sh
+++ doc/LandIce/summit/summit_gcc_modules.sh
@@ -6,9 +6,11 @@ module unload xl
 module load gcc/9.1.0
 module load spectrum-mpi/10.4.0.3-20210112
 module load git cmake
-module load boost/1.76.0
+#module load boost/1.76.0
+module load boost/1.77.0
 module load hdf5/1.10.7
-module load netcdf-c/4.8.0
+#module load netcdf-c/4.8.0
+module load netcdf-c/4.8.1
 module load netcdf-fortran/4.4.5
 module load parallel-netcdf/1.12.2
 module load netlib-lapack/3.9.1
