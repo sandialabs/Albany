260b10103d4405a4613c1f2494635d42ad497353
mcarlson801 <maxx.l.carlson@gmail.com>
2022-10-12 14:28:05 -0700

    Update perlmutter modules to match E3SM, updated Trilinos configs to use ZEN3 instead of ZEN2

---
+++ doc/LandIce/perlmutter/pm_cpu_amdclang_modules.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+module unload cray-hdf5-parallel
+module unload cray-netcdf-hdf5parallel
+module unload cray-parallel-netcdf
+module unload PrgEnv-gnu
+module unload PrgEnv-nvidia
+module unload cudatoolkit
+module unload craype-accel-nvidia80
+module unload craype-accel-host
+module unload perftools-base
+module unload perftools
+module unload darshan
+
+module load PrgEnv-aocc
+module load aocc/3.2.0
+module load craype-accel-host
+module load cray-libsci
+module load craype
+module load cray-mpich/8.1.15
+module load cray-hdf5-parallel/1.12.1.1
+module load cray-netcdf-hdf5parallel/4.8.1.1
+module load cray-parallel-netcdf/1.12.2.1
+module load cmake/3.22.0
+module load boost
+
+export MPICH_ENV_DISPLAY=1
+export MPICH_VERSION_DISPLAY=1
+export OMP_STACKSIZE=128M
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export HDF5_USE_FILE_LOCKING=FALSE
+export MPICH_GPU_SUPPORT_ENABLED=0
+export FI_CXI_RX_MATCH_MODE=software
