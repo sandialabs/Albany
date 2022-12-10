ffc729f543c746f557cb7c8a965233e2fe106aaa
Irina Tezaur <ikalash@sandia.gov>
2022-12-09 22:05:06 -0800

    Updating scripts.

--- doc/dashboards/camobap.ca.sandia.gov/create-new-cdash-cmake-script.sh
+++ doc/dashboards/camobap.ca.sandia.gov/create-new-cdash-cmake-script.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-sed -e '/CDASH-TRILINOS-FILE.TXT/r /nightlyCDash/cdash-frag.txt' -e '/CDASH-TRILINOS-FILE.TXT/d' /nightlyCDash/ctest_nightly_trilinos_tmp.cmake >& /nightlyCDash/ctest_nightly_trilinos.cmake
+if [ "$1" == "" ]; then
+  echo "Argument 1 (case num) not provided!" 
+  exit 0
+fi
+
+build=$1
+
+if [ $build = "regular" ]; then
+  name="albany"
+fi
+if [ $build = "no-epetra" ]; then
+  name="albanyT"
+fi
+if [ $build = "fpe" ]; then
+  name="albanyFPE"
+fi
+if [ $build = "openmp" ]; then
+  name="albanyOpenmp"
+fi
+if [ $build = "cali" ]; then
+  name="cismAlbany"
+fi
+
+sed -e "/CDASH-ALBANY-FILE.TXT/r /nightlyCDash/cdash-$name-frag.txt" -e "/CDASH-ALBANY-FILE.TXT/d" "/nightlyCDash/ctest_nightly_"$name"_tmp.cmake" >& /nightlyCDash/ctest_nightly_$name.cmake
