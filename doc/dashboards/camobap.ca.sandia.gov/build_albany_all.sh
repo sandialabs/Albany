b5b8b03e19240fd33f5c70efbfc45c8e774d8fb0
Irina Tezaur <ikalash@sandia.gov>
2022-12-12 16:52:18 -0800

    Updating scripts on camobap to hopefully prevent openmp build from taking a ridiculously long amount of time.

--- doc/dashboards/camobap.ca.sandia.gov/build_albany_all.sh
+++ doc/dashboards/camobap.ca.sandia.gov/build_albany_all.sh
@@ -7,8 +7,8 @@ rm -rf results*
 bash nightly_cron_script_albany.sh #Albany
 bash nightly_cron_script_cismAlbany.sh #CISM-Albany
 bash nightly_cron_script_albanyT.sh #Albany, No Epetra
-bash nightly_cron_script_albany_openmp.sh #Albany, OpenMP KokkosNode
 bash nightly_cron_script_albany_fpe.sh #Albany FPE check on
+bash nightly_cron_script_albany_openmp.sh #Albany, OpenMP KokkosNode
 #bash nightly_cron_script_trilinos.sh #Trilinos with extended scalar types                                         
 #bash nightly_cron_script_trilinos_eti_ld.sh #Trilinos with long double and ETI 
 #bash nightly_cron_script_alegra-xfem.sh; bash process_results_alegra-xfem.sh; bash send_email_alegra-xfem.sh
