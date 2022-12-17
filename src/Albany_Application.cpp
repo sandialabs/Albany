ec6cd2ba0870e8d87952e64c30c6c28d21cf1506
Luca Bertagna <lbertag@sandia.gov>
2022-12-16 23:14:59 -0700

    Remove try-catch block when creating dist params in Application

--- src/Albany_Application.cpp
+++ src/Albany_Application.cpp
@@ -652,73 +652,70 @@ Application::finalSetUp(
       disc,
       comm));
 
-  try {
-    // Create Distributed parameters and initialize them with data stored in the
-    // mesh.
-    const StateInfoStruct& distParamSIS = disc->getNodalParameterSIS();
-    for (const auto& sis : distParamSIS) {
-      // Get name of distributed parameter
-      const std::string& param_name = sis->name;
+  // Create Distributed parameters and initialize them with data stored in the
+  // mesh.
+  const StateInfoStruct& distParamSIS = disc->getNodalParameterSIS();
+  for (const auto& sis : distParamSIS) {
+    // Get name of distributed parameter
+    const std::string& param_name = sis->name;
 
-      // Get parameter vector spaces and build parameter vector
-      // Create distributed parameter and set workset_elem_dofs
-      auto parameter = Teuchos::rcp(new DistributedParameter(
-          param_name, disc->getNewDOFManager(param_name)));
+    // Get parameter vector spaces and build parameter vector
+    // Create distributed parameter and set workset_elem_dofs
+    auto parameter = Teuchos::rcp(new DistributedParameter(
+        param_name, disc->getNewDOFManager(param_name)));
 
-      // Get the vector and lower/upper bounds, and fill them with available
-      // data
-      Teuchos::RCP<Thyra_Vector> dist_param = parameter->vector();
-      Teuchos::RCP<Thyra_Vector> dist_param_lowerbound =
-          parameter->lower_bounds_vector();
-      Teuchos::RCP<Thyra_Vector> dist_param_upperbound =
-          parameter->upper_bounds_vector();
+    // Get the vector and lower/upper bounds, and fill them with available
+    // data
+    Teuchos::RCP<Thyra_Vector> dist_param = parameter->vector();
+    Teuchos::RCP<Thyra_Vector> dist_param_lowerbound =
+        parameter->lower_bounds_vector();
+    Teuchos::RCP<Thyra_Vector> dist_param_upperbound =
+        parameter->upper_bounds_vector();
 
-      std::stringstream lowerbound_name, upperbound_name;
-      lowerbound_name << param_name << "_lowerbound";
-      upperbound_name << param_name << "_upperbound";
+    std::stringstream lowerbound_name, upperbound_name;
+    lowerbound_name << param_name << "_lowerbound";
+    upperbound_name << param_name << "_upperbound";
 
-      // Initialize parameter with data stored in the mesh
-      disc->getField(*dist_param, param_name);
-      const auto& nodal_param_states = disc->getNodalParameterSIS();
-      bool        has_lowerbound(false), has_upperbound(false);
-      for (int ist = 0; ist < static_cast<int>(nodal_param_states.size());
-           ist++) {
-        has_lowerbound = has_lowerbound || (nodal_param_states[ist]->name ==
-                                            lowerbound_name.str());
-        has_upperbound = has_upperbound || (nodal_param_states[ist]->name ==
-                                            upperbound_name.str());
-      }
-      if (has_lowerbound) {
-        disc->getField(*dist_param_lowerbound, lowerbound_name.str());
-      } else {
-        dist_param_lowerbound->assign(std::numeric_limits<ST>::lowest());
-      }
-      if (has_upperbound) {
-        disc->getField(*dist_param_upperbound, upperbound_name.str());
-      } else {
-        dist_param_upperbound->assign(std::numeric_limits<ST>::max());
-      }
-      // JR: for now, initialize to constant value from user input if requested.
-      // This needs to be generalized.
-      if (params->sublist("Problem").isType<Teuchos::ParameterList>(
-              "Topology Parameters")) {
-        Teuchos::ParameterList& topoParams =
-            params->sublist("Problem").sublist("Topology Parameters");
-        if (topoParams.isType<std::string>("Entity Type") &&
-            topoParams.isType<double>("Initial Value")) {
-          if (topoParams.get<std::string>("Entity Type") ==
-                  "Distributed Parameter" &&
-              topoParams.get<std::string>("Topology Name") == param_name) {
-            double initVal = topoParams.get<double>("Initial Value");
-            dist_param->assign(initVal);
-          }
+    // Initialize parameter with data stored in the mesh
+    disc->getField(*dist_param, param_name);
+    const auto& nodal_param_states = disc->getNodalParameterSIS();
+    bool        has_lowerbound(false), has_upperbound(false);
+    for (int ist = 0; ist < static_cast<int>(nodal_param_states.size());
+         ist++) {
+      has_lowerbound = has_lowerbound || (nodal_param_states[ist]->name ==
+                                          lowerbound_name.str());
+      has_upperbound = has_upperbound || (nodal_param_states[ist]->name ==
+                                          upperbound_name.str());
+    }
+    if (has_lowerbound) {
+      disc->getField(*dist_param_lowerbound, lowerbound_name.str());
+    } else {
+      dist_param_lowerbound->assign(std::numeric_limits<ST>::lowest());
+    }
+    if (has_upperbound) {
+      disc->getField(*dist_param_upperbound, upperbound_name.str());
+    } else {
+      dist_param_upperbound->assign(std::numeric_limits<ST>::max());
+    }
+    // JR: for now, initialize to constant value from user input if requested.
+    // This needs to be generalized.
+    if (params->sublist("Problem").isType<Teuchos::ParameterList>(
+            "Topology Parameters")) {
+      Teuchos::ParameterList& topoParams =
+          params->sublist("Problem").sublist("Topology Parameters");
+      if (topoParams.isType<std::string>("Entity Type") &&
+          topoParams.isType<double>("Initial Value")) {
+        if (topoParams.get<std::string>("Entity Type") ==
+                "Distributed Parameter" &&
+            topoParams.get<std::string>("Topology Name") == param_name) {
+          double initVal = topoParams.get<double>("Initial Value");
+          dist_param->assign(initVal);
         }
       }
-
-      // Add parameter to the distributed parameter library
-      distParamLib->add(parameter->name(), parameter);
     }
-  } catch (const std::logic_error&) {
+
+    // Add parameter to the distributed parameter library
+    distParamLib->add(parameter->name(), parameter);
   }
 
   // Now setup response functions (see note above)
