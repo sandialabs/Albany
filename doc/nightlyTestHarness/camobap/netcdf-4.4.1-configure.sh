42c29cfd7d3044d9f17f156dd60cdd594ea85e84
Irina Tezaur <ikalash@sandia.gov>
2022-12-23 16:09:22 -0800

    Enabling HDF5 in camobap nightly builds.  Adding build scripts for HDF5 and NetCDF with HDF5 enabled.

--- doc/nightlyTestHarness/camobap/netcdf-4.4.1-configure.sh
+++ doc/nightlyTestHarness/camobap/netcdf-4.4.1-configure.sh
@@ -1,6 +1,10 @@
-./configure CC=/usr/lib64/openmpi/bin/mpicc FC=/usr/lib64/openmpi/bin/mpifort CXX=/usr/lib64/openmpi/bin/mpicxx \
-      CXXFLAGS="-fPIC -O3 -march=native" \
-      CFLAGS="-fPIC -O3 -march=native" \
-      LDFLAGS="-fPIC -O3 -march=native" \
-      FCFLAGS="-fPIC -O3 -march=native" \
-      --prefix=/nightlyCDash/albany-tpls-gcc-11.1.1-openmpi-4.1.0 --disable-doxygen --disable-netcdf4 #--enable-netcdf4 #--enable-pnetcdf
+
+H5DIR=/nightlyCDash/albany-tpls-gcc-11.1.1-openmpi-4.1.0
+./configure CC=/tpls/install/bin/mpicc FC=/tpls/install/bin/mpifort CXX=/tpls/install/bin/mpicxx \
+      CXXFLAGS="-fPIC -O3 -march=native -I${H5DIR}/include" \
+      CPPFLAGS="-fPIC -O3 -march=native -I${H5DIR}/include" \
+      CFLAGS="-fPIC -O3 -march=native -I${H5DIR}/include" \
+      LDFLAGS="-fPIC -O3 -march=native -I${H5DIR}/include" \
+      FCFLAGS="-fPIC -O3 -march=native -I${H5DIR}/include" \
+      LDFLAGS="-L${H5DIR}/lib" \
+      --prefix=$H5DIR --disable-doxygen --disable-netcdf4 #--enable-netcdf4 #--enable-pnetcdf
