f4bc1c4bb1c16f8c75754dbf824750a867ee6471
mcarlson801 <maxx.l.carlson@gmail.com>
2023-01-03 08:52:14 -0800

    Updating perlmutter module scripts

--- doc/LandIce/perlmutter/pm_cpu_gnu_modules.sh
+++ doc/LandIce/perlmutter/pm_cpu_gnu_modules.sh
@@ -4,6 +4,7 @@ module unload cray-netcdf-hdf5parallel
 module unload cray-parallel-netcdf
 module unload PrgEnv-gnu
 module unload PrgEnv-nvidia
+module unload gpu
 module unload cudatoolkit
 module unload craype-accel-nvidia80
 module unload craype-accel-host
@@ -17,14 +18,14 @@ module load gcc/11.2.0
 module load craype-accel-host
 module load cray-libsci
 module load craype
-module load cray-mpich/8.1.15
-module load cray-hdf5-parallel/1.12.1.1
-module load cray-netcdf-hdf5parallel/4.8.1.1
-module load cray-parallel-netcdf/1.12.2.1
+module load cray-mpich/8.1.22
+module load cray-hdf5-parallel/1.12.2.1
+module load cray-netcdf-hdf5parallel/4.9.0.1
+module load cray-parallel-netcdf/1.12.3.1
 module load cmake/3.22.0
-module load boost
 
 module load e4s
+spack env activate -V e4s
 spack load superlu@5.3.0%gcc@11.2.0
 
 export MPICH_ENV_DISPLAY=1
