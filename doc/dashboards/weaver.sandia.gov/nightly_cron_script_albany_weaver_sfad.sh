71f18a3f2aae0eba54bb35bc2c0e78e21a941625
Jerry Watkins <jwatkin@sandia.gov>
2023-01-25 17:56:22 -0700

    Remove SFAD4 and SFAD8 builds

--- doc/dashboards/weaver.sandia.gov/nightly_cron_script_albany_weaver_sfad.sh
+++ doc/dashboards/weaver.sandia.gov/nightly_cron_script_albany_weaver_sfad.sh
@@ -6,7 +6,7 @@ cd $BASE_DIR
 BUILD_OPT="$1"
 
 if [ -z "$BUILD_OPT" ]; then
-   echo "Please supply an argument: sfad4, sfad6, sfad8, sfad12 or sfad24"
+   echo "Please supply an argument: sfad6, sfad12 or sfad24"
    exit 1;
 fi
 
@@ -19,14 +19,8 @@ export jenkins_trilinos_dir=/home/projects/albany/nightlyCDashWeaver/repos/Trili
 bash convert-cmake-to-cdash-albany.sh sfad
 bash create-new-cdash-cmake-script-albany.sh sfad
 
-if [ "$BUILD_OPT" = "sfad4" ] ; then
-  LOG_FILE=$BASE_DIR/nightly_log_weaverAlbanySFad4.txt
-fi
 if [ "$BUILD_OPT" = "sfad6" ] ; then
   LOG_FILE=$BASE_DIR/nightly_log_weaverAlbanySFad6.txt
-fi
-if [ "$BUILD_OPT" = "sfad8" ] ; then
-  LOG_FILE=$BASE_DIR/nightly_log_weaverAlbanySFad8.txt
 fi
 if [ "$BUILD_OPT" = "sfad12" ] ; then
   LOG_FILE=$BASE_DIR/nightly_log_weaverAlbanySFad12.txt
