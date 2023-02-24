bcf9e2630ddcc03bb5fac2402496cf29ca51aa8e
Irina Tezaur <ikalash@sandia.gov>
2023-02-24 13:49:13 -0800

    Updating cmake used to build Trilinos on camobap.

--- doc/nightlyTestHarness/camobap/build_trilinos_all.sh
+++ doc/nightlyTestHarness/camobap/build_trilinos_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export PATH=$PATH:/tpls/install/ninja/build-cmake
+export PATH=/home/ikalash/Downloads/cmake-3.26.0-rc4-linux-x86_64/bin:$PATH:/tpls/install/ninja/build-cmake
+rm -rf Results
 ./run_trilinos.sh set_irinas_env.in MPI >& out #Trilinos with Serial KokkosNode
 ./run_trilinos_openmp.sh set_irinas_env.in MPI >& out_openMP  #Trilinos with OpenMP KokkosNode 
 ./run_trilinos_debug.sh set_irinas_env.in MPI >& out_Debug  #Trilinos with Serial KokkosNode + dbg build 
