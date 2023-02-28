275f5995eda5bb0e7348216fd52b4baecc14cf11
Irina Tezaur <ikalash@sandia.gov>
2023-02-28 10:16:11 -0800

    Correcting cmake path in script.

--- doc/nightlyTestHarness/camobap/build_trilinos_all.sh
+++ doc/nightlyTestHarness/camobap/build_trilinos_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export PATH=/home/ikalash/Install/cmake-3.24.3/bin:$PATH:/tpls/install/ninja/build-cmake
+export PATH=/nightlyCDash/albany-tpls-gcc-11.1.1-openmpi-4.1.0/bin:$PATH:/tpls/install/ninja/build-cmake
+which cmake
+cmake --version
 rm -rf Results
 ./run_trilinos.sh set_irinas_env.in MPI >& out #Trilinos with Serial KokkosNode
 ./run_trilinos_openmp.sh set_irinas_env.in MPI >& out_openMP  #Trilinos with OpenMP KokkosNode 
