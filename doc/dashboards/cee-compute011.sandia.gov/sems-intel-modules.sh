de0c636f8c7c24df67af4cdbed8d283eef1f2c5f
Irina K. Tezaur <ikalash@sandia.gov>
2023-03-22 13:36:24 -0600

    Updating CEE intel nightlies to use different netcdf/pnetcdf module.
    Hopefully this will fix broken build starting tomorrow.

--- doc/dashboards/cee-compute011.sandia.gov/sems-intel-modules.sh
+++ doc/dashboards/cee-compute011.sandia.gov/sems-intel-modules.sh
@@ -9,8 +9,10 @@ module load sems-dev-ninja/1.10.1
 module load sems-dev-boost/1.70.0
 module load sems-dev-hdf5/1.10.7
 module load sems-dev-intel-mkl/2020.4.304
-module load sems-dev-netcdf-c/4.8.1-parallel
-module load sems-dev-parallel-netcdf/1.12.2
+#module load sems-dev-netcdf-c/4.8.1-parallel
+#module load sems-dev-parallel-netcdf/1.12.2
+module load sems-dev-netcdf-c/4.7.3
+module load sems-dev-parallel-netcdf/1.12.3 
 module load sems-dev-superlu/5.3.0
 module load sems-dev-zlib/1.2.11
 module list
