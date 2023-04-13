141e86c4d8a0619299e1ea976e61038d545e2892
Jerry Watkins <jwatkin@sandia.gov>
2023-04-13 08:34:01 -0700

    Delete blake_gcc_modules_submit.sh

--- doc/dashboards/blake.sandia.gov/blake_gcc_modules_submit.sh
+++
@@ -1,7 +0,0 @@
-module purge
-module load devpack/latest/openmpi/2.1.2/gcc/7.2.0
-module load openblas/0.2.20/gcc/7.2.0
-module unload cmake
-module load cmake/3.19.3
-module load python/3.7.3
-module list
