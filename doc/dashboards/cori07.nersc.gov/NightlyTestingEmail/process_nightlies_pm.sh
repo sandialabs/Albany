a50c196515c16fc8403a102d3a28df399109d65e
Irina Tezaur <ikalash@sandia.gov>
2023-05-10 19:54:23 -0700

    Updating post-proc scripts to correctly check for Perlmutter builds only every other Sat., to coincide with testing.

---
+++ doc/dashboards/cori07.nersc.gov/NightlyTestingEmail/process_nightlies_pm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -rf TriBITS
+rm -rf test_history
+rm -rf *html
+rm -rf *out
+rm -rf *json
+rm -rf  albanyNightlyBuildsTwoif.csv 
+
+git clone git@github.com:TriBITSPub/TriBITS.git
+
+
+now=$(date +"%Y-%m-%d")
+
+./albany_cdash_status_pm.sh --date=$now --email-from-address=ikalash@cori.nersc.gov --send-email-to=ikalash@sandia.gov,mperego@sandia.gov,jwatkin@sandia.gov,lbertag@sandia.gov,knliege@sandia.gov,maxcarl@sandia.gov 
+
+
