488d32350f58b4ed172a7fb2c3e2843e70c3aafd
mcarlson801 <maxx.l.carlson@gmail.com>
2023-05-15 14:29:01 -0600

    Updated weaver_modules_cuda.sh to reflect weaver module changes

--- doc/dashboards/weaver.sandia.gov/weaver_modules_cuda.sh
+++ doc/dashboards/weaver.sandia.gov/weaver_modules_cuda.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 module purge 
-module load git/2.31.1 cmake/3.21.2
-module load cuda/11.2.2/gcc/8.3.1
-module load openmpi/4.1.1/gcc/8.3.1/cuda/11.2.2
-module load openblas/0.3.18/gcc/8.3.1
-module load metis/5.1.0/gcc/8.3.1
-module load zlib/1.2.11/gcc/8.3.1
-module load hdf5/1.10.7/gcc/8.3.1/openmpi/4.1.1
-module load parallel-netcdf/1.12.2/gcc/8.3.1/openmpi/4.1.1
-module load netcdf-c/4.8.1/gcc/8.3.1/openmpi/4.1.1
-module load parmetis/4.0.3/gcc/8.3.1/openmpi/4.1.1
-module load boost/1.70.0/gcc/8.3.1
-module load superlu/5.3.0/gcc/8.3.1
-module load ucx/1.12.1/gcc/8.3.1
+module load gcc/8.3.1
+module load openmpi/4.1.4
+module load cuda/11.8.0
+module load git/2.39.1
+module load cmake/3.25.1
+module load python/3.10.8
+module load openblas/0.3.23
+module load metis/5.1.0
+module load zlib/1.2.13
+module load hdf5/1.12.2
+module load parallel-netcdf/1.12.3
+module load netcdf-c/4.9.0
+module load parmetis/4.0.3
+module load boost/1.80.0
+module load superlu/5.3.0
 module list
 
 export CUDA_MANAGED_FORCE_DEVICE_ALLOC=1
