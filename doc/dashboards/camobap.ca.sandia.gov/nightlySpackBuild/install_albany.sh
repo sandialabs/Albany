fe0c12a7c1e4c7eaa32aa20f9e8f2dbd60bbaed9
Irina Tezaur <ikalash@sandia.gov>
2023-05-25 10:50:27 -0700

    Updating nightly spack build to have +optimization flag.

--- doc/dashboards/camobap.ca.sandia.gov/nightlySpackBuild/install_albany.sh
+++ doc/dashboards/camobap.ca.sandia.gov/nightlySpackBuild/install_albany.sh
@@ -6,4 +6,4 @@ export https_proxy=http://proxy.sandia.gov:80
 . share/spack/setup-env.sh
 #spack compiler find
 #cp ../config.yaml etc/spack/defaults/config.yaml
-spack --insecure install --dirty --keep-stage albany%gcc@11.1.0+mpas+py >& spack_albany.out
+spack --insecure install --dirty --keep-stage albany%gcc@11.1.0+mpas+py+optimization >& spack_albany.out
