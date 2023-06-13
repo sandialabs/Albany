08c190ae2cd658129497647b89f648a1d9b09e61
mcarlson801 <maxx.l.carlson@gmail.com>
2023-06-13 13:10:45 -0600

    Simplified and optimized UVM-free LoadSideSetStatefield implementation

--- src/evaluators/state/PHAL_LoadSideSetStateField_Def.hpp
+++ src/evaluators/state/PHAL_LoadSideSetStateField_Def.hpp
@@ -133,6 +133,8 @@ loadNodeState(typename Traits::EvalData workset)
   // associates to each 3d-side GID the corresponding 2d-cell GID
   const auto& side3d_to_cell2d = workset.disc->getSideToSideSetCellMap().at(sideSetName);
 
+  auto field_h_mirror = Kokkos::create_mirror(field.get_view());
+
   auto sideSet = workset.sideSetViews->at(sideSetName);
   for (int sideSet_idx = 0; sideSet_idx < sideSet.size; ++sideSet_idx) {
     // Get the side GID
@@ -148,32 +150,23 @@ loadNodeState(typename Traits::EvalData workset)
     const auto nodes2d = bulkData.begin_nodes(cell2d);
 
     if (dims.size() == 2) {
-      auto field_d_view = Kokkos::subview(field.get_view(), sideSet_idx, Kokkos::ALL);
-      auto field_d_view_tmp = Kokkos::View<ScalarType*, PHX::Device>("field_d_view_tmp",numNodes);
-      auto field_h_mirror = Kokkos::create_mirror_view(field_d_view_tmp);
       for (int inode=0; inode<numNodes; ++inode) {
         const double* data = stk::mesh::field_data(*stk_field,nodes2d[node_map[inode]]);
-        field_h_mirror(inode) = *data;
+        field_h_mirror(sideSet_idx,inode) = *data;
       }
-      Kokkos::deep_copy(field_d_view_tmp, field_h_mirror);
-      Kokkos::deep_copy(field_d_view, field_d_view_tmp);
     } else if (dims.size() == 3) {
-      auto field_d_view = Kokkos::subview(field.get_view(), sideSet_idx, Kokkos::ALL, Kokkos::ALL);
-      auto field_d_view_tmp = Kokkos::View<ScalarType**, PHX::Device>("field_d_view_tmp",numNodes,dims[2]);;
-      auto field_h_mirror = Kokkos::create_mirror_view(field_d_view_tmp);
       for (int inode=0; inode<numNodes; ++inode) {
         const double* data = stk::mesh::field_data(*stk_field,nodes2d[node_map[inode]]);
         for (int idim=0; idim<static_cast<int>(dims[2]); ++idim) {
-          field_h_mirror(inode,idim) = data[idim];
+          field_h_mirror(sideSet_idx,inode,idim) = data[idim];
         }
       }
-      Kokkos::deep_copy(field_d_view_tmp, field_h_mirror);
-      Kokkos::deep_copy(field_d_view, field_d_view_tmp);
     } else {
       TEUCHOS_TEST_FOR_EXCEPTION (true, std::runtime_error,
           "Error! Unsupported field dimension. However, you should have gotten an error before!\n");
     }
   }
+  Kokkos::deep_copy(field.get_view(), field_h_mirror);
 }
 
 template<typename EvalT, typename Traits, typename ScalarType>
@@ -216,6 +209,8 @@ loadElemState(typename Traits::EvalData workset)
   TEUCHOS_TEST_FOR_EXCEPTION (stk_field==nullptr, std::runtime_error,
       "Error! STK field ptr is null.\n");
 
+  auto field_h_mirror = Kokkos::create_mirror(field.get_view());
+
   // Loop on the sides of this sideSet that are in this workset
   auto sideSet = workset.sideSetViews->at(sideSetName);
   for (int sideSet_idx = 0; sideSet_idx < sideSet.size; ++sideSet_idx) {
@@ -227,22 +222,17 @@ loadElemState(typename Traits::EvalData workset)
 
     const double* data = stk::mesh::field_data(*stk_field,cell2d);
     if (dims.size() == 1) {
-      auto field_d_view = Kokkos::subview(field.get_view(), sideSet_idx);
-      Kokkos::deep_copy(field_d_view, *data);
+      field_h_mirror(sideSet_idx) = *data;
     } else if (dims.size() == 2) {
-      auto field_d_view = Kokkos::subview(field.get_view(), sideSet_idx, Kokkos::ALL);
-      auto field_d_view_tmp = Kokkos::View<ScalarType*, PHX::Device>("field_d_view_tmp",dims[1]);
-      auto field_h_mirror = Kokkos::create_mirror_view(field_d_view);
       for (int idim=0; idim<static_cast<int>(dims[1]); ++idim) {
         field_h_mirror(sideSet_idx,idim) = data[idim];
       }
-      Kokkos::deep_copy(field_d_view_tmp, field_h_mirror);
-      Kokkos::deep_copy(field_d_view, field_d_view_tmp);
     } else {
       TEUCHOS_TEST_FOR_EXCEPTION (true, std::runtime_error,
           "Error! Unsupported field dimension. However, you should have gotten an error before!\n");
     }
   }
+  Kokkos::deep_copy(field.get_view(), field_h_mirror);
 }
 
 } // Namespace PHAL
