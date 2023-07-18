b085afe8a2cc95ce10f8b24d6194ef0ea069f2cb
Irina Tezaur <ikalash@sandia.gov>
2023-07-18 13:26:04 -0700

    Adding --timeout option to ctest line in spack nightly, to prevent code from hanging when encountering failing tests.

--- doc/dashboards/camobap.ca.sandia.gov/nightlySpackBuild/run_ctest.sh
+++ doc/dashboards/camobap.ca.sandia.gov/nightlySpackBuild/run_ctest.sh
@@ -1,7 +1,7 @@
 cp cd_spack_build_dir.sh /tmp/ikalash/spack-stage/spack-stage-albany-develop* 
 cd /tmp/ikalash/spack-stage/spack-stage-albany-develop* >& /nightlySpackBuild/spack_cd_albany.out
 source cd_spack_build_dir.sh
-ctest -V >& spack_ctest.out
+ctest --timeout 1200 -V >& spack_ctest.out
 mv spack_ctest.out /nightlySpackBuild/spack_ctest.out
 cd /nightlySpackBuild
 source /nightlySpackBuild/postproc_spack.sh
