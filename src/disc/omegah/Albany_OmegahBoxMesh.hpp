903150a7c569b36131e1b129aea0bb4abda33604
Luca Bertagna <lbertag@sandia.gov>
2023-08-07 11:09:07 -0600

    Added more implementation to OmegahBoxMesh and start unit tests implementation

--- src/disc/omegah/Albany_OmegahBoxMesh.hpp
+++ src/disc/omegah/Albany_OmegahBoxMesh.hpp
@@ -1,6 +1,7 @@
 #ifndef ALBANY_OMEGAH_BOX_MESH_HPP
 #define ALBANY_OMEGAH_BOX_MESH_HPP
 
+#include "Omega_h_mesh.hpp"
 #include "Albany_OmegahAbstractMesh.hpp"
 
 namespace Albany {
@@ -11,9 +12,29 @@ public:
   OmegahBoxMesh (const Teuchos::RCP<Teuchos::ParameterList>& params,
                  const Teuchos::RCP<const Teuchos_Comm>& comm, const int numParams);
 
+  void setFieldData (const Teuchos::RCP<const Teuchos_Comm>& commT,
+                     const Teuchos::RCP<Albany::StateInfoStruct>& sis,
+                     const unsigned int worksetSize,
+                     const std::map<std::string,Teuchos::RCP<StateInfoStruct> >& side_set_sis) override
+  {
+    printf("TODO: add code to setup data to store states to mesh.\n");
+  }
+  void setBulkData (const Teuchos::RCP<const Teuchos_Comm>& commT,
+                    const Teuchos::RCP<StateInfoStruct>& sis,
+                    const unsigned int worksetSize,
+                    const std::map<std::string,Teuchos::RCP<StateInfoStruct> >& side_set_sis) override;
+
+  ViewLR<const double*,DeviceMemSpace> coords_dev  () const { return m_coords_d; }
+  ViewLR<const double*,HostMemSpace>   coords_host () const { return m_coords_h; }
 protected:
+
   Teuchos::RCP<const Teuchos::ParameterList>
   getValidDiscretizationParameters() const;
+
+  Omega_h::Mesh  m_mesh;
+
+  ViewLR<const double*,DeviceMemSpace>  m_coords_d;
+  ViewLR<      double*,HostMemSpace>    m_coords_h;
 };
 
 } // namespace Albany
