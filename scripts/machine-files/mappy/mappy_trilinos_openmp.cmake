75570fed64c7710455499446eefe784f41b541e1
Luca Bertagna <lbertag@sandia.gov>
2023-10-02 11:55:43 -0600

    Fix (and renamed) mappy mach files for trilinos

---
+++ scripts/machine-files/mappy/mappy_trilinos_openmp.cmake
@@ -0,0 +1,14 @@
+# The user may have a symlink to this file somewhere, so make sure
+# we resolve links first, so that relative paths are in terms of
+# the *real* file
+get_filename_component(THIS_FILE ${CMAKE_CURRENT_LIST_FILE} REALPATH)
+get_filename_component(THIS_PATH ${THIS_FILE} DIRECTORY)
+
+# Get common mappy settings
+include(${THIS_PATH}/mappy_trilinos_common.cmake)
+
+# Set Kokkos device
+include(${THIS_PATH}/../kokkos/device/openmp.cmake)
+
+# Enable OpenMP in trilinos
+set (Trilinos_ENABLE_OpenMP ON CACHE BOOL "")
