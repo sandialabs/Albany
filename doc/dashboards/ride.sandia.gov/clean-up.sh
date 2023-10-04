d3fc8307833c293894f11768a02173d25b7f9752
Irina Tezaur <ikalash@sandia.gov>
2023-10-04 10:00:29 -0700

    Removing test scripts for ride, since we don't test there anymore.

--- doc/dashboards/ride.sandia.gov/clean-up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rm -rf /projects/albany/nightlyAlbanyCDash/cdash-frag.txt
-rm -rf /projects/albany/nightlyAlbanyCDash/ctest_nightly_trilinos.cmake 
--- doc/dashboards/ride.sandia.gov/create-new-cdash-cmake-script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -e '/CDASH-TRILINOS-FILE.TXT/r cdash-frag.txt' -e '/CDASH-TRILINOS-FILE.TXT/d' ctest_nightly_trilinos_tmp.cmake >& ctest_nightly_trilinos.cmake
