70c4d62599e2b1a06712126297c0b32f82197c27
Jerry Watkins <jwatkin@sandia.gov>
2023-11-22 13:05:54 -0600

    Add chrysalis build files

---
+++ chrysalis_gnu_modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+module purge
+module load subversion/1.14.0-e4smcy3
+module load perl/5.32.0-bsnc6lt
+module load cmake/3.24.2-whgdv7y
+module load gcc/9.2.0-ugetvbp
+module load intel-mkl/2020.4.304-n3b5fye
+module load openmpi/4.1.3-sxfyy4k
+module load hdf5/1.10.7-j3zxncu
+module load netcdf-c/4.4.1-7ohuiwq
+module load netcdf-cxx/4.2-tkg465k
+module load netcdf-fortran/4.4.4-k2zu3y5
+module load parallel-netcdf/1.11.0-mirrcz7
+module list
+
