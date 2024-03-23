b1b17dddb1ba0b73a2c8aee1ace9f7d288bb4acd
Irina Tezaur <ikalash@sandia.gov>
2024-03-22 19:47:41 -0700

    Adding scripts for nightly spack build on cee-compute006 with Xylar's requested build options.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/send_email_spack.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/send_email_spack.sh
@@ -2,12 +2,16 @@
 
 #source $1 
 
-TTT=`grep "(Failed)" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
-TTTT=`grep "(Not Run)" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
-TTTTT=`grep "(Timeout)" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
-TT=`grep "...   Passed" /projects/albany/nightlySpackBuild/spack_ctest.out -c`
+TTT=`grep "(Failed)" /nightlySpackBuild/spack_ctest.out -c`
+TTTT=`grep "(Not Run)" /nightlySpackBuild/spack_ctest.out -c`
+TTTTT=`grep "(Timeout)" /nightlySpackBuild/spack_ctest.out -c`
+TT=`grep "...   Passed" /nightlySpackBuild/spack_ctest.out -c`
 
-#/bin/mail -s "Albany ($ALBANY_BRANCH): $TTT" "albany-regression@software.sandia.gov" < $ALBOUTDIR/albany_runtests.out
-#/bin/mail -s "Albany Spack Build, camobap.ca.sandia.gov: $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "albany@sandia.gov, daibane@sandia.gov, tjfulle@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /projects/albany/nightlySpackBuild/results_spack
-/bin/mail -s "Albany Spack Build (linux-rhel7-haswell, gcc-9.2.0): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov" < /projects/albany/nightlySpackBuild/results_spack
-#/bin/mail -s "Albany Spack Build (linux-rhel7-haswell, gcc-10.1.1): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" "ikalash@sandia.gov" < /projects/albany/nightlySpackBuild/results_spack
+
+echo "Subject: Albany Spack Build, camobap (linux-rhel8, gcc-11.1.0): $TT tests passed, $TTT tests failed, $TTTT tests not run, $TTTTT timeouts" >& a
+echo "" >& b
+cat a b >& c
+cat c results_spack >& d
+rm a b c
+mv d results_spack
+cat results_spack | /usr/sbin/sendmail -F ikalash@camobap.ca.sandia.gov -t "ikalash@sandia.gov, mperego@sandia.gov, lbertag@sandia.gov"
