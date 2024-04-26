9d95e16f273c17eee37e2b288ab3a928b719b6f3
mcarlson801 <maxx.l.carlson@gmail.com>
2024-04-26 16:17:07 -0600

    Update Perlmutter weekly testing scripts

--- doc/dashboards/perlmutter-p1.nersc.gov/cuda-uvm/pm_gpu_gnu_modules.sh
+++ doc/dashboards/perlmutter-p1.nersc.gov/cuda-uvm/pm_gpu_gnu_modules.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-module unload cray-hdf5-parallel
-module unload cray-netcdf-hdf5parallel
-module unload cray-parallel-netcdf
-module unload PrgEnv-gnu
-module unload PrgEnv-nvidia
-module unload gpu
-module unload cudatoolkit
-module unload craype-accel-nvidia80
-module unload craype-accel-host
-module unload perftools-base
-module unload perftools
-module unload darshan
 
+module load cpe/23.03
 module load PrgEnv-gnu/8.3.3
 module load gcc/11.2.0
 module load cudatoolkit/11.7
@@ -38,5 +27,5 @@ export MPICH_GPU_SUPPORT_ENABLED=1
 export CUDATOOLKIT_VERSION_STRING=${CRAY_CUDATOOLKIT_VERSION#*_}
 
 export CUDA_MANAGED_FORCE_DEVICE_ALLOC=1
-export CUDA_LAUNCH_BLOCKING=1
-export TPETRA_ASSUME_GPU_AWARE_MPI=0
+export TPETRA_ASSUME_GPU_AWARE_MPI=1
+export FI_HMEM_CUDA_USE_GDRCOPY=0
