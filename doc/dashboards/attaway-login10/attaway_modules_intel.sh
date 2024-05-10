0840effe613cc9b159896411e4e26702e1dee709
Irina K. Tezaur <ikalash@sandia.gov>
2024-05-10 13:35:06 -0600

    Updating attaway modules so that attaway build works again.

--- doc/dashboards/attaway-login10/attaway_modules_intel.sh
+++ doc/dashboards/attaway-login10/attaway_modules_intel.sh
@@ -4,12 +4,14 @@ module load cmake/3.25.2
 module load sems-git/2.29.0
 module load sems-ninja/1.10.1
 module load sems-python/3.9.0
-module load aue/intel-oneapi-compilers/2023.2.0
+module load aue/intel-oneapi-compilers/2024.1.0
 module load gnu/12.1.1
-module load aue/openmpi/4.1.6-intel-2023.2.0
-module load aue/boost/1.83.0-oneapi-2023.2.0-openmpi-4.1.6
+module load aue/openmpi/4.1.6-oneapi-2024.1.0
+module load aue/boost/1.83.0-oneapi-2024.1.0-openmpi-4.1.6
 module load aue/zlib/1.3
-module load aue/netcdf-c/4.9.2-oneapi-2023.2.0-openmpi-4.1.6
-module load aue/parallel-netcdf/1.12.3-oneapi-2023.2.0-openmpi-4.1.6
-module load aue/intel-oneapi-mkl/2023.2.0-oneapi-2023.2.0
-module load aue/hdf5/1.14.2-oneapi-2023.2.0-openmpi-4.1.6
+module load aue/netcdf-c/4.9.2-oneapi-2024.1.0-openmpi-4.1.6
+module load aue/parallel-netcdf/1.12.3-oneapi-2024.1.0-openmpi-4.1.6
+module load aue/intel-oneapi-mkl/2024.1.0
+module load aue/hdf5/1.14.2-oneapi-2024.1.0-openmpi-4.1.6
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NETCDF_ROOT/lib:$PNETCDF_ROOT/lib:$HDF5_ROOT/lib
+module list
