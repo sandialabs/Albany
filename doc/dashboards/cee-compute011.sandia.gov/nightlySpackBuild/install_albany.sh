1b2911327d675ba7535c360823a36e2313bba3fd
Irina K. Tezaur <ikalash@sandia.gov>
2024-05-18 15:32:01 -0600

    Fixing spack nightly build on CEE.

--- doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/install_albany.sh
+++ doc/dashboards/cee-compute011.sandia.gov/nightlySpackBuild/install_albany.sh
@@ -7,4 +7,4 @@ export https_proxy=http://proxy.sandia.gov:80
 . share/spack/setup-env.sh
 #spack compiler find
 #cp ../config.yaml etc/spack/defaults/config.yaml
-spack --insecure install --dirty --keep-stage albany@develop%gcc@10.1.0+mpas~py+unit_tests >& spack_albany.out
+spack --insecure install --dirty --keep-stage albany@develop%gcc@10.3.0+mpas~py+unit_tests >& spack_albany.out
