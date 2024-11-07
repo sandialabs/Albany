76713a4cb7a209fec8ae8ce3cd23cdd2944ba936
Irina Tezaur <ikalash@sandia.gov>
2024-11-07 10:22:02 -0800

    ALI: fixing CALI build errors from changes to createSolver() routine introduced yesterday in a PR.

--- src/landIce/interfaceWithCISM/ali_driver.cpp
+++ src/landIce/interfaceWithCISM/ali_driver.cpp
@@ -646,7 +646,7 @@ void ali_driver_run(AliToGlimmer * ftg_ptr, double& cur_time_yr, double time_inc
     //if (!first_time_step)
     //  std::cout << "previousSolution: " << *previousSolution << std::endl;
     auto albanyModel = slvrfctry->createModel(albanyApp);
-    solver = slvrfctry->createSolver(reducedMpiCommT, albanyModel, Teuchos::null, true);
+    solver = slvrfctry->createSolver(albanyModel, Teuchos::null, true);
 
     Teuchos::ParameterList solveParams;
     solveParams.set("Compute Sensitivities", false);
