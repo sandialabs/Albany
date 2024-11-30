6b59754f976a2755d3b3c8c96642f13454033b40
Irina Tezaur <ikalash@sandia.gov>
2024-11-29 21:03:56 -0800

    Adding Trilinos and Albany build scripts for Irina's new machine, hayka. It runs RHEL9.

---
+++ doc/nightlyTestHarness/hayka/modules_hayka.sh
@@ -0,0 +1,16 @@
+module purge
+module load sems-gcc/11.4.0
+module load sems-openmpi/4.1.6
+module load  sems-boost/1.84.0
+module load sems-openblas/0.3.10
+module load sems-netcdf-cxx/4.2       
+module load sems-hdf5/1.14.3       
+module load sems-netcdf-fortran/4.6.1   
+module load sems-netcdf-c/4.9.2    
+module load sems-parallel-netcdf/1.12.3   
+module load sems-zlib/1.3
+module load sems-gdb/14.1
+module load sems-python/3.8.17
+module load sems-cmake/3.27.9
+module load sems-ninja/1.11.1
+
