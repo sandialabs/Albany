4bbf6c53013af1b11e39a1459ead87f2219efbda
Irina Tezaur <ikalash@sandia.gov>
2024-12-17 17:27:37 -0800

    Turning off pyAlbany in nightly spack build on camobap for the time being.

--- doc/dashboards/camobap.ca.sandia.gov/nightlySpackBuild/install_albany.sh
+++ doc/dashboards/camobap.ca.sandia.gov/nightlySpackBuild/install_albany.sh
@@ -6,4 +6,5 @@ export https_proxy=http://proxy.sandia.gov:80
 . share/spack/setup-env.sh
 #spack compiler find
 #cp ../config.yaml etc/spack/defaults/config.yaml
-spack --insecure install --dirty --keep-stage albany@develop%gcc@11.1.0+mpas+py+optimization+mesh_depends_on_params+omegah >& spack_albany.out
+#spack --insecure install --dirty --keep-stage albany@develop%gcc@11.1.0+mpas+py+optimization+mesh_depends_on_params+omegah >& spack_albany.out
+spack --insecure install --dirty --keep-stage albany@develop%gcc@11.1.0+mpas+optimization+mesh_depends_on_params+omegah >& spack_albany.out
