72ef81d4493491ec00f56ead4df6be52418d939c
Luca Bertagna <lbertag@sandia.gov>
2025-04-08 15:50:59 -0600

    squash add osh mesh support to disc

--- tests/unit/disc/omegah/box_mesh.cpp
+++ tests/unit/disc/omegah/box_mesh.cpp
@@ -12,6 +12,11 @@
 #include "Teuchos_UnitTestHelpers.hpp"
 #include "Teuchos_LocalTestingHelpers.hpp"
 
+Teuchos::RCP<Albany::OmegahGenericMesh>
+omegahGenericMesh(const Teuchos::RCP<Teuchos::ParameterList>& p) {
+  return Teuchos::rcp(new Albany::OmegahGenericMesh(p));
+}
+
 TEUCHOS_UNIT_TEST(OmegahBoxMesh, 2D)
 {
   using namespace Albany;
@@ -21,7 +26,6 @@ TEUCHOS_UNIT_TEST(OmegahBoxMesh, 2D)
   Teuchos::Array<int> nelems(2);
   nelems[0] = 4;
   nelems[1] = 3;
-  const int num_params = 2;
 
   auto params = Teuchos::rcp(new Teuchos::ParameterList(""));
   params->set<std::string>("Mesh Creation Method","Box2D");
@@ -31,21 +35,21 @@ TEUCHOS_UNIT_TEST(OmegahBoxMesh, 2D)
   // ---------------------------------- //
 
   // Missing required parameter
-  TEST_THROW (OmegahGenericMesh(params),std::logic_error);
+  TEST_THROW (omegahGenericMesh(params),std::logic_error);
 
   // Wrong dimensions
   params->set<Teuchos::Array<int>>("Number of Elements",Teuchos::Array<int>(3,1));
-  TEST_THROW (OmegahGenericMesh(params),std::logic_error);
+  TEST_THROW (omegahGenericMesh(params),std::logic_error);
   params->set<Teuchos::Array<int>>("Number of Elements",nelems);
   params->set<Teuchos::Array<double>>("Box Scaling",Teuchos::Array<double>(1,1));
-  TEST_THROW (OmegahGenericMesh(params),std::logic_error);
+  TEST_THROW (omegahGenericMesh(params),std::logic_error);
 
   // ---------------------------------- //
   //        Test normal oprations       //
   // ---------------------------------- //
 
   params->remove("Box Scaling");
-  auto mesh = Teuchos::rcp(new OmegahGenericMesh(params));
+  auto mesh = omegahGenericMesh(params);
 
   auto coords = mesh->coords_host();
   auto omegah_mesh = mesh->getOmegahMesh();
@@ -68,7 +72,6 @@ TEUCHOS_UNIT_TEST(OmegahBoxMesh, 1D)
 
   Teuchos::Array<int> nelems(1);
   nelems[0] = 4;
-  const int num_params = 1;
 
   auto params = Teuchos::rcp(new Teuchos::ParameterList(""));
   params->set<std::string>("Mesh Creation Method","Box2D");
@@ -78,21 +81,21 @@ TEUCHOS_UNIT_TEST(OmegahBoxMesh, 1D)
   // ---------------------------------- //
 
   // Missing required parameter
-  TEST_THROW (OmegahGenericMesh(params),std::logic_error);
+  TEST_THROW (omegahGenericMesh(params),std::logic_error);
 
   // Wrong dimensions
   params->set<Teuchos::Array<int>>("Number of Elements",Teuchos::Array<int>(2,1));
-  TEST_THROW (OmegahGenericMesh(params),std::logic_error);
+  TEST_THROW (omegahGenericMesh(params),std::logic_error);
   params->set<Teuchos::Array<int>>("Number of Elements",nelems);
   params->set<Teuchos::Array<double>>("Box Scaling",Teuchos::Array<double>(3,1));
-  TEST_THROW (OmegahGenericMesh(params),std::logic_error);
+  TEST_THROW (omegahGenericMesh(params),std::logic_error);
 
   // ---------------------------------- //
   //        Test normal oprations       //
   // ---------------------------------- //
 
   params->remove("Box Scaling");
-  auto mesh = Teuchos::rcp(new OmegahGenericMesh(params));
+  auto mesh = omegahGenericMesh(params);
 
   auto coords = mesh->coords_host();
   auto omegah_mesh = mesh->getOmegahMesh();
