9aa1680361b626c9e944425af53e0995c6c947d3
Irina K. Tezaur <ikalash@sandia.gov>
2025-04-15 13:55:05 -0600

    Adding Max to list of recipients from my.cdash.org.  Will add others once post processing is stable.

--- doc/dashboards/cee-compute011.sandia.gov/NightlyTestingEmail/process_nightlies_my.cdash.org.sh
+++ doc/dashboards/cee-compute011.sandia.gov/NightlyTestingEmail/process_nightlies_my.cdash.org.sh
@@ -13,6 +13,6 @@ git clone git@github.com:TriBITSPub/TriBITS.git
 now=$(date +"%Y-%m-%d")
 
 #./albany_cdash_status_my.cdash.org.sh --date=$now --email-from-address=ikalash@cee-compute003.sandia.gov --send-email-to=ikalash@sandia.gov,mperego@sandia.gov,jwatkin@sandia.gov,lbertag@sandia.gov,maxcarl@sandia.gov 
-./albany_cdash_status_my.cdash.org.sh --date=$now --email-from-address=ikalash@cee-compute003.sandia.gov --send-email-to=ikalash@sandia.gov
+./albany_cdash_status_my.cdash.org.sh --date=$now --email-from-address=ikalash@cee-compute003.sandia.gov --send-email-to=ikalash@sandia.gov,maxcarl@sandia.gov 
 
 
