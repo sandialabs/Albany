d156140f0e8a63b426a0d1ef55c07cc3df8b0e30
Luca Bertagna <lbertag@sandia.gov>
2025-05-14 08:34:27 -0600

    Remove pointless arg to disc method

--- src/disc/Albany_AbstractDiscretization.hpp
+++ src/disc/Albany_AbstractDiscretization.hpp
@@ -308,8 +308,7 @@ public:
       const std::string&  field_name,
       bool                overlapped) = 0;
 
-  virtual void
-  setFieldData(const Teuchos::RCP<StateInfoStruct>& sis) = 0;
+  virtual void setFieldData() = 0;
 
   // Update mesh internals, such as coordinates, DOF numbers, etc.
   // To be run either after creation or after modification/adaptation.
