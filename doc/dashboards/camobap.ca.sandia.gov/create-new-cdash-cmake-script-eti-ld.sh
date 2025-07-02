e9d56165e44216ad2e97f838af824cba7ca365c1
Irina Tezaur <ikalash@sandia.gov>
2025-07-02 11:38:12 -0700

    Removing obsolete unused nightly testing scripts.

--- doc/dashboards/camobap.ca.sandia.gov/create-new-cdash-cmake-script-eti-ld.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -e '/CDASH-TRILINOS-FILE.TXT/r /nightlyCDash/cdash-frag-ld-eti.txt' -e '/CDASH-TRILINOS-FILE.TXT/d' /nightlyCDash/ctest_nightly_trilinos_eti_ld_tmp.cmake >& /nightlyCDash/ctest_nightly_trilinos_eti_ld.cmake
