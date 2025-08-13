f1de956c5fc3da4ea024698609925d42b591b850
Luca Bertagna <lbertag@sandia.gov>
2025-08-13 12:35:09 -0600

    Add implementation of new methods to OmegahMeshFieldAccessor

--- src/disc/omegah/Albany_OmegahMeshFieldAccessor.cpp
+++ src/disc/omegah/Albany_OmegahMeshFieldAccessor.cpp
@@ -108,6 +108,128 @@ addStateStructs(const StateInfoStruct& sis)
   }
 }
 
+void OmegahMeshFieldAccessor::createStateArrays (const WorksetArray<int>& worksets_sizes)
+{
+  int num_elems = m_mesh->nelems();
+  int num_nodes = m_mesh->nverts();
+
+  // We don't have workset sizes in here, so use single workset
+  elemStateArrays.resize(1);
+  nodeStateArrays.resize(1);
+
+  // Elem states
+  int ws_start = 0;
+  int num_ws = worksets_sizes.size();
+  for (const auto& st : elem_sis) {
+    auto data = m_tags.at(st->name).data();
+    auto dim = st->dim;
+    int stride = 1;
+    for (auto d : dim) stride *= d;
+    stride /= dim[0];
+
+    for (int ws=0; ws<num_ws; ++ws) {
+      switch (dim.size()) {
+        case 1:
+          elemStateArrays[ws][st->name].reset_from_dev_ptr(data,num_elems); break;
+        case 2:
+          elemStateArrays[ws][st->name].reset_from_dev_ptr(data,num_elems,dim[1]); break;
+        case 3:
+          elemStateArrays[ws][st->name].reset_from_dev_ptr(data,num_elems,dim[1],dim[2]); break;
+        case 4:
+          elemStateArrays[ws][st->name].reset_from_dev_ptr(data,num_elems,dim[1],dim[2],dim[3]); break;
+        default:
+          throw std::runtime_error("Error! Unsupported rank for elem state '" + st->name + "'.\n");
+      }
+      data += worksets_sizes[ws]*stride;
+    }
+  }
+
+  // Nodal states
+  // NOTE: nodal states have just 1 workset
+  for (const auto& st : nodal_sis) {
+    auto data = m_tags.at(st->name).data();
+    auto dim = st->dim;
+    if (st->entity != StateStruct::NodalData) {
+      dim.erase(dim.begin()); // NodalDistParameter and NodalDataToElemNode have <Cell> as first dim
+    }
+    switch (dim.size()) {
+      case 1:
+        nodeStateArrays[0][st->name].reset_from_dev_ptr(data,num_nodes); break;
+      case 2:
+        nodeStateArrays[0][st->name].reset_from_dev_ptr(data,num_nodes,dim[1]); break;
+      case 3:
+        nodeStateArrays[0][st->name].reset_from_dev_ptr(data,num_nodes,dim[1],dim[2]); break;
+      default:
+        throw std::runtime_error("Error! Unsupported rank for node state '" + st->name + "'.\n");
+    }
+  }
+
+  // Global states
+  for (const auto& st : global_sis) {
+    auto& state = globalStates[st->name];
+    if (st->dim.size()==1) {
+      state.reset_from_host_ptr(&mesh_scalar_states[st->name],1);
+    } else if (st->dim.size()==1) {
+      state.reset_from_host_ptr(mesh_vector_states[st->name].data(),st->dim[0]);
+    } else {
+      throw std::runtime_error("Error! Unsupported rank for global state '" + st->name + "'.\n");
+    }
+  }
+}
+
+void OmegahMeshFieldAccessor::transferNodeStatesToElemStates ()
+{
+  int num_elems = m_mesh->nelems();
+  auto elem_nodes = m_mesh->ask_elem_verts();
+  auto elem_nodes_h = hostRead(elem_nodes);
+  int num_elem_nodes = elem_nodes.size() / num_elems;
+
+  for (const auto& st : nodal_sis) {
+    if (st->entity!=StateStruct::NodalDataToElemNode)
+      continue;
+    const auto& dim = st->dim;
+    const auto rank = st->dim.size();
+
+    const auto& node_state = m_mesh->get_tag<ST>(0,st->name)->array();
+          auto& elem_state = elemStateArrays[0][st->name];
+    switch (rank) {
+      case 2:
+        elem_state.resize(st->name,num_elems,dim[1]); break;
+      case 3:
+        elem_state.resize(st->name,num_elems,dim[1],dim[2]); break;
+      case 4:
+        elem_state.resize(st->name,num_elems,dim[1],dim[2],dim[3]); break;
+      default:
+        throw std::runtime_error("Error! Unsupported rank for node state '" + st->name + "'.\n");
+    }
+
+    auto& elem_state_h = elem_state.host();
+    auto  node_state_h = hostRead(node_state);
+    for (int i=0; i<num_elems; ++i) {
+      for (int j=0; j<num_elem_nodes; ++j) {
+        switch(rank) {
+          case 2:
+            elem_state_h(i, j) = node_state_h[elem_nodes_h[i*num_elem_nodes+j]];
+            break;
+          case 3:
+            for (size_t k=0; k<dim[2]; ++k) {
+              auto offset = i*num_elem_nodes*dim[2]+j*dim[2]+k;
+              elem_state_h(i, j, k) = node_state_h[elem_nodes_h[offset]];
+            } break;
+          case 4:
+            for (size_t k=0; k<dim[2]; ++k) {
+              for (size_t l=0; l<dim[3]; ++l) {
+                auto offset = i*num_elem_nodes*dim[2]*dim[3]+j*dim[2]*dim[3]+k*dim[3]+l;
+                elem_state_h(i, j, k, l) = node_state_h[elem_nodes_h[offset]];
+              }
+            } break;
+        }
+      }
+    }
+    elem_state.sync_to_dev();
+  }
+}
+
 void OmegahMeshFieldAccessor::
 fillVector (Thyra_Vector&        field_vector,
             const std::string&   field_name,
