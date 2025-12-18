aaded214ab99377ee6530c92971e2da74c40e39f
Luca Bertagna <lbertag@sandia.gov>
2025-12-17 18:42:01 -0700

    Start working on unit tests for ExtrudedDiscretization

---
+++ tests/unit/disc/extruded/ExtrudedDisc.cpp
@@ -0,0 +1,188 @@
+//*****************************************************************//
+//    Albany 3.0:  Copyright 2016 Sandia Corporation               //
+//    This Software is released under the BSD license detailed     //
+//    in the file "license.txt" in the top-level Albany directory  //
+//*****************************************************************//
+
+#include "Albany_UnitTestSession.hpp"
+#include "Albany_Utils.hpp"
+#include "Albany_StringUtils.hpp"
+#include "Albany_CommUtils.hpp"
+#include "Albany_DiscretizationFactory.hpp"
+
+#include <Teuchos_CommHelpers.hpp>
+#include <Teuchos_UnitTestHelpers.hpp>
+#include <Teuchos_LocalTestingHelpers.hpp>
+
+#include <random>
+
+constexpr int num_layers = 2;
+constexpr int nelem_x = 2;
+constexpr int nelem_y = 2;
+constexpr double ztop = 1.0;
+
+Teuchos::RCP<Albany::AbstractDiscretization>
+create_extruded_disc (const Teuchos::RCP<const Teuchos_Comm>& comm, bool logically_extruded)
+{
+  using namespace Albany;
+
+  auto top_level_params = Teuchos::rcp(new Teuchos::ParameterList(""));
+  auto disc_params = Teuchos::sublist(top_level_params,"Discretization");
+  if (logically_extruded)
+    disc_params->set<std::string>("Method", "Extruded");
+  else
+    disc_params->set<std::string>("Method", "STKExtruded");
+
+  disc_params->set<int>("NumLayers", num_layers);
+  disc_params->set<int>("Number Of Time Derivatives", 0);
+  disc_params->set<bool>("Columnwise Ordering", true);
+  disc_params->sublist("Side Set Discretizations").set("Side Sets",Teuchos::Array<std::string>{"basalside"});
+  auto& basal_params = disc_params->sublist("Side Set Discretizations").sublist("basalside");
+  basal_params.set<std::string>("Method", "STK2D");
+  basal_params.set<int>("Number Of Time Derivatives", 0);
+  basal_params.set<int>("1D Elements", nelem_x);
+  basal_params.set<int>("2D Elements", nelem_y);
+  basal_params.set<std::string>("Cell Topology", "Triangle");
+
+  auto& basal_req = basal_params.sublist("Required Fields Info");
+  basal_req.set<int>("Number Of Fields",2);
+  auto& H_req = basal_req.sublist("Field 0");
+  H_req.set<std::string>("Field Name","thickness");
+  H_req.set<std::string>("Field Type","Node Scalar");
+  H_req.set<std::string>("Field Origin","File");
+  H_req.set<double>("Field Value",ztop);
+  auto& zs_req = basal_req.sublist("Field 1");
+  zs_req.set<std::string>("Field Name","surface_height");
+  zs_req.set<std::string>("Field Type","Node Scalar");
+  zs_req.set<std::string>("Field Origin","File");
+  zs_req.set<double>("Field Value",ztop);
+
+  std::map<std::string, Teuchos::RCP<StateInfoStruct> > ss_sis;
+  if (not logically_extruded) {
+    // STKExtruded needs H and zs to be declared as side states
+    auto& bsis = ss_sis["basalside"] = Teuchos::rcp(new StateInfoStruct());
+    bsis->push_back(Teuchos::rcp(new StateStruct("thickness",StateStruct::NodalDataToElemNode,{nelem_x*nelem_y,3})));
+    bsis->push_back(Teuchos::rcp(new StateStruct("surface_height",StateStruct::NodalDataToElemNode,{nelem_x*nelem_y,3})));
+  }
+
+  DiscretizationFactory factory(top_level_params,comm,false);
+
+  factory.createMeshSpecs();
+  auto disc = factory.createDiscretization(2,{},Teuchos::null,ss_sis);
+
+  return disc;
+}
+
+TEUCHOS_UNIT_TEST (ExtrudedDisc,Coordinates)
+{
+  using namespace Albany;
+
+  auto comm = getDefaultComm();
+
+  auto disc = create_extruded_disc(comm,true);
+  auto stk_disc = create_extruded_disc(comm,false);
+  auto bdisc = disc->getSideSetDiscretizations().at("basalside");
+
+  // +-------------------------------------------------------+
+  // |       Test coordinates against manual extrusion       |
+  // +-------------------------------------------------------+
+
+  const auto& coords = disc->getCoords();
+  const auto& bcoords = bdisc->getCoords();
+  const auto& layers_data = disc->getMeshStruct()->layers_data;
+
+  TEST_EQUALITY(layers_data.cell.gid->numLayers,num_layers);
+  TEST_EQUALITY(layers_data.cell.lid->numLayers,num_layers);
+
+  TEST_EQUALITY(disc->getNumWorksets(),bdisc->getNumWorksets());
+  int num_ws = disc->getNumWorksets();
+  auto ws_sizes = disc->getWorksetsSizes();
+  auto bws_sizes = bdisc->getWorksetsSizes();
+
+  auto ws_elems = disc->getWsElementLIDs().host();
+  auto bws_elems = bdisc->getWsElementLIDs().host();
+
+  TEST_EQUALITY (coords.size(),num_ws);
+  TEST_EQUALITY (bcoords.size(),num_ws);
+
+  int num_belem_nodes = bcoords[0][0].size();
+  double dz = ztop / num_layers;
+  for (int ws=0; ws<coords.size(); ++ws) {
+    TEST_EQUALITY(coords[ws].size(),bcoords[ws].size()*num_layers);
+    for (int ibe=0; ibe<bcoords[ws].size(); ++ibe) {
+      for (int in=0; in<num_belem_nodes; ++in) {
+        auto bxy = bcoords[ws][ibe][in];
+        for (int il=0; il<num_layers; ++il) {
+          int ie = layers_data.cell.lid->getId(ibe,il);
+          for (int lev : {0,1}) {
+            auto xyz = coords[ws][ie][in+lev*num_belem_nodes];
+            TEST_EQUALITY (bxy[0],xyz[0]);
+            TEST_EQUALITY (bxy[1],xyz[1]);
+
+            TEST_FLOATING_EQUALITY (xyz[2],dz * (il+lev),1e-7);
+          }
+        }
+      }
+    }
+  }
+
+  // +-------------------------------------------------------+
+  // |       Test coordinates against stk discretization     |
+  // +-------------------------------------------------------+
+
+  const auto& stk_coords = stk_disc->getCoords();
+
+  TEST_EQUALITY(disc->getNumWorksets(),stk_disc->getNumWorksets());
+
+  TEST_EQUALITY (stk_coords.size(),num_ws);
+
+  for (int ws=0; ws<coords.size(); ++ws) {
+    TEST_EQUALITY(coords[ws].size(),stk_coords[ws].size());
+    for (int ie=0; ie<coords[ws].size(); ++ie) {
+      TEST_EQUALITY(coords[ws][ie].size(),stk_coords[ws][ie].size());
+      for (int in=0; in<coords[ws][ie].size(); ++in) {
+        auto xyz = coords[ws][ie][in];
+        auto stk_xyz = stk_coords[ws][ie][in];
+        TEST_EQUALITY (xyz[0],stk_xyz[0]);
+        TEST_EQUALITY (xyz[1],stk_xyz[1]);
+        TEST_FLOATING_EQUALITY (xyz[2],stk_xyz[2],1e-7);
+      }
+    }
+  }
+}
+
+TEUCHOS_UNIT_TEST (ExtrudedDisc,DOFs)
+{
+  using namespace Albany;
+
+  auto comm = getDefaultComm();
+
+  auto disc = create_extruded_disc(comm,true);
+  auto stk_disc = create_extruded_disc(comm,false);
+
+  // +-------------------------------------------------------+
+  // |        Test dofs ids against stk discretization       |
+  // +-------------------------------------------------------+
+
+  const auto& dof_mgr = disc->getDOFManager();
+  const auto& dof_mgr_stk = disc->getDOFManager();
+
+  TEST_EQUALITY(dof_mgr->cell_indexer()->getNumLocalElements(),
+                dof_mgr_stk->cell_indexer()->getNumLocalElements());
+
+  const int num_elems = dof_mgr->cell_indexer()->getNumLocalElements();
+  const auto& lids = dof_mgr->elem_dof_lids().host();
+  const auto& lids_stk = dof_mgr->elem_dof_lids().host();
+  TEST_EQUALITY(lids.extent(1),lids_stk.extent(1));
+  const int ndofs = lids.extent(1);
+  for (int ie=0; ie<num_elems; ++ie) {
+    const auto& gids = dof_mgr->getElementGIDs(ie);
+    const auto& gids_stk = dof_mgr_stk->getElementGIDs(ie);
+
+    TEST_COMPARE_ARRAYS(gids,gids_stk);
+
+    for (int idof=0; idof<ndofs; ++idof) {
+      TEST_EQUALITY(lids(ie,idof),lids_stk(ie,idof));
+    }
+  }
+}
