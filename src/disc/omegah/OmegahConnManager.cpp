db428038f3d79f3d0fa5b3583e886af962947eb7
Cameron Smith <smithc11@rpi.edu>
2025-12-23 11:14:43 -0500

    need to call ghost mesh helper functions elsewhere

--- src/disc/omegah/OmegahConnManager.cpp
+++ src/disc/omegah/OmegahConnManager.cpp
@@ -13,89 +13,12 @@
 #include <Omega_h_for.hpp> //parallel_for
 #include <Omega_h_file.hpp> //write_array, write_parallel
 #include <Omega_h_array_ops.hpp> //get_max
-#include <Omega_h_atomics.hpp> //atomic_fetch_add
 #include <Omega_h_int_scan.hpp> //offset_scan
-#include <Omega_h_mark.hpp> //collect_marked
-#include <Omega_h_map.hpp> //unmap, fan_reduce
 
 #include "OmegahPermutation.hpp"
+#include "OmegahGhost.hpp"
 
 #include <fstream>
-
-namespace {
-  Omega_h::LO getNumOwnedElms(Omega_h::Mesh& mesh) {
-    auto elmDim = mesh.dim();
-    auto isElmOwned = mesh.owned(elmDim);
-    return Omega_h::get_sum(isElmOwned);
-  }
-
-  /**
-   * \brief Get entities in the closure of owned elements
-   *
-   * This function identifies mesh entities that are in the closure of owned elements,
-   * matching the behavior of element-based partitions without ghosts. An entity is
-   * considered to be in the closure of owned elements if it bounds at least one
-   * owned element.
-   *
-   * \param[in] cmesh The Omega_h mesh (const reference)
-   * \param[in] dim The topological dimension of entities to query (0=vertices, 1=edges,
-   *                2=faces, 3=regions). Must be in range [0, 3].
-   *
-   * \return A mask array (Omega_h::Read<Omega_h::I8>) where each entry corresponds to
-   *         an entity of dimension \p dim. The value is 1 if the entity is in the
-   *         closure of at least one owned element, 0 otherwise. The array has length
-   *         equal to the number of entities of dimension \p dim in the mesh.
-   *
-   * \throws std::logic_error if dim < 0 or dim > 3
-   * \throws std::logic_error if the computed mask size doesn't match the number of entities
-   *
-   * \note For entities with dimension equal to the mesh dimension (elements), this
-   *       simply returns the owned mask from the mesh.
-   *
-   * \note For entities with dimension less than the mesh dimension, the function:
-   *       - Queries upward adjacencies (entity-to-element)
-   *       - Maps element ownership status to entity adjacencies
-   *       - Uses fan_reduce with MAX to determine if any adjacent element is owned
-   */
-  Omega_h::Read<Omega_h::I8> getEntsInClosureOfOwnedElms(const Omega_h::Mesh& cmesh, int dim) {
-    //Omegah isn't very const friendly and the Teuchos RCP returns const pointers/refs
-    auto mesh = const_cast<Omega_h::Mesh&>(cmesh);
-    std::stringstream ss;
-    ss << "Error! Invalid mesh entity dimension passed to " << __func__
-       << " . dim = " << dim << " requested.\n";
-    TEUCHOS_TEST_FOR_EXCEPTION ((dim<0 || dim >3), std::logic_error, ss.str());
-    const auto elmDim = mesh.dim();
-    if( dim == elmDim ) {
-      return mesh.owned(elmDim);
-    } else {
-      auto entToElm = mesh.ask_up(dim, elmDim);
-      auto isElmOwned = mesh.owned(elmDim);
-      //create an array that replaces the element indices that are adjacent to
-      //each ent with the ownership status of the element (1:owned, 0:o.w.)
-      auto entToOwned = Omega_h::unmap(entToElm.ab2b, isElmOwned, 1);
-      // For each ent of dimension dim, where dim!=meshDim, get max ownership among adjacent elements
-      // (will be 1 if any element is owned, 0 otherwise)
-      auto entHasOwned = Omega_h::fan_reduce(entToElm.a2ab, Omega_h::read(entToOwned),1,OMEGA_H_MAX);
-      TEUCHOS_TEST_FOR_EXCEPTION (entHasOwned.size() != mesh.nents(dim), std::logic_error,
-        "Error! Incorrect array size when counting entities in the closure of owned mesh elements.\n");
-      return entHasOwned;
-    }
-  }
-
-  //Tell Albany about entities that are in the closure of owned elements.  This
-  //matches what was done in an element based partition without ghosts.
-  Omega_h::LO getNumEntsInClosureOfOwnedElms(const Omega_h::Mesh& cmesh, int dim) {
-    //Omegah isn't very const friendly and the Teuchos RCP returns const pointers/refs
-    auto mesh = const_cast<Omega_h::Mesh&>(cmesh);
-    std::stringstream ss;
-    ss << "Error! Invalid mesh entity dimension passed to " << __func__
-       << " . dim = " << dim << " requested.\n";
-    TEUCHOS_TEST_FOR_EXCEPTION ((dim<0 || dim >3), std::logic_error, ss.str());
-    auto entHasOwnedAdjElm = getEntsInClosureOfOwnedElms(cmesh,dim);
-    return Omega_h::get_sum(entHasOwnedAdjElm);
-  }
-
-}
 
 namespace Albany {
 
@@ -104,7 +27,7 @@ Omega_h::Read<Omega_h::I8> getIsEntInPart(const OmegahGenericMesh& albanyMesh, c
   const auto& mesh = albanyMesh.getOmegahMesh();
   const int part_dim = albanyMesh.part_dim(part_name);
   if(part_name == albanyMesh.meshSpecs[0]->ebName) {
-    return getEntsInClosureOfOwnedElms(*mesh,part_dim);
+    return OmegahGhost::getEntsInClosureOfOwnedElms(*mesh,part_dim);
   } else {
     //only hit in unit test for 'lateral side' part_name - that test passes, moving on // you fool!
     return mesh->get_array<Omega_h::I8>(part_dim, part_name);
@@ -133,7 +56,7 @@ LO getNumEntsInPart(const OmegahGenericMesh& albanyMesh, const std::string& part
   const auto& mesh = albanyMesh.getOmegahMesh();
   const int part_dim = albanyMesh.part_dim(part_name);
   if(part_name == albanyMesh.meshSpecs[0]->ebName) {
-    return getNumEntsInClosureOfOwnedElms(*mesh,part_dim);
+    return OmegahGhost::getNumEntsInClosureOfOwnedElms(*mesh,part_dim);
   } else {
     const auto isInPart = getIsEntInPart(albanyMesh,part_name);
     return Omega_h::get_sum<Omega_h::I8>(isInPart);
@@ -143,7 +66,7 @@ LO getNumEntsInPart(const OmegahGenericMesh& albanyMesh, const std::string& part
 [[nodiscard]]
 std::vector<LO> OmegahConnManager::getLocalElmIds(const std::string& part_name) const {
   if(part_name==albanyMesh->meshSpecs[0]->ebName) {
-    const auto numElms = getNumOwnedElms(*mesh);
+    const auto numElms = OmegahGhost::getNumOwnedElms(*mesh);
     std::vector<LO> localElmIds(numElms);
     std::iota(localElmIds.begin(), localElmIds.end(), 0);
     return localElmIds;
@@ -198,17 +121,12 @@ GO OmegahConnManager::getElementGlobalId(LO localElmtId) const {
 }
 
 int OmegahConnManager::getOwnedElementCount() const {
-    return getNumOwnedElms(*mesh);
+    return OmegahGhost::getNumOwnedElms(*mesh);
 }
 
 Omega_h::HostRead<Omega_h::GO> OmegahConnManager::getOwnedElementGids() const {
   const int dim = albanyMesh->part_dim(elem_block_name());
-  assert(mesh->has_tag(dim, "global"));
-  auto globals_d = mesh->globals(dim);
-  auto owned_d = mesh->owned(dim);
-  auto keptIndicies_d = Omega_h::collect_marked(owned_d);
-  auto ownedGlobals_d = Omega_h::unmap(keptIndicies_d, globals_d, 1);
-  return Omega_h::HostRead<Omega_h::GO>(ownedGlobals_d);
+  return OmegahGhost::getOwnedElementGids(*mesh,dim);
 }
 
 std::vector<GO>
@@ -251,7 +169,7 @@ std::array<LO,4> getDofsPerEnt(const panzer::FieldPattern & fp)
 Omega_h::GOs createGlobalEntDofNumbering(Omega_h::Mesh& mesh, const LO entityDim, const LO dofsPerEnt, const GO startingOffset) {
   if(!dofsPerEnt)
     return Omega_h::GOs();
-  const auto isEntInPart = getEntsInClosureOfOwnedElms(mesh,entityDim);
+  const auto isEntInPart = OmegahGhost::getEntsInClosureOfOwnedElms(mesh,entityDim);
   const int numEnts = mesh.nents(entityDim);
   const int numDofs = numEnts*dofsPerEnt;
   auto worldComm = mesh.library()->world();
@@ -680,13 +598,16 @@ std::vector<int> OmegahConnManager::getConnectivityMask (const std::string& sub_
       elmToDim[dim] = mesh->ask_down(part_dim,dim);
       mask[dim] = mesh->get_array<Omega_h::I8>(dim, sub_part_name);
     } else {
-      mask[dim] = Omega_h::Read<Omega_h::I8>(getNumEntsInClosureOfOwnedElms(*mesh, dim), 0);
+      auto numEnts = OmegahGhost::getNumEntsInClosureOfOwnedElms(*mesh, dim);
+      mask[dim] = Omega_h::Read<Omega_h::I8>(numEnts, 0);
     }
   }
-  if(m_dofsPerEnt[part_dim] > 0)
+  if(m_dofsPerEnt[part_dim] > 0) {
     mask[part_dim] = mesh->get_array<Omega_h::I8>(part_dim, sub_part_name);
-  else
-    mask[part_dim] = Omega_h::Read<Omega_h::I8>(getNumEntsInClosureOfOwnedElms(*mesh, part_dim), 0);
+  } else {
+    auto numEnts = OmegahGhost::getNumEntsInClosureOfOwnedElms(*mesh, part_dim);
+    mask[part_dim] = Omega_h::Read<Omega_h::I8>(numEnts, 0);
+  }
 
   auto elm2dofMask = createElementToDofConnectivityMask(mask, elmToDim);
   // transfer to host
---
+++ src/disc/omegah/OmegahGhost.cpp
@@ -0,0 +1,84 @@
+#include "OmegahGhost.hpp"
+#include <Omega_h_map.hpp> //unmap, fan_reduce
+#include <Omega_h_mark.hpp> //collect_marked
+#include <Omega_h_array_ops.hpp> //get_sum
+
+namespace OmegahGhost {
+
+  Omega_h::LO getNumOwnedElms(Omega_h::Mesh& mesh) {
+    auto elmDim = mesh.dim();
+    auto isElmOwned = mesh.owned(elmDim);
+    return Omega_h::get_sum(isElmOwned);
+  }
+
+  Omega_h::HostRead<Omega_h::GO> getOwnedElementGids(Omega_h::Mesh& mesh, int dim) {
+    OMEGA_H_CHECK(dim >= 0 && dim <= mesh.dim());
+    OMEGA_H_CHECK(mesh.has_tag(dim, "global"));
+    auto globals_d = mesh.globals(dim);
+    auto owned_d = mesh.owned(dim);
+    auto keptIndicies_d = Omega_h::collect_marked(owned_d);
+    auto ownedGlobals_d = Omega_h::unmap(keptIndicies_d, globals_d, 1);
+    return Omega_h::HostRead<Omega_h::GO>(ownedGlobals_d);
+  }
+
+  /**
+   * \brief Get entities in the closure of owned elements
+   *
+   * This function identifies mesh entities that are in the closure of owned elements,
+   * matching the behavior of element-based partitions without ghosts. An entity is
+   * considered to be in the closure of owned elements if it bounds at least one
+   * owned element.
+   *
+   * \param[in] cmesh The Omega_h mesh (const reference)
+   * \param[in] dim The topological dimension of entities to query (0=vertices, 1=edges,
+   *                2=faces, 3=regions). Must be in range [0, 3].
+   *
+   * \return A mask array (Omega_h::Read<Omega_h::I8>) where each entry corresponds to
+   *         an entity of dimension \p dim. The value is 1 if the entity is in the
+   *         closure of at least one owned element, 0 otherwise. The array has length
+   *         equal to the number of entities of dimension \p dim in the mesh.
+   *
+   * \throws std::logic_error if dim < 0 or dim > 3
+   * \throws std::logic_error if the computed mask size doesn't match the number of entities
+   *
+   * \note For entities with dimension equal to the mesh dimension (elements), this
+   *       simply returns the owned mask from the mesh.
+   *
+   * \note For entities with dimension less than the mesh dimension, the function:
+   *       - Queries upward adjacencies (entity-to-element)
+   *       - Maps element ownership status to entity adjacencies
+   *       - Uses fan_reduce with MAX to determine if any adjacent element is owned
+   */
+  Omega_h::Read<Omega_h::I8> getEntsInClosureOfOwnedElms(const Omega_h::Mesh& cmesh, int dim) {
+    //Omegah isn't very const friendly and the Teuchos RCP returns const pointers/refs
+    auto mesh = const_cast<Omega_h::Mesh&>(cmesh);
+    std::stringstream ss;
+    OMEGA_H_CHECK(dim >= 0 && dim <= mesh.dim());
+    const auto elmDim = mesh.dim();
+    if( dim == elmDim ) {
+      return mesh.owned(elmDim);
+    } else {
+      auto entToElm = mesh.ask_up(dim, elmDim);
+      auto isElmOwned = mesh.owned(elmDim);
+      //create an array that replaces the element indices that are adjacent to
+      //each ent with the ownership status of the element (1:owned, 0:o.w.)
+      auto entToOwned = Omega_h::unmap(entToElm.ab2b, isElmOwned, 1);
+      // For each ent of dimension dim, where dim!=meshDim, get max ownership among adjacent elements
+      // (will be 1 if any element is owned, 0 otherwise)
+      auto entHasOwned = Omega_h::fan_reduce(entToElm.a2ab, Omega_h::read(entToOwned),1,OMEGA_H_MAX);
+      OMEGA_H_CHECK(entHasOwned.size() == mesh.nents(dim));
+      return entHasOwned;
+    }
+  }
+
+  //Tell Albany about entities that are in the closure of owned elements.  This
+  //matches what was done in an element based partition without ghosts.
+  Omega_h::LO getNumEntsInClosureOfOwnedElms(const Omega_h::Mesh& cmesh, int dim) {
+    //Omegah isn't very const friendly and the Teuchos RCP returns const pointers/refs
+    auto mesh = const_cast<Omega_h::Mesh&>(cmesh);
+    OMEGA_H_CHECK(dim >= 0 && dim <= mesh.dim());
+    auto entHasOwnedAdjElm = getEntsInClosureOfOwnedElms(cmesh,dim);
+    return Omega_h::get_sum(entHasOwnedAdjElm);
+  }
+
+} //end OmegahGhost namespace
