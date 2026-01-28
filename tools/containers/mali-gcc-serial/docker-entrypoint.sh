d778fc8f14e35150c78cc4932615eba9f44118d5
Matthew Hoffman <mhoffman@lanl.gov>
2026-01-28 14:30:34 -0700

    Adjustment to previous changes
    The prompt and bindings don't get applied to the user's env when placed
    in the entrypoint file, so I moved them to get inserted into the user's
    bashrc.  But it's best practice to keep the compass load operation in
    the entrypoint file and out of the bashrc.

--- tools/containers/mali-gcc-serial/docker-entrypoint.sh
+++ tools/containers/mali-gcc-serial/docker-entrypoint.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# useful env changes
-export CLICOLOR=1
-# make prompt show time, user@where, and current path
-export PS1="\[\e[32;1m\]\t \u@\h \[\e[0m\]\[\e[33;1m\]\w\[\e[0m\]\[\e[32;1m\]\$\[\e[0m\] "
-bind "\"\e[A\": history-search-backward"
-bind "\"\e[B\": history-search-forward"
-git config --global url."https://github.com/".insteadOf git@github.com:
-
 # Load compass
 source load_compass_1.8.0_mpich.sh
 echo "compass conda env activated"
