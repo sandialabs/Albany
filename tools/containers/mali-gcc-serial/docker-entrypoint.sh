1a582f86d5568267e59c1cdbadc467e76b489bdf
Matthew Hoffman <mhoffman@lanl.gov>
2026-02-27 15:38:46 -0700

    Compass developer install in the container
    This commit switches from a compass release to a compass developer
    install in the container.  This allows use of versions of compass newer
    than the current release.
    
    Note that for this to work, I had to introduce an option to allow insecure
    certificates.  This may not be acceptable for some applications, but it must
    be deliberately set to get that behavior.

--- tools/containers/mali-gcc-serial/docker-entrypoint.sh
+++ tools/containers/mali-gcc-serial/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# Global cert behavior switch
+if [[ "${ALLOW_INSECURE_CERTS:-false}" == "true" ]]; then
+	unset SSL_CERT_FILE REQUESTS_CA_BUNDLE CURL_CA_BUNDLE
+	export SSL_NO_VERIFY=1
+else
+	export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+	export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+	export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
+fi
+
 # Load compass
 #source load_compass_1.8.0_mpich.sh
 #echo "compass conda env activated"
