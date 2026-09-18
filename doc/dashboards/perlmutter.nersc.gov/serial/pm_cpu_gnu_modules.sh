dfe1ca372c239c27a8d390476aa6bcb4863015fa
Mauro Perego <perego.mauro@gmail.com>
2026-09-17 17:34:42 -0700

    Update pm-cpu scripts

--- doc/dashboards/perlmutter.nersc.gov/serial/pm_cpu_gnu_modules.sh
+++ doc/dashboards/perlmutter.nersc.gov/serial/pm_cpu_gnu_modules.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-module load PrgEnv-gnu/8.5.0
-module load gcc-native/12.3
+module load PrgEnv-gnu/8.7.0
+module load gcc-native/14
 module load craype-accel-host
-module load cray-libsci/23.12.5
-module load craype/2.7.32
-module load cray-mpich/8.1.30
-module load cray-hdf5-parallel/1.12.2.9
-module load cray-netcdf-hdf5parallel/4.9.0.9
+module load cray-libsci/26.03.0
+module load craype/2.7.36
+module load cray-mpich/9.1.0
+module load cray-hdf5-parallel/1.14.3.9
+module load cray-netcdf-hdf5parallel/4.9.2.3
 module load cray-parallel-netcdf/1.12.3.13
 module load cmake/3.30.2
 
@@ -21,8 +21,8 @@ export OMP_PLACES=threads
 export HDF5_USE_FILE_LOCKING=FALSE
 export FI_CXI_RX_MATCH_MODE=software
 
-export SUPERLU_DIR=/global/cfs/cdirs/fanssie/software/tpls/superlu-7.0.0
-export BOOST_DIR=/global/common/software/fanssie/boost-1.72.0/gcc-12.3.0
+export SUPERLU_DIR=/global/common/software/fanssie/superlu/7.0.1/gcc/14.3
+export BOOST_DIR=/global/common/software/fanssie/boost/1.83.0/gcc/14.3
 
 # Need this to avoid error when running seacas decomp
 export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
