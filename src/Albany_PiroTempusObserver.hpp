0afd28ccf987e35b5348a6771e553e10c0cb8740
Luca Bertagna <lbertag@sandia.gov>
2026-09-24 10:27:36 -0600

    Fixes and upgrades to tempus observer
    - split adapt from observtion: former at step start, latter at step end
    - factor out the unpacking of tempus state into x/xdot/xdotdot/dxdp
    - do away with const cast: tempys history always returns nonconst state

--- src/Albany_PiroTempusObserver.hpp
+++ src/Albany_PiroTempusObserver.hpp
@@ -19,8 +19,13 @@ public:
   PiroTempusObserver(const Teuchos::RCP<Application>& app,
                      const Teuchos::RCP<const Thyra_ModelEvaluator>& model);
 
-  // Observe the end of each time step in the time loop
+  // Observe the beginning of each time step in the time loop
+  // This is where adapatation happens (if any)
   void observeStartTimeStep(const Tempus::Integrator<ST>& integrator) override;
+
+  // Observe the end of each time step in the time loop
+  // This is where saving to mesh happens (if needed)
+  void observeEndTimeStep(const Tempus::Integrator<ST>& integrator) override;
 protected:
 
   bool is_first_time_step_ = true;
